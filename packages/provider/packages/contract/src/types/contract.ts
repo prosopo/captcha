971b5971ac2474857e7e7c1de43fca0df0c2ffed
Chris Taylor <forgetso86@gmail.com>
2022-02-11 09:47:20 +0000

    provider: Unlink remote repository

---
+++ packages/provider/packages/contract/src/types/contract.ts
@@ -0,0 +1,112 @@
+// Copyright (C) 2021-2022 Prosopo (UK) Ltd.
+// This file is part of provider <https://github.com/prosopo-io/provider>.
+//
+// provider is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// provider is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with provider.  If not, see <http://www.gnu.org/licenses/>.
+import {Registry} from 'redspot/types/provider'
+import {AccountId, Balance, Hash} from '@polkadot/types/interfaces'
+import {u16, u32} from '@polkadot/types'
+import Contract from '@redspot/patract/contract'
+import {AbiMessage} from '@polkadot/api-contract/types'
+import {TypeDef} from '@polkadot/types-create/types'
+import {AnyJson} from '@polkadot/types/types/codec'
+import {z} from 'zod'
+import { Signer } from 'redspot/provider'
+import {Network} from "redspot/types";
+
+export enum GovernanceStatus {
+    Active = 'Active',
+    Inactive = 'Inactive',
+    Deactivated = 'Deactivated'
+}
+
+export const PayeeSchema = z.enum(['Provider', 'Dapp', 'None'])
+export const Payee = PayeeSchema.Values
+export type Payee = z.infer<typeof PayeeSchema>;
+
+export interface Provider {
+    status: GovernanceStatus,
+    balance: Balance,
+    fee: u32,
+    payee: Payee,
+    // rust name style, hence snake case
+    service_origin: Hash | string,
+    captcha_dataset_id: Hash | string,
+}
+
+export interface RandomProvider {
+    provider: Provider,
+    block_number: string,
+}
+
+export interface Dapp {
+    status: GovernanceStatus,
+    balance: Balance,
+    owner: AccountId,
+    min_difficulty: u16,
+    client_origin: Hash,
+}
+
+export interface CaptchaData {
+    provider: AccountId,
+    merkle_tree_root: Hash,
+    captcha_type: u16
+}
+
+export interface ContractTxResponse {
+    args: string[],
+    event: {
+        args: [{
+            name: string,
+            type: {
+                info: number,
+                type: string
+            }
+        }, {
+            name: string,
+            type: TypeDef
+        }],
+        docs: [],
+        identifier: string,
+        index: number
+    },
+    name: string
+}
+
+export interface ContractApiInterface {
+    contract?: Contract
+    network: Network
+    mnemonic?: string
+    signer?: Signer | undefined
+    deployerAddress: string
+    contractAddress: string
+    patract: any;
+
+    getContract(): Promise<void>
+
+    getSigner(): Promise<void>
+
+    changeSigner(mnemonic: string): Promise<void>
+
+    createAccountAndAddToKeyring(): [string, string]
+
+    contractCall<T>(contractFunction: string, args: T[], value?: number | string, atBlock?: string | Uint8Array): Promise<AnyJson>
+
+    contractTx<T>(signedContract: Contract, contractMethodName: string, encodedArgs: T[], value: number | undefined): Promise<AnyJson>
+
+    contractQuery<T>(signedContract: Contract, contractMethodName: string, encodedArgs: T[]): Promise<AnyJson>
+
+    getContractMethod(contractMethodName: string): AbiMessage
+
+    getStorage<T>(key: string, decodingFn: (registry: Registry, data: Uint8Array) => T): Promise<T>
+}
