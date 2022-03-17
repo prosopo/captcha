d1d8338e10d9dc92a7ff5c1daa45cdea86bcb02a
Chris Taylor <forgetso86@gmail.com>
2022-03-17 12:21:42 +0000

    provider: Fix to get API working

--- packages/provider/packages/core/src/cli/argv.ts
+++ packages/provider/packages/core/src/cli/argv.ts
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with provider.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Payee, PayeeSchema } from '@prosopo/contract/types';
+import { Payee, PayeeSchema } from '@prosopo/contract';
 import parser from 'cron-parser';
 import pm2 from 'pm2';
 import { cwd } from 'process';
