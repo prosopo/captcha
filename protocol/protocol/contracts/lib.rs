0f04c2f70c3f39f449346d3100b79948461e67f7
Sinisa Canak <sinisa.canak997@gmail.com>
2022-03-18 15:24:58 +0100

    protocol: Captcha time limit (#53)
    protocol: * remove ink-experimental-engine
    protocol: 
    protocol: * add timestamps

--- protocol/protocol/contracts/lib.rs
+++ protocol/protocol/contracts/lib.rs
@@ -125,7 +125,7 @@ pub mod prosopo {
     }
 
     /// RandomProvider is selected randomly by the contract for the client side application
-    #[derive(scale::Encode, scale::Decode)]
+    #[derive(PartialEq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub struct RandomProvider {
         provider: Provider,
@@ -198,6 +198,8 @@ pub mod prosopo {
         contract: AccountId,
         // The Provider AccountId that is permitted to approve or disapprove the commitment
         provider: AccountId,
+        // Time of completion
+        completed_at: Timestamp,
     }
 
     /// DApps are distributed apps who want their users to be verified by Providers, either paying
@@ -245,8 +247,15 @@ pub mod prosopo {
         // commented until block timestamp is available in ink unit tests
         // created: Timestamp,
         // updated: Timestamp,
-        //last_correct_captcha: Timestamp,
-        //last_correct_captcha_dapp_id: AccountId,
+        last_correct_captcha: Timestamp,
+        last_correct_captcha_dapp_id: AccountId,
+    }
+
+    #[derive(scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub struct LastCorrectCaptcha {
+        pub before_ms: u32,
+        pub dapp_id: AccountId,
     }
 
     // Contract storage
@@ -836,6 +845,7 @@ pub mod prosopo {
                 status: CaptchaStatus::Pending,
                 contract,
                 provider,
+                completed_at: self.env().block_timestamp(),
             };
 
             self.create_new_dapp_user(caller);
@@ -859,8 +869,8 @@ pub mod prosopo {
                 let user = User {
                     correct_captchas: 0,
                     incorrect_captchas: 0,
-                    //last_correct_captcha: (),
-                    //last_correct_captcha_dapp_id: (),
+                    last_correct_captcha: 0,
+                    last_correct_captcha_dapp_id: AccountId::default(),
                 };
                 self.dapp_users.insert(account, &user);
                 self.dapp_user_accounts.push(account);
@@ -897,6 +907,8 @@ pub mod prosopo {
             if commitment_mut.status == CaptchaStatus::Pending {
                 commitment_mut.status = CaptchaStatus::Approved;
                 user.correct_captchas += 1;
+                user.last_correct_captcha = commitment.completed_at;
+                user.last_correct_captcha_dapp_id = commitment.contract;
                 self.captcha_solution_commitments
                     .insert(captcha_solution_commitment_id, &commitment_mut);
                 self.dapp_users.insert(&commitment.account, &user);
@@ -1023,6 +1035,22 @@ pub mod prosopo {
                 user.correct_captchas / (user.correct_captchas + user.incorrect_captchas) * 100
                     >= threshold.into(),
             )
+        }
+
+        #[ink(message)]
+        pub fn dapp_operator_last_correct_captcha(
+            &self,
+            user: AccountId,
+        ) -> Result<LastCorrectCaptcha, Error> {
+            let user = self.get_dapp_user(user)?;
+
+            Ok(LastCorrectCaptcha {
+                before_ms: u32::try_from(
+                    self.env().block_timestamp() - user.last_correct_captcha,
+                )
+                .unwrap(),
+                dapp_id: user.last_correct_captcha_dapp_id,
+            })
         }
 
         // Disputes and governance messages
@@ -1265,7 +1293,6 @@ pub mod prosopo {
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
     /// module and test functions are marked with a `#[test]` attribute.
     /// The below code is technically just normal Rust code.
-    #[cfg(not(feature = "ink-experimental-engine"))]
     #[cfg(test)]
     mod tests {
         /// Imports `ink_lang` so we can use `#[ink::test]`.
@@ -1274,8 +1301,12 @@ pub mod prosopo {
         use ink_env::hash::HashOutput;
         use ink_lang as ink;
 
+        use crate::prosopo::Error::ProviderInactive;
+
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
+
+        type Event = <Prosopo as ::ink_lang::reflect::ContractEventBase>::Type;
 
         /// We test if the default constructor does its job.
         #[ink::test]
@@ -1357,22 +1388,6 @@ pub mod prosopo {
             result.as_mut()[0..copy_len].copy_from_slice(&hash_output[0..copy_len]);
             result
         }
-    }
-
-    #[cfg(feature = "ink-experimental-engine")]
-    #[cfg(test)]
-    mod tests_experimental_engine {
-        use ink_env::hash::Blake2x256;
-        use ink_env::hash::CryptoHash;
-        use ink_env::hash::HashOutput;
-        use ink_lang as ink;
-
-        use crate::prosopo::Error::ProviderInactive;
-
-        use super::*;
-        use ink_env::test::EmittedEvent;
-
-        type Event = <Prosopo as ::ink_lang::reflect::ContractEventBase>::Type;
 
         /// Provider Register Helper
         fn generate_provider_data(id: u8, port: &str, fee: u32) -> (AccountId, Hash, u32) {
@@ -2141,33 +2156,22 @@ pub mod prosopo {
         }
 
         // // Test get random provider
-        // #[ink::test]
-        // fn test_get_random_active_provider() {
-        //     let operator_account = AccountId::from([0x1; 32]);
-        //     let mut contract = Prosopo::default(operator_account);
-        //     let provider_account = AccountId::from([0x2; 32]);
-        //     let service_origin = str_to_hash("https://localhost:2424".to_string());
-        //     let fee: u32 = 0;
-        //     contract.provider_register(service_origin, fee, Payee::Provider, provider_account);
-        //     let fee2: u32 = 100;
-        //     ink_env::test::set_caller::<ink_env::DefaultEnvironment>(provider_account);
-        //     let balance = 1000;
-        //     ink_env::test::set_value_transferred::<ink_env::DefaultEnvironment>(balance);
-        //     contract.provider_update(service_origin, fee, Payee::Dapp, provider_account);
-        //     let registered_provider_account = contract.providers.get(&provider_account);
-        //     let selected_provider = contract.get_random_active_provider();
-        //     assert!(selected_provider.unwrap() == registered_provider_account.unwrap());
-        // }
-
-        /// Helper function for converting string to Hash
-        fn str_to_hash(str: String) -> Hash {
-            let mut result = Hash::default();
-            let len_result = result.as_ref().len();
-            let mut hash_output = <<Blake2x256 as HashOutput>::Type as Default>::default();
-            <Blake2x256 as CryptoHash>::hash((&str).as_ref(), &mut hash_output);
-            let copy_len = core::cmp::min(hash_output.len(), len_result);
-            result.as_mut()[0..copy_len].copy_from_slice(&hash_output[0..copy_len]);
-            result
+        #[ink::test]
+        fn test_get_random_active_provider() {
+            let operator_account = AccountId::from([0x1; 32]);
+            let mut contract = Prosopo::default(operator_account);
+            let provider_account = AccountId::from([0x2; 32]);
+            let service_origin = str_to_hash("https://localhost:2424".to_string());
+            let fee: u32 = 0;
+            contract.provider_register(service_origin, fee, Payee::Provider, provider_account);
+            let fee2: u32 = 100;
+            ink_env::test::set_caller::<ink_env::DefaultEnvironment>(provider_account);
+            let balance = 1000;
+            ink_env::test::set_value_transferred::<ink_env::DefaultEnvironment>(balance);
+            contract.provider_update(service_origin, fee, Payee::Dapp, provider_account);
+            let registered_provider_account = contract.providers.get(&provider_account);
+            let selected_provider = contract.get_random_active_provider(provider_account);
+            assert!(selected_provider.unwrap().provider == registered_provider_account.unwrap());
         }
     }
 }
