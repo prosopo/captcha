0fc47d29517a71c1fe310435ffa621cc332f9ab4
Seth Montenegro <seth@hyx.net>
2022-04-12 12:37:55 +0100

    provider: get contract_address + express cors middleware  (#79)
    provider: * + express cors middleware
    provider: 
    provider: * rm cors from package.json..
    provider: 
    provider: * - comment @redspot/watcher
    provider: 
    provider: Co-authored-by: seht <seht@hyx.net>
    provider: Co-authored-by: Chris <forgetso86@gmail.com>

--- packages/provider/packages/core/src/cli/cli.ts
+++ packages/provider/packages/core/src/cli/cli.ts
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with provider.  If not, see <http://www.gnu.org/licenses/>.
 import express from 'express';
+import cors from 'cors';
 
 import { mnemonicValidate } from '@polkadot/util-crypto';
 
@@ -22,10 +23,12 @@ import { Environment } from '../env';
 import { ERRORS, handleErrors } from '../errors';
 import { processArgs } from './argv';
 
+const port = 3000;
+
 const app = express();
 
 app.use(express.json());
-const port = 3000;
+app.use(cors());
 
 async function main () {
   if (!process.env.PROVIDER_MNEMONIC) {
