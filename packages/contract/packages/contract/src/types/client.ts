38bed697d28a13e5ced6e5bafeb72060970083a7
seht <seht@hyx.net>
2022-04-20 11:08:51 +0100

    contract: provider-core index

---
+++ packages/contract/packages/contract/src/types/client.ts
@@ -0,0 +1,14 @@
+// export interface CaptchaPuzzle {
+//     captchaId: string;
+//     datasetId: string;
+// }
+
+// export interface CaptchaPuzzleWithProof {
+//     captcha: CaptchaPuzzle;
+//     proof: string[][];
+// }
+
+// export interface CaptchaPuzzleResponse {
+//     captchas: CaptchaPuzzleWithProof[];
+//     requestHash: string;
+// }
