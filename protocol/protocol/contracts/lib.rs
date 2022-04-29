10dc300965fa7386c4fb58448410f536ae02952b
Chris Taylor <forgetso86@gmail.com>
2022-04-29 15:58:14 +0100

    protocol: hotfix: add experimental default decorator so that contract builds

--- protocol/protocol/contracts/lib.rs
+++ protocol/protocol/contracts/lib.rs
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with provider.  If not, see <http://www.gnu.org/licenses/>.
+#![feature(derive_default_enum)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use self::prosopo::{Prosopo, ProsopoRef};
