d977ecea814044d7b3134e5f7bb761ebae63681b
Seth Montenegro <seth@hyx.net>
2022-05-02 15:50:44 +0100

    provider: .env >> DATABASE_HOST (#101)
    provider: Co-authored-by: seht <seht@hyx.net>

--- packages/provider/packages/core/prosopo.config.ts
+++ packages/provider/packages/core/prosopo.config.ts
@@ -58,8 +58,8 @@ export default {
     database: {
         development: {
             type: 'mongo',
-            endpoint: `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@provider-db:27017`,
-            dbname: 'prosopo'
+            endpoint: `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}`,
+            dbname: process.env.DATABASE_NAME,
         }
     }
 } as ProsopoConfig;
