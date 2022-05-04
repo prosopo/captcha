c25aa1d4039e9b6ef5281869e8df339ec70a741c
seht <seht@hyx.net>
2022-05-04 01:16:21 +0100

    contract: rm sub packages, split contract

--- packages/contract/packages/core/scripts/license.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in $(git ls-files | grep "\.ts$"); do
-  year=$(git log --follow --diff-filter=A $i | grep Date | tail -1 | awk '{ print $6 }')
-  echo "Adding GPL3 header w/ year $year to $i"
-  cat $(pwd)/scripts/LICENSEHEADER.txt |sed "s|THEDATE|$year|" | cat - $i > /tmp/temp && mv /tmp/temp $i
-done
-
