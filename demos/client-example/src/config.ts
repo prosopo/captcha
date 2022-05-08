4e64f4355606dfeaf8ef6b1b1c0b915077a6a189
seht <seht@hyx.net>
2022-05-08 10:54:59 +0100

    client-example: config

--- demos/client-example/src/config.ts
+++ demos/client-example/src/config.ts
@@ -4,9 +4,9 @@
 // REACT_APP_DAPP_CONTRACT_ADDRESS=5FzjruAqyhRGV81pMb4yznNS7t52hNB8u2VC2N1P22j5QLY9
 // https://create-react-app.dev/docs/adding-custom-environment-variables/
 
-import { IProCaptchaConfig } from '@prosopo/procaptcha';
+import { ProCaptchaConfig } from '@prosopo/procaptcha';
 
-const config: IProCaptchaConfig = {
+const config: ProCaptchaConfig = {
     "providerApi.baseURL": process.env.REACT_APP_API_BASE_URL,
     "providerApi.prefix": process.env.REACT_APP_API_PATH_PREFIX,
     "dappAccount": process.env.REACT_APP_DAPP_CONTRACT_ADDRESS,
