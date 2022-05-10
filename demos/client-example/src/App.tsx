4f376b8a3e449b8f8cdca244dccd43ceda42be8b
seht <seht@hyx.net>
2022-05-10 20:26:35 +0100

    client-example: context manager

--- demos/client-example/src/App.tsx
+++ demos/client-example/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 // import { HttpProvider } from "@polkadot/rpc-provider";
 import {
   Box,
@@ -8,7 +8,7 @@ import {
 
 import config from "./config";
 
-import { ProCaptchaComponent } from "@prosopo/procaptcha-react";
+import { ProCaptchaComponent, ProCaptchaManager, CaptchaManagerState } from "@prosopo/procaptcha-react";
 
 import "./App.css";
 
@@ -16,26 +16,46 @@ function App() {
 
   const [showCaptchas, setShowCaptchas] = useState(false);
 
-  const [account, setAccount] = useState('');
+  function reducer(state, action) {
+    return { ...state, ...action };
+  }
+
+  const initialState = { account: null, contract: null, provider: null } as CaptchaManagerState;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const toggleShowCaptcha = () => {
+    setShowCaptchas(!showCaptchas);
+  };
 
   const onAccountChange = (account) => {
     console.log("ACCOUNT CHANGED CALLBACK", account.address);
-    setAccount(account.address);
+    // setAccount(account.address);
+  }
+
+  const onSubmit = (submitResult) => {
+    console.log("CAPTCHA SUBMIT CALLBACK", submitResult);
+    if (submitResult instanceof Error) {
+      //
+    }
   }
 
   const onCancel = () => {
     setShowCaptchas(false);
   };
 
-  const toggleShowCaptcha = () => {
-    setShowCaptchas(!showCaptchas);
-  };
+  const onSolved = () => {
+    console.log("ALL CAPTCHAS ANSWERED");
+  }
 
   return (
     <Box className={"App"}>
 
       {showCaptchas && (
-        <ProCaptchaComponent config={config} callbacks={{onAccountChange, onCancel}} />
+        <>
+        <ProCaptchaManager.Provider value={{state, dispatch}}>
+          <ProCaptchaComponent config={config} callbacks={{onAccountChange, onSubmit, onCancel, onSolved}} />
+        </ProCaptchaManager.Provider>
+        </>
       )}
 
       {!showCaptchas && (
