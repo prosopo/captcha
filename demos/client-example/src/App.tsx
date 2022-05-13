8b677c7c310b8463c957b65249ade4811925a455
seht <seht@hyx.net>
2022-05-13 14:40:25 +0100

    client-example: pack-client

--- demos/client-example/src/App.tsx
+++ demos/client-example/src/App.tsx
@@ -1,15 +1,18 @@
-import { useState, useReducer, Reducer, SyntheticEvent } from "react";
+import { useState, useReducer } from "react";
 import { Box, Button, Typography } from "@mui/material";
-import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
 
-import { 
-  ProCaptchaComponent,
-  ProCaptchaManager,
+import {
   captchaManagerReducer,
+  statusReducer,
   TSubmitResult,
   TExtensionAccount,
+  ProCaptchaClient,
+} from "@prosopo/procaptcha";
+
+import {
+  ProCaptchaComponent,
+  ProCaptchaManager,
   ExtensionAccountSelect,
-  onExtensionAccountChange 
 } from "@prosopo/procaptcha-react";
 
 import config from "./config";
@@ -18,20 +21,19 @@ import "./App.css";
 
 function App() {
 
-  const [state, dispatch] = useReducer(captchaManagerReducer, {config});
+  const [state, dispatch] = useReducer(captchaManagerReducer, { config });
+  const [status, updateStatus] = useReducer(statusReducer, {});
 
   const [showCaptchas, setShowCaptchas] = useState(false);
 
-  const [status, setStatus] = useState('');
-
   const showCaptchaClick = () => {
     setShowCaptchas(true);
-    setStatus("");
+    updateStatus({info: ""});
   };
 
   const onAccountChange = (account: TExtensionAccount) => {
     console.log("onAccountChange: ACCOUNT CHANGED", account.address);
-    setStatus("Selected account: " + account.meta.name);
+    updateStatus({info: "Selected account: " + account.meta.name});
     setShowCaptchas(true);
   }
 
@@ -47,36 +49,30 @@ function App() {
 
   const onCancel = () => {
     setShowCaptchas(false);
-    setStatus("");
+    updateStatus({info: ""});
   };
 
   const onSolved = () => {
     setShowCaptchas(false);
     console.log("onSolved: ALL CAPTCHAS ANSWERED");
-    setStatus("All captchas answered...");
+    updateStatus({info: "All captchas answered..."});
   }
 
   const onClick = (solution: number[]) => {
     console.log("onClick: ", solution);
   }
 
-  const onAccountChangeEvent = (e: SyntheticEvent<Element, Event>, account: InjectedAccountWithMeta | null) => {
-    if (!account || !state.extension || !state.contractAddress) {
-        return;
-    }
-    onExtensionAccountChange(account, {state, dispatch}, (account, contract, provider) => {
-      onAccountChange(account);
-    });
-  };
+  const clientInterface = new ProCaptchaClient({state, dispatch}, {status, updateStatus}, { onAccountChange, onSubmit, onCancel, onSolved, onClick });
 
   return (
     <Box className={"App"}>
 
-      {status && <Box className={"status"}>{status}</Box>}
-      
-      <ProCaptchaManager.Provider value={{state: state, dispatch}}>
-        {state.extension && !state.account && <ExtensionAccountSelect value={state.account} options={state.extension.getAllAcounts()} onChange={onAccountChangeEvent} />}
-        {showCaptchas && <ProCaptchaComponent callbacks={{onAccountChange, onSubmit, onCancel, onSolved, onClick}} />}
+      {status.info && <Box className={"status"}>{status.info}</Box>}
+      {status.error && <Box className={"status error"}>{status.error}</Box>}
+
+      <ProCaptchaManager.Provider value={{ state, dispatch }}>
+        {state.extension && !state.account && <ExtensionAccountSelect value={state.account} options={state.extension.getAllAcounts()} onChangeEvent={clientInterface.onExtensionAccountChange.bind(clientInterface)} />}
+        {showCaptchas && <ProCaptchaComponent clientInterface={clientInterface} />}
       </ProCaptchaManager.Provider>
 
       {!showCaptchas &&
