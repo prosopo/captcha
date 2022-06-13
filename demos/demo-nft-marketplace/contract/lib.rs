e98272e89c8c2695eb40f266cd4c0f0bbc73944a
Sinisa Canak <sinisa.canak997@gmail.com>
2022-06-13 11:11:21 +0200

    demo-nft-marketplace: update contract

--- demos/demo-nft-marketplace/contract/lib.rs
+++ demos/demo-nft-marketplace/contract/lib.rs
@@ -7,17 +7,27 @@ pub mod demo_nft_contract {
     use serde::Serialize;
     use serde_json_core as serde_json;
 
-    use brush::contracts::psp34::extensions::enumerable::*;
-    use brush::contracts::psp34::extensions::metadata::*;
+    use brush::{
+        contracts::{
+            ownable::*,
+            psp34::extensions::{enumerable::*, metadata::*},
+        },
+        modifiers,
+    };
 
+    use ink_prelude::format;
     use ink_prelude::{string::String, vec::Vec};
     use ink_storage::traits::SpreadAllocate;
-    use ink_prelude::format;
 
     use prosopo::ProsopoRef;
 
     #[derive(
-        Default, SpreadAllocate, PSP34Storage, PSP34MetadataStorage, PSP34EnumerableStorage,
+        Default,
+        SpreadAllocate,
+        PSP34Storage,
+        PSP34MetadataStorage,
+        PSP34EnumerableStorage,
+        OwnableStorage,
     )]
     #[ink(storage)]
     pub struct DemoNFT {
@@ -28,6 +38,8 @@ pub mod demo_nft_contract {
         metadata: PSP34MetadataData,
         #[PSP34EnumerableStorageField]
         enumdata: PSP34EnumerableData,
+        #[OwnableStorageField]
+        ownable: OwnableData,
         // / The address of the prosopo bot protection contract
         prosopo_account: AccountId,
     }
@@ -38,30 +50,67 @@ pub mod demo_nft_contract {
         id: Id,
         owner: AccountId,
         token_uri: String,
+        on_sale: bool,
     }
 
     #[derive(Serialize)]
     pub struct Metadata {
         name: String,
         description: String,
-        image: String
+        image: String,
+    }
+
+    enum Attribute {
+        TokenUri,
+        OnSale,
+    }
+
+    impl Attribute {
+        pub fn to_key(&self) -> Vec<u8> {
+            let key = match self {
+                Attribute::TokenUri => "token_uri",
+                Attribute::OnSale => "on_sale",
+            };
+
+            String::from(key).into_bytes()
+        }
+    }
+
+    fn bool_to_bytes(value: bool) -> Vec<u8> {
+        match value {
+            true => Vec::from([1]),
+            false => Vec::from([0]),
+        }
+    }
+
+    fn bytes_to_bool(value: Vec<u8>) -> bool {
+        value.last().unwrap().to_be() == 1
     }
 
     impl PSP34 for DemoNFT {}
 
     impl PSP34Metadata for DemoNFT {}
     impl PSP34Enumerable for DemoNFT {}
+    impl Ownable for DemoNFT {}
 
     impl DemoNFT {
         #[ink(constructor)]
         pub fn new(prosopo_account: AccountId) -> Self {
             ink_lang::codegen::initialize_contract(|instance: &mut Self| {
+                let caller = instance.env().caller();
+                instance._init_with_owner(caller);
                 instance.prosopo_account = prosopo_account;
             })
         }
 
         #[ink(message)]
-        pub fn mint(&mut self, name: String, description: String, image: String) -> Result<Id, PSP34Error> {
+        #[modifiers(only_owner)]
+        pub fn mint(
+            &mut self,
+            name: String,
+            description: String,
+            image: String,
+        ) -> Result<Id, PSP34Error> {
             let id = Id::U8(self.next_id);
             self._mint_to(Self::env().caller(), id.clone())?;
             self.next_id += 1;
@@ -70,23 +119,27 @@ pub mod demo_nft_contract {
                 description,
                 image,
             };
-            let stringified = serde_json::to_string::<Metadata, 128>(&metadata).unwrap();
+            let stringified = serde_json::to_string::<Metadata, 256>(&metadata).unwrap();
             let encoded = base64::encode(stringified);
 
             let token_uri = format!("data:application/json;base64,{}", encoded);
             self._set_attribute(
                 id.clone(),
-                String::from("token_uri").into_bytes(),
-                token_uri.into_bytes()
+                Attribute::TokenUri.to_key(),
+                // String::from("token_uri").into_bytes(),
+                token_uri.into_bytes(),
             );
+            self._set_attribute(id.clone(), Attribute::OnSale.to_key(), bool_to_bytes(true));
 
             Ok(id)
         }
 
         #[ink(message)]
         pub fn token_uri(&self, token: Id) -> String {
-            let token_uri_key = String::from("token_uri").into_bytes();
-            let token_uri_attribute = self.get_attribute(token, token_uri_key).unwrap();
+            // let token_uri_key = String::from("token_uri").into_bytes();
+            let token_uri_attribute = self
+                .get_attribute(token, Attribute::TokenUri.to_key())
+                .unwrap();
             String::from_utf8(token_uri_attribute).unwrap()
         }
 
@@ -97,6 +150,14 @@ pub mod demo_nft_contract {
             index: &u128,
         ) -> Result<Id, PSP34Error> {
             self.enumdata.enumerable.get_by_index(owner, index)
+        }
+
+        #[ink(message)]
+        pub fn on_sale(&self, token: Id) -> bool {
+            let on_sale_attribute = self
+                .get_attribute(token, Attribute::OnSale.to_key())
+                .unwrap();
+            bytes_to_bool(on_sale_attribute)
         }
 
         /// Calls the `Prosopo` contract to check if `user` is human
@@ -117,34 +178,63 @@ pub mod demo_nft_contract {
                     .unwrap();
         }
 
+        #[ink(message)]
+        pub fn buy(&mut self, token: Id) -> Result<(), String> {
+            let token_owner = self.owner_of(token.clone());
+
+            if token_owner.is_none() {
+                return Err(String::from("Token not found."));
+            }
+
+            let owner = token_owner.unwrap();
+
+            if !self.on_sale(token.clone()) {
+                return Err(String::from("Token not for sale."));
+            }
+
+            let caller = Self::env().caller();
+
+            self.psp34
+                .operator_approvals
+                .insert((owner.clone(), caller.clone(), Some(token.clone())), &());
+
+            self.transfer(caller, token.clone(), Vec::new());
+
+            self._set_attribute(token, Attribute::OnSale.to_key(), bool_to_bytes(false));
+
+            Ok(())
+        }
+
         /// Fetches newest tokens
         #[ink(message)]
         pub fn get_tokens(
             &self,
-            page_size: u128,
-            page_index: u128,
+            page_size: Option<u8>,
+            offset: Option<u128>,
             owner: Option<AccountId>,
         ) -> Result<Vec<NFT>, String> {
-            if page_size == 0 {
+            let size: u128 = page_size.unwrap_or(20).into();
+
+            if size == 0 {
                 return Err(String::from("Invalid page size! Must be 1 or greater."));
             }
 
             let mut tokens = Vec::new();
 
-            let offset = page_size * page_index;
+            let _offset = if offset.is_some() { offset.unwrap() } else { 0 };
             let balance: u128 = if owner.is_some() {
                 self.balance_of(owner.unwrap()).into()
             } else {
                 self.total_supply()
             };
 
-            if balance <= offset {
+            if balance <= _offset {
                 return Ok(tokens);
             }
 
-            let mut i = balance - offset - 1;
-            let end = if balance > offset + page_size {
-                balance - offset - page_size
+            let mut i = balance - _offset - 1;
+            let end = if balance > _offset + size {
+                balance - _offset - size
             } else {
                 0
             };
@@ -157,12 +247,14 @@ pub mod demo_nft_contract {
                     self._owner_of(&token.clone()).unwrap()
                 };
 
-                let token_uri = self.token_uri(token.clone()); 
+                let token_uri = self.token_uri(token.clone());
+                let on_sale = self.on_sale(token.clone());
 
                 tokens.push(NFT {
                     id,
                     owner,
                     token_uri,
+                    on_sale,
                 });
 
                 if i == 0 {
@@ -193,59 +285,6 @@ pub mod demo_nft_contract {
                 contract.prosopo_account.cmp(&operator_account),
                 Ordering::Equal
             );
-        }
-
-        #[ink::test]
-        fn test_fetching_works() {
-            let operator_account = AccountId::from([0x1; 32]);
-            let mut contract = DemoNFT::new(operator_account);
-
-            ink_env::test::set_caller::<ink_env::DefaultEnvironment>(operator_account);
-
-            let token1 = contract.mint(String::from("name1"), String::from("desc1"), String::from("image1")).unwrap();
-            let _token2 = contract.mint(String::from("name2"), String::from("desc2"), String::from("image2")).unwrap();
-            let token3 = contract.mint(String::from("name3"), String::from("desc3"), String::from("image3")).unwrap();
-
-            let new_account = AccountId::from([0x2; 32]);
-
-            ink_env::test::set_caller::<ink_env::DefaultEnvironment>(new_account);
-
-            let _token4 = contract.mint(String::from("name4"), String::from("desc4"), String::from("image4")).unwrap();
-            let token5 = contract.mint(String::from("name5"), String::from("desc5"), String::from("image5")).unwrap();
-
-            let mut tokens = contract.get_tokens(10, 0, None);
-            let mut tokens_unwrapped: Vec<NFT> = tokens.ok().unwrap();
-            assert!(tokens_unwrapped.len() == 5);
-            assert!(tokens_unwrapped[0].token_uri == "data:application/json;base64,eyJuYW1lIjoibmFtZTUiLCJkZXNjcmlwdGlvbiI6ImRlc2M1IiwiaW1hZ2UiOiJpbWFnZTUifQ==");
-            assert!(tokens_unwrapped[0].id == token5);
-
-            tokens = contract.get_tokens(10, 0, Some(operator_account));
-            tokens_unwrapped = tokens.ok().unwrap();
-            assert!(tokens_unwrapped.len() == 3);
-            assert!(tokens_unwrapped[0].token_uri == "data:application/json;base64,eyJuYW1lIjoibmFtZTMiLCJkZXNjcmlwdGlvbiI6ImRlc2MzIiwiaW1hZ2UiOiJpbWFnZTMifQ==");
-            assert!(tokens_unwrapped[0].id == token3);
-
-            tokens = contract.get_tokens(10, 1, None);
-            tokens_unwrapped = tokens.ok().unwrap();
-            assert!(tokens_unwrapped.len() == 0);
-
-            tokens = contract.get_tokens(2, 0, None);
-            tokens_unwrapped = tokens.ok().unwrap();
-            assert!(tokens_unwrapped.len() == 2);
-            assert!(tokens_unwrapped[0].token_uri == "data:application/json;base64,eyJuYW1lIjoibmFtZTUiLCJkZXNjcmlwdGlvbiI6ImRlc2M1IiwiaW1hZ2UiOiJpbWFnZTUifQ==");
-            assert!(tokens_unwrapped[0].id == token5);
-
-            tokens = contract.get_tokens(2, 1, None);
-            tokens_unwrapped = tokens.ok().unwrap();
-            assert!(tokens_unwrapped.len() == 2);
-            assert!(tokens_unwrapped[0].token_uri == "data:application/json;base64,eyJuYW1lIjoibmFtZTMiLCJkZXNjcmlwdGlvbiI6ImRlc2MzIiwiaW1hZ2UiOiJpbWFnZTMifQ==");
-            assert!(tokens_unwrapped[0].id == token3);
-
-            tokens = contract.get_tokens(2, 2, None);
-            tokens_unwrapped = tokens.ok().unwrap();
-            assert!(tokens_unwrapped.len() == 1);
-            assert!(tokens_unwrapped[0].token_uri == "data:application/json;base64,eyJuYW1lIjoibmFtZTEiLCJkZXNjcmlwdGlvbiI6ImRlc2MxIiwiaW1hZ2UiOiJpbWFnZTEifQ==");
-            assert!(tokens_unwrapped[0].id == token1);
         }
     }
 }
