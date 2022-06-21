14bee597bf91ce674a83081ef98e8c39cc8d126f
Chris <forgetso86@gmail.com>
2022-06-21 16:25:45 +0100

    procaptcha-react: Add license (#13)

--- packages/procaptcha-react/src/components/ExtensionAccountSelect.tsx
+++ packages/procaptcha-react/src/components/ExtensionAccountSelect.tsx
@@ -1,3 +1,18 @@
+// Copyright (C) 2021-2022 Prosopo (UK) Ltd.
+// This file is part of procaptcha-react <https://github.com/prosopo-io/procaptcha-react>.
+//
+// procaptcha-react is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// procaptcha-react is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with procaptcha-react.  If not, see <http://www.gnu.org/licenses/>.
 import { SyntheticEvent } from "react";
 import { Autocomplete, TextField } from "@mui/material";
 import { TExtensionAccount } from "@prosopo/procaptcha";
