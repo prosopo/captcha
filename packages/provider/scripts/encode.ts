ea524863fd634232775c55af8038594fb9c7a38f
Chris <forgetso86@gmail.com>
2022-06-22 12:49:02 +0100

    provider: Milestone2 docs (#139)
    provider: * Updated readme and fixed provider update cli command
    provider: 
    provider: * Utility for encoding or decoding addresses, strings, and hexStrings
    provider: 
    provider: * Rename
    provider: 
    provider: * Fixes to commands, readme, and tests
    provider: - Fixes dev commands in package.json
    provider: - Adds coverage command in package.json
    provider: - Adds coverage package c8
    provider: - Adds env tests
    provider: - Removes the mockdb and uses the real db class with in-memory db in tests
    provider: - Update the error handling in the db layer to use ProsopoEnvError
    provider: 
    provider: * Fixes to commands, readme, and tests - Fixes dev commands in package.json - Adds coverage command in package.json - Adds coverage package c8 - Adds env tests - Removes the mockdb and uses the real db class with in-memory db in tests - Update the error handling in the db layer to use ProsopoEnvError
    provider: 
    provider: * Add license headers
    provider: 
    provider: * Remove coverage stat

--- packages/provider/scripts/encode.ts
+++
@@ -1,3 +0,0 @@
-import {decodeAddress, encodeAddress} from '@polkadot/util-crypto'
-import {u8aToHex} from '@polkadot/util'
-console.log(u8aToHex(decodeAddress("5CYKEXWU3L4zWregopd2bTjoZLH9yHBpfXaqkFaTfxacaSA1")))
