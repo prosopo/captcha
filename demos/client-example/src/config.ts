e4b704fd8aa755f9d493cbf6090c7c5f8bfba63b
seht <seht@hyx.net>
2022-06-27 19:52:05 +0100

    client-example: wsprovider

--- demos/client-example/src/config.ts
+++ demos/client-example/src/config.ts
@@ -10,6 +10,7 @@ const config: ProsopoCaptchaConfig = {
     "providerApi.baseURL": process.env.REACT_APP_API_BASE_URL || '',
     "providerApi.prefix": process.env.REACT_APP_API_PATH_PREFIX || '',
     "dappAccount": process.env.REACT_APP_DAPP_CONTRACT_ADDRESS || '',
+    "dappUrl": process.env.REACT_APP_DAPP_CONTRACT_URL || '',
 }
 
 export default config;
