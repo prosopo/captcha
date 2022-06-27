4b5a72b9a73d3d293b37f912843d1bcfd36fbf29
Chris <forgetso86@gmail.com>
2022-06-27 15:56:06 +0100

    client-example: Develop (#15)
    client-example: * npm
    client-example: 
    client-example: * yarn
    client-example: 
    client-example: * pause procaptcha-react
    client-example: 
    client-example: * target esnext
    client-example: 
    client-example: * depcheck
    client-example: 
    client-example: * depcheck
    client-example: 
    client-example: * config
    client-example: 
    client-example: * config
    client-example: 
    client-example: * config
    client-example: 
    client-example: * config
    client-example: 
    client-example: * config
    client-example: 
    client-example: * Get images to show
    client-example: 
    client-example: * reset captcha solution on submit
    client-example: 
    client-example: * @prosopo/procaptcha-react
    client-example: 
    client-example: * procaptcha-react
    client-example: 
    client-example: * typo
    client-example: 
    client-example: * context manager
    client-example: 
    client-example: * context extension
    client-example: 
    client-example: * promises resolving...
    client-example: 
    client-example: * status ui
    client-example: 
    client-example: * on solved show button
    client-example: 
    client-example: * fix onSumit
    client-example: 
    client-example: * app status, error
    client-example: 
    client-example: * fix build
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * decoupled account selected
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * pack-client
    client-example: 
    client-example: * format
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * clean context state
    client-example: 
    client-example: * useCaptcha
    client-example: 
    client-example: * Fix blake2AsHex is not a function with modified webpack config rules
    client-example: 
    client-example: * Manual merge develop
    client-example: 
    client-example: * keep account
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * procaptcha parseSolutions
    client-example: 
    client-example: * mini-css-extract-plugin
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * up pkg
    client-example: 
    client-example: * up pkg
    client-example: 
    client-example: * rm rewired
    client-example: 
    client-example: * eg accounts outside context
    client-example: 
    client-example: * tidy
    client-example: 
    client-example: * Milestone2 docs (#14)
    client-example: 
    client-example: * Updated docs
    client-example: 
    client-example: * Notes about config
    client-example: 
    client-example: Co-authored-by: seht <seht@hyx.net>
    client-example: Co-authored-by: Seth Montenegro <seth@hyx.net>

--- demos/client-example/src/App.tsx
+++ demos/client-example/src/App.tsx
@@ -1,237 +1,92 @@
-import React, { useState, useEffect, useMemo, SyntheticEvent } from "react";
-import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
-// import { HttpProvider } from "@polkadot/rpc-provider";
-import {
-  Box,
-  Button,
-  Typography,
-  Autocomplete,
-  TextField
-} from "@mui/material";
-
-// import config from "./config";
+import { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
 
 import {
-  ProsopoRandomProviderResponse,
-  ProsopoCaptchaResponse,
-  ProsopoContract,
-  Extension,
-  ProviderApi,
-  getExtension,
-  getProsopoContract,
-  ProCaptcha,
-  getConfig
+  TCaptchaSubmitResult,
+  TExtensionAccount,
 } from "@prosopo/procaptcha";
 
-// import {
-//   CaptchaWidget,
-// } from "@prosopo/procaptcha-react";
-import { CaptchaWidget } from "./components/CaptchaWidget";
+import {
+  CaptchaComponent,
+  CaptchaContextManager,
+  ExtensionAccountSelect,
+  useCaptcha,
+} from "@prosopo/procaptcha-react";
+
+import config from "./config";
 
 import "./App.css";
-import { useStyles } from "./app.styles"; // TODO procatcha-react
 
-// const { providerApi } = config;
 
 function App() {
 
-  const classes = useStyles();
-
-  const [contractAddress, setContractAddress] = useState<string>('');
-  const [contract, setContract] = useState<ProsopoContract | null>(null);
-  const [extension, setExtension] = useState<Extension | null>(null);
-
-  // const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
-  const [account, setAccount] = useState<InjectedAccountWithMeta | null>(null);
-
   const [showCaptchas, setShowCaptchas] = useState(false);
-  const [totalCaptchas, setTotalCaptchas] = useState(0);
-  const [currentCaptchaIndex, setCurrentCaptchaIndex] = useState(0);
 
-  // let currentCaptcha: ProsopoCaptcha | undefined;
-  // const accounts = contract.extension?.getAllAcounts();
-
-  const [provider, setProvider] = useState<ProsopoRandomProviderResponse | null>(null);
-
-  const [captchaChallenge, setCaptchaChallenge] = useState<ProsopoCaptchaResponse | null>(null);
-  const [captchaSolution, setCaptchaSolution] = useState<number[]>([]);
-
-  const providerApi = new ProviderApi(getConfig());
-
-  useEffect(() => {
-    Promise.all([providerApi.getContractAddress(), getExtension()])
-      .then(result => {
-          const [_contractAddress, _extension] = result;
-          setContractAddress(_contractAddress.contractAddress);
-          setExtension(_extension);
-          // setAccounts(_extension.getAllAcounts());
-          console.log("CONTRACT", _contractAddress.contractAddress);
-      })
-      .catch(err => {
-          console.error(err);
-      });
-
-  }, []);
-
-  useEffect(() => {
-    setTotalCaptchas(captchaChallenge?.captchas.length ?? 0);
-    setCurrentCaptchaIndex(0);
-  }, [captchaChallenge]);
-
-  useEffect(() => {
-    console.log("CLICK SOLUTION", captchaSolution);
-  }, [captchaSolution]);
-
-  const toggleShowCaptchas = () => {
-    setShowCaptchas(!showCaptchas);
-    setAccount(null);
+  const showCaptchaClick = () => {
+    setShowCaptchas(true);
+    status.update({ info: "" });
   };
 
-  const cancelCaptchas = () => {
-    setCaptchaChallenge(null);
+  const onAccountChange = (account: TExtensionAccount) => {
+    setShowCaptchas(true);
+    status.update({ info: "Selected account: " + account?.meta.name });
+    console.log("CAPTCHA API", clientInterface.getCaptchaApi());
+  };
+
+  const onSubmit = (submitResult: TCaptchaSubmitResult | Error) => {
+    if (submitResult instanceof Error) {
+      status.update({ error: ["onSubmit: CAPTCHA SUBMIT ERROR", submitResult] });
+      return;
+    }
+    const [result, tx] = submitResult;
+    status.update({ info: ["onSubmit: CAPTCHA SUBMIT STATUS", result.status] });
+  };
+
+  const onSolved = ([result, tx,  commitment]: TCaptchaSubmitResult) => {
     setShowCaptchas(false);
-    setAccount(null);
-    setCurrentCaptchaIndex(0);
-  };
 
-  const submitCaptchaSolution = async () => {
-    if (!extension || !contract || !provider || !captchaChallenge) {
-      // TODO throw error
-      return;
-    }
-
-    const signer = extension.getInjected().signer;
-    // const { nonce } = await contract.getApi().query.system.account(account.address!);
-    console.log("SIGNER", signer);
-
-    const proCaptcha = new ProCaptcha(contract, provider, providerApi);
-    const currentCaptcha = captchaChallenge.captchas[currentCaptchaIndex];
-    const { captchaId, datasetId } = currentCaptcha.captcha;
-
-    // TODO loading...
-
-    try {
-      const solved = await proCaptcha.solveCaptchaChallenge(signer, captchaChallenge.requestHash, captchaId, datasetId, captchaSolution);
-      console.log("CAPTCHA SOLVED", solved);
-      // alert(solved.status); // TODO
-    } catch (err) {
-      console.error(err);
-      // alert(err.message);
-    }
-
-    const nextCaptchaIndex = currentCaptchaIndex + 1;
-
-    if (nextCaptchaIndex < totalCaptchas) {
-      setCurrentCaptchaIndex(nextCaptchaIndex);
-    } else {
-      // TODO after all captchas solved.
-      cancelCaptchas();
-    }
-
-  };
-
-
-  const onAccountChange = (e: SyntheticEvent<Element, Event>, account: any) => {
-    if (!extension || !contractAddress) {
-      return;
-    }
-    extension.setAccount(account.address).then(async (account) => {
-      setAccount(account);
-
-      const _contract = await getProsopoContract(contractAddress, getConfig('dappAccount') as string, account);
-      setContract(_contract);
-
-      const _provider = await _contract.getRandomProvider();
-      setProvider(_provider);
-
-      console.log("PROVIDER", _provider);
-
-      const proCaptcha = new ProCaptcha(_contract, _provider, providerApi);
-      setCaptchaChallenge(await proCaptcha.getCaptchaChallenge());
-    });
-  };
-
-  const onCaptchaSolutionClick = (index: number) => {
-    if (captchaSolution.includes(index)) {
-      setCaptchaSolution(captchaSolution.filter(item => item !== index));
-    } else {
-      setCaptchaSolution([...captchaSolution, index]);
-    }
+    status.update({ info: ["onSolved:", `Captcha solution status: ${commitment.status}`] });
   }
 
+  const onChange = (solution: number[][]) => {
+    console.log("onChange:", solution);
+  };
+
+  const onCancel = () => {
+    setShowCaptchas(false);
+    status.update({ info: "" });
+  };
+
+  const clientInterface = useCaptcha({ config }, { onAccountChange, onChange, onSubmit, onSolved, onCancel });
+
+  const manager = clientInterface.manager;
+  const status = clientInterface.status;
+
   return (
-    <Box className={classes.root}>
-      {showCaptchas && !account && (
-        <Autocomplete
-          disablePortal
-          id="select-accounts"
-          options={extension?.getAllAcounts() || []}
-          value={account}
-          isOptionEqualToValue={(option, value) =>
-            option.address === value.address
-          }
-          onChange={onAccountChange}
-          sx={{ width: 550 }}
-          getOptionLabel={(option: any) =>
-            `${option.meta.name}\n${option.address}`
-          }
-          renderInput={(params) => (
-            <TextField {...params} label="Select account" />
-          )}
-        />
-      )}
+    <Box className={"App"}>
 
-      {showCaptchas && account && (
-        <Box className={classes.captchasContainer}>
-          <Box className={classes.captchasHeader}>
-            <Typography className={classes.captchasHeaderLabel}>
-              Select all images with a bus.
-            </Typography>
-          </Box>
+      {status.state.info && <Box className={"status"}>{status.state.info}</Box>}
+      {status.state.error && <Box className={"status error"}>{status.state.error}</Box>}
 
-          <Box className={classes.captchasBody}>
+      {clientInterface.getExtension() && !manager.state.account &&
+        <ExtensionAccountSelect
+          value={manager.state.account}
+          options={clientInterface.getExtension().getAccounts()}
+          onChange={clientInterface.onAccountChange.bind(clientInterface)}
+        />}
 
-            {captchaChallenge && <CaptchaWidget challenge={captchaChallenge[currentCaptchaIndex]} solution={captchaSolution} solutionClickEvent={onCaptchaSolutionClick} />}
+      <CaptchaContextManager.Provider value={manager}>
+        {showCaptchas &&
+          <CaptchaComponent {...{ clientInterface }} />}
+      </CaptchaContextManager.Provider>
 
-            <Box className={classes.dotsContainer}>
-              {Array.from(Array(totalCaptchas).keys()).map((item, index) => {
-                return (
-                  <Box
-                    key={index}
-                    className={classes.dot}
-                    style={{
-                      backgroundColor: currentCaptchaIndex === item ? "#CFCFCF" : "#FFFFFF"
-                    }}
-                  />
-                );
-              })}
-            </Box>
-
-          </Box>
-
-          <Box className={classes.captchasFooter}>
-            <Button onClick={cancelCaptchas} variant="text">
-              Cancel
-            </Button>
-            <Button onClick={submitCaptchaSolution} variant="contained">
-              {currentCaptchaIndex === totalCaptchas - 1
-                ? "Submit"
-                : "Next"}
-            </Button>
-          </Box>
-        </Box>
-      )}
-
-      {!showCaptchas && !account && (
-        <Button
-          onClick={toggleShowCaptchas}
-          classes={{ root: classes.iAmHumanButton }}
-        >
-          <Typography className={classes.iAmHumanButtonLabel}>
+      {!showCaptchas &&
+        <Button onClick={showCaptchaClick} className={"iAmHumanButton"}>
+          <Typography className={"iAmHumanButtonLabel"}>
             I am human
           </Typography>
-        </Button>
-      )}
+        </Button>}
+
     </Box>
   );
 }
--- demos/client-example/src/index.tsx
+++ demos/client-example/src/index.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 import CssBaseline from "@mui/material/CssBaseline";
-import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <CssBaseline />
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.Fragment>,
+  document.getElementById('root') as HTMLElement
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
