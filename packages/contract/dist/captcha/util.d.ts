807325b970c63453ec4f20ef1f7f9cd8aebbf54b
seht <seht@hyx.net>
2022-07-14 07:57:15 +0100

    contract: .npmignore

--- packages/contract/dist/captcha/util.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function hexHash(data: string | Uint8Array): string;
-export declare function imageHash(path: string): Promise<string>;
-//# sourceMappingURL=util.d.ts.map
--- packages/contract/dist/logger.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const logger: import("consola").Consola;
-//# sourceMappingURL=logger.d.ts.map
