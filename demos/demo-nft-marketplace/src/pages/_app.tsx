355d734ad1a463daecfd7c233b08648537c0253f
Lupus7 <sinisa.canak997@gmail.com>
2022-07-20 10:11:42 +0200

    demo-nft-marketplace: add loading bar on route change

--- demos/demo-nft-marketplace/src/pages/_app.tsx
+++ demos/demo-nft-marketplace/src/pages/_app.tsx
@@ -1,19 +1,38 @@
 import { ProsopoProvider } from 'components/Prosopo';
 import Navbar from 'components/Navbar/Navbar';
 import { AppProps } from 'next/app';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import LoadingBar, { LoadingBarRef } from 'react-top-loading-bar';
 import '../styles/global.css';
+import { useRouter } from 'next/router';
 
 const queryClient = new QueryClient({});
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const ref = useRef<LoadingBarRef>(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const startLoadingBar = () => ref.current.continuousStart(0, 0);
+    const completeLoadingBar = () => ref.current.complete();
+
+    router.events.on('routeChangeStart', startLoadingBar);
+    router.events.on('routeChangeComplete', completeLoadingBar);
+    return () => {
+      router.events.off('routeChangeStart', startLoadingBar);
+      router.events.off('routeChangeComplete', completeLoadingBar);
+    };
+  }, [router.events]);
+
   return (
     <ProsopoProvider>
       <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
       <style>model-viewer {'{height:100%;max-height:100%;}'}</style>
 
       <QueryClientProvider client={queryClient}>
+        <LoadingBar ref={ref} color="#FD4857" />
         <Navbar />
         <Component {...pageProps} />
       </QueryClientProvider>
