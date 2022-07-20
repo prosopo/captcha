5f9150477e60c67a067dc132b854806dd9252e8d
Lupus7 <sinisa.canak997@gmail.com>
2022-07-20 11:02:13 +0200

    demo-nft-marketplace: remove unused package

--- demos/demo-nft-marketplace/src/pages/_app.tsx
+++ demos/demo-nft-marketplace/src/pages/_app.tsx
@@ -2,12 +2,9 @@ import { ProsopoProvider } from 'components/Prosopo';
 import Navbar from 'components/Navbar/Navbar';
 import { AppProps } from 'next/app';
 import React, { FC, useEffect, useRef } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import LoadingBar, { LoadingBarRef } from 'react-top-loading-bar';
 import '../styles/global.css';
 import { useRouter } from 'next/router';
-
-const queryClient = new QueryClient({});
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   const ref = useRef<LoadingBarRef>(null);
@@ -30,12 +27,9 @@ const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
     <ProsopoProvider>
       <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
       <style>model-viewer {'{height:100%;max-height:100%;}'}</style>
-
-      <QueryClientProvider client={queryClient}>
-        <LoadingBar ref={ref} color="#FD4857" />
-        <Navbar />
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <LoadingBar ref={ref} color="#FD4857" />
+      <Navbar />
+      <Component {...pageProps} />
     </ProsopoProvider>
   );
 };
