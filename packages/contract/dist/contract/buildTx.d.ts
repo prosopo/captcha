4798ab1bab3b847c6f46e0644e617d32377e20c4
Chris Taylor <forgetso86@gmail.com>
2022-07-28 20:06:20 +0100

    contract: remove dist from develop

--- packages/contract/dist/contract/buildTx.d.ts
+++
@@ -1,6 +0,0 @@
-import type { SignerOptions } from '@polkadot/api/types';
-import { SubmittableExtrinsic } from '@polkadot/api/types';
-import type { Registry } from '@polkadot/types/types';
-import { TransactionResponse } from '../types/contract';
-export declare function buildTx(registry: Registry, extrinsic: SubmittableExtrinsic<'promise'>, signer: string, options?: Partial<SignerOptions>): Promise<TransactionResponse>;
-//# sourceMappingURL=buildTx.d.ts.map
