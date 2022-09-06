5dad04a99c5d1ba04c1ba0f6e3f86b6409ddcea9
Lupus7 <sinisa.canak997@gmail.com>
2022-09-06 14:18:40 +0200

    provider: fix types

--- packages/provider/src/types/api.ts
+++ packages/provider/src/types/api.ts
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { z } from 'zod'
-import { Captcha, CaptchaSolutionSchema } from '@prosopo/contract'
+import { Captcha, CaptchaSolutionSchema, Override, RawSolution } from '@prosopo/contract'
 import { AnyJson } from '@polkadot/types/types/codec'
 
 export interface CaptchaWithProof {
-    captcha: Captcha
+    captcha: Override<Captcha, { solution?: RawSolution[] }>
     proof: string[][]
 }
 
