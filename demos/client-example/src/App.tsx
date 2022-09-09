ed1f36fb34a52b10bb9943dd2915f319ed137d1e
Chris Taylor <forgetso86@gmail.com>
2022-09-09 12:24:06 +0100

    client-example: css

--- demos/client-example/src/App.tsx
+++ demos/client-example/src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import {useState} from "react";
+import {Box, Button, Typography} from "@mui/material";
 
 import {
-  TCaptchaSubmitResult,
-  TExtensionAccount,
+    TCaptchaSubmitResult,
+    TExtensionAccount,
 } from "@prosopo/procaptcha";
 
 import {
-  CaptchaComponent,
-  CaptchaContextManager,
-  ExtensionAccountSelect,
-  useCaptcha,
+    CaptchaComponent,
+    CaptchaContextManager,
+    ExtensionAccountSelect,
+    useCaptcha,
 } from "@prosopo/procaptcha-react";
 
 import config from "./config";
@@ -20,76 +20,85 @@ import "./App.css";
 
 function App() {
 
-  const [showCaptchas, setShowCaptchas] = useState(false);
+    const [showCaptchas, setShowCaptchas] = useState(false);
 
-  const showCaptchaClick = () => {
-    setShowCaptchas(true);
-    status.update({ info: "" });
-  };
+    const showCaptchaClick = () => {
+        setShowCaptchas(true);
+        status.update({info: ""});
+    };
 
-  const onAccountChange = (account: TExtensionAccount) => {
-    setShowCaptchas(true);
-    status.update({ info: "Selected account: " + account?.meta.name });
-    console.log("CAPTCHA API", clientInterface.getCaptchaApi());
-  };
+    const onAccountChange = (account: TExtensionAccount) => {
+        setShowCaptchas(true);
+        status.update({info: "Selected account: " + account?.meta.name});
+        console.log("CAPTCHA API", clientInterface.getCaptchaApi());
+    };
 
-  const onSubmit = (submitResult: TCaptchaSubmitResult | Error) => {
-    if (submitResult instanceof Error) {
-      status.update({ error: ["onSubmit: CAPTCHA SUBMIT ERROR", submitResult] });
-      return;
+    const onSubmit = (submitResult: TCaptchaSubmitResult | Error) => {
+        if (submitResult instanceof Error) {
+            status.update({error: ["onSubmit: CAPTCHA SUBMIT ERROR", submitResult]});
+            return;
+        }
+        const [result, tx] = submitResult;
+        status.update({info: ["onSubmit: CAPTCHA SUBMIT STATUS", result.status]});
+    };
+
+    const onSolved = ([result, tx, commitment]: TCaptchaSubmitResult) => {
+        setShowCaptchas(false);
+
+        status.update({info: ["onSolved:", `Captcha solution status: ${commitment.status}`]});
     }
-    const [result, tx] = submitResult;
-    status.update({ info: ["onSubmit: CAPTCHA SUBMIT STATUS", result.status] });
-  };
 
-  const onSolved = ([result, tx,  commitment]: TCaptchaSubmitResult) => {
-    setShowCaptchas(false);
+    const onChange = (solution: number[][]) => {
+        console.log("onChange:", solution);
+    };
 
-    status.update({ info: ["onSolved:", `Captcha solution status: ${commitment.status}`] });
-  }
+    const onCancel = () => {
+        setShowCaptchas(false);
+        status.update({info: ""});
+    };
 
-  const onChange = (solution: number[][]) => {
-    console.log("onChange:", solution);
-  };
+    const clientInterface = useCaptcha({config}, {onAccountChange, onChange, onSubmit, onSolved, onCancel});
 
-  const onCancel = () => {
-    setShowCaptchas(false);
-    status.update({ info: "" });
-  };
+    const manager = clientInterface.manager;
+    const status = clientInterface.status;
 
-  const clientInterface = useCaptcha({ config }, { onAccountChange, onChange, onSubmit, onSolved, onCancel });
+    return (
+        <Box className={"App"}>
+            <div className={"flex-container"}>
+                <div style={{order: 1}}>
+                    <span>
+                      <pre id="json">{JSON.stringify(manager.state, null, 2)}</pre>
+                    </span>
+                </div>
 
-  const manager = clientInterface.manager;
-  const status = clientInterface.status;
+                <div style={{order: 2}}>
+                    {status.state.info && <Box className={"status"}>{status.state.info}</Box>}
+                    {status.state.error && <Box className={"status error"}>{status.state.error}</Box>}
 
-  return (
-    <Box className={"App"}>
+                    {clientInterface.getExtension() && !manager.state.account &&
+                      <ExtensionAccountSelect
+                        value={manager.state.account}
+                        options={clientInterface.getExtension().getAccounts()}
+                        onChange={clientInterface.onAccountChange.bind(clientInterface)}
+                      />}
 
-      {status.state.info && <Box className={"status"}>{status.state.info}</Box>}
-      {status.state.error && <Box className={"status error"}>{status.state.error}</Box>}
 
-      {clientInterface.getExtension() && !manager.state.account &&
-        <ExtensionAccountSelect
-          value={manager.state.account}
-          options={clientInterface.getExtension().getAccounts()}
-          onChange={clientInterface.onAccountChange.bind(clientInterface)}
-        />}
-      <div><span style={{width: "30%", wordBreak: "break-all"}}>{JSON.stringify(manager.state)}</span></div>
+                    <CaptchaContextManager.Provider value={manager}>
+                        {showCaptchas &&
+                          <CaptchaComponent {...{clientInterface}} />}
+                    </CaptchaContextManager.Provider>
 
-      <CaptchaContextManager.Provider value={manager}>
-        {showCaptchas &&
-          <CaptchaComponent {...{ clientInterface }} />}
-      </CaptchaContextManager.Provider>
+                    {!showCaptchas &&
+                      <Button onClick={showCaptchaClick} className={"iAmHumanButton"}>
+                        <Typography className={"iAmHumanButtonLabel"}>
+                          I am human
+                        </Typography>
+                      </Button>}
+                </div>
+            </div>
 
-      {!showCaptchas &&
-        <Button onClick={showCaptchaClick} className={"iAmHumanButton"}>
-          <Typography className={"iAmHumanButtonLabel"}>
-            I am human
-          </Typography>
-        </Button>}
-
-    </Box>
-  );
+        </Box>
+    );
 }
 
 export default App;
