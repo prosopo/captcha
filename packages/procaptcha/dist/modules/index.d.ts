05c1ec3377028863c6629942c1ba1cbd2c31453b
Lupus7 <sinisa.canak997@gmail.com>
2022-09-28 16:31:56 +0200

    procaptcha: rm dist

--- packages/procaptcha/dist/modules/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './contract';
-export * from './extension';
-export * from './storage';
-export * from './client';
-export * from './ProsopoCaptchaApi';
-export * from './ProsopoCaptchaClient';
-export * from './ProsopoCaptchaStateClient';
-//# sourceMappingURL=index.d.ts.map
