10cb978f8cc223e29d2f231f3457080b86cfdaad
Lupus7 <sinisa.canak997@gmail.com>
2022-09-28 16:47:14 +0200

    contract: update ProsopoEnvError to support i18n

--- packages/contract/src/handlers.ts
+++ packages/contract/src/handlers.ts
@@ -13,15 +13,53 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with contract. If not, see <http://www.gnu.org/licenses/>.
-import {DispatchError} from "@polkadot/types/interfaces";
+import { DispatchError } from "@polkadot/types/interfaces";
+import { TranslationKey, i18n as i18next, translationKeys } from "@prosopo/i18n"
 
-export class ProsopoEnvError extends Error {
-    constructor(error: Error | string, context?: string, ...params: any[]) {
+type TOptions = Record<string, string>;
+
+function translateOrFallback(key: string | undefined, options?: TOptions, fallback?: string, i18n = i18next): string {
+    try {
+        if (key) {
+            return i18n.t(key, options);
+        } else {
+            return fallback || key!;
+        }
+    } catch {
+        return fallback || key!;
+    }
+}
+
+export abstract class ProsopoBaseError extends Error {
+    protected tKey: TranslationKey | undefined;
+    protected tParams: Record<string, string>;
+
+    getTranslated(i18n: typeof i18next): string {
+        return translateOrFallback(this.tKey, this.tParams, this.message, i18n);
+    }
+}
+
+export class ProsopoEnvError extends ProsopoBaseError {
+    constructor(error: ProsopoEnvError);
+    constructor(error: Error, context?: TranslationKey, options?: TOptions, ...params: any[]);
+    constructor(error: TranslationKey, context?: string, options?: TOptions, ...params: any[]);
+
+    constructor(error: Error | TranslationKey, context?: TranslationKey | string, options?: TOptions, ...params: any[]) {            
         const isError = error instanceof Error;
-        super(isError ? error.message : error);
-        this.name = context && `${ProsopoEnvError.name}@${context}` || ProsopoEnvError.name;
+        super(isError ? error.message : translateOrFallback(error, options));
+        this.name = context && `${ProsopoEnvError.name}@${translateOrFallback(context, options)}` || ProsopoEnvError.name;
         if (isError) {
             this.cause = error;
+            if (error instanceof ProsopoEnvError) {
+                this.tKey = error.tKey;
+                this.tParams = error.tParams;
+            } else if (translationKeys.includes(context as TranslationKey)) {
+                this.tKey = context as TranslationKey;        
+                this.tParams = options || {};
+            } 
+        } else {
+            this.tKey = error as TranslationKey;        
+            this.tParams = options || {};
         }
         
         console.error('\n********************* ERROR *********************\n');
