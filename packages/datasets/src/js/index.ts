353e918c02c3280f559756e608242e79c53edb36
Chris Taylor <forgetso86@gmail.com>
2022-10-11 15:47:25 +0100

    datasets: Remove errors from index

--- packages/datasets/src/js/index.ts
+++ packages/datasets/src/js/index.ts
@@ -14,5 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with provider.  If not, see <http://www.gnu.org/licenses/>.
 export * from './captcha/index'
-export * from './errors'
 export * from './types/index'
