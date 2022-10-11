9c6a4e6d4164e769e2b7b7f06d6edb78f3748a14
Chris Taylor <forgetso86@gmail.com>
2022-10-11 20:07:34 +0100

    protocol: Update GitHub URLs

--- protocol/protocol/contracts/lib.rs
+++ protocol/protocol/contracts/lib.rs
@@ -1,5 +1,5 @@
 // Copyright (C) 2021-2022 Prosopo (UK) Ltd.
-// This file is part of provider <https://github.com/prosopo-io/provider>.
+// This file is part of provider <https://github.com/prosopo/provider>.
 //
 // provider is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
