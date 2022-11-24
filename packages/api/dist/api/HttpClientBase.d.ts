99d69934a5fb1a911d269ccdcc7e06ba6ad689e0
Chris Taylor <forgetso86@gmail.com>
2022-11-24 15:22:28 +0000

    api: Ignore dist and add merkle proof type

--- packages/api/dist/api/HttpClientBase.d.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosInstance, AxiosResponse } from 'axios';
-export declare class HttpClientBase {
-    protected readonly axios: AxiosInstance;
-    constructor(baseURL: string, prefix?: string);
-    protected responseHandler: (response: AxiosResponse) => any;
-    protected errorHandler: (error: any) => Promise<never>;
-}
-export default HttpClientBase;
-//# sourceMappingURL=HttpClientBase.d.ts.map
--- packages/api/dist/api/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './HttpClientBase';
-export * from './ProviderApi';
-//# sourceMappingURL=index.d.ts.map
