5ef7a1eeb3f66a00d8782b174bd6b663380df215
Chris Taylor <forgetso86@gmail.com>
2022-12-08 14:11:54 +0000

    common: Add common repository

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,6 @@
-export { default as i18n } from "./i18n";
-export * from "./react";
-export * from "./node";
-export { TFunctionParams, TranslationKey, translationKeys } from "./utils";
-export * from "./custom";
+export { default as i18n } from './i18n'
+export * from './react'
+export * from './node'
+export { TFunctionParams, TranslationKey, translationKeys } from './utils'
+export * from './custom'
+export * from './error'
