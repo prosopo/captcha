418a743a617c2456c6f2fd7d621078a981e62509
George Oastler <goastler4@gmail.com>
2023-02-24 14:17:47 +0000

    protocol: fixed span

--- protocol/protocol/contracts/inject_self_macro/src/lib.rs
+++ protocol/protocol/contracts/inject_self_macro/src/lib.rs
@@ -30,19 +30,13 @@ fn inject_impl (
     input: TokenStream,
 ) -> Result<TokenStream, &'static str>
 {
-    // parse::Nothing for `params`.
     if let Some(_) = params.into_iter().next() {
         return Err("unexpected attribute arguments".into());
     }
 
-    let input2 = handle(input.clone());
+    let output = handle(input.clone());
 
-    // let mut ts = TokenStream::new();
-    // ts.extend(input2.clone());
-    // let output = ts.to_string();
-    // println!("output:\n{}\n", output);
-
-    Ok(input2.into_iter().collect())
+    Ok(output.into_iter().collect())
 }
 
 fn handle(input: TokenStream) -> TokenStream {
@@ -52,35 +46,33 @@ fn handle(input: TokenStream) -> TokenStream {
     let mut found_fn = false;
     while let Some(tt) = input.next() {
         match tt {
-            TokenTree::Group(g) => {
+            TokenTree::Group(mut g) => {
+                
+                let span = g.span();
                 let mut sub_ts = handle(g.stream());
                 if found_fn {
                     if g.delimiter() == Delimiter::Brace {
-                        let inject = quote!(
+                        let mut inject = quote!(
                             macro_rules! get_self {
                                 () => {
                                     &self
                                 }
                             }
                         );
-                        let mut inject_ts = TokenStream::new();
-                        inject_ts.extend(inject);
-                        inject_ts.extend(sub_ts);
-                        sub_ts = inject_ts;
-
+                        inject.extend(sub_ts);
+                        sub_ts = inject;
                         found_fn = false;
                     }
                 }
-                output.push(TokenTree::Group(Group::new(
-                    g.delimiter(),
-                    sub_ts,
-                )));
+                g = Group::new(g.delimiter(), sub_ts);
+                g.set_span(span);
+                output.push(g.into());
             }
             TokenTree::Ident(i) => {
                 if i.to_string() == "fn" {
-                    found_fn = true;
+                    found_fn = true;                    
                 }
-                output.push(TokenTree::Ident(i));
+                output.push(i.into());
             }
             _ => output.push(tt),
         }
--- protocol/protocol/contracts/named_functions_macro/src/lib.rs
+++ protocol/protocol/contracts/named_functions_macro/src/lib.rs
@@ -8,7 +8,7 @@ use proc_macro2::{
     TokenTree,
 };
 
-use quote::{quote};
+use quote::quote;
 
 #[proc_macro_attribute] 
 pub fn named_functions (
@@ -30,50 +30,40 @@ fn named_impl (
     input: TokenStream,
 ) -> Result<TokenStream, &'static str>
 {
-    // parse::Nothing for `params`.
     if let Some(_) = params.into_iter().next() {
         return Err("unexpected attribute arguments".into());
     }
 
-    let input2 = handle(input.clone());
+    let output = handle(input);
 
-    // let mut ts = TokenStream::new();
-    // ts.extend(input2.clone());
-    // let output = ts.to_string();
-    // println!("output:\n{}\n", output);
-
-    Ok(input2.into_iter().collect())
+    Ok(output.into_iter().collect())
 }
 
 fn handle(input: TokenStream) -> TokenStream {
-
     let mut input = input.into_iter().peekable();
     let mut output = Vec::<TokenTree>::new();
     let mut found_fn = false;
     let mut fname = TokenTree::from(Literal::string(&String::from("unknown")));
     while let Some(tt) = input.next() {
         match tt {
-            TokenTree::Group(g) => {
+            TokenTree::Group(mut g) => {
+                let span = g.span();
                 let mut sub_ts = handle(g.stream());
                 if found_fn {
                     if g.delimiter() == Delimiter::Brace {
-                        let inject = quote!(
+                        let mut inject = quote!(
                             macro_rules! function_name {() => (
                                 #fname
                             )}
                         );
-                        let mut inject_ts = TokenStream::new();
-                        inject_ts.extend(inject);
-                        inject_ts.extend(sub_ts);
-                        sub_ts = inject_ts;
-
+                        inject.extend(sub_ts);
+                        sub_ts = inject;
                         found_fn = false;
                     }
                 }
-                output.push(TokenTree::Group(Group::new(
-                    g.delimiter(),
-                    sub_ts,
-                )));
+                g = Group::new(g.delimiter(), sub_ts);
+                g.set_span(span);
+                output.push(g.into());
             }
             TokenTree::Ident(i) => {
                 if i.to_string() == "fn" {
@@ -81,7 +71,7 @@ fn handle(input: TokenStream) -> TokenStream {
                     let s = input.peek().unwrap().to_string();
                     fname = TokenTree::from(Literal::string(&s));
                 }
-                output.push(TokenTree::Ident(i));
+                output.push(i.into());
             }
             _ => output.push(tt),
         }
