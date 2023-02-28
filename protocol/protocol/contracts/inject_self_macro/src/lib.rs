0781a80be5412c11431362b332e9cbbe43856a6f
George Oastler <goastler4@gmail.com>
2023-02-28 17:20:47 +0000

    protocol: rust fmt inject_self

--- protocol/protocol/contracts/inject_self_macro/src/lib.rs
+++ protocol/protocol/contracts/inject_self_macro/src/lib.rs
@@ -1,35 +1,23 @@
 extern crate proc_macro2;
 
-use proc_macro2::{
-    Delimiter,
-    Group,
-    Literal,
-    TokenStream,
-    TokenTree,
-};
+use proc_macro2::{Delimiter, Group, Literal, TokenStream, TokenTree};
 
-use quote::{quote};
+use quote::quote;
 
-#[proc_macro_attribute] 
-pub fn inject_self (
+#[proc_macro_attribute]
+pub fn inject_self(
     params: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     inject_impl(params.into(), input.into())
         .unwrap_or_else(|err| {
             let err = TokenTree::from(Literal::string(err));
-            quote!(
-                ::core::compile_error! { #err }
-            )
+            quote!(::core::compile_error! { #err })
         })
         .into()
 }
 
-fn inject_impl (
-    params: TokenStream,
-    input: TokenStream,
-) -> Result<TokenStream, &'static str>
-{
+fn inject_impl(params: TokenStream, input: TokenStream) -> Result<TokenStream, &'static str> {
     if params.into_iter().next().is_some() {
         return Err("unexpected attribute arguments");
     }
@@ -40,14 +28,12 @@ fn inject_impl (
 }
 
 fn handle(input: TokenStream) -> TokenStream {
-
     let input = input.into_iter().peekable();
     let mut output = Vec::<TokenTree>::new();
     let mut found_fn = false;
     for tt in input {
         match tt {
             TokenTree::Group(mut g) => {
-                
                 let span = g.span();
                 let mut sub_ts = handle(g.stream());
                 if found_fn && g.delimiter() == Delimiter::Brace {
@@ -55,7 +41,7 @@ fn handle(input: TokenStream) -> TokenStream {
                         macro_rules! get_self {
                             () => {
                                 &self
-                            }
+                            };
                         }
                     );
                     inject.extend(sub_ts);
@@ -68,7 +54,7 @@ fn handle(input: TokenStream) -> TokenStream {
             }
             TokenTree::Ident(i) => {
                 if i == "fn" {
-                    found_fn = true;                    
+                    found_fn = true;
                 }
                 output.push(i.into());
             }
