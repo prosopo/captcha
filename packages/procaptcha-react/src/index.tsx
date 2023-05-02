d7faaba17d1c9b4d04cd3a5a4262e03402037387
hugh <hugh@prosopo.io>
2023-05-02 13:41:01 +0100

    initial setup with static config

---
+++ packages/procaptcha-react/src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Procaptcha } from './components/Procaptcha';
+
+const config = {
+    userAccountAddress: '',
+    web2: process.env.REACT_APP_WEB2 === 'true',
+    dappName: 'Prosopo',
+    network: {
+        endpoint: 'ws://127.0.0.1:9944',
+        prosopoContract: {
+            address: '5FnxNv4GRpEmenypr1Scvh8gCGHjmYUT5Cz3nBWr8WU76AW6',
+            name: 'prosopo',
+        },
+        dappContract: {
+            address: '5HAbczrPszB9QVUaLtgEjkKEA6McNNSnqGmgSJbu5kQnUhMF',
+            name: 'dapp',
+        },
+    },
+    solutionThreshold: 80,
+}
+
+ReactDOM.render(<Procaptcha config={config} />, document.getElementById('root'));
