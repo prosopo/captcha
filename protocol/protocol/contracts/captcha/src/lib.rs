533fb25470cf19f32ff5b747a1290dc6192be737
George Oastler <goastler4@gmail.com>
2023-05-26 15:57:02 +0100

    protocol: fix author in ctor instantiate guard

--- protocol/protocol/contracts/captcha/src/lib.rs
+++ protocol/protocol/contracts/captcha/src/lib.rs
@@ -367,11 +367,7 @@ pub mod captcha {
             max_provider_fee: Balance,
             rewind_window: u8,
         ) -> Self {
-            let instantiator = AccountId::from([
-                212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44,
-                133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125,
-            ]); // alice
-            if Self::env().caller() != instantiator {
+            if Self::env().caller() != AccountId::from(AUTHOR) {
                 panic!("Not authorised to instantiate this contract");
             }
             Self::new_unguarded(
