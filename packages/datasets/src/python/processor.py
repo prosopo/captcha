61aadcdc65d147398f13c7107dba51d514dbf14a
hugh <hugh@prosopo.io>
2023-06-13 10:55:55 +0100

    processer changes for multiple folder structure with progress bar

--- packages/datasets/src/python/processor.py
+++ packages/datasets/src/python/processor.py
@@ -5,21 +5,20 @@ import string
 import os
 from skimage import transform
 from skimage.util import img_as_ubyte
+from tqdm.notebook import tqdm
 
 
 class ImageProcessor:
-    def __init__(self, image_paths, font_path):
-        for path in image_paths:
-            if not os.path.exists(path):
-                raise ValueError(f"Image file does not exist: {path}")
-
+    def __init__(self, font_path):
         if not os.path.exists(font_path):
             raise ValueError("Font file does not exist.")
 
-        self.image_paths = image_paths
         self.font_path = font_path
 
     def find_coeffs(self, pa, pb):
+        """
+        Calculates the coefficients for a perspective transformation used to distort an image
+        """
         matrix = []
         for p1, p2 in zip(pa, pb):
             matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0] * p1[0], -p2[0] * p1[1]])
@@ -58,7 +57,7 @@ class ImageProcessor:
             text_image.height - height
         ) / 3
 
-        text_draw.text((x_position, y_position), random_word, (0, 0, 0, 126), font=font)
+        text_draw.text((x_position, y_position), random_word, (0, 0, 0, 90), font=font)
 
         # Randomly rotate the text
         angle = random.randint(-45, 45)
@@ -81,10 +80,10 @@ class ImageProcessor:
                 (self.image.width, 0),
                 (
                     np.random.randint(
-                        int(self.image.width * 0.8), int(self.image.width * 1.2)
+                        int(self.image.width * 0.9), int(self.image.width * 1.1)
                     ),
                     np.random.randint(
-                        int(self.image.height * 0.8), int(self.image.height * 1.2)
+                        int(self.image.height * 0.9), int(self.image.height * 1.1)
                     ),
                 ),
                 (0, self.image.height),
@@ -99,7 +98,7 @@ class ImageProcessor:
         )
 
     def blur_and_contrast(self) -> None:
-        blurred_image = self.image.filter(ImageFilter.GaussianBlur(radius=2))
+        blurred_image = self.image.filter(ImageFilter.GaussianBlur(radius=1))
         self.image = ImageOps.autocontrast(blurred_image.convert("RGB"), cutoff=1)
 
     def multiple_swirls(self):
@@ -111,17 +110,26 @@ class ImageProcessor:
         # Loop through multiple centres
         for _ in range(num_swirls):
             # Randomly select a centre
-            y_center, x_center = np.random.randint(0, image_array.shape[0]), np.random.randint(0, image_array.shape[1])
+            y_center, x_center = np.random.randint(
+                0, image_array.shape[0]
+            ), np.random.randint(0, image_array.shape[1])
 
             # Swirl transformation parameters
             rotation = np.random.rand() * np.pi
             strength = 10 * np.random.rand()
-            radius = min(image_array.shape[0], image_array.shape[1]) / (2. * np.random.rand() + 2.)
-            image_array = transform.swirl(image_array, center=(y_center, x_center), rotation=rotation, strength=strength, radius=radius)
+            radius = min(image_array.shape[0], image_array.shape[1]) / (
+                2.0 * np.random.rand() + 2.0
+            )
+            image_array = transform.swirl(
+                image_array,
+                center=(y_center, x_center),
+                rotation=rotation,
+                strength=strength,
+                radius=radius,
+            )
 
         swirled_image_array = Image.fromarray(img_as_ubyte(image_array))
         self.image = Image.blend(self.image, swirled_image_array, alpha=0.2)
-
 
     def get_filename_without_extension(self, filepath):
         return os.path.splitext(os.path.basename(filepath))[0]
@@ -142,3 +150,43 @@ class ImageProcessor:
 
             output_path = os.path.join(output_folder, filename)
             self.image.save(output_path)
+
+    def process_folder(self, source_folder: str, target_folder: str) -> None:
+        labels = [
+            name
+            for name in os.listdir(source_folder)
+            if os.path.isdir(os.path.join(source_folder, name))
+        ]
+
+        for label in tqdm(
+            labels, desc="Processing folders"
+        ):  # wrap your loop in tqdm for a progress bar
+            print(f"Processing images in folder: {label}")
+
+            source_subfolder = os.path.join(source_folder, label)
+            target_subfolder = os.path.join(target_folder, label)
+            os.makedirs(target_subfolder, exist_ok=True)
+
+            image_paths = [
+                os.path.join(source_subfolder, image)
+                for image in os.listdir(source_subfolder)
+            ]
+
+            self.image_paths = image_paths
+
+            for i, image_path in enumerate(
+                tqdm(self.image_paths, desc=f"Processing images in {label} folder")
+            ):  # wrap this loop in tqdm for another progress bar
+                self.image = Image.open(image_path)
+
+                self.add_noise()
+                self.add_text()
+                self.multiple_swirls()
+                self.distort_image()
+                self.blur_and_contrast()
+
+                filename = (
+                    f"{self.get_filename_without_extension(image_path)}_processed.png"
+                )
+                output_path = os.path.join(target_subfolder, filename)
+                self.image.save(output_path)
