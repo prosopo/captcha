7724630cbe2cea933ee891c9b64647d072e9e683
Chris Taylor <forgetso86@gmail.com>
2023-06-20 11:43:27 +0100

    just use two different files for webpack instead of trying to inherit. MiniCssBundler doesn't work when inheriting

--- demos/client-example/webpack.config.production.js
+++ demos/client-example/webpack.config.production.js
@@ -1,6 +1,12 @@
-
+const {loadEnv} = require('@prosopo/cli')
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const InterpolateHtmlPlugin = require('interpolate-html-plugin')
+const PUBLIC_URL = process.env.PUBLIC_URL || '/';
+const libraryName = 'prosopo_client_example_bundle'
+const mode = 'production'
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const config = require('./webpack.config.base.js');
 
 // plugin that exits the process once compilation is done
 const donePlugin =   {
@@ -19,12 +25,86 @@ const productionPlugins = [
   new MiniCssExtractPlugin(),
   donePlugin
 ]
-const rules = config.module.rules
-rules[2].use[0] = MiniCssExtractPlugin.loader
 
-module.exports = {
-  mode: 'production',
-  plugins: config.plugins.concat(productionPlugins),
-  rules: rules,
-  ...config,
+
+loadEnv()
+module.exports  = (env, argv) => {
+  return {
+    mode: mode,
+    target: 'web',
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      fallback: {},
+    },
+    entry: './src/index.tsx',
+    output: {
+      filename: `${libraryName}.[name].bundle.js`,
+      path: path.resolve(__dirname, 'dist'),
+      library: libraryName,
+      chunkFilename: `${libraryName}.[name].bundle.js`,
+      publicPath: PUBLIC_URL,
+
+    },
+    devServer: {
+      port: process.env.REACT_APP_PORT,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+          options: {},
+        },
+        {
+          test: /\.jpg$/,
+          type: "asset/resource",
+        },
+        {
+          // If you enable `experiments.css` or `experiments.futureDefaults`, please uncomment line below
+          // type: "javascript/auto",
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          //exclude: /(node_modules)/,
+          test: /\.(ts|tsx)$/,
+          resolve: {
+            fullySpecified: false,
+          },
+          use: [
+            {
+              loader: require.resolve('ts-loader'),
+              options: {
+                configFile: 'tsconfig.webpack.json',
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      ...productionPlugins,
+      new InterpolateHtmlPlugin({PUBLIC_URL: ''}),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+        favicon: './public/favicon-32x32.png'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.PUBLIC_URL': PUBLIC_URL,
+        'process.env.REACT_APP_SERVER_URL': JSON.stringify(process.env.REACT_APP_SERVER_URL || 'http://localhost:9228'),
+        'process.env.REACT_APP_WEB2': JSON.stringify(process.env.REACT_APP_WEB2 || 'true'),
+        'process.env.REACT_APP_SUBSTRATE_ENDPOINT': JSON.stringify(process.env.REACT_APP_SUBSTRATE_ENDPOINT || 'http://localhost:9944'),
+        'process.env.REACT_APP_PROSOPO_CONTRACT_ADDRESS': JSON.stringify(process.env.REACT_APP_PROSOPO_CONTRACT_ADDRESS || ''),
+        'process.env.REACT_APP_DAPP_CONTRACT_ADDRESS': JSON.stringify(process.env.REACT_APP_DAPP_CONTRACT_ADDRESS || ''),
+      }),
+    ]
+  }
+
 }
