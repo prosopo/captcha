fed5825a29ee9da9d8d904bae3cfec053dee02d5
George Oastler <goastler4@gmail.com>
2023-06-20 10:28:48 +0100

    fix exec in cli

--- dev/src/contract/import.ts
+++ dev/src/contract/import.ts
@@ -1,25 +1,9 @@
-import { exec } from 'child_process'
+import { ExecOutput, exec } from '../util'
 
-async function importContract(absPathToABIs: string, absPathToOutput: string): Promise<void> {
+async function importContract(relPathToABIs: string, relPathToOutput: string): Promise<ExecOutput> {
     //TODO import typechain when it's working https://github.com/Brushfam/typechain-polkadot/issues/73
-    const cmd = `npx @727-ventures/typechain-polkadot --in ${absPathToABIs} --out ${absPathToOutput}`
-    console.log(`Running ${cmd}`)
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`${error.message}`)
-                return reject(error)
-            }
-
-            if (stderr) {
-                console.warn(`${stderr}`)
-                return resolve()
-            }
-
-            console.log(`${stdout}`)
-            resolve()
-        })
-    })
+    const cmd = `npx @727-ventures/typechain-polkadot --in ${relPathToABIs} --out ${relPathToOutput}`
+    return exec(cmd)
 }
 
 export default importContract
