fc72a51f6c21085fdee47d0dd619f935ff238cdd
Chris Taylor <forgetso86@gmail.com>
2023-07-03 16:17:49 +0100

    Fix import

--- packages/provider/src/batch/commitments.ts
+++ packages/provider/src/batch/commitments.ts
@@ -13,7 +13,7 @@ import { Logger } from '@prosopo/common'
 import { ProsopoCaptchaContract, ProsopoContractError, batch, encodeStringArgs, oneUnit } from '@prosopo/contract'
 import { SubmittableExtrinsic } from '@polkadot/api/types'
 import { WeightV2 } from '@polkadot/types/interfaces'
-import { checkIfTaskIsRunning } from '@prosopo/provider'
+import { checkIfTaskIsRunning } from '../util'
 import { randomAsHex } from '@polkadot/util-crypto'
 
 const BN_TEN_THOUSAND = new BN(10_000)
