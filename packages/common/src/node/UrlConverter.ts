12f4cb2ea62a0aef346563e452b14d50cb6d69ee
Chris Taylor <forgetso86@gmail.com>
2023-07-07 14:27:27 +0100

    Update copyright notices at the top of source files

--- packages/common/src/node/UrlConverter.ts
+++ packages/common/src/node/UrlConverter.ts
@@ -1,3 +1,16 @@
+// Copyright 2021-2023 Prosopo (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 class UrlConverter {
     private readonly symbols = [
         '', // empty string == termination symbol / noop. This may occur at the end of a byte array where the first 2 bits are used in the final symbol and the remaining 6 bits are ignored. But, because of the spacing, the remaining 6 bits appear to be a symbol. These 6 bits will be set to 000000. Thus we map 000000 (decimal number 0) to the empty string here.
--- packages/provider/tests/data/captchas.ts
+++ packages/provider/tests/data/captchas.ts
@@ -1,3 +1,16 @@
+// Copyright 2021-2023 Prosopo (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 export const captchaData = {
     datasetId: '0x0105797b3529e14b9ff72548ef16865d4d9e0f79f1e393decd2f435ff5f2cc0c',
     captchas: [
