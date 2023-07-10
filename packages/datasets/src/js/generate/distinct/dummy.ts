d2a73007f0a0c304f5a18704b17460406cbf6310
George Oastler <goastler4@gmail.com>
2023-07-10 14:48:23 +0100

    Added dummy data generation to test captcha dataset generation

---
+++ packages/datasets/src/js/generate/distinct/dummy.ts
@@ -0,0 +1,29 @@
+// generate some dummy data for testing the generate fns
+
+import { LabelledItem, UnlabelledItem } from '@prosopo/types';
+import fs from 'fs'
+
+const nLabels = 4;
+const nLabelledImages = 100;
+const nUnlabelledImages = 100;
+
+const labelledImages: LabelledItem[] = [];
+const unlabelledImages: UnlabelledItem[] = [];
+
+for(let i = 0; i < nLabelledImages; i++) {
+    const label = i % nLabels;
+    labelledImages.push({
+        label: label.toString(),
+        data: `abc${i}`,
+    })
+}
+
+for(let i = 0; i < nUnlabelledImages; i++) {
+    unlabelledImages.push({
+        data: `def${i}`,
+    })
+}
+
+fs.writeFileSync('labelled.json', JSON.stringify(labelledImages))
+fs.writeFileSync('unlabelled.json', JSON.stringify(unlabelledImages))
+fs.writeFileSync('labels.json', JSON.stringify(Array.from(Array(nLabels).keys()).map(x => x.toString())))
