fcfb26dd410bf2ff522e2e67f2876e42125ad051
HughParry <90424587+HughParry@users.noreply.github.com>
2023-07-13 15:57:31 +0100

    Provider gui frontend init (#254)
    * initial frontend pages
    
    * global state and chrome polkadot apps setup
    
    * Provider api hook
    
    * profile mock data
    
    * Profile types/interfaces
    
    * initial global state
    
    * Global mocks
    
    * placeholder stepper page
    
    * spacer component
    
    * changes ready for pr of initial shape of gui
    
    * Submodule removed
    
    * Resolving PR requested changes
    
    * mocks rename
    
    ---------
    
    Co-authored-by: hugh <hugh@prosopo.io>

---
+++ provider-gui/mocks/global-state-mocks.ts
@@ -0,0 +1,20 @@
+import { GlobalState } from '../types/global-state-types'
+import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types'
+import { actionsMock, datasetMock, environmentMock, summaryMock } from './profile-mocks'
+
+// Mock for injected Polkadot account
+const accountMock: InjectedAccountWithMeta = {
+    address: 'polkadotAddress',
+    meta: {
+        name: 'AccountName',
+        source: 'polkadot.js',
+    },
+}
+
+const globalStateMock: GlobalState = {
+    profile: { environment: environmentMock, actions: actionsMock, dataset: datasetMock, summary: summaryMock },
+    accounts: [accountMock],
+    currentAccount: accountMock.address,
+}
+
+export default globalStateMock
