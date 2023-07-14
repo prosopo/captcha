01c55530c4a114f32d77ca58cd92061e610fef18
George Oastler <goastler4@gmail.com>
2023-07-14 21:48:19 +0100

    add consola to dataset generation with log level

--- packages/datasets/src/js/cli.ts
+++ packages/datasets/src/js/cli.ts
@@ -1,6 +1,7 @@
 // Take a set of data and generate a bunch of captchas
 
 import { hideBin } from 'yargs/helpers'
+import consola, { LogLevels } from 'consola'
 import flatten from './flatten/cli'
 import generate from './generate/cli'
 import scale from './scale/cli'
@@ -9,10 +10,18 @@ import yargs from 'yargs'
 const main = async () => {
     await yargs(hideBin(process.argv))
         .help()
+        .option('logLevel', {
+            type: 'string',
+            description: 'Verbosity of logging',
+        })
+        .middleware((argv) => {
+            consola.level = LogLevels[argv.logLevel || 'log'] || LogLevels.log
+        })
         .command(generate)
         .command(flatten)
         .command(scale)
         .strictCommands()
+        .showHelpOnFail(false, 'Specify --help for available options')
         .fail(false)
         .parse()
 }
