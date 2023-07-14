8d6c4ebb4dc7b3484dc11c40043dec08b4ad1adf
George Oastler <goastler4@gmail.com>
2023-07-14 13:34:34 +0100

    fix dataset generation choosing from collections with duplicates

--- packages/datasets/src/js/generate/distinct/generate.ts
+++ packages/datasets/src/js/generate/distinct/generate.ts
@@ -1,6 +1,6 @@
 import { Args } from './args'
 import { CaptchaTypes, CaptchaWithoutId, Captchas, Item, LabelledItem, RawSolution } from '@prosopo/types'
-import { checkDuplicates } from '../util'
+import { checkDuplicates, choice } from '../util'
 import bcrypt from 'bcrypt'
 import fs from 'fs'
 import seedrandom from 'seedrandom'
@@ -19,7 +19,6 @@ export default async (args: Args) => {
     if (unlabelledMapFile && !fs.existsSync(unlabelledMapFile)) {
         throw new Error(`unlabelled map file does not exist: ${unlabelledMapFile}`)
     }
-
     const labelsFile: string | undefined = args.labels
     const seed: number = args.seed || 0
     const size: number = args.size || 9
@@ -31,19 +30,15 @@ export default async (args: Args) => {
     const saltRounds = 10
     const allowDuplicatesLabelled = args.allowDuplicatesLabelled || args.allowDuplicates || false
     const allowDuplicatesUnlabelled = args.allowDuplicatesUnlabelled || args.allowDuplicates || false
-
     const uint32 = () => Math.abs(random.int32())
-
     // load the map to get the labelled and unlabelled data
     const labelled: LabelledItem[] = labelledMapFile ? JSON.parse(fs.readFileSync(labelledMapFile, 'utf8')) : []
     const unlabelled: Item[] = unlabelledMapFile ? JSON.parse(fs.readFileSync(unlabelledMapFile, 'utf8')) : []
-
     // check for duplicates
     checkDuplicates(labelled, unlabelled, {
         allowDuplicatesLabelled,
         allowDuplicatesUnlabelled,
     })
-
     // split the labelled data by label
     const labelToImages: { [label: string]: Item[] } = {}
     for (const entry of labelled) {
@@ -51,7 +46,6 @@ export default async (args: Args) => {
         labelToImages[entry.label].push(entry)
     }
     const targets = Object.keys(labelToImages)
-
     // load the labels from file
     // these are the labels that unlabelled data will be assigned to
     // note that these can be different to the labels in the map file as the labelled data is independent of the unlabelled data in terms of labels
@@ -62,63 +56,68 @@ export default async (args: Args) => {
         // else default to the labels in the labelled data
         labels.push(...[...targets])
     }
-
     // generate n solved captchas
     const solvedCaptchas: CaptchaWithoutId[] = []
     for (let i = 0; i < solved; i++) {
-        console.log(`generating solved captcha ${i}`)
-        if (labelled.length <= size) {
-            throw new Error(`labelled map file does not contain enough data: ${labelledMapFile}`)
-        }
-        if (targets.length <= 0) {
+        console.log(`generating solved captcha ${i + 1} of ${solved}`)
+
+        if (targets.length <= 1) {
             throw new Error(`not enough different labels in labelled data: ${labelledMapFile}`)
         }
+
         // uniformly sample targets
         const target = targets[i % targets.length]
         const notTargets = targets.filter((t) => t !== target)
-        if (notTargets.length <= 0) {
-            throw new Error(`not enough different labels in labelled data: ${labelledMapFile}`)
-        }
+
         // how many correct items should be in the captcha?
-        const correct = (uint32() % (maxCorrect - minCorrect + 1)) + minCorrect
+        const nCorrect = (uint32() % (maxCorrect - minCorrect + 1)) + minCorrect
         // how many incorrect items should be in the captcha?
-        const incorrect = size - correct
+        const nIncorrect = size - nCorrect
+
+        const targetItems: Item[] = labelToImages[target]
+        const notTargetItems: Item[] = notTargets.map((notTarget) => labelToImages[notTarget]).flat()
+
+        if (targetItems.length < nCorrect) {
+            throw new Error(`not enough images for target (${target})`)
+        }
+        if (notTargetItems.length < nIncorrect) {
+            throw new Error(`not enough non-matching images for target (${target})`)
+        }
+
         // get the correct items
-        const correctItems = new Set<Item>()
-        while (correctItems.size < correct) {
-            // get a random image from the target
-            const image = labelToImages[target][uint32() % labelToImages[target].length]
-            correctItems.add(image)
-        }
+        const correctItems: Item[] = choice(targetItems, nCorrect, uint32, { withReplacement: false }).choices
+
         // get the incorrect items
-        const incorrectItems = new Set<Item>()
-        while (incorrectItems.size < incorrect) {
-            // get a random image from the target
-            const index = uint32() % notTargets.length
-            const notTarget = notTargets[index]
-            const imgs = labelToImages[notTarget]
-            const image = imgs[uint32() % imgs.length]
-            incorrectItems.add(image)
-        }
-        const items: Item[] = [...correctItems, ...incorrectItems].map((item) => {
-            return {
-                data: item.data,
-                hash: item.hash,
-                type: item.type,
-            }
-        })
+        const incorrectItems: Item[] = choice(notTargetItems, nIncorrect, uint32, { withReplacement: false }).choices
 
+        let items: Item[] = [...correctItems, ...incorrectItems]
+        const indices: number[] = [...Array(items.length).keys()]
         // shuffle the items
-        for (let i = 0; i < items.length; i++) {
-            const j = uint32() % items.length
-            const tmp = items[i]
-            items[i] = items[j]
-            items[j] = tmp
+        for (let i = indices.length - 1; i > 0; i--) {
+            const j = uint32() % (i + 1)
+            ;[indices[i], indices[j]] = [indices[j], indices[i]]
         }
+        items = indices.map((i) => items[i])
+        // items = items.map((item) => {
+        //     return {
+        //         data: item.data,
+        //         hash: item.hash,
+        //         type: item.type,
+        //     }
+        // })
 
-        // get the solution
-        const correctData = new Set<string>([...correctItems].map((item) => item.data))
-        const solution: RawSolution[] = [...Array(items.length).keys()].filter((i) => correctData.has(items[i].data))
+        // the first n indices are the correct items
+        const solution: RawSolution[] = indices
+            .map((index, i) => {
+                return {
+                    pre: index, // the index of the item in the items array before shuffle
+                    post: i, // the index of the item in the shuffled array
+                }
+            })
+            .filter((item) => item.pre < correctItems.length) // keep all items that were in the first n slots of the original item array - these were the correct items
+            .map((item) => {
+                return item.post // return the index in the shuffled array
+            })
 
         const salt = bcrypt.genSaltSync(saltRounds)
         // create the captcha
@@ -130,11 +129,10 @@ export default async (args: Args) => {
         }
         solvedCaptchas.push(captcha)
     }
-
     // generate n unsolved captchas
     const unsolvedCaptchas: CaptchaWithoutId[] = []
     for (let i = 0; i < unsolved; i++) {
-        console.log(`generating unsolved captcha ${i}`)
+        console.log(`generating unsolved captcha ${i + 1} of ${unsolved}`)
         if (unlabelled.length <= size) {
             throw new Error(`unlabelled map file does not contain enough data: ${unlabelledMapFile}`)
         }
@@ -146,27 +144,23 @@ export default async (args: Args) => {
         const index = uint32() % labels.length
         const target = labels[index]
         // randomly pick images from the unlabelled data
-        const itemSet = new Set<Item>()
-        while (itemSet.size < size) {
-            const item = unlabelled[uint32() % unlabelled.length]
-            itemSet.add(item)
+        const itemSet: Item[] = choice(unlabelled, size, uint32, { withReplacement: false }).choices
+        // shuffle the items
+        let items: Item[] = [...itemSet]
+        const indices: number[] = [...Array(items.length).keys()]
+        // shuffle the items
+        for (let i = indices.length - 1; i > 0; i--) {
+            const j = uint32() % indices.length
+            ;[indices[i], indices[j]] = [indices[j], indices[i]]
         }
-        const items: Item[] = [...itemSet].map((item) => {
+        items = indices.map((i) => items[i])
+        items = items.map((item) => {
             return {
                 data: item.data,
                 hash: item.hash,
                 type: item.type,
             }
         })
-
-        // shuffle the items
-        for (let i = 0; i < items.length; i++) {
-            const j = uint32() % items.length
-            const tmp = items[i]
-            items[i] = items[j]
-            items[j] = tmp
-        }
-
         const salt = bcrypt.genSaltSync(saltRounds)
         // create the captcha
         const captcha: CaptchaWithoutId = {
@@ -176,7 +170,6 @@ export default async (args: Args) => {
         }
         unsolvedCaptchas.push(captcha)
     }
-
     // write to file
     const output: Captchas = {
         captchas: [...solvedCaptchas, ...unsolvedCaptchas],
--- packages/datasets/src/js/generate/union/generate.ts
+++ packages/datasets/src/js/generate/union/generate.ts
@@ -1,6 +1,6 @@
 import { Args } from './args'
 import { CaptchaTypes, CaptchaWithoutId, Item, LabelledItem, RawSolution } from '@prosopo/types'
-import { checkDuplicates } from '../util'
+import { checkDuplicates, choice } from '../util'
 import bcrypt from 'bcrypt'
 import fs from 'fs'
 import seedrandom from 'seedrandom'
@@ -28,7 +28,6 @@ export default async (args: Args) => {
     if (unlabelledMapFile && !fs.existsSync(unlabelledMapFile)) {
         throw new Error(`Unlabelled map file does not exist: ${unlabelledMapFile}`)
     }
-
     const labelsFile: string | undefined = args.labels
     const seed: number = args.seed || 0
     const size: number = args.size || 9
@@ -37,29 +36,23 @@ export default async (args: Args) => {
     const saltRounds = 10
     const allowDuplicatesLabelled = args.allowDuplicatesLabelled || args.allowDuplicates || false
     const allowDuplicatesUnlabelled = args.allowDuplicatesUnlabelled || args.allowDuplicates || false
-
     const uint32 = () => Math.abs(random.int32())
-
     const minIncorrect: number = Math.max(args.minIncorrect || 1, 1) // at least 1 incorrect image
     const minLabelled: number = minCorrect + minIncorrect // min incorrect + correct
     const maxLabelled: number = Math.min(args.maxLabelled || size, size) // at least 1 labelled image
     const count: number = args.count || 0
-
     // the captcha contains n images. Each of these images are either labelled, being correct or incorrect against the target, or unlabelled. To construct one of these captchas, we need to decide how many of the images should be labelled vs unlabelled, and then how many of the labelled images should be correct vs incorrect
     // in the traditional captcha, two rounds are produced, one with labelled images and the other with unlabelled images. This gives 18 images overall, 9 labels produced.
     // the parameters for generation can regulate how many labels are collected vs how much of a test the captcha posses. E.g. 18 images could have 16 unlabelled and 2 labelled, or 2 unlabelled and 16 labelled. The former is a better test of the user being human, but the latter is a better for maximising label collection.
     // if we focus on a single captcha round of 9 images, we must have at least 1 labelled correct image in the captcha for it to work, otherwise it's just a labelling phase, which normally isn't a problem but if we're treating these as tests for humanity too then we need some kind of test in there. (e.g. we abolish the labelled then unlabelled pattern of the challenge rounds in favour of mixing labelled and unlabelled data, but we then run a small chance of serving two completely unlabelled rounds if we don't set the min number of labelled images to 1 per captcha round)
-
     // load the map to get the labelled and unlabelled data
     const labelled: LabelledItem[] = labelledMapFile ? JSON.parse(fs.readFileSync(labelledMapFile, 'utf8')) : []
     const unlabelled: Item[] = unlabelledMapFile ? JSON.parse(fs.readFileSync(unlabelledMapFile, 'utf8')) : []
-
     // check for duplicates
     checkDuplicates(labelled, unlabelled, {
         allowDuplicatesLabelled,
         allowDuplicatesUnlabelled,
     })
-
     // split the labelled data by label
     const labelToImages: { [label: string]: Item[] } = {}
     for (const entry of labelled) {
@@ -67,7 +60,6 @@ export default async (args: Args) => {
         labelToImages[entry.label].push(entry)
     }
     const targets = Object.keys(labelToImages)
-
     // load the labels from file
     // these are the labels that unlabelled data will be assigned to
     // note that these can be differen to the labels in the map file as the labelled data is independent of the unlabelled data in terms of labels
@@ -78,10 +70,15 @@ export default async (args: Args) => {
         // else use the labels from the labelled data
         labels.push(...[...targets])
     }
-
     // generate n captchas
     const captchas: CaptchaUnion[] = []
     for (let i = 0; i < count; i++) {
+        console.log(`generating captcha ${i + 1} of ${count}`)
+
+        if (targets.length <= 1) {
+            throw new Error(`not enough different labels in labelled data: ${labelledMapFile}`)
+        }
+
         // uniformly sample targets
         const target = targets[i % targets.length]
         const notTargets = targets.filter((t) => t !== target)
@@ -91,23 +88,26 @@ export default async (args: Args) => {
         const maxCorrect = nLabelled - minCorrect
         const nCorrect = (uint32() % (maxCorrect - minCorrect + 1)) + minCorrect
         const nIncorrect = nLabelled - nCorrect
+        const nUnlabelled = size - nLabelled
+
+        const targetItems = labelToImages[target]
+        const notTargetItems: Item[] = notTargets.map((notTarget) => labelToImages[notTarget]).flat()
+
+        if (nUnlabelled > unlabelled.length) {
+            throw new Error(`not enough unlabelled data`)
+        }
+        if (nCorrect > targetItems.length) {
+            throw new Error(`not enough images for target (${target})`)
+        }
+        if (nIncorrect > notTargetItems.length) {
+            throw new Error(`not enough non-matching images for target (${target})`)
+        }
 
         // get the correct items
-        const correctItems = new Set<Item>()
-        while (correctItems.size < nCorrect) {
-            // get a random image from the target
-            const image = labelToImages[target][uint32() % labelToImages[target].length]
-            correctItems.add(image)
-        }
+        const correctItems: Item[] = choice(targetItems, nCorrect, uint32, { withReplacement: false }).choices
 
         // get the incorrect items
-        const incorrectItems = new Set<Item>()
-        while (incorrectItems.size < nIncorrect) {
-            // get a random image from the target
-            const notTarget = notTargets[uint32() % notTargets.length]
-            const image = labelToImages[notTarget][uint32() % labelToImages[notTarget].length]
-            incorrectItems.add(image)
-        }
+        const incorrectItems: Item[] = choice(notTargetItems, nIncorrect, uint32, { withReplacement: false }).choices
 
         // get the unlabelled items
         const unlabelledItems = new Set<Item>()
@@ -117,7 +117,15 @@ export default async (args: Args) => {
             unlabelledItems.add(image)
         }
 
-        const items: Item[] = [...correctItems, ...incorrectItems, ...unlabelledItems].map((item) => {
+        let items: Item[] = [...correctItems, ...incorrectItems, ...unlabelledItems]
+        const indices: number[] = [...Array(items.length).keys()]
+        // shuffle the items
+        for (let i = indices.length - 1; i > 0; i--) {
+            const j = uint32() % (i + 1)
+            ;[indices[i], indices[j]] = [indices[j], indices[i]]
+        }
+        items = indices.map((i) => items[i])
+        items = items.map((item) => {
             return {
                 data: item.data,
                 hash: item.hash,
@@ -125,20 +133,31 @@ export default async (args: Args) => {
             }
         })
 
-        // shuffle the items
-        for (let i = 0; i < items.length; i++) {
-            const j = uint32() % items.length
-            const tmp = items[i]
-            items[i] = items[j]
-            items[j] = tmp
-        }
+        // the first n indices are the correct items
+        const solution: RawSolution[] = indices
+            .map((index, i) => {
+                return {
+                    pre: index, // the index of the item in the items array before shuffle
+                    post: i, // the index of the item in the shuffled array
+                }
+            })
+            .filter((item) => item.pre < correctItems.length) // keep all items that were in the first n slots of the original item array - these were the correct items
+            .map((item) => {
+                return item.post // return the index in the shuffled array
+            })
 
-        // get the solutions
-        const correctData = new Set<string>([...correctItems].map((item) => item.data))
-        const solution: RawSolution[] = [...Array(items.length).keys()].filter((i) => correctData.has(items[i].data))
-
-        const unlabelledData = new Set<string>([...unlabelledItems].map((item) => item.data))
-        const unlabelledIndices = [...Array(items.length).keys()].filter((i) => unlabelledData.has(items[i].data))
+        // the unlabelled indices were after the correct and incorrect
+        const unlabelledIndices: RawSolution[] = indices
+            .map((index, i) => {
+                return {
+                    pre: index, // the index of the item in the items array before shuffle
+                    post: i, // the index of the item in the shuffled array
+                }
+            })
+            .filter((item) => item.pre >= correctItems.length + incorrectItems.length) // keep all items that were in the first n slots of the original item array - these were the correct items
+            .map((item) => {
+                return item.post // return the index in the shuffled array
+            })
 
         const salt = bcrypt.genSaltSync(saltRounds)
         // create the captcha
@@ -151,7 +170,6 @@ export default async (args: Args) => {
         }
         captchas.push(captcha)
     }
-
     // write to file
     const output: Captchas = {
         captchas,
