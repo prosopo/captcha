6910835184c02792d09901f90e3f27bcf89bb1c7
HughParry <90424587+HughParry@users.noreply.github.com>
2023-07-26 20:21:24 +0100

    Linting fix for contract package (#287)
    * Linting fix for contract package
    
    * polkadot version types bump fix

--- packages/contract/src/typechain/proxy/types-returns/proxy.ts
+++ packages/contract/src/typechain/proxy/types-returns/proxy.ts
@@ -11,19 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import type BN from 'bn.js';
-import type {ReturnNumber} from '@727-ventures/typechain-types';
 
 export enum LangError {
-	couldNotReadInput = 'CouldNotReadInput'
+    couldNotReadInput = 'CouldNotReadInput',
 }
 
 export type AccountId = string | number[]
 
 export enum Error {
-	notAuthorised = 'NotAuthorised',
-	transferFailed = 'TransferFailed',
-	setCodeHashFailed = 'SetCodeHashFailed',
-	invalidDestination = 'InvalidDestination'
+    notAuthorised = 'NotAuthorised',
+    transferFailed = 'TransferFailed',
+    setCodeHashFailed = 'SetCodeHashFailed',
+    invalidDestination = 'InvalidDestination',
 }
-
