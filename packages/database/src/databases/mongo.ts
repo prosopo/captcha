b7a62bba0b3daa3a6421214c9ca2af9be0d76914
Chris <forgetso86@gmail.com>
2023-07-31 15:34:15 +0100

    Workflow linting checker (#318)
    * Update linting rules for webpack configs. Ignore typechain. Add lint checker in workflow
    
    * hash the entire repo
    
    * remove linting errors where there is no other solution
    
    * hash everything, mark 2
    
    * Add debug
    
    * Compare unstaged changes
    
    * Update package lock

--- packages/database/src/databases/mongo.ts
+++ packages/database/src/databases/mongo.ts
@@ -12,19 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-// Copyright 2021-2022 Prosopo (UK) Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 import {
     ArgumentTypes,
     Captcha,
@@ -133,6 +120,7 @@ export class ProsopoDatabase extends AsyncFactory implements Database {
     /** Close connection to the database */
     async close(): Promise<void> {
         this.logger.info(`Closing connection to ${this.url}`)
+        // eslint-disable-next-line no-async-promise-executor
         await new Promise<void>(async (resolve, reject): Promise<void> => {
             mongoose.connection
                 .close()
