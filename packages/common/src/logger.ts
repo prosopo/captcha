a5be87799f9ec5ff3cfb4c17a0ad664a4e33d4e8
George Oastler <goastler4@gmail.com>
2023-07-31 16:15:16 +0100

    built logger adapter framework for using different loggers

--- packages/common/src/logger.ts
+++ packages/common/src/logger.ts
@@ -13,16 +13,60 @@
 // limitations under the License.
 import consola, { LogLevel as ConsolaLogLevel } from 'consola'
 
-export type Logger = typeof consola
-export type LogLevel = ConsolaLogLevel
-export const LogLevel = ConsolaLogLevel
+export interface Logger {
+    log(message: unknown, ...args: unknown[]): void
+    info(message: unknown, ...args: unknown[]): void
+    debug(message: unknown, ...args: unknown[]): void
+    trace(message: unknown, ...args: unknown[]): void
+    warn(message: unknown, ...args: unknown[]): void
+    error(message: unknown, ...args: unknown[]): void
+    setLogLevel(level: LogLevel): void
+    getLogLevel(): LogLevel
+}
+
+export enum LogLevel {
+    Silent = Number.NEGATIVE_INFINITY,
+    Error = 0,
+    Warn = 1,
+    Log = 2,
+    Info = 3,
+    Debug = 4,
+    Trace = 5,
+    Verbose = Number.POSITIVE_INFINITY,
+}
 
 // Create a new logger with the given level and scope
-export function getLogger(level: LogLevel, scope: string): Logger {
-    return consola.create({ level }).withScope(scope)
+export function getLogger(logLevel: LogLevel | string, scope: string): Logger {
+    const level = typeof logLevel === 'string' ? LogLevel[logLevel] : logLevel
+    return getLoggerAdapterConsola(level, scope)
 }
 
 // Get the default logger (i.e. the global logger)
 export function getLoggerDefault(): Logger {
-    return consola
+    return getLoggerAdapterConsola(LogLevel.Info, '')
+}
+
+const getLoggerAdapterConsola = (logLevel: LogLevel, scope: string): Logger => {
+    // automatically maps 0,1,2... to consola's log levels (which conveniently match... :O )
+    const convertToLevel = (logLevel: LogLevel): ConsolaLogLevel => {
+        return ConsolaLogLevel[LogLevel[logLevel] as keyof typeof ConsolaLogLevel]
+    }
+    const convertFromLevel = (logLevel: ConsolaLogLevel): LogLevel => {
+        return LogLevel[ConsolaLogLevel[logLevel] as keyof typeof LogLevel]
+    }
+    const logger = consola.create({ level: convertToLevel(logLevel) }).withScope(scope)
+    return {
+        log: logger.log,
+        info: logger.info,
+        debug: logger.debug,
+        trace: logger.trace,
+        warn: logger.warn,
+        error: logger.error,
+        setLogLevel: (level: LogLevel) => {
+            logger.level = convertToLevel(level)
+        },
+        getLogLevel: () => {
+            return convertFromLevel(logger.level)
+        },
+    }
 }
