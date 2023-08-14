83b9da104bb3bd3d5901f8186a2e46573763ffa7
George Oastler <goastler4@gmail.com>
2023-08-14 14:55:52 +0100

    fix common config import in contracts

--- protocol/contracts/proxy/src/lib.rs
+++ protocol/contracts/proxy/src/lib.rs
@@ -19,7 +19,7 @@
 pub mod proxy {
 
     use common::err;
-    use common::config;
+    use common::common::config;
     #[allow(unused_imports)]
     use ink::env::debug_println as debug;
     #[allow(unused_imports)] // do not remove StorageLayout, it is used in derives
