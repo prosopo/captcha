33009e013a1797fb598591e58bdbba1535e024f2
Chris Taylor <forgetso86@gmail.com>
2023-08-16 14:14:57 +0100

    Fix yargs stuff

---
+++ packages/cli/src/commands/providerRegister.ts
@@ -0,0 +1,46 @@
+import { Logger } from '@prosopo/common'
+import { ProviderEnvironment } from '@prosopo/types-env'
+import { Tasks } from '@prosopo/provider'
+import { stringToU8a } from '@polkadot/util'
+import { validatePayee } from './validators.js'
+import { wrapQuery } from '@prosopo/contract'
+export default (env: ProviderEnvironment, tasks: Tasks, cmdArgs?: { logger?: Logger }) => {
+    const logger = cmdArgs?.logger || env.logger
+
+    return {
+        command: 'provider_register',
+        decription: 'Register a Provider',
+        builder: (yargs) =>
+            yargs
+                .option('url', {
+                    type: 'string' as const,
+                    demand: true,
+                    desc: 'The provider service origin (URI)',
+                } as const)
+                .option('fee', {
+                    type: 'number',
+                    demand: true,
+                    desc: 'The fee to pay per solved captcha',
+                } as const)
+                .option('payee', {
+                    type: 'string' as const,
+                    demand: true,
+                    desc: 'The person who receives the fee (`Provider` or `Dapp`)',
+                } as const),
+        handler: async (argv) => {
+            const providerRegisterArgs: Parameters<typeof tasks.contract.query.providerRegister> = [
+                Array.from(stringToU8a(argv.url)),
+                argv.fee,
+                argv.payee,
+                {
+                    value: 0,
+                },
+            ]
+            await wrapQuery(tasks.contract.query.providerRegister, tasks.contract.query)(...providerRegisterArgs)
+            const result = await tasks.contract.tx.providerRegister(...providerRegisterArgs)
+
+            logger.info(JSON.stringify(result, null, 2))
+        },
+        middlewares: [validatePayee],
+    }
+}
