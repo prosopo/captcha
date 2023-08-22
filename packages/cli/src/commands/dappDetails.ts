d5d26713712314201fb11ec5260b23787fef4d36
Chris <forgetso86@gmail.com>
2023-08-22 14:19:02 +0100

    Compos-able cli (#388)
    * Fix yargs stuff
    
    * break down cli
    
    * remove .js extensions
    
    * shorten imports

---
+++ packages/cli/src/commands/dappDetails.ts
@@ -0,0 +1,30 @@
+import { Logger } from '@prosopo/common'
+import { ProviderEnvironment } from '@prosopo/types-env'
+import { Tasks } from '@prosopo/provider'
+import { validateAddress } from './validators'
+export default (env: ProviderEnvironment, tasks: Tasks, cmdArgs?: { logger?: Logger }) => {
+    const logger = cmdArgs?.logger || env.logger
+
+    return {
+        command: 'dapp_details',
+        description: 'List details of a single Dapp',
+        builder: (yargs) =>
+            yargs.option('address', {
+                type: 'string' as const,
+                demand: true,
+                desc: 'The AccountId of the Dapp',
+            } as const),
+        handler: async (argv) => {
+            try {
+                const result = (await tasks.contract.query.getDapp(validateAddress(argv.address).address)).value
+                    .unwrap()
+                    .unwrap()
+
+                logger.info(JSON.stringify(result, null, 2))
+            } catch (err) {
+                logger.error(err)
+            }
+        },
+        middlewares: [validateAddress],
+    }
+}
---
+++ packages/cli/src/commands/providerUpdate.ts
@@ -0,0 +1,55 @@
+import { Logger } from '@prosopo/common'
+import { ProviderEnvironment } from '@prosopo/types-env'
+import { Tasks } from '@prosopo/provider'
+import { validateAddress, validatePayee } from './validators'
+import { wrapQuery } from '@prosopo/contract'
+export default (env: ProviderEnvironment, tasks: Tasks, cmdArgs?: { logger?: Logger }) => {
+    const logger = cmdArgs?.logger || env.logger
+
+    return {
+        command: 'provider_update',
+        description: 'Update a Provider',
+        builder: (yargs) =>
+            yargs
+                .option('url', {
+                    type: 'string' as const,
+                    demand: false,
+                    desc: 'The provider service origin (URI)',
+                } as const)
+                .option('fee', {
+                    type: 'number',
+                    demand: false,
+                    desc: 'The fee to pay per solved captcha',
+                } as const)
+                .option('payee', {
+                    type: 'string' as const,
+                    demand: false,
+                    desc: 'The person who receives the fee (`Provider` or `Dapp`)',
+                } as const)
+                .option('value', {
+                    type: 'number',
+                    demand: false,
+                    desc: 'The value to stake in the contract',
+                } as const),
+        handler: async (argv) => {
+            const provider = (await tasks.contract.query.getProvider(argv.address, {})).value.unwrap().unwrap()
+            if (provider && (argv.url || argv.fee || argv.payee || argv.value)) {
+                await wrapQuery(tasks.contract.query.providerUpdate, tasks.contract.query)(
+                    argv.url ? argv.url.toString() : provider.url,
+                    argv.fee || provider.fee,
+                    argv.payee || provider.payee,
+                    { value: argv.value || 0 }
+                )
+                const result = await tasks.contract.tx.providerUpdate(
+                    argv.url || provider.url,
+                    argv.fee || provider.fee,
+                    argv.payee || provider.payee,
+                    { value: argv.value || 0 }
+                )
+
+                logger.info(JSON.stringify(result, null, 2))
+            }
+        },
+        middlewares: [validateAddress, validatePayee],
+    }
+}
