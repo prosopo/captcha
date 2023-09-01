15a67ccaaa63ea89f2e6312f263b63d1d1f42a83
George Oastler <goastler4@gmail.com>
2023-09-01 14:55:13 +0100

    fix seedrandom seed type being string only

--- packages/util/src/util.ts
+++ packages/util/src/util.ts
@@ -3,7 +3,7 @@ import seedrandom from 'seedrandom'
 
 // set the seed for the global rng, i.e. seed `Math.random()`
 export const setSeedGlobal = (seed: number | string) => {
-    seedrandom(seed, { global: true })
+    seedrandom(seed.toString(), { global: true })
 }
 
 // create a new lodash instance with the current Math.random() and other global state
@@ -16,7 +16,7 @@ export const seedLodash = (seed: number | string) => {
     // take a snapshot of the current Math.random() fn
     const orig = Math.random
     // replace Math.random with the seeded random
-    seedrandom(seed, { global: true })
+    seedrandom(seed.toString(), { global: true })
     // runInContext() creates a new lodash instance using the seeded Math.random()
     // the context is a snapshot of the state of the global javascript environment, i.e. Math.random() updated to the seedrandom instance
     const lodash = _lodash.runInContext()
@@ -28,7 +28,7 @@ export const seedLodash = (seed: number | string) => {
 
 // create a new rng with the given seed
 export const rng = (seed: number | string) => {
-    const rng = seedrandom(seed)
+    const rng = seedrandom(seed.toString())
     return {
         double: () => rng.double(),
         float: () => rng.quick(),
