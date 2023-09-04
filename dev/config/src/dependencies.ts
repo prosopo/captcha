b034c9e623068323da3bb21886d92818015db86f
George Oastler <goastler4@gmail.com>
2023-09-04 18:57:07 +0100

    fix array access

--- dev/config/src/dependencies.ts
+++ dev/config/src/dependencies.ts
@@ -2,6 +2,7 @@ import { Glob } from 'glob'
 import { ProsopoEnvError, getLogger } from '@prosopo/common'
 import child_process from 'child_process'
 import util from 'util'
+import { at } from '@prosopo/util'
 const logger = getLogger(`Info`, `config.dependencies.js`)
 /**
  * Get the dependencies for a package
@@ -37,7 +38,7 @@ export async function getDependencies(packageName?: string): Promise<string[]> {
     // for each line, split on "/" and take the last part
     stdout.split('\n').forEach((line) => {
         const parts = line.split('node_modules/')
-        deps.push(parts[parts.length - 1])
+        deps.push(at(parts,parts.length - 1))
     })
     return deps
 }
