e01b8fc28d1306d1697074400bbd3e8268ced54a
George Oastler <goastler4@gmail.com>
2023-09-05 18:42:11 +0100

    fix provider_accounts cmd call

--- packages/cli/src/commands/providerAccounts.ts
+++ packages/cli/src/commands/providerAccounts.ts
@@ -2,6 +2,7 @@ import { Logger } from '@prosopo/common'
 import { ProviderEnvironment } from '@prosopo/types-env'
 import { Tasks } from '@prosopo/provider'
 import { ArgumentsCamelCase, Argv } from 'yargs'
+import { get } from '@prosopo/util'
 
 export default (env: ProviderEnvironment, tasks: Tasks, cmdArgs?: { logger?: Logger }) => {
     const logger = cmdArgs?.logger || env.logger
@@ -12,7 +13,8 @@ export default (env: ProviderEnvironment, tasks: Tasks, cmdArgs?: { logger?: Log
         builder: (yargs: Argv) => yargs,
         handler: async (argv: ArgumentsCamelCase) => {
             try {
-                const result = await tasks.contract.contract['providerAccounts']()
+                const func = get<() => unknown>(tasks.contract.contract,'providerAccounts')
+                const result = await func()
 
                 logger.info(JSON.stringify(result, null, 2))
             } catch (err) {
