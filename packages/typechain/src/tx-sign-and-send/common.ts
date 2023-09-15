58ce5f9d370ea1fc1e2359dca40f65be1d46bf2c
George Oastler <goastler4@gmail.com>
2023-09-15 20:39:36 +0100

    typechain

---
+++ packages/typechain/src/tx-sign-and-send/common.ts
@@ -0,0 +1,81 @@
+/* This file is auto-generated */
+
+import type { ContractPromise } from '@polkadot/api-contract';
+import type { KeyringPair } from '@polkadot/keyring/types';
+import type { ApiPromise } from '@polkadot/api';
+import type { GasLimit, GasLimitAndRequiredValue, Result } from '@727-ventures/typechain-types';
+import { txSignAndSend } from '@727-ventures/typechain-types';
+import type * as ArgumentTypes from '../types-arguments/common.js';
+import type BN from 'bn.js';
+// @ts-ignore
+import type {EventRecord} from "@polkadot/api/submittable";
+import {decodeEvents} from '../shared/utils.js';
+import EVENT_DATA_TYPE_DESCRIPTIONS from '../event-data/common.json.js';
+
+
+export default class Methods {
+	readonly __nativeContract : ContractPromise;
+	readonly __keyringPair : KeyringPair;
+	readonly __apiPromise: ApiPromise;
+
+	constructor(
+		apiPromise: ApiPromise,
+		nativeContract : ContractPromise,
+		keyringPair : KeyringPair,
+	) {
+		this.__apiPromise = apiPromise;
+		this.__nativeContract = nativeContract;
+		this.__keyringPair = keyringPair;
+	}
+
+	/**
+	* getCaller
+	*
+	*/
+	"getCaller" (
+		__options ? : GasLimit,
+	){
+		return txSignAndSend( this.__apiPromise, this.__nativeContract, this.__keyringPair, "getCaller", (events: EventRecord) => {
+			return decodeEvents(events, this.__nativeContract, EVENT_DATA_TYPE_DESCRIPTIONS);
+		}, [], __options);
+	}
+
+	/**
+	* getCallerBytes
+	*
+	*/
+	"getCallerBytes" (
+		__options ? : GasLimit,
+	){
+		return txSignAndSend( this.__apiPromise, this.__nativeContract, this.__keyringPair, "getCallerBytes", (events: EventRecord) => {
+			return decodeEvents(events, this.__nativeContract, EVENT_DATA_TYPE_DESCRIPTIONS);
+		}, [], __options);
+	}
+
+	/**
+	* getAccountBytes
+	*
+	* @param { ArgumentTypes.AccountId } account,
+	*/
+	"getAccountBytes" (
+		account: ArgumentTypes.AccountId,
+		__options ? : GasLimit,
+	){
+		return txSignAndSend( this.__apiPromise, this.__nativeContract, this.__keyringPair, "getAccountBytes", (events: EventRecord) => {
+			return decodeEvents(events, this.__nativeContract, EVENT_DATA_TYPE_DESCRIPTIONS);
+		}, [account], __options);
+	}
+
+	/**
+	* getGitCommitId
+	*
+	*/
+	"getGitCommitId" (
+		__options ? : GasLimit,
+	){
+		return txSignAndSend( this.__apiPromise, this.__nativeContract, this.__keyringPair, "getGitCommitId", (events: EventRecord) => {
+			return decodeEvents(events, this.__nativeContract, EVENT_DATA_TYPE_DESCRIPTIONS);
+		}, [], __options);
+	}
+
+}
