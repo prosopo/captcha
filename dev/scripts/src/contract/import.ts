a24da6b592a44dfd1a7f1cf2003737cdff6ed795
George Oastler <goastler4@gmail.com>
2023-09-15 10:36:17 +0100

    typechain add eslint ignore for ts-ignore

--- dev/scripts/src/contract/import.ts
+++ dev/scripts/src/contract/import.ts
@@ -36,7 +36,7 @@ async function importContract(pathToAbis: string, pathToOutput: string) {
                 // eslint-disable-next-line no-useless-escape
                 const regex = /import[\s]+(.+?)[\s]+from[\s]+([\"\']\..+)/gm
                 const fileContents = fs.readFileSync(filePath, 'utf8')
-                const replaced = fileContents.replace(regex, (match, p1, p2) => {
+                let replaced = fileContents.replace(regex, (match, p1, p2) => {
                     const name = p1.toString()
                     const srcQuoted = p2.toString()
                     const src = getPath(srcQuoted)
@@ -54,6 +54,12 @@ async function importContract(pathToAbis: string, pathToOutput: string) {
                     }
                     console.log(`Replacing \n\t${match}\nwith\n\t${result}\nin ${filePath}`)
                     return `${result}`
+                })
+                // eslint-disable-next-line no-useless-escape
+                replaced = replaced.replace(/(^(?!\s*\/\/\s*eslint).*)\n+(\s*)\/\/\s*@ts-ignore/gm, (match, p1, p2) => {
+                    const result = `${p1}\n${p2}// eslint-disable-next-line @typescript-eslint/ban-ts-comment\n${p2}// @ts-ignore`
+                    console.log(`Replacing \n\t${match}\nwith\n\t${result}\nin ${filePath}`)
+                    return result
                 })
                 fs.writeFileSync(filePath, replaced)
             }
