a0fb1db30387c08d4315fccd3579ff7d59acdffc
George Oastler <goastler4@gmail.com>
2023-09-15 11:07:21 +0100

    rust fmt

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -70,7 +70,10 @@ pub mod common {
 
         /// Get the git commit id from when this contract was built
         pub fn get_git_commit_id() -> [u8; 20] {
-            let env_git_commit_id: [u8; 20] = [247, 15, 86, 37, 67, 132, 91, 243, 44, 114, 47, 0, 153, 63, 44, 240, 57, 194, 66, 240, ];
+            let env_git_commit_id: [u8; 20] = [
+                247, 15, 86, 37, 67, 132, 91, 243, 44, 114, 47, 0, 153, 63, 44, 240, 57, 194, 66,
+                240,
+            ];
             env_git_commit_id
         }
 
