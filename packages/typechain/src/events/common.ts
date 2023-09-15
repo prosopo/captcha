04d9ba41d990a0df33603be740a58d2721f3ef38
George Oastler <goastler4@gmail.com>
2023-09-15 19:45:06 +0100

    typechain

---
+++ packages/typechain/src/events/common.ts
@@ -0,0 +1,45 @@
+import type * as EventTypes from '../event-types/common.js';
+import type {ContractPromise} from "@polkadot/api-contract";
+import type {ApiPromise} from "@polkadot/api";
+import EVENT_DATA_TYPE_DESCRIPTIONS from '../event-data/common.json' assert { type: 'json.js' };
+import {getEventTypeDescription} from '../shared/utils.js';
+import {handleEventReturn} from "@727-ventures/typechain-types";
+
+export default class EventsClass {
+	readonly __nativeContract : ContractPromise;
+	readonly __api : ApiPromise;
+
+	constructor(
+		nativeContract : ContractPromise,
+		api : ApiPromise,
+	) {
+		this.__nativeContract = nativeContract;
+		this.__api = api;
+	}
+
+
+	private __subscribeOnEvent(
+		callback : (args: any[], event: any) => void,
+		filter : (eventName: string) => boolean = () => true
+	) {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		return this.__api.query.system.events((events) => {
+			events.forEach((record: any) => {
+				const { event } = record;
+
+				if (event.method == 'ContractEmitted') {
+					const [address, data] = record.event.data;
+
+					if (address.toString() === this.__nativeContract.address.toString()) {
+						const {args, event} = this.__nativeContract.abi.decodeEvent(data);
+
+						if (filter(event.identifier.toString()))
+							callback(args, event);
+					}
+				}
+			});
+		});
+	}
+
+}
