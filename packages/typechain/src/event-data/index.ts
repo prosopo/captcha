acccea1680be59d165134498888777fe54e63cb9
George Oastler <goastler4@gmail.com>
2023-09-15 21:22:19 +0100

    index.js files

---
+++ packages/typechain/src/event-data/index.ts
@@ -0,0 +1,3 @@
+export { default as Captcha } from './captcha.json'
+export { default as Common } from './common.json'
+export { default as Proxy } from './proxy.json'
---
+++ packages/typechain/src/events/index.ts
@@ -0,0 +1,3 @@
+export * as Captcha from './captcha.js'
+export * as Common from './common.js'
+export * as Proxy from './proxy.js'
