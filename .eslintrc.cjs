fcde680a72c5d5bd24cfed1cf1168f1b15cfff72
George Oastler <goastler4@gmail.com>
2023-09-22 10:05:09 +0100

    add toml rules to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     extends: [
         'plugin:yaml/recommended',
         'plugin:json/recommended',
+        'plugin:toml/standard',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended', // must be last!
