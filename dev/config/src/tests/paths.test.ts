6d778b60043f433316d6f082123f07b0ec761e22
George Oastler <goastler4@gmail.com>
2023-09-25 16:10:56 +0100

    move path tests

--- dev/config/src/tests/paths.test.ts
+++ dev/config/src/tests/paths.test.ts
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { describe, expect, test } from 'vitest'
-import { flattenObj } from '@prosopo/util/src/util.js'
-import { getPaths } from '../../../packages/common/src/index.js'
+import { flattenObj } from '@prosopo/util'
+import { getPaths } from '../projectInfo.js'
 import fs from 'fs'
 
 describe('paths', () => {
