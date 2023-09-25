ec50fb02c0939722bdebb7f44ade4a30378798f5
George Oastler <goastler@users.noreply.github.com>
2023-09-25 09:33:00 +0100

    combine user signature parts now that ink supports 64 length arrays (#425)
    * combine user signature parts now that ink supports 64 length arrays
    
    * typechain
    
    * Revert "typechain"
    
    This reverts commit 3623ecee12a0412f14dac51750b39369887400cb.
    
    * bump typechain version
    
    * update typechain types
    
    * Revert "update typechain types"
    
    This reverts commit 24069d583071e6d20a9fc3b96455e44ef69ac0eb.
    
    * use ink v4.3.0 with sr25519 in contracts
    
    * working typechain snapshot
    
    * change user_signature to [u8;64]
    
    * cargo workspace fields inheritance unsupported, revert
    
    * consolidate contract dependencies
    
    * change disable-macro version to 0.1.17
    
    * add disable macro to common contract
    
    * add rust version requirement to cargo tomls
    
    * add sr25519_verify() fn to common contract
    
    * lint
    
    * add paths for packages to the config pkg
    
    * remove types specifier for config pkg package.json
    
    * add project paths to config pkg
    
    * use project paths in import_contract typechain
    
    * fix paths to strings
    
    * fix paths leading slash
    
    * make paths use strings instead of URL due to string concatenation issues
    
    * fix typechain generation
    
    * fix relative paths
    
    * add common contract typechain
    
    * fix protocol path
    
    * add step to typechain to add .js extension and json assertion to relative imports
    
    * fix typechain version
    
    * fix typechain version
    
    * use custom ink v4.3.0 with sr25519
    
    * add getContractNames() fn and rename paths.js to projectInfo.js
    
    * update typechain
    
    * typechain
    
    * fmt
    
    * lint
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * lint
    
    * lint
    
    * Update package-lock.json
    
    * lint

--- packages/contract/src/typechain/captcha/events/captcha.ts
+++ packages/contract/src/typechain/captcha/events/captcha.ts
@@ -1,47 +1,44 @@
-// Copyright 2021-2023 Prosopo (UK) Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-import type { ApiPromise } from '@polkadot/api'
-import type { ContractPromise } from '@polkadot/api-contract'
+import type * as EventTypes from '../event-types/captcha.js'
+import type {ContractPromise} from "@polkadot/api-contract";
+import type {ApiPromise} from "@polkadot/api";
+import EVENT_DATA_TYPE_DESCRIPTIONS from '../event-data/captcha.json' assert { type: 'json' }
+import {getEventTypeDescription} from '../shared/utils.js'
+import {handleEventReturn} from "@727-ventures/typechain-types";
 
 export default class EventsClass {
-    readonly __nativeContract: ContractPromise
-    readonly __api: ApiPromise
+	readonly __nativeContract : ContractPromise;
+	readonly __api : ApiPromise;
 
-    constructor(nativeContract: ContractPromise, api: ApiPromise) {
-        this.__nativeContract = nativeContract
-        this.__api = api
-    }
+	constructor(
+		nativeContract : ContractPromise,
+		api : ApiPromise,
+	) {
+		this.__nativeContract = nativeContract;
+		this.__api = api;
+	}
 
-    private __subscribeOnEvent(
-        callback: (args: any[], event: any) => void,
-        filter: (eventName: string) => boolean = () => true
-    ) {
-        // @ts-ignore
-        return this.__api.query.system.events((events) => {
-            events.forEach((record: any) => {
-                const { event } = record
 
-                if (event.method == 'ContractEmitted') {
-                    const [address, data] = record.event.data
+	private __subscribeOnEvent(
+		callback : (args: any[], event: any) => void,
+		filter : (eventName: string) => boolean = () => true
+	) {
+		// @ts-ignore
+		return this.__api.query.system.events((events) => {
+			events.forEach((record: any) => {
+				const { event } = record;
 
-                    if (address.toString() === this.__nativeContract.address.toString()) {
-                        const { args, event } = this.__nativeContract.abi.decodeEvent(data)
+				if (event.method == 'ContractEmitted') {
+					const [address, data] = record.event.data;
 
-                        if (filter(event.identifier.toString())) callback(args, event)
-                    }
-                }
-            })
-        })
-    }
+					if (address.toString() === this.__nativeContract.address.toString()) {
+						const {args, event} = this.__nativeContract.abi.decodeEvent(data);
+
+						if (filter(event.identifier.toString()))
+							callback(args, event);
+					}
+				}
+			});
+		});
+	}
+
 }
