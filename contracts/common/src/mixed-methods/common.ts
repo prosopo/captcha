b492cfcf184984b290fc163b545d41f8025ef731
George Oastler <goastler4@gmail.com>
2023-09-28 17:18:22 +0100

    tpyechain

--- contracts/common/src/mixed-methods/common.ts
+++ contracts/common/src/mixed-methods/common.ts
@@ -1,110 +1,88 @@
 /* This file is auto-generated */
 
-import { handleReturnType, queryOkJSON } from '@727-ventures/typechain-types'
-import type * as ArgumentTypes from '../types-arguments/common.js'
-import type * as ReturnTypes from '../types-returns/common.js'
-import type { ApiPromise } from '@polkadot/api'
-import type { ContractPromise } from '@polkadot/api-contract'
-import type { GasLimit, Result } from '@727-ventures/typechain-types'
-import type { KeyringPair } from '@polkadot/keyring/types'
-import type { QueryReturnType } from '@727-ventures/typechain-types'
+import type { ContractPromise } from '@polkadot/api-contract';
+import type { ApiPromise } from '@polkadot/api';
+import type { KeyringPair } from '@polkadot/keyring/types';
+import type { GasLimit, GasLimitAndRequiredValue, Result } from '@727-ventures/typechain-types';
+import type { QueryReturnType } from '@727-ventures/typechain-types';
+import { queryOkJSON, queryJSON, handleReturnType } from '@727-ventures/typechain-types';
+import { txSignAndSend } from '@727-ventures/typechain-types';
+import type * as ArgumentTypes from '../types-arguments/common.js';
+import type * as ReturnTypes from '../types-returns/common.js';
+import type BN from 'bn.js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { getTypeDescription } from './../shared/utils.js'
+import {ReturnNumber} from '@727-ventures/typechain-types';
+import {getTypeDescription} from './../shared/utils.js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import DATA_TYPE_DESCRIPTIONS from '../data/common.json' assert { type: 'json' }
+import type { EventRecord } from '@polkadot/types/interfaces';
+import {decodeEvents} from '../shared/utils.js';
+import DATA_TYPE_DESCRIPTIONS from '../data/common.json' assert { type: 'json' };
+import EVENT_DATA_TYPE_DESCRIPTIONS from '../event-data/common.json' assert { type: 'json' };
+
 
 export default class Methods {
-    readonly __nativeContract: ContractPromise
-    readonly __keyringPair: KeyringPair
-    readonly __callerAddress: string
-    readonly __apiPromise: ApiPromise
+	readonly __nativeContract : ContractPromise;
+	readonly __keyringPair : KeyringPair;
+	readonly __callerAddress : string;
+	readonly __apiPromise: ApiPromise;
 
-    constructor(apiPromise: ApiPromise, nativeContract: ContractPromise, keyringPair: KeyringPair) {
-        this.__apiPromise = apiPromise
-        this.__nativeContract = nativeContract
-        this.__keyringPair = keyringPair
-        this.__callerAddress = keyringPair.address
-    }
+	constructor(
+		apiPromise : ApiPromise,
+		nativeContract : ContractPromise,
+		keyringPair : KeyringPair,
+	) {
+		this.__apiPromise = apiPromise;
+		this.__nativeContract = nativeContract;
+		this.__keyringPair = keyringPair;
+		this.__callerAddress = keyringPair.address;
+	}
 
-    /**
-     * getCaller
-     *
-     * @returns { Result<ReturnTypes.AccountId, ReturnTypes.LangError> }
-     */
-    getCaller(__options: GasLimit): Promise<QueryReturnType<Result<ReturnTypes.AccountId, ReturnTypes.LangError>>> {
-        return queryOkJSON(
-            this.__apiPromise,
-            this.__nativeContract,
-            this.__callerAddress,
-            'getCaller',
-            [],
-            __options,
-            (result) => {
-                return handleReturnType(result, getTypeDescription(3, DATA_TYPE_DESCRIPTIONS))
-            }
-        )
-    }
+	/**
+	* getCaller
+	*
+	* @returns { Result<ReturnTypes.AccountId, ReturnTypes.LangError> }
+	*/
+	"getCaller" (
+		__options: GasLimit,
+	): Promise< QueryReturnType< Result<ReturnTypes.AccountId, ReturnTypes.LangError> > >{
+		return queryOkJSON( this.__apiPromise, this.__nativeContract, this.__callerAddress, "getCaller", [], __options, (result) => { return handleReturnType(result, getTypeDescription(3, DATA_TYPE_DESCRIPTIONS)); });
+	}
 
-    /**
-     * getCallerBytes
-     *
-     * @returns { Result<Array<number>, ReturnTypes.LangError> }
-     */
-    getCallerBytes(__options: GasLimit): Promise<QueryReturnType<Result<Array<number>, ReturnTypes.LangError>>> {
-        return queryOkJSON(
-            this.__apiPromise,
-            this.__nativeContract,
-            this.__callerAddress,
-            'getCallerBytes',
-            [],
-            __options,
-            (result) => {
-                return handleReturnType(result, getTypeDescription(7, DATA_TYPE_DESCRIPTIONS))
-            }
-        )
-    }
+	/**
+	* getCallerBytes
+	*
+	* @returns { Result<Array<number>, ReturnTypes.LangError> }
+	*/
+	"getCallerBytes" (
+		__options: GasLimit,
+	): Promise< QueryReturnType< Result<Array<number>, ReturnTypes.LangError> > >{
+		return queryOkJSON( this.__apiPromise, this.__nativeContract, this.__callerAddress, "getCallerBytes", [], __options, (result) => { return handleReturnType(result, getTypeDescription(7, DATA_TYPE_DESCRIPTIONS)); });
+	}
 
-    /**
-     * getAccountBytes
-     *
-     * @param { ArgumentTypes.AccountId } account,
-     * @returns { Result<Array<number>, ReturnTypes.LangError> }
-     */
-    getAccountBytes(
-        account: ArgumentTypes.AccountId,
-        __options: GasLimit
-    ): Promise<QueryReturnType<Result<Array<number>, ReturnTypes.LangError>>> {
-        return queryOkJSON(
-            this.__apiPromise,
-            this.__nativeContract,
-            this.__callerAddress,
-            'getAccountBytes',
-            [account],
-            __options,
-            (result) => {
-                return handleReturnType(result, getTypeDescription(7, DATA_TYPE_DESCRIPTIONS))
-            }
-        )
-    }
+	/**
+	* getAccountBytes
+	*
+	* @param { ArgumentTypes.AccountId } account,
+	* @returns { Result<Array<number>, ReturnTypes.LangError> }
+	*/
+	"getAccountBytes" (
+		account: ArgumentTypes.AccountId,
+		__options: GasLimit,
+	): Promise< QueryReturnType< Result<Array<number>, ReturnTypes.LangError> > >{
+		return queryOkJSON( this.__apiPromise, this.__nativeContract, this.__callerAddress, "getAccountBytes", [account], __options, (result) => { return handleReturnType(result, getTypeDescription(7, DATA_TYPE_DESCRIPTIONS)); });
+	}
 
-    /**
-     * getGitCommitId
-     *
-     * @returns { Result<Array<number>, ReturnTypes.LangError> }
-     */
-    getGitCommitId(__options: GasLimit): Promise<QueryReturnType<Result<Array<number>, ReturnTypes.LangError>>> {
-        return queryOkJSON(
-            this.__apiPromise,
-            this.__nativeContract,
-            this.__callerAddress,
-            'getGitCommitId',
-            [],
-            __options,
-            (result) => {
-                return handleReturnType(result, getTypeDescription(8, DATA_TYPE_DESCRIPTIONS))
-            }
-        )
-    }
+	/**
+	* getGitCommitId
+	*
+	* @returns { Result<Array<number>, ReturnTypes.LangError> }
+	*/
+	"getGitCommitId" (
+		__options: GasLimit,
+	): Promise< QueryReturnType< Result<Array<number>, ReturnTypes.LangError> > >{
+		return queryOkJSON( this.__apiPromise, this.__nativeContract, this.__callerAddress, "getGitCommitId", [], __options, (result) => { return handleReturnType(result, getTypeDescription(8, DATA_TYPE_DESCRIPTIONS)); });
+	}
+
 }
--- contracts/proxy/src/types-arguments/proxy.ts
+++ contracts/proxy/src/types-arguments/proxy.ts
@@ -1,113 +1,114 @@
-import type BN from 'bn.js'
+import type BN from 'bn.js';
 
 export enum Error {
-    notAuthorised = 'NotAuthorised',
-    transferFailed = 'TransferFailed',
-    setCodeHashFailed = 'SetCodeHashFailed',
-    invalidDestination = 'InvalidDestination',
-    unknownMessage = 'UnknownMessage',
-    providerAccountExists = 'ProviderAccountExists',
-    providerExists = 'ProviderExists',
-    providerAccountDoesNotExist = 'ProviderAccountDoesNotExist',
-    providerDoesNotExist = 'ProviderDoesNotExist',
-    providerInsufficientFunds = 'ProviderInsufficientFunds',
-    providerInactive = 'ProviderInactive',
-    providerUrlUsed = 'ProviderUrlUsed',
-    dappExists = 'DappExists',
-    dappDoesNotExist = 'DappDoesNotExist',
-    dappInactive = 'DappInactive',
-    dappInsufficientFunds = 'DappInsufficientFunds',
-    captchaDataDoesNotExist = 'CaptchaDataDoesNotExist',
-    commitDoesNotExist = 'CommitDoesNotExist',
-    dappUserDoesNotExist = 'DappUserDoesNotExist',
-    noActiveProviders = 'NoActiveProviders',
-    datasetIdSolutionsSame = 'DatasetIdSolutionsSame',
-    codeNotFound = 'CodeNotFound',
-    unknown = 'Unknown',
-    invalidContract = 'InvalidContract',
-    invalidPayee = 'InvalidPayee',
-    invalidCaptchaStatus = 'InvalidCaptchaStatus',
-    noCorrectCaptcha = 'NoCorrectCaptcha',
-    notEnoughActiveProviders = 'NotEnoughActiveProviders',
-    providerFeeTooHigh = 'ProviderFeeTooHigh',
-    commitAlreadyExists = 'CommitAlreadyExists',
-    notAuthor = 'NotAuthor',
+	notAuthorised = 'NotAuthorised',
+	transferFailed = 'TransferFailed',
+	setCodeHashFailed = 'SetCodeHashFailed',
+	invalidDestination = 'InvalidDestination',
+	unknownMessage = 'UnknownMessage',
+	providerAccountExists = 'ProviderAccountExists',
+	providerExists = 'ProviderExists',
+	providerAccountDoesNotExist = 'ProviderAccountDoesNotExist',
+	providerDoesNotExist = 'ProviderDoesNotExist',
+	providerInsufficientFunds = 'ProviderInsufficientFunds',
+	providerInactive = 'ProviderInactive',
+	providerUrlUsed = 'ProviderUrlUsed',
+	dappExists = 'DappExists',
+	dappDoesNotExist = 'DappDoesNotExist',
+	dappInactive = 'DappInactive',
+	dappInsufficientFunds = 'DappInsufficientFunds',
+	captchaDataDoesNotExist = 'CaptchaDataDoesNotExist',
+	commitDoesNotExist = 'CommitDoesNotExist',
+	dappUserDoesNotExist = 'DappUserDoesNotExist',
+	noActiveProviders = 'NoActiveProviders',
+	datasetIdSolutionsSame = 'DatasetIdSolutionsSame',
+	codeNotFound = 'CodeNotFound',
+	unknown = 'Unknown',
+	invalidContract = 'InvalidContract',
+	invalidPayee = 'InvalidPayee',
+	invalidCaptchaStatus = 'InvalidCaptchaStatus',
+	noCorrectCaptcha = 'NoCorrectCaptcha',
+	notEnoughActiveProviders = 'NotEnoughActiveProviders',
+	providerFeeTooHigh = 'ProviderFeeTooHigh',
+	commitAlreadyExists = 'CommitAlreadyExists',
+	notAuthor = 'NotAuthor'
 }
 
 export enum LangError {
-    couldNotReadInput = 'CouldNotReadInput',
+	couldNotReadInput = 'CouldNotReadInput'
 }
 
 export interface ProxyMessages {
-    getGitCommitId?: null
-    getAdmin?: null
-    getDestination?: null
-    proxyWithdraw?: string | number | BN
-    proxyTerminate?: null
-    proxySetCodeHash?: Array<number | string | BN>
+	getGitCommitId ? : null,
+	getAdmin ? : null,
+	getDestination ? : null,
+	proxyWithdraw ? : (string | number | BN),
+	proxyTerminate ? : null,
+	proxySetCodeHash ? : Array<(number | string | BN)>
 }
 
 export class ProxyMessagesBuilder {
-    static GetGitCommitId(): ProxyMessages {
-        return {
-            getGitCommitId: null,
-        }
-    }
-    static GetAdmin(): ProxyMessages {
-        return {
-            getAdmin: null,
-        }
-    }
-    static GetDestination(): ProxyMessages {
-        return {
-            getDestination: null,
-        }
-    }
-    static ProxyWithdraw(value: string | number | BN): ProxyMessages {
-        return {
-            proxyWithdraw: value,
-        }
-    }
-    static ProxyTerminate(): ProxyMessages {
-        return {
-            proxyTerminate: null,
-        }
-    }
-    static ProxySetCodeHash(value: Array<number | string | BN>): ProxyMessages {
-        return {
-            proxySetCodeHash: value,
-        }
-    }
+	static GetGitCommitId(): ProxyMessages {
+		return {
+			getGitCommitId: null,
+		};
+	}
+	static GetAdmin(): ProxyMessages {
+		return {
+			getAdmin: null,
+		};
+	}
+	static GetDestination(): ProxyMessages {
+		return {
+			getDestination: null,
+		};
+	}
+	static ProxyWithdraw(value: (string | number | BN)): ProxyMessages {
+		return {
+			proxyWithdraw: value,
+		};
+	}
+	static ProxyTerminate(): ProxyMessages {
+		return {
+			proxyTerminate: null,
+		};
+	}
+	static ProxySetCodeHash(value: Array<(number | string | BN)>): ProxyMessages {
+		return {
+			proxySetCodeHash: value,
+		};
+	}
 }
 
 export interface ProxyReturnTypes {
-    u8X32?: Array<number | string | BN>
-    u8X20?: Array<number | string | BN>
-    accountId?: AccountId
-    void?: null
+	u8X32 ? : Array<(number | string | BN)>,
+	u8X20 ? : Array<(number | string | BN)>,
+	accountId ? : AccountId,
+	void ? : null
 }
 
 export class ProxyReturnTypesBuilder {
-    static U8x32(value: Array<number | string | BN>): ProxyReturnTypes {
-        return {
-            u8X32: value,
-        }
-    }
-    static U8x20(value: Array<number | string | BN>): ProxyReturnTypes {
-        return {
-            u8X20: value,
-        }
-    }
-    static AccountId(value: AccountId): ProxyReturnTypes {
-        return {
-            accountId: value,
-        }
-    }
-    static Void(): ProxyReturnTypes {
-        return {
-            void: null,
-        }
-    }
+	static U8x32(value: Array<(number | string | BN)>): ProxyReturnTypes {
+		return {
+			u8X32: value,
+		};
+	}
+	static U8x20(value: Array<(number | string | BN)>): ProxyReturnTypes {
+		return {
+			u8X20: value,
+		};
+	}
+	static AccountId(value: AccountId): ProxyReturnTypes {
+		return {
+			accountId: value,
+		};
+	}
+	static Void(): ProxyReturnTypes {
+		return {
+			void: null,
+		};
+	}
 }
 
 export type AccountId = string | number[]
+
