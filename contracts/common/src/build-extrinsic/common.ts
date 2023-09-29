574bb83c176c588b30c9004e6bab2634712c4aa0
George Oastler <goastler4@gmail.com>
2023-09-29 08:24:34 +0100

    lint

--- contracts/common/src/build-extrinsic/common.ts
+++ contracts/common/src/build-extrinsic/common.ts
@@ -1,65 +1,55 @@
 /* This file is auto-generated */
 
-import type { ContractPromise } from '@polkadot/api-contract';
-import type { GasLimit, GasLimitAndRequiredValue } from '@727-ventures/typechain-types';
-import { buildSubmittableExtrinsic } from '@727-ventures/typechain-types';
-import type * as ArgumentTypes from '../types-arguments/common.js';
-import type BN from 'bn.js';
-import type { ApiPromise } from '@polkadot/api';
-
-
+import { buildSubmittableExtrinsic } from '@727-ventures/typechain-types'
+import type * as ArgumentTypes from '../types-arguments/common.js'
+import type { ApiPromise } from '@polkadot/api'
+import type { ContractPromise } from '@polkadot/api-contract'
+import type { GasLimit } from '@727-ventures/typechain-types'
 
 export default class Methods {
-	readonly __nativeContract : ContractPromise;
-	readonly __apiPromise: ApiPromise;
+    readonly __nativeContract: ContractPromise
+    readonly __apiPromise: ApiPromise
 
-	constructor(
-		nativeContract : ContractPromise,
-		apiPromise: ApiPromise,
-	) {
-		this.__nativeContract = nativeContract;
-		this.__apiPromise = apiPromise;
-	}
-	/**
-	 * getCaller
-	 *
-	*/
-	"getCaller" (
-		__options: GasLimit,
-	){
-		return buildSubmittableExtrinsic( this.__apiPromise, this.__nativeContract, "getCaller", [], __options);
-	}
+    constructor(nativeContract: ContractPromise, apiPromise: ApiPromise) {
+        this.__nativeContract = nativeContract
+        this.__apiPromise = apiPromise
+    }
+    /**
+     * getCaller
+     *
+     */
+    getCaller(__options: GasLimit) {
+        return buildSubmittableExtrinsic(this.__apiPromise, this.__nativeContract, 'getCaller', [], __options)
+    }
 
-	/**
-	 * getCallerBytes
-	 *
-	*/
-	"getCallerBytes" (
-		__options: GasLimit,
-	){
-		return buildSubmittableExtrinsic( this.__apiPromise, this.__nativeContract, "getCallerBytes", [], __options);
-	}
+    /**
+     * getCallerBytes
+     *
+     */
+    getCallerBytes(__options: GasLimit) {
+        return buildSubmittableExtrinsic(this.__apiPromise, this.__nativeContract, 'getCallerBytes', [], __options)
+    }
 
-	/**
-	 * getAccountBytes
-	 *
-	 * @param { ArgumentTypes.AccountId } account,
-	*/
-	"getAccountBytes" (
-		account: ArgumentTypes.AccountId,
-		__options: GasLimit,
-	){
-		return buildSubmittableExtrinsic( this.__apiPromise, this.__nativeContract, "getAccountBytes", [account], __options);
-	}
+    /**
+     * getAccountBytes
+     *
+     * @param { ArgumentTypes.AccountId } account,
+     */
+    getAccountBytes(account: ArgumentTypes.AccountId, __options: GasLimit) {
+        return buildSubmittableExtrinsic(
+            this.__apiPromise,
+            this.__nativeContract,
+            'getAccountBytes',
+            [account],
+            __options
+        )
+    }
 
-	/**
-	 * getGitCommitId
-	 *
-	*/
-	"getGitCommitId" (
-		__options: GasLimit,
-	){
-		return buildSubmittableExtrinsic( this.__apiPromise, this.__nativeContract, "getGitCommitId", [], __options);
-	}
-
+    /**
+     * getGitCommitId
+     *
+     */
+    getGitCommitId(__options: GasLimit) {
+        return buildSubmittableExtrinsic(this.__apiPromise, this.__nativeContract, 'getGitCommitId', [], __options)
+    }
 }
--- contracts/common/src/types-returns/common.ts
+++ contracts/common/src/types-returns/common.ts
@@ -1,9 +1,5 @@
-import type BN from 'bn.js';
-import type {ReturnNumber} from '@727-ventures/typechain-types';
-
 export enum LangError {
-	couldNotReadInput = 'CouldNotReadInput'
+    couldNotReadInput = 'CouldNotReadInput',
 }
 
 export type AccountId = string | number[]
-
