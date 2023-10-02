e5660c630f18e4acbe0c4e19ebd8c485f34172f9
George Oastler <goastler@users.noreply.github.com>
2023-10-02 17:13:00 +0100

    Set version script (#669)
    * add command to set version
    
    * fix await and file discovery
    
    * fix devDependency version setting
    
    * vscode ts-node launch
    
    * fix newline missing
    
    * handle versioning in tomls
    
    * switch to working toml lib
    
    * workspace toml support
    
    * toggle on js versioning
    
    * Update package-lock.json
    
    * add newlines
    
    * only replace version if already set in package jsons
    
    * set version 0.2.5 using script
    
    * remove json comment
    
    * lint
    
    * remove npm version script
    
    * make npm script set version then npm i
    
    * typo in version script command
    
    * Update package-lock.json
    
    ---------
    
    Co-authored-by: Chris <forgetso86@gmail.com>

--- dev/scripts/src/cli/index.ts
+++ dev/scripts/src/cli/index.ts
@@ -22,6 +22,7 @@ import { loadEnv } from '@prosopo/cli'
 import { setup } from '../setup/index.js'
 import { updateEnvFiles } from '../util/index.js'
 import path from 'path'
+import setVersion from '../scripts/setVersion.js'
 import yargs from 'yargs'
 
 const paths = getPaths()
@@ -153,6 +154,14 @@ export async function processArgs(args: string[]) {
                         `node dist/cli/index.js import_contract --in=${inDir} --out=${paths.contractPackagesDir}/${contract}/src`
                     )
                 }
+            },
+        })
+        .command({
+            command: 'version',
+            describe: 'Set the version of packages',
+            builder: (yargs) => yargs.option('v', { type: 'string', demand: true }),
+            handler: async (argv) => {
+                await setVersion(String(argv.v))
             },
         }).argv
 }
