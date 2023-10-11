35e68892b07c67c3aa1d1c981ab02fc903627d5f
George Oastler <goastler4@gmail.com>
2023-10-11 18:37:59 +0100

    flatten testing

--- packages/datasets-fs/src/tests/flatten.test.ts
+++ packages/datasets-fs/src/tests/flatten.test.ts
@@ -1,13 +1,93 @@
 import { Flatten } from '../commands/flatten.js'
-import { fsEq } from '../utils/utils.js'
+import { fsEq, fsWalk, readDataJson } from './utils.js'
+import fs from 'fs'
+import { blake2b } from '@noble/hashes/blake2b'
+import { u8aToHex } from '@polkadot/util'
 
 describe('flatten', () => {
     test('flattens hierarchical data', async () => {
         const input = `${__dirname}/data/hierarchical`
         const output = `${__dirname}/results/flat`
         await new Flatten().parseThenExec([`--input=${input}`, `--output=${output}`])
+
         // make sure the results are the same as the expected results
         const expected = `${__dirname}/data/flat`
         fsEq(output, expected)
+
+        const data = readDataJson(`${output}/data.json`)
+        // check each image from the hierarchical data is in the flat data
+        let hierCount = 0
+        for (const pth of fsWalk(input)) {
+            // if pth is not img, ignore
+            if (!pth.endsWith('.jpg') && !pth.endsWith('.png')) {
+                continue
+            }
+            hierCount++
+            // get the category
+            const category = pth.split('/').slice(-2)[0]
+            const content = fs.readFileSync(pth)
+            // check the image is in the flat data
+            let name = ''
+            for (const pth2 of fsWalk(output)) {
+                // if pth2 is not file, ignore
+                if (!fs.statSync(pth2).isFile()) {
+                    continue
+                }
+                const content2 = fs.readFileSync(pth2)
+                if (content === content2) {
+                    const str = pth2.split('/').slice(-1)[0]
+                    if (str === undefined) {
+                        throw new Error(`unable to parse ${pth2}`)
+                    }
+                    name = str
+                    break
+                }
+            }
+            if (!name) {
+                throw new Error(`unable to find image ${pth} in output`)
+            }
+            // check the image is in the data json
+            const item = data.items.find((item) => {
+                const name2 = item.data.split('/').slice(-1)[0]
+                if (name2 !== name) {
+                    return false
+                }
+                return true
+            })
+            if (item === undefined) {
+                throw new Error(`unable to find image ${pth} in data.json`)
+            }
+            // correct name, so check category
+            if (item.label != category) {
+                throw new Error(`expected ${category} but found ${item.label}`)
+            }
+            // type should be image
+            if (item.type !== 'image') {
+                throw new Error(`expected image type but found ${item.type}`)
+            }
+            // file name should be the hash
+            if (item.hash !== name.split('.')[0]) {
+                throw new Error(`expected ${name.split('.')[0]} hash but found ${item.hash}`)
+            }
+            // hash should be the hash of the image content
+            const hash = u8aToHex(blake2b(content))
+            if (item.hash !== hash) {
+                throw new Error(`expected ${hash} hash but found ${item.hash}`)
+            }
+        }
+
+        // count the number of images in the flat data
+        let flatCount = 0
+        for (const pth of fsWalk(output)) {
+            if (!pth.endsWith('.jpg') && !pth.endsWith('.png')) {
+                continue
+            }
+            flatCount++
+        }
+
+        // check same number of images in flat data
+        if (hierCount !== flatCount) {
+            throw new Error(`expected ${hierCount} images but found ${flatCount}`)
+        }
     })
 })
--- packages/datasets-fs/src/tests/utils.ts
+++ packages/datasets-fs/src/tests/utils.ts
@@ -1,13 +1,11 @@
+import { DataSchema } from '@prosopo/types'
 import { at } from '@prosopo/util'
 import fs from 'fs'
 
 // recursively list files in a directory
 export function* fsWalk(pth: string): Generator<string> {
-    // if path is a file, return it
+    yield pth
     const stat = fs.statSync(pth)
-    if (stat.isFile()) {
-        yield pth
-    }
     // if path is a directory, recurse
     if (stat.isDirectory()) {
         const files = fs.readdirSync(pth)
@@ -22,18 +20,29 @@ export function* fsWalk(pth: string): Generator<string> {
 export const fsEq = (pth1: string, pth2: string) => {
     const it1 = fsWalk(pth1)
     const it2 = fsWalk(pth2)
-    const files1 = [...it1]
-    const files2 = [...it2]
+    // drop the root path
+    const files1 = [...it1].map((f) => f.slice(pth1.length))
+    const files2 = [...it2].map((f) => f.slice(pth2.length))
     if (files1.length !== files2.length) {
         return false
     }
     // sort files so in same order
     files1.sort()
     files2.sort()
+    if (!files1.every((f, i) => f === files2[i])) {
+        // the two lists of files are not the same
+        return false
+    }
     // compare contents of each file
     for (let i = 0; i < files1.length; i++) {
         const file1 = at(files1, i)
         const file2 = at(files2, i)
+        const stat1 = fs.statSync(file1)
+        if (stat1.isDirectory()) {
+            // already checked above
+            continue
+        }
+        // files, so compare contents
         const content1 = fs.readFileSync(file1)
         const content2 = fs.readFileSync(file2)
         if (!content1.equals(content2)) {
@@ -41,4 +50,13 @@ export const fsEq = (pth1: string, pth2: string) => {
         }
     }
     return true
+}
+
+export const readDataJson = (pth: string) => {
+    let content = fs.readFileSync(pth).toString()
+    // TODO use getPaths() here to find the repo dir
+    content = content.replace('${repo}', __dirname + '/../../../..')
+    const dataJson = JSON.parse(content.toString())
+    const data = DataSchema.parse(dataJson)
+    return data
 }
