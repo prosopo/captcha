1eeadb3f30a60a5e9aad456e4e6a67b8f299fb76
George Oastler <goastler4@gmail.com>
2023-10-13 10:56:08 +0100

    add progress bar to get

--- packages/datasets-fs/src/commands/get.ts
+++ packages/datasets-fs/src/commands/get.ts
@@ -3,6 +3,7 @@ import { get } from '@prosopo/util'
 import { lodash } from '@prosopo/util'
 import { z } from 'zod'
 import fs from 'fs'
+import cliProgress from 'cli-progress'
 
 export const ArgsSchema = InputArgsSchema.extend({})
 export type ArgsSchemaType = typeof ArgsSchema
@@ -22,7 +23,9 @@ export class Get extends InputCliCommand<ArgsSchemaType> {
 
     public override async _run(args: Args) {
         await super._run(args)
-
+        const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+        
+        const list: string[] = []
         const traverse = async (data: any) => {
             if (data instanceof Array) {
                 for (let i = 0; i < data.length; i++) {
@@ -33,26 +36,7 @@ export class Get extends InputCliCommand<ArgsSchemaType> {
                     if (key == 'data') {
                         const value = get(data, key)
                         const url = z.string().parse(value)
-                        if (url.startsWith('http')) {
-                            try {
-                                const response = await fetch(url)
-                                if (!response.ok) {
-                                    this.logger.error(`GET ${url} ${response.status} ${response.statusText}`)
-                                } else {
-                                    this.logger.log(`GET ${url} OK`)
-                                }
-                            } catch (err) {
-                                this.logger.error(err)
-                            }
-                        } else {
-                            // resolve locally
-                            try {
-                                fs.readFileSync(url)
-                                this.logger.log(`GET ${url} OK`)
-                            } catch (err) {
-                                this.logger.error(`GET ${url} ${err}`)
-                            }
-                        }
+                        list.push(url)
                     } else {
                         await traverse(get(data, key))
                     }
@@ -66,6 +50,32 @@ export class Get extends InputCliCommand<ArgsSchemaType> {
         // read the map file
         const data: any = JSON.parse(fs.readFileSync(file, 'utf8'))
         await traverse(data)
+
+        bar.start(list.length, 0)
+        for (const url of list) {
+            bar.increment()
+            if (url.startsWith('http')) {
+                try {
+                    const response = await fetch(url)
+                    if (!response.ok) {
+                        this.logger.error(`GET ${url} ${response.status} ${response.statusText}`)
+                    } else {
+                        this.logger.log(`GET ${url} OK`)
+                    }
+                } catch (err) {
+                    this.logger.error(err)
+                }
+            } else {
+                // resolve locally
+                try {
+                    fs.readFileSync(url)
+                    this.logger.log(`GET ${url} OK`)
+                } catch (err) {
+                    this.logger.error(`GET ${url} ${err}`)
+                }
+            }
+        }
+        bar.stop()
     }
 
     public override getDescription(): string {
