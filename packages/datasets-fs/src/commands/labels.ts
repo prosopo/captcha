0a171d4345b25ab115469819bc81df1a9c796fc1
George Oastler <goastler@users.noreply.github.com>
2023-10-17 10:06:39 +0100

    Dataset fs cli restructure (#683)
    * fix npm run cli cmd
    
    * make img live in tmp folder while handling
    
    * coded setup routine for in/out cli
    
    * object merging in util
    
    * improve merge util fn
    
    * add Loggable class for logging inheritance
    
    * add kebab case util method
    
    * add a CliBuilder class to help with cli construction
    
    * make shared IO based cli setup
    
    * move flatten to new command format
    
    * refactor IO cli template
    
    * split input/output template into two separate classes
    
    * add missing await
    
    * change cliBuilder to cliCommand
    
    * get command
    
    * fix get command
    
    * convert scale command to resize command in new format
    
    * move relocate command to new format
    
    * move labels command to new format
    
    * move generate commands over to new format
    
    * add description to cli commands
    
    * refactor cli construction
    
    * rename CliBuilder to CliCommand
    
    * make cli use loggable
    
    * fix missing checkDuplicates code and fix imports
    
    * add test data
    
    * remove alpha and add 8-bit colour space per channel to resize command
    
    * fix import
    
    * fix cli awaits
    
    * switch debug console logs to use logger
    
    * move data
    
    * flatten test
    
    * flatten test data
    
    * flatten testing
    
    * fix dataset flatten test
    
    * fix remove output only if exists and overwrite
    
    * gitignore test results
    
    * resize test
    
    * missing .gitignore newline
    
    * fix test_results gitignore
    
    * fix data locations
    
    * make cli demand a command
    
    * fixed default command when no command given
    
    * undo debug
    
    * relocate test
    
    * substitute/restore data path for tests using before/after
    
    * fix for resize writing output data
    
    * rerun fixed resize cmd
    
    * add repo var to data json for tests
    
    * Update relocated_data.json
    
    * pretty print
    
    * sleep fn
    
    * fix fsWalk when files are deleted during
    
    * combine tests for single before/after usage
    
    * missing .jpeg extension fix
    
    * fix find and replace before tests run
    
    * move cli classes and add composite command class to cli
    
    * generate tests
    
    * move core generate routines to parent generate class
    
    * add more data to test data to fulfil minimum image count requirements per label
    
    * Update generateV1.ts
    
    * regenerate test data flat
    
    * regenerate test data flat resized
    
    * regenerate relocated test data
    
    * generated captchas_v1.json
    
    * fix resize aspect ratio
    
    * regenerate test data resized
    
    * regenerated relocate data
    
    * generated more test data
    
    * add tests for captchas_v2.json
    
    * add test for captcha unlabelled != solutions
    
    * fix create parent directory in commands before writing single files
    
    * fix parsing non-DataSchema files with string substitution
    
    * remove captcha schema testing via refinement, unable to use with other stuff, do unit test instead
    
    * lint
    
    * vscode settings
    
    * added many more captcha to test data
    
    * demand seed
    
    * updated readme with test data generation commands
    
    * fix tests
    
    * add extra tests for distribution of correct, incorrect, labelled and unlabelled in generate-v2 output
    
    * lint
    
    * add progress bar to flatten cmd
    
    * fix progress bar totals and duplicate detection
    
    * use a single progress bar in generateV1
    
    * progress bar for generateV2
    
    * add progress bar to get
    
    * add progress bar to resize command
    
    * use jpgs as output from resize
    
    * use imgdir for tmp files
    
    * lint
    
    * remove logs from generateV1
    
    * fix tests mkdir
    
    * only rmdir if exists
    
    * remove class level arrow funcs
    
    * use foreach instead of loop
    
    * refactor duplicate check
    
    * split long function
    
    * split generateV1 method into smaller methods
    
    * split generateV2 into smaller methods
    
    * make let into const
    
    * remove double toString
    
    * lint
    
    * fix lodash
    
    * fix duplication checks
    
    * fix dupe tests

---
+++ packages/datasets-fs/src/commands/labels.ts
@@ -0,0 +1,54 @@
+import { InputOutputArgsSchema as InputOutputArgsSchema, InputOutputCliCommand } from '../utils/inputOutput.js'
+import { LabelledDataSchema, LabelledItem } from '@prosopo/types'
+import { ProsopoEnvError } from '@prosopo/common'
+import { lodash } from '@prosopo/util'
+import { z } from 'zod'
+import fs from 'fs'
+
+export const ArgsSchema = InputOutputArgsSchema.extend({})
+export type ArgsSchemaType = typeof ArgsSchema
+export type Args = z.infer<ArgsSchemaType>
+
+export class Labels extends InputOutputCliCommand<ArgsSchemaType> {
+    public override getArgSchema() {
+        return ArgsSchema
+    }
+
+    public override getOptions() {
+        return lodash().merge(super.getOptions(), {
+            input: {
+                description: 'JSON file containing labelled data',
+            },
+            output: {
+                description: 'Where to put the JSON file containing labels',
+            },
+        })
+    }
+
+    public override async _run(args: Args) {
+        await super._run(args)
+
+        const file = args.input
+        if (!fs.existsSync(file)) {
+            throw new ProsopoEnvError(new Error(`file does not exist: ${file}`), 'FS.FILE_NOT_FOUND')
+        }
+
+        const labelled: LabelledItem[] = file
+            ? LabelledDataSchema.parse(JSON.parse(fs.readFileSync(file, 'utf8'))).items
+            : []
+
+        const labels = new Set<string>()
+        for (const item of labelled) {
+            labels.add(item.label)
+        }
+        const labelArray = Array.from(labels)
+        labelArray.sort()
+
+        fs.mkdirSync(args.output.split('/').slice(0, -1).join('/'), { recursive: true })
+        fs.writeFileSync(args.output, JSON.stringify({ labels: labelArray }, null, 4))
+    }
+
+    public override getDescription(): string {
+        return 'get all labels from some data'
+    }
+}
--- packages/datasets-fs/src/labels/args.ts
+++
@@ -1,9 +0,0 @@
-// Args for generating a dataset
-
-import { z } from 'zod'
-
-export const argsSchema = z.object({
-    data: z.string(),
-})
-
-export type Args = z.infer<typeof argsSchema>
