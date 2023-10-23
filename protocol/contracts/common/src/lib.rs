a62143c6dd64eb5ac41c488d1eb2506991b39bbc
HughParry <90424587+HughParry@users.noreply.github.com>
2023-10-23 13:42:13 +0100

    version bump to 0.2.10 (#726)
    * version bump to 0.2.10
    
    * Typechain changes
    
    * typechain
    
    ---------
    
    Co-authored-by: hugh <hugh@prosopo.io>

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -71,8 +71,8 @@ pub mod common {
         /// Get the git commit id from when this contract was built
         pub fn get_git_commit_id() -> [u8; 20] {
             let env_git_commit_id: [u8; 20] = [
-                8, 163, 48, 88, 166, 54, 183, 233, 125, 90, 48, 8, 251, 105, 209, 153, 247, 115,
-                156, 173,
+                253, 125, 163, 58, 237, 228, 143, 190, 10, 232, 167, 31, 170, 102, 214, 85, 141,
+                240, 213, 77,
             ];
             env_git_commit_id
         }
