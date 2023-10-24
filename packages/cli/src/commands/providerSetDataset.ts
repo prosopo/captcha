ddc8b4aa10918e833c29d3f52b2bddf21ed863bb
hugh <hugh@prosopo.io>
2023-10-24 13:26:50 +0100

    fixing build step

--- packages/cli/src/commands/providerSetDataset.ts
+++ packages/cli/src/commands/providerSetDataset.ts
@@ -1,11 +1,11 @@
-import { ArgumentsCamelCase, Argv } from 'yargs'
 import { KeyringPair } from '@polkadot/keyring/types'
 import { LogLevel, Logger, getLogger } from '@prosopo/common'
-import { ProsopoConfigOutput } from '@prosopo/types'
 import { ProviderEnvironment } from '@prosopo/env'
 import { Tasks } from '@prosopo/provider'
-import { loadJSONFile } from '../files.js'
+import { ProsopoConfigOutput } from '@prosopo/types'
+import { ArgumentsCamelCase, Argv } from 'yargs'
 import { z } from 'zod'
+import { loadJSONFile } from '../files.js'
 
 export default (pair: KeyringPair, config: ProsopoConfigOutput, cmdArgs?: { logger?: Logger }) => {
     const logger = cmdArgs?.logger || getLogger(LogLevel.enum.info, 'cli.provider_set_data_set')
@@ -25,7 +25,7 @@ export default (pair: KeyringPair, config: ProsopoConfigOutput, cmdArgs?: { logg
                 await env.isReady()
                 const tasks = new Tasks(env)
                 const file = z.string().parse(argv.file)
-                const jsonFile = loadJSONFile(file, logger) as JSON
+                const jsonFile = loadJSONFile(file) as JSON
                 logger.info(`Loaded JSON from ${file}`)
                 const result = await tasks.providerSetDatasetFromFile(jsonFile)
                 logger.info(JSON.stringify(result, null, 2))
