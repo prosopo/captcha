800d3b582a500b6ddaaf1a0fba0669c4242d15cb
HughParry <90424587+HughParry@users.noreply.github.com>
2023-10-30 16:17:40 +0000

    adding the typechain build commands to package.json (#750)
    * adding the typechain build commands to package.json
    
    * package-lock bump
    
    * reinstall
    
    * downgrading polkadot types
    
    * please typechain please
    
    * Typechain
    
    * typechain again
    
    * version bumps
    
    * bumping all versions to 10.10.1 for polkadot
    
    * git commit id bump
    
    * fixing script
    
    * package json formatting
    
    ---------
    
    Co-authored-by: hugh <hugh@prosopo.io>

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -71,8 +71,8 @@ pub mod common {
         /// Get the git commit id from when this contract was built
         pub fn get_git_commit_id() -> [u8; 20] {
             let env_git_commit_id: [u8; 20] = [
-                105, 227, 59, 142, 225, 109, 83, 241, 65, 161, 24, 54, 152, 104, 121, 39, 209, 161,
-                211, 25,
+                248, 206, 221, 2, 252, 88, 142, 207, 117, 95, 175, 170, 7, 156, 211, 234, 135, 207,
+                8, 249,
             ];
             env_git_commit_id
         }
