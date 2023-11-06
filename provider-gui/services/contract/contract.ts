411ff0c8d3b4a02a66f00551e7259bd9e8c6aca2
HughParry <90424587+HughParry@users.noreply.github.com>
2023-11-06 12:15:45 +0000

    Provider gui as status checker for providers on contract (#769)
    * initial fixes and routing changes
    
    * Fixing cjs and methods called
    
    * Fixes and use of useeffect in fetching data
    
    * linting and import fixes
    
    * Tidying up topbar
    
    * Cleaning up unused code
    
    * Put provider details in a data grid
    
    * Form control and remove table
    
    * renaming dapp enable
    
    * Removing non required interface
    
    ---------
    
    Co-authored-by: Chris Taylor <forgetso86@gmail.com>

--- provider-gui/services/contract/contract.ts
+++ provider-gui/services/contract/contract.ts
@@ -3,7 +3,6 @@ import { Tasks } from '@prosopo/provider'
 import { defaultConfig } from '@prosopo/cli'
 
 export const getContractApi = async (account: string) => {
-    // Todo: spread the default env, add in the stuff we require for gui
     const env = new ProviderEnvironment(defaultConfig())
     const tasks = new Tasks(env)
     return (await tasks.contract.query.getProvider(account)).value.unwrap().unwrap()
