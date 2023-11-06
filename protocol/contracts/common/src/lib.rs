b16d7f4b8aa595ae4c6424ff4c6d4fd22e84b5c0
HughParry <90424587+HughParry@users.noreply.github.com>
2023-11-06 15:46:36 +0000

    Migrating to forked prosopo typechain (#772)
    * Testing version bump from 727 to prosopo for typechain
    
    * Pinning correct typechain versions
    
    * package lock bump
    
    * Linting
    
    * Bump to 1.1.10 and typechain build

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -71,8 +71,8 @@ pub mod common {
         /// Get the git commit id from when this contract was built
         pub fn get_git_commit_id() -> [u8; 20] {
             let env_git_commit_id: [u8; 20] = [
-                186, 2, 108, 13, 55, 208, 18, 7, 33, 229, 175, 33, 76, 95, 31, 199, 135, 158, 169,
-                80,
+                191, 130, 198, 43, 173, 199, 125, 66, 225, 235, 157, 89, 251, 180, 43, 150, 216,
+                115, 233, 214,
             ];
             env_git_commit_id
         }
