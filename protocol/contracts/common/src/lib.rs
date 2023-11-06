79e1b505927681f06618e883589a41b80216a06c
Chris Taylor <forgetso86@gmail.com>
2023-11-06 22:19:04 +0000

    Reinstate backend vite config

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -71,8 +71,8 @@ pub mod common {
         /// Get the git commit id from when this contract was built
         pub fn get_git_commit_id() -> [u8; 20] {
             let env_git_commit_id: [u8; 20] = [
-                96, 197, 129, 249, 133, 208, 100, 74, 215, 33, 121, 101, 101, 83, 245, 80, 54, 14,
-                26, 83,
+                133, 89, 26, 94, 45, 209, 62, 196, 109, 233, 38, 224, 71, 86, 62, 117, 49, 85, 8,
+                185,
             ];
             env_git_commit_id
         }
