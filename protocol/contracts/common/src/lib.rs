75d43c93f652deca1935dd586904d7deca21fe9c
Chris Taylor <forgetso86@gmail.com>
2023-11-16 16:46:55 +0000

    Import contracts

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -71,8 +71,8 @@ pub mod common {
         /// Get the git commit id from when this contract was built
         pub fn get_git_commit_id() -> [u8; 20] {
             let env_git_commit_id: [u8; 20] = [
-                42, 71, 213, 187, 227, 33, 84, 141, 17, 248, 35, 14, 93, 180, 169, 14, 45, 213,
-                247, 246,
+                255, 152, 122, 134, 207, 7, 235, 188, 199, 148, 157, 171, 45, 50, 205, 181, 148,
+                163, 130, 107,
             ];
             env_git_commit_id
         }
