009d30a712fc83f69c10f12ebd4f1d07cc50a11f
George Oastler <goastler4@gmail.com>
2023-12-08 17:14:31 +0000

    start eslint config from scratch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,60 +1,15 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2022: true,
-        node: true,
-    },
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        // 'plugin:yaml/recommended',
-        // 'plugin:json/recommended',
-        // 'plugin:toml/standard',
-        // 'eslint:recommended',
-        // 'plugin:regexp/recommended',
-        // 'prettier', // must be last!
+        'plugin:@typescript-eslint/recommended-type-checked',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        sourceType: 'module',
+        project: true,
+        tsconfigRootDir: __dirname,
     },
     plugins: [
-        'workspaces',
-        // 'unused-imports',
-        '@typescript-eslint/recommended',
-        // 'sort-imports-es6-autofix',
-        'prettier'
+        '@typescript-eslint'
     ],
     root: true,
-    rules: {
-        // 'no-unused-vars': 'off',
-        // 'unused-imports/no-unused-imports': 'error',
-        // 'unused-imports/no-unused-vars': [
-        //     'warn',
-        //     {
-        //         vars: 'all',
-        //         varsIgnorePattern: '^_',
-        //         args: 'after-used',
-        //         argsIgnorePattern: '^_',
-        //     },
-        // ],
-        // //"indent": ["error", 4],
-        // //"indent": "off",
-        // 'sort-imports': [
-        //     'error',
-        //     {
-        //         ignoreDeclarationSort: true,
-        //         allowSeparatedGroups: false,
-        //     },
-        // ],
-        // 'sort-imports-es6-autofix/sort-imports-es6': [
-        //     2,
-        //     {
-        //         ignoreCase: false,
-        //         ignoreMemberSort: false,
-        //         memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        //     },
-        // ],
-        // 'json/*': ['error', { allowComments: true }],
-    },
-}
+};
