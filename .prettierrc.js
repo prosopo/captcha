ef9c12ccfcfd5bdc868fb43e74c9948c66a7058b
George Oastler <goastler4@gmail.com>
2023-12-11 14:55:05 +0000

    move file extensions for linting into prettier and eslint config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,37 @@
 export default {
+    overrides: [
+        {
+            files: [
+                '*.html',
+                '*.ts',
+                '*.tsx',
+                '*.js',
+                '*.jsx',
+                '*.mjs',
+                '*.cjs',
+                '*.json',
+                '*.yaml',
+                '*.yml',
+                '*.toml',
+                '*.d.ts',
+                '*.cts',
+                '*.mts',
+                '.*.json',
+                '.*.js',
+                '.*.mjs',
+                '.*.cjs',
+                '.*.yaml',
+                '.*.yml',
+                '.*.toml',
+            ],
+            rules: {
+
     trailingComma: 'es5',
     tabWidth: 4,
     semi: false,
     singleQuote: true,
     printWidth: 120,
+            }
+        },
+    ],
 }
