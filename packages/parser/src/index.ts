70cab1d91fc53b9a5abfea33a4badc4283eb7590
George Oastler <goastler4@gmail.com>
2023-12-12 18:26:09 +0000

    palceholder pick and omit

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -369,6 +369,40 @@ class ObjectParser<T extends {}> extends BaseParser<T> {
     }
 }
 
+class PickParser<T extends {}, U extends keyof T> extends BaseParser<Pick<T, U>> {
+    constructor(private schema: Parseable<T>) {
+        super()
+    }
+
+    override parseShape(value: unknown, options?: ParseOptions | undefined): Pick<T, U> {
+        // TODO allow optional keys to do picked parsing
+        throw new Error("Method not implemented.")
+    }
+
+    override validate(value: Pick<T, U>): void {
+        super.validate(value)
+        // TODO allow optional keys to do picked validation
+        throw new Error("Method not implemented.")
+    }
+}
+
+class OmitParser<T extends {}, U extends keyof T> extends BaseParser<Omit<T, U>> {
+    constructor(private schema: Parseable<T>) {
+        super()
+    }
+
+    override parseShape(value: unknown, options?: ParseOptions | undefined): Omit<T, U> {
+        // TODO allow optional keys to do omitted parsing
+        throw new Error("Method not implemented.")
+    }
+
+    override validate(value: Omit<T, U>): void {
+        super.validate(value)
+        // TODO allow optional keys to do omitted validation
+        throw new Error("Method not implemented.")
+    }
+}
+
 class ArrayParser<T> extends BaseParser<T[]> {
     constructor(private schema: Parser<T>) {
         super()
