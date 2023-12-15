eac0cb4ace8c12ed51de799c3504bba29d15463d
George Oastler <goastler4@gmail.com>
2023-12-15 09:31:42 +0000

    populate error outside of common

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -13,6 +13,8 @@
 // limitations under the License.
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+use core::marker::PhantomData;
+
 pub use self::common::{Common, CommonRef};
 
 #[cfg(feature = "test-dependency")]
@@ -73,15 +75,71 @@ macro_rules! lazy {
     };
 }
 
-/// The errors that can be returned by the Proxy contract.
+/// The errors that can be returned by contracts. This is generic so it can be adjusted to any environment which the contract is being run on.
 #[derive(PartialEq, Debug, Eq, Clone, Copy, scale::Encode, scale::Decode)]
 #[cfg_attr(
     any(feature = "std", feature = "ink-as-dependency"),
     derive(scale_info::TypeInfo)
 )]
-// #[cfg_attr(any(feature = "std", feature = "ink-as-dependency"), derive(ink::storage::traits::StorageLayout))]
-pub enum CustomError<Env: ink::env::Environment> {
-    NotAuthorised(Env::AccountId),
+pub enum ContractError<Env: ink::env::Environment> {
+    _PhantomVariant(PhantomData<Env>), // this is a placeholder to allow the enum to be generic, do not use! This can be removed when at least 1 of the error variants use the env type
+    NotAuthorised,
+    TransferFailed,
+    SetCodeHashFailed,
+    InvalidDestination,
+    UnknownMessage,
+    /// Returned if provider account exists when it shouldn't
+    ProviderAccountExists,
+    /// Returned if provider exists when it shouldn't
+    ProviderExists,
+    /// Returned if provider account does not exists when it shouldn't
+    ProviderAccountDoesNotExist,
+    /// Returned if provider does not exist when it should
+    ProviderDoesNotExist,
+    /// Returned if provider has insufficient funds to operate
+    ProviderInsufficientFunds,
+    /// Returned if provider is inactive and trying to use the service
+    ProviderInactive,
+    /// Returned if url is already used by another provider
+    ProviderUrlUsed,
+    /// Returned if dapp exists when it shouldn't
+    DappExists,
+    /// Returned if dapp does not exist when it should
+    DappDoesNotExist,
+    /// Returned if dapp is inactive and trying to use the service
+    DappInactive,
+    /// Returned if dapp has insufficient funds to operate
+    DappInsufficientFunds,
+    /// Returned if captcha data does not exist
+    CaptchaDataDoesNotExist,
+    /// Returned if solution commitment does not exist when it should
+    CommitDoesNotExist,
+    /// Returned if dapp user does not exist when it should
+    DappUserDoesNotExist,
+    /// Returned if there are no active providers
+    NoActiveProviders,
+    /// Returned if the dataset ID and dataset ID with solutions are identical
+    DatasetIdSolutionsSame,
+    /// CodeNotFound ink env error
+    CodeNotFound,
+    /// An unknown ink env error has occurred
+    Unknown,
+    /// Invalid contract
+    InvalidContract,
+    /// Invalid payee. Returned when the payee value does not exist in the enum
+    InvalidPayee,
+    /// Returned if not all captcha statuses have been handled
+    InvalidCaptchaStatus,
+    /// No correct captchas in history (either history is empty or all captchas are incorrect)
+    NoCorrectCaptcha,
+    /// Returned if not enough providers are active
+    NotEnoughActiveProviders,
+    /// Returned if provider fee is too high
+    ProviderFeeTooHigh,
+    /// Returned if the commitment already exists
+    CommitAlreadyExists,
+    /// Returned if the caller is not the author
+    NotAuthor,
 }
 
 pub trait Common2 {
@@ -89,10 +147,10 @@ pub trait Common2 {
 
     fn check_is_admin(
         account: <Self::Env as ink::env::Environment>::AccountId,
-    ) -> Result<(), CustomError<Self::Env>> {
+    ) -> Result<(), ContractError<Self::Env>> {
         // want to be able to throw the error here, so need to have the enum variant in scope
         // Ok(())
-        Err(CustomError::NotAuthorised(account))
+        Err(ContractError::NotAuthorised)
     }
 }
 
@@ -103,8 +161,8 @@ impl Common2 for Common2Default {
 
     fn check_is_admin(
         account: <Self::Env as ink::env::Environment>::AccountId,
-    ) -> Result<(), CustomError<Self::Env>> {
-        Err(CustomError::NotAuthorised(account))
+    ) -> Result<(), ContractError<Self::Env>> {
+        Err(ContractError::NotAuthorised)
     }
 }
 
