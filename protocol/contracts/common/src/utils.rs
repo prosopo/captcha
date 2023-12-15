90a94ad7ae613d7cd0d505390b62d4fc6e89ea44
George Oastler <goastler4@gmail.com>
2023-12-15 12:19:56 +0000

    move macros

--- protocol/contracts/common/src/utils.rs
+++ protocol/contracts/common/src/utils.rs
@@ -2,3 +2,49 @@
 pub fn account_id_bytes(account: &ink::primitives::AccountId) -> &[u8; 32] {
     AsRef::<[u8; 32]>::as_ref(account)
 }
+
+/// Print and return an error in ink
+#[macro_export]
+macro_rules! err {
+    ($self_:ident, $err:expr) => {{
+        ink::env::debug_println!(
+            "ERROR:
+    type: {:?}
+    block: {:?}
+    caller: {:?}
+",
+            $err,
+            $self_.env().block_number(),
+            $self_.env().caller(),
+        );
+        Err($err)
+    }};
+}
+
+#[macro_export]
+macro_rules! err_fn {
+    ($self_:ident, $err:expr) => {
+        || {
+            ink::env::debug_println!(
+                "ERROR:
+        type: {:?}
+        block: {:?}
+        caller: {:?}
+    ",
+                $err,
+                $self_.env().block_number(),
+                $self_.env().caller(),
+            );
+            $err
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! lazy {
+    ($lazy:expr, $func:ident, $value:expr) => {
+        let mut contents = $lazy.get_or_default();
+        contents.$func($value);
+        $lazy.set(&contents);
+    };
+}
