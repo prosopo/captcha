373625a6950d0a08f58c50d79262d91efabda92b
Chris Taylor <forgetso86@gmail.com>
2023-12-18 13:47:08 +0000

    Trigger only on form interaction

--- packages/procaptcha/src/modules/collector.ts
+++ packages/procaptcha/src/modules/collector.ts
@@ -2,14 +2,21 @@ export type CoordEvent = [number, number, number]
 interface MutableRefObject<T> {
     current: T
 }
+
+const COLLECTOR_LIMIT = 1000
+
 export const storeLog = (event: CoordEvent[], events: CoordEvent[]) => {
     //store in react state
     events = events ? events.concat(event) : event
+    if (events.length > COLLECTOR_LIMIT) {
+        events = events.slice(events.length - COLLECTOR_LIMIT)
+    }
     // store in session storage for debugging only
     window.sessionStorage.setItem('procaptchaLog', JSON.stringify(events))
 }
 
 export const logEvent = (event: MouseEvent | TouchEvent, events: CoordEvent[]) => {
+    console.log('Existing events', events)
     const coords =
         event instanceof MouseEvent
             ? [[event.screenX, event.screenY, event.timeStamp] as CoordEvent]
@@ -19,19 +26,33 @@ export const logEvent = (event: MouseEvent | TouchEvent, events: CoordEvent[]) =
 
 export const startCollector = (events: MutableRefObject<CoordEvent[]>) => {
     console.log('Starting collector')
-    document.addEventListener('mousemove', (e) => {
-        logEvent(e, events.current)
-    })
-    document.addEventListener('touchstart', (e) => {
-        logEvent(e, events.current)
-    })
-    document.addEventListener('touchend', (e) => {
-        logEvent(e, events.current)
-    })
-    document.addEventListener('touchcancel', (e) => {
-        logEvent(e, events.current)
-    })
-    document.addEventListener('touchmove', (e) => {
-        logEvent(e, events.current)
-    })
+    const form = findContainingForm(document.activeElement as HTMLElement)
+    if (form) {
+        console.log('Found form', form)
+        form.addEventListener('mousemove', (e) => {
+            logEvent(e, events.current)
+        })
+        form.addEventListener('touchstart', (e) => {
+            logEvent(e, events.current)
+        })
+        form.addEventListener('touchend', (e) => {
+            logEvent(e, events.current)
+        })
+        form.addEventListener('touchcancel', (e) => {
+            logEvent(e, events.current)
+        })
+        form.addEventListener('touchmove', (e) => {
+            logEvent(e, events.current)
+        })
+    }
+}
+
+const findContainingForm = (element: HTMLElement): HTMLFormElement | null => {
+    if (element.tagName === 'FORM') {
+        return element as HTMLFormElement
+    }
+    if (element.parentElement) {
+        return findContainingForm(element.parentElement)
+    }
+    return null
 }
