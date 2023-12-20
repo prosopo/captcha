2916d4a40007facc6e2e2a3f9b12204d67299c7c
George Oastler <goastler4@gmail.com>
2023-12-20 09:25:25 +0000

    fix tests

--- packages/parser/src/tests/parsing.test.ts
+++ packages/parser/src/tests/parsing.test.ts
@@ -1,127 +1,111 @@
-import { describe, expect, test, it } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { pString } from '../parsers/StringParser.js';
 import { pNumber } from '../parsers/NumberParser.js';
+import { pBigInt } from '../parsers/BigIntParser.js';
+import { pBoolean } from '../parsers/BooleanParser.js';
 
-
-describe("string parsing", () => {
-    it("should parse a string", () => {
-        expect(pString().parse("hello")).toEqual("hello");
+describe("bigint", () => {
+    test("parses", () => {
+        expect(() => pBigInt().parse(undefined)).to.throw();
+        expect(() => pBigInt().parse(null)).to.throw();
+        expect(() => pBigInt().parse("")).to.throw();
+        expect(() => pBigInt().parse(true)).to.throw();
+        expect(() => pBigInt().parse(123)).to.throw();
+        expect(pBigInt().parse(BigInt(456))).to.equal(BigInt(456));
+        expect(() => pBigInt().parse("hello")).to.throw();
+        expect(() => pBigInt().parse("789")).to.throw();
+        expect(() => pBigInt().parse("hello123")).to.throw();
     });
 
-    it("should not parse number as string", () => {
-        expect(() => pString().parse(123)).toThrow();
+    test("coerces", () => {
+        expect(() => pBigInt().parse(undefined, { coerce: true })).to.throw();
+        expect(() => pBigInt().parse(null, { coerce: true })).to.throw();
+        expect(pBigInt().parse("", { coerce: true })).to.equal(0n);
+        expect(pBigInt().parse(true, { coerce: true })).to.equal(0n);
+        expect(pBigInt().parse(false, { coerce: true })).to.equal(0n);
+        expect(pBigInt().parse(BigInt(456), { coerce: true })).to.equal(BigInt(456));
+        expect(() => pBigInt().parse("hello", { coerce: true })).to.throw(); // not numeric
+        expect(pBigInt().parse("789", { coerce: true })).to.equal(BigInt(789));
+        expect(() => pBigInt().parse("hello123", { coerce: true })).to.throw(); // not numeric
+    })
+})
+
+describe("boolean", () => {
+    test("parses", () => {
+        expect(() => pBoolean().parse(undefined)).to.throw();
+        expect(() => pBoolean().parse(null)).to.throw();
+        expect(() => pBoolean().parse("")).to.throw();
+        expect(pBoolean().parse(true)).to.equal(true);
+        expect(() => pBoolean().parse(123)).to.throw();
+        expect(() => pBoolean().parse(BigInt(456))).to.throw();
+        expect(() => pBoolean().parse("hello")).to.throw();
+        expect(() => pBoolean().parse("789")).to.throw();
+        expect(() => pBoolean().parse("hello123")).to.throw();
     });
 
-    it("should not parse boolean as string", () => {
-        expect(() => pString().parse(true)).toThrow();
+    test("coerces", () => {
+        expect(pBoolean().parse(undefined, { coerce: true })).to.equal(NaN);
+        expect(pBoolean().parse(null, { coerce: true })).to.equal(0);
+        expect(pBoolean().parse("", { coerce: true })).to.equal(0);
+        expect(pBoolean().parse(true, { coerce: true })).to.equal(1);
+        expect(pBoolean().parse(false, { coerce: true })).to.equal(0);
+        expect(pBoolean().parse(BigInt(456), { coerce: true })).to.equal(true);
+        expect(pBoolean().parse(BigInt(0), { coerce: true })).to.equal(false);
+        expect(pBoolean().parse("hello", { coerce: true })).to.equal(true);
+        expect(pBoolean().parse("789", { coerce: true })).to.equal(true);
+        expect(pBoolean().parse("hello123", { coerce: true })).to.equal(true);
+    })
+})
+
+describe("string", () => {
+    test("parses", () => {
+        expect(() => pString().parse(undefined)).to.throw();
+        expect(() => pString().parse(null)).to.throw();
+        expect(pString().parse("")).to.equal("");
+        expect(() => pString().parse(true)).to.throw();
+        expect(() => pString().parse(123)).to.throw();
+        expect(() => pString().parse(BigInt(456))).to.throw();
+        expect(pString().parse("hello")).to.equal("hello");
+        expect(pString().parse("789")).to.equal("789");
+        expect(pString().parse("hello123")).to.equal("hello123");
     });
 
-    it("should not parse bigint as string", () => {
-        expect(() => pString().parse(BigInt(123))).toThrow();
+    test("coerces", () => {
+        expect(pString().parse(undefined, { coerce: true })).to.equal("undefined");
+        expect(pString().parse(null, { coerce: true })).to.equal("null");
+        expect(pString().parse("", { coerce: true })).to.equal("");
+        expect(pString().parse(true, { coerce: true })).to.equal("true");
+        expect(pString().parse(false, { coerce: true })).to.equal("false");
+        expect(pString().parse(BigInt(456), { coerce: true })).to.equal("456");
+        expect(pString().parse(BigInt("1234567890123456789012345678901234567890"), { coerce: true })).to.equal("1234567890123456789012345678901234567890"); // too big
+        expect(pString().parse("hello", { coerce: true })).to.equal("hello");
+        expect(pString().parse("789", { coerce: true })).to.equal("789");
+        expect(pString().parse("hello123", { coerce: true })).to.equal("hello123");
+    })
+})
+
+describe("number", () => {
+    test("parses", () => {
+        expect(() => pNumber().parse(undefined)).to.throw();
+        expect(() => pNumber().parse(null)).to.throw();
+        expect(() => pNumber().parse("")).to.throw();
+        expect(() => pNumber().parse(true)).to.throw();
+        expect(pNumber().parse(123)).to.equal(123);
+        expect(() => pNumber().parse(BigInt(456))).to.throw();
+        expect(() => pNumber().parse("hello")).to.throw();
+        expect(() => pNumber().parse("789")).to.throw();
+        expect(() => pNumber().parse("hello123")).to.throw();
     });
 
-    it("should not parse null as string", () => {
-        expect(() => pString().parse(null)).toThrow();
-    });
-
-    it("should not parse undefined as string", () => {
-        expect(() => pString().parse(undefined)).toThrow();
-    });
-
-    it("should coerce number to string", () => {
-        expect(pString().parse(123, {
-            coerce: true
-        })).toEqual("123");
-    });
-
-    it("should coerce boolean to string", () => {
-        expect(pString().parse(true, {
-            coerce: true
-        })).toEqual("true");
-    });
-
-    it("should coerce bigint to string", () => {
-        expect(pString().parse(BigInt(123), {
-            coerce: true
-        })).toEqual("123");
-    });
-
-    it("should coerce null to string", () => {
-        expect(pString().parse(null, {
-            coerce: true
-        })).toEqual("null");
-    });
-
-    it("should coerce undefined to string", () => {
-        expect(pString().parse(undefined, {
-            coerce: true
-        })).toEqual("undefined");
-    });
-});
-
-describe("number parsing", () => {
-    it("should parse a number", () => {
-        expect(pNumber().parse(123)).toEqual(123);
-    });
-
-    it("should not parse string as number", () => {
-        expect(() => pNumber().parse("123")).toThrow();
-    });
-
-    it("should not parse boolean as number", () => {
-        expect(() => pNumber().parse(true)).toThrow();
-    });
-
-    it("should not parse bigint as number", () => {
-        expect(() => pNumber().parse(BigInt(123))).toThrow();
-    });
-
-    it("should not parse null as number", () => {
-        expect(() => pNumber().parse(null)).toThrow();
-    });
-
-    it("should not parse undefined as number", () => {
-        expect(() => pNumber().parse(undefined)).toThrow();
-    });
-
-    it("should error on empty string", () => {
-        expect(() => pNumber().parse("")).toThrow();
-    });
-
-    it("should error when coercing alpha string to number", () => {
-        expect(() => pNumber().parse("abc", {
-            coerce: true
-        })).toThrow();
-    });
-
-    it("should coerce string to number", () => {
-        expect(pNumber().parse("123", {
-            coerce: true
-        })).toEqual(123);
-    });
-
-    it("should coerce boolean to number", () => {
-        expect(pNumber().parse(true, {
-            coerce: true
-        })).toEqual(1);
-    });
-
-    it("should coerce bigint to number", () => {
-        expect(pNumber().parse(BigInt(123), {
-            coerce: true
-        })).toEqual(123);
-    });
-
-    it("should coerce null to number", () => {
-        expect(pNumber().parse(null, {
-            coerce: true
-        })).toEqual(0);
-    });
-
-    it("should coerce undefined to number", () => {
-        expect(pNumber().parse(undefined, {
-            coerce: true
-        })).toEqual(NaN);
-    });
-    
+    test("coerces", () => {
+        expect(pNumber().parse(undefined, { coerce: true })).to.equal(NaN);
+        expect(pNumber().parse(null, { coerce: true })).to.equal(0);
+        expect(pNumber().parse("", { coerce: true })).to.equal(0);
+        expect(pNumber().parse(true, { coerce: true })).to.equal(1);
+        expect(pNumber().parse(false, { coerce: true })).to.equal(0);
+        expect(pNumber().parse(BigInt(456), { coerce: true })).to.equal(456);
+        expect(pNumber().parse("hello", { coerce: true })).to.equal(NaN); // not numeric
+        expect(pNumber().parse("789", { coerce: true })).to.equal(789);
+        expect(pNumber().parse("hello123", { coerce: true })).to.equal(NaN); // not numeric
+    })
 })
