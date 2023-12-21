a2094d5e7f4018cb84540ef520cc47dc5557d54d
George Oastler <goastler4@gmail.com>
2023-12-21 12:09:54 +0000

    test object parser

--- packages/parser/src/tests/parsing.test.ts
+++ packages/parser/src/tests/parsing.test.ts
@@ -3,59 +3,156 @@ import { pString } from '../parsers/StringParser.js';
 import { pNumber } from '../parsers/NumberParser.js';
 import { pBigInt } from '../parsers/BigIntParser.js';
 import { pBoolean } from '../parsers/BooleanParser.js';
+import { pObject } from '../parsers/ObjectParser.js';
 
 describe("bigint", () => {
     test("parses", () => {
-        expect(() => pBigInt().parse(undefined)).to.throw();
-        expect(() => pBigInt().parse(null)).to.throw();
-        expect(() => pBigInt().parse("")).to.throw();
-        expect(() => pBigInt().parse(true)).to.throw();
-        expect(() => pBigInt().parse(123)).to.throw();
-        expect(pBigInt().parse(BigInt(456))).to.equal(BigInt(456));
-        expect(() => pBigInt().parse("hello")).to.throw();
-        expect(() => pBigInt().parse("789")).to.throw();
-        expect(() => pBigInt().parse("hello123")).to.throw();
+        expect(() => pBigInt().parse(undefined)).toThrowError();
+        expect(() => pBigInt().parse(null)).toThrowError();
+        expect(() => pBigInt().parse("")).toThrowError();
+        expect(() => pBigInt().parse(true)).toThrowError();
+        expect(() => pBigInt().parse(123)).toThrowError();
+        expect(pBigInt().parse(BigInt(456))).toEqual(BigInt(456));
+        expect(() => pBigInt().parse("hello")).toThrowError();
+        expect(() => pBigInt().parse("789")).toThrowError();
+        expect(() => pBigInt().parse("hello123")).toThrowError();
     });
 })
 
 describe("boolean", () => {
     test("parses", () => {
-        expect(() => pBoolean().parse(undefined)).to.throw();
-        expect(() => pBoolean().parse(null)).to.throw();
-        expect(() => pBoolean().parse("")).to.throw();
-        expect(pBoolean().parse(true)).to.equal(true);
-        expect(() => pBoolean().parse(123)).to.throw();
-        expect(() => pBoolean().parse(BigInt(456))).to.throw();
-        expect(() => pBoolean().parse("hello")).to.throw();
-        expect(() => pBoolean().parse("789")).to.throw();
-        expect(() => pBoolean().parse("hello123")).to.throw();
+        expect(() => pBoolean().parse(undefined)).toThrowError();
+        expect(() => pBoolean().parse(null)).toThrowError();
+        expect(() => pBoolean().parse("")).toThrowError();
+        expect(pBoolean().parse(true)).toEqual(true);
+        expect(() => pBoolean().parse(123)).toThrowError();
+        expect(() => pBoolean().parse(BigInt(456))).toThrowError();
+        expect(() => pBoolean().parse("hello")).toThrowError();
+        expect(() => pBoolean().parse("789")).toThrowError();
+        expect(() => pBoolean().parse("hello123")).toThrowError();
     });
 })
 
 describe("string", () => {
     test("parses", () => {
-        expect(() => pString().parse(undefined)).to.throw();
-        expect(() => pString().parse(null)).to.throw();
-        expect(pString().parse("")).to.equal("");
-        expect(() => pString().parse(true)).to.throw();
-        expect(() => pString().parse(123)).to.throw();
-        expect(() => pString().parse(BigInt(456))).to.throw();
-        expect(pString().parse("hello")).to.equal("hello");
-        expect(pString().parse("789")).to.equal("789");
-        expect(pString().parse("hello123")).to.equal("hello123");
+        expect(() => pString().parse(undefined)).toThrowError();
+        expect(() => pString().parse(null)).toThrowError();
+        expect(pString().parse("")).toEqual("");
+        expect(() => pString().parse(true)).toThrowError();
+        expect(() => pString().parse(123)).toThrowError();
+        expect(() => pString().parse(BigInt(456))).toThrowError();
+        expect(pString().parse("hello")).toEqual("hello");
+        expect(pString().parse("789")).toEqual("789");
+        expect(pString().parse("hello123")).toEqual("hello123");
     });
 })
 
 describe("number", () => {
     test("parses", () => {
-        expect(() => pNumber().parse(undefined)).to.throw();
-        expect(() => pNumber().parse(null)).to.throw();
-        expect(() => pNumber().parse("")).to.throw();
-        expect(() => pNumber().parse(true)).to.throw();
-        expect(pNumber().parse(123)).to.equal(123);
-        expect(() => pNumber().parse(BigInt(456))).to.throw();
-        expect(() => pNumber().parse("hello")).to.throw();
-        expect(() => pNumber().parse("789")).to.throw();
-        expect(() => pNumber().parse("hello123")).to.throw();
+        expect(() => pNumber().parse(undefined)).toThrowError();
+        expect(() => pNumber().parse(null)).toThrowError();
+        expect(() => pNumber().parse("")).toThrowError();
+        expect(() => pNumber().parse(true)).toThrowError();
+        expect(pNumber().parse(123)).toEqual(123);
+        expect(() => pNumber().parse(BigInt(456))).toThrowError();
+        expect(() => pNumber().parse("hello")).toThrowError();
+        expect(() => pNumber().parse("789")).toThrowError();
+        expect(() => pNumber().parse("hello123")).toThrowError();
     });
+})
+
+describe("object", () => {
+    test("fails on undefined", () => {
+        expect(() => pObject({}).parse(undefined)).toThrowError();
+    })
+
+    test("fails on null", () => {
+        expect(() => pObject({}).parse(null)).toThrowError();
+    })
+
+    test("fails on empty string", () => {
+        expect(() => pObject({}).parse("")).toThrowError();
+    })
+
+    test("fails on true", () => {
+        expect(() => pObject({}).parse(true)).toThrowError();
+    })
+
+    test("fails on number", () => {
+        expect(() => pObject({}).parse(123)).toThrowError();
+    })
+
+    test("fails on bigint", () => {
+        expect(() => pObject({}).parse(BigInt(456))).toThrowError();
+    })
+
+    test("parses simple object", () => {
+        const input: unknown = {
+            a: 1,
+            b: "hello",
+            c: true,
+        }
+        expect(pObject({
+            a: pNumber(),
+            b: pString(),
+            c: pBoolean(),
+        }).parse(input)).toEqual({
+            a: 1,
+            b: "hello",
+            c: true,
+        });
+    })
+
+    test("parses simple object fail", () => {
+        const input: unknown = {
+            a: 1,
+            b: "hello",
+            c: 3,
+        }
+        expect(() => pObject({
+            a: pNumber(),
+            b: pString(),
+            c: pBoolean(),
+        }).parse(input)).toThrowError('3');
+    })
+
+    test("parses nested objects", () => {
+        const input: unknown = {
+            a: 1,
+            b: {
+                c: "hello",
+                d: true,
+            },
+        }
+        expect(pObject({
+            a: pNumber(),
+            b: pObject({
+                c: pString(),
+                d: pBoolean(),
+            }),
+        }).parse(input)).toEqual({
+            a: 1,
+            b: {
+                c: "hello",
+                d: true,
+            },
+        });
+    })
+
+    test("parses nested objects fail in nested", () => {
+        const input: unknown = {
+            a: 1,
+            b: {
+                c: "hello",
+                d: 3,
+            },
+        }
+        expect(() => pObject({
+            a: pNumber(),
+            b: pObject({
+                c: pString(),
+                d: pBoolean(),
+            }),
+        }).parse(input)).toThrowError('3');
+    })
 })
