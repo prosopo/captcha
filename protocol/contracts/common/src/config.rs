c2b5f4be33bb856d1f6c74a79765dda3587325b8
George Oastler <goastler4@gmail.com>
2024-01-02 12:01:06 +0000

    rename error trait

--- protocol/contracts/common/src/config.rs
+++ protocol/contracts/common/src/config.rs
@@ -1,13 +1,13 @@
-use super::errors::ContractError;
+use super::errors::Error;
 use scale::Decode;
 
 // Trait implementing config functions for contracts
 pub trait Config<Env: ink::env::Environment> {
     fn check_is_admin(
         account: <Env as ink::env::Environment>::AccountId,
-    ) -> Result<(), ContractError<Env>> {
+    ) -> Result<(), Error<Env>> {
         if account != Self::get_admin()? {
-            return Err(ContractError::NotAuthorised);
+            return Err(Error::NotAuthorised);
         }
         Ok(())
     }
@@ -21,14 +21,14 @@ pub trait Config<Env: ink::env::Environment> {
     }
 
     /// the admin which can control this contract. set to author/instantiator by default
-    fn get_admin() -> Result<<Env as ink::env::Environment>::AccountId, ContractError<Env>> {
+    fn get_admin() -> Result<<Env as ink::env::Environment>::AccountId, Error<Env>> {
         let env_admin_bytes: [u8; 32] = [
             212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133,
             88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125,
         ];
         let encoded = scale::Encode::encode(&env_admin_bytes);
         <Env as ink::env::Environment>::AccountId::decode(&mut &encoded[..])
-            .map_err(|_| ContractError::AccountIdDecodeFailed)
+            .map_err(|_| Error::AccountIdDecodeFailed)
     }
 }
 
