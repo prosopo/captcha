84a9b4c06bda39af4582d06546f3d7f7ea7be1c3
George Oastler <goastler4@gmail.com>
2024-01-03 17:25:22 +0000

    rename account helper methods

--- protocol/contracts/common/src/account_utils.rs
+++ protocol/contracts/common/src/account_utils.rs
@@ -7,23 +7,23 @@ pub fn get_unused_account() -> AccountId {
     Account::nth_role(0, "unused").account_id()
 }
 
-pub fn get_admin_account(index: u128) -> Account {
+pub fn nth_admin_account(index: u128) -> Account {
     Account::nth_role(index, "admin")
 }
 
-pub fn get_provider_account(index: u128) -> Account {
+pub fn nth_provider_account(index: u128) -> Account {
     Account::nth_role(index, "provider")
 }
 
-pub fn get_dapp_account(index: u128) -> Account {
+pub fn nth_dapp_account(index: u128) -> Account {
     Account::nth_role(index, "dapp")
 }
 
-pub fn get_user_account(index: u128) -> Account {
+pub fn nth_user_account(index: u128) -> Account {
     Account::nth_role(index, "user")
 }
 
-pub fn get_contract_account(index: u128) -> Account {
+pub fn nth_contract_account(index: u128) -> Account {
     let account = Account::nth_role(index, "contract");
     // mark the account as a contract
     set_contract(account.account_id());
@@ -31,6 +31,6 @@ pub fn get_contract_account(index: u128) -> Account {
     account
 }
 
-pub fn get_code_hash(index: u128) -> [u8; 32] {
+pub fn nth_code_hash(index: u128) -> [u8; 32] {
     Account::nth_role(index, "code_hash").pair().public().0
 }
