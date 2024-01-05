b0f3947fa8a3b8e7a1b03dc8b7d804829cd3e7c0
George Oastler <goastler4@gmail.com>
2024-01-05 14:37:27 +0000

    make utils trait based, implement tests

--- protocol/contracts/common/src/utils.rs
+++ protocol/contracts/common/src/utils.rs
@@ -1,7 +1,6 @@
-/// get the account id in byte array format
-pub fn account_id_bytes(account: &ink::primitives::AccountId) -> &[u8; 32] {
-    AsRef::<[u8; 32]>::as_ref(account)
-}
+use crate::Error;
+use core::marker::PhantomData;
+use scale_info::prelude::vec::Vec;
 
 /// Print and return an error in ink
 #[macro_export]
@@ -47,4 +46,127 @@ macro_rules! lazy {
         contents.$func($value);
         $lazy.set(&contents);
     };
+}
+
+/// EnvUtils contains several util fns which need the environment to be defined.
+pub trait Utils<Env: ink::env::Environment> {
+    /// Convert a byte array to payload. This wraps the '<Bytes>' tag around the byte array to conform with signing convention.
+    fn to_payload(message: &Vec<u8>) -> Vec<u8> {
+        // if payload is already wrapped in <Bytes> tag, return it
+        if message.starts_with(b"<Bytes>") && message.ends_with(b"</Bytes>") {
+            return message.clone();
+        }
+        [b"<Bytes>", message.as_slice(), b"</Bytes>"].concat()
+    }
+
+    /// get the account id in byte array format
+    fn account_id_bytes(account: &Env::AccountId) -> [u8; 32] {
+        let bytes = account.as_ref();
+        // this will panic if account is not 32 bytes long, which may be the case in non-standard environments.
+        // Note: this is a weird issue with ink, accounts are used as 32-byte arrays in some places but the environment can set their length to something else, rendering the two incompatible.
+        bytes.try_into().expect("account length to be 32-bytes")
+    }
+
+    /// Sr25519 verify a message, converting message to payload before verifying
+    fn sr25519_verify(
+        signature: &[u8; 64],
+        message: &Vec<u8>,
+        account: &Env::AccountId,
+    ) -> Result<(), Error<Env>> {
+        let public_key = Self::account_id_bytes(account);
+        ink::env::sr25519_verify(signature, Self::to_payload(message).as_slice(), &public_key)
+            .map_err(|_| Error::InvalidSignature {
+                public_key: public_key.to_vec(),
+                message: message.clone(),
+                signature: signature.to_vec(),
+            })
+    }
+}
+
+/// DefaultEnvUtils implements EnvUtils for the default environment
+pub enum DefaultUtils<Env: ink::env::Environment> {
+    _Marker(PhantomData<Env>), // marker to use generic parameter
+}
+
+impl<Env: ink::env::Environment> Utils<Env> for DefaultUtils<Env> {}
+
+#[cfg(test)]
+mod tests {
+    use super::Utils as UtilsTrait;
+    type Env = ink::env::DefaultEnvironment;
+    type Utils = super::DefaultUtils<Env>;
+    use scale_info::TypeInfo;
+
+    #[test]
+    fn test_to_payload() {
+        let message = vec![1, 2, 3];
+        let payload = Utils::to_payload(&message);
+        assert_eq!(
+            payload,
+            vec![
+                b'<', b'B', b'y', b't', b'e', b's', b'>', 1, 2, 3, b'<', b'/', b'B', b'y', b't',
+                b'e', b's', b'>'
+            ]
+        );
+        // check a subsequent call returns the same result, i.e. doesn't wrap the payload again
+        let payload2 = Utils::to_payload(&payload);
+        assert_eq!(payload, payload2);
+    }
+
+    #[test]
+    fn test_account_id_bytes() {
+        let account = ink::primitives::AccountId::from([1u8; 32]);
+        let bytes = Utils::account_id_bytes(&account);
+        assert_eq!(bytes, [1u8; 32]);
+    }
+
+    /// A dummy env with long account ids
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    #[cfg_attr(feature = "std", derive(TypeInfo))]
+    pub enum LongAccountEnvironment {}
+
+    impl ink::env::Environment for LongAccountEnvironment {
+        const MAX_EVENT_TOPICS: usize = 4;
+
+        type AccountId = [u8; 64];
+        type Balance = u128;
+        type Hash = [u8; 32];
+        type Timestamp = u128;
+        type BlockNumber = u128;
+        type ChainExtension = ink::env::NoChainExtension;
+    }
+    type LongAccountEnvironmentUtils = super::DefaultUtils<LongAccountEnvironment>;
+
+    #[test]
+    #[should_panic(expected = "account length to be 32-bytes")]
+    fn test_account_id_bytes_long() {
+        let account = [1u8; 64];
+        // should panic, account is not 32bytes long
+        LongAccountEnvironmentUtils::account_id_bytes(&account);
+    }
+
+    /// A dummy env with short account ids
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    #[cfg_attr(feature = "std", derive(TypeInfo))]
+    pub enum ShortAccountEnvironment {}
+
+    impl ink::env::Environment for ShortAccountEnvironment {
+        const MAX_EVENT_TOPICS: usize = 4;
+
+        type AccountId = [u8; 0];
+        type Balance = u128;
+        type Hash = [u8; 32];
+        type Timestamp = u128;
+        type BlockNumber = u128;
+        type ChainExtension = ink::env::NoChainExtension;
+    }
+    type ShortAccountEnvironmentUtils = super::DefaultUtils<ShortAccountEnvironment>;
+
+    #[test]
+    #[should_panic(expected = "account length to be 32-bytes")]
+    fn test_account_id_bytes_short() {
+        let account = [1u8; 0];
+        // should panic, account is not 32bytes long
+        ShortAccountEnvironmentUtils::account_id_bytes(&account);
+    }
 }
