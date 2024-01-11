423836622c582a297e6f2251329ab990023d7fa8
Hugh <hughglynparry@gmail.com>
2024-01-11 14:03:30 +0000

    Unused test file

--- packages/provider/src/util/verifyDapp.ts
+++
@@ -1,7 +0,0 @@
-import { ProviderEnvironment } from "@prosopo/env"
-
-const config = 
-const env = new ProviderEnvironment(config, pair)
-await env.isReady()
-const tasks = new Tasks(env)
-const result = (await tasks.contract.query.getDapp(validateAddress(argv).address)).value.unwrap().unwrap()
