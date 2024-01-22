766c8a430a9d419fd0b4a5d9f42ed83e8f9bcb2e
George Oastler <goastler4@gmail.com>
2024-01-22 14:57:48 +0000

    intersect parser tests

--- packages/parser/src/parsers/IntersectParser.ts
+++ packages/parser/src/parsers/IntersectParser.ts
@@ -10,7 +10,14 @@ export class IntersectParser<const T extends Parser<unknown>[]> implements BaseP
     public constructor(private parsers: T) {}
 
     parse(value: unknown): ParserShapeIntersect<T> {
-        return null!
+        // parse each parser in turn, all should succeed
+        // TODO don't drop extra keys per parser / find a way to restore them / merge output of all parsers
+        for (const parser of this.parsers) {
+            // not using the output of the parsers, just checking they don't throw
+            parser.parse(value)
+            // TODO merge output of all parsers?
+        }
+        return value as ParserShapeIntersect<T>
     }   
 }
 export const pIntersect = <const T extends Parser<unknown>[]>(parsers: T) => new IntersectParser(parsers)
