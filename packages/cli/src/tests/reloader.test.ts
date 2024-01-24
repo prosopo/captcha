112d736364372127f9834d24343742d9edc5b46b
Chris Taylor <forgetso86@gmail.com>
2024-01-24 12:02:52 +0000

    Fix reloading-test

--- packages/cli/src/tests/reloader.test.ts
+++ packages/cli/src/tests/reloader.test.ts
@@ -1,4 +1,4 @@
-import { assert, describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { getCurrentFileDirectory } from '@prosopo/util'
 import { promisify } from 'util'
 import { spawn } from 'child_process'
@@ -7,60 +7,75 @@ import path from 'path'
 
 describe('reloading api', () => {
     test.only('api reloads after changing .env file', async () => {
-        // get file location
-        const dir = getCurrentFileDirectory(import.meta.url)
+        try {
+            // get file location
+            const dir = getCurrentFileDirectory(import.meta.url)
 
-        // get root directory of this package
-        const rootDir = dir.split('/').slice(0, -2).join('/')
-        console.log('rootDir', rootDir)
+            // get root directory of this package
+            const rootDir = dir.split('/').slice(0, -2).join('/')
+            console.log('rootDir', rootDir)
 
-        // run API
-        const child = spawn(`npm`, ['run', 'cli', '--', '--api'], { cwd: rootDir })
-
-        let appended = false
-        child.stdout.on('data', (data) =>
-            onData(data, rootDir, appended).then((hasAppended) => {
-                appended = hasAppended
-                console.log('onData ran')
+            // run API
+            const child = spawn(`npm`, ['run', 'cli', '--', '--api'], {
+                cwd: rootDir,
+                env: { ...process.env, NODE_ENV: 'test' },
             })
-        )
 
-        child.stdout.on('error', (e) => {
-            throw new Error(e.message)
-        })
+            let appended = false
+            child.stdout.on('data', (data) =>
+                onData(data, rootDir, appended).then((result) => {
+                    appended = result.appended
+                    const kill = result.kill
+                    console.log('onData ran, appended', appended, 'kill', kill)
+                    if (kill) {
+                        child.kill()
+                        expect(appended).toBe(true)
+                        remainOpen = false
+                    }
+                })
+            )
 
-        let remainOpen = true
+            child.stdout.on('error', (e) => {
+                console.log('error', e)
+                process.exit(1)
+            })
 
-        child.stdout.on('close', () => {
-            console.log('closed')
-            remainOpen = true // change
-        })
+            let remainOpen = true
 
-        while (remainOpen) {
-            await new Promise((resolve) => setTimeout(resolve, 1000))
+            child.stdout.on('close', (result: any) => {
+                console.log('closed', result)
+                remainOpen = true // change
+            })
+
+            while (remainOpen) {
+                await new Promise((resolve) => setTimeout(resolve, 1000))
+            }
+        } catch (e) {
+            console.log('error', e)
+            process.exit(1)
         }
     }, 120000)
 })
 
-const onData = async (data: any, rootDir: string, appended: boolean) => {
+const onData = async (data: any, rootDir: string, appended: boolean): Promise<{ appended: boolean; kill: boolean }> => {
     console.log(`stdout:\n${data}`)
+    await new Promise((resolve) => setTimeout(resolve, 500))
+
     // find out which .env file is being used
     if (data.includes('Running main process...')) {
         if (!appended) {
-            const envPath = path.resolve('../../dev/scripts/.env.test')
+            const envPath = path.resolve(`${rootDir}/.env.test`)
 
-            // modify .env file
-            console.log('Modifying', envPath)
             // Append a key value pair to the .env file
             await promisify(fs.appendFile)(envPath, 'TEST=TEST\n')
-            await promisify(fs.appendFile)(envPath, 'TEST1=TEST1\n')
-            await promisify(fs.appendFile)(envPath, 'TEST2=TEST2\n')
-            console.log('Appended to', envPath)
-            return true
+            return {
+                appended: true,
+                kill: false,
+            }
         }
-    } else if (appended) {
-        console.log('appended', data)
-        assert(data.includes('Stopping API'))
     }
-    return false
+    if (appended) {
+        return { appended: true, kill: data.includes('Stopping API') }
+    }
+    return { appended: false, kill: false }
 }
