d3528c36865f7f8df767caeb653ded399e2f9a2e
George Oastler <goastler4@gmail.com>
2024-01-25 17:34:49 +0000

    fix circular reference of logger and error

--- packages/common/src/logger.ts
+++ packages/common/src/logger.ts
@@ -74,7 +74,9 @@ const getLoggerAdapterConsola = (logLevel: LogLevel, scope: string): Logger => {
                     logLevel = ConsolaLogLevels.fatal
                     break
                 default:
-                    throw new ProsopoEnvError('CONFIG.INVALID_LOG_LEVEL', { context: { logLevel } })
+                    // this cannot be a ProsopoEnvError. The default logger calls this method, which creates a new ProsopoEnvError, which requires the default logger, which hasn't been constructed yet, leading to ts not being able to find getLoggerDefault() during runtime as it has not completed yet (I think).
+                    // Either way, this should never happen in runtime, this error is just an edge case, every log level should be translated properly.
+                    throw new Error(`Invalid log level translation to consola's log level: ${level}`)
             }
             logger.level = logLevel
             currentLevel = level
