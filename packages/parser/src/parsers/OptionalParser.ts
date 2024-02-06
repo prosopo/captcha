5d7a5af09a3e926a154708e1e611982193e94e33
George Oastler <goastler4@gmail.com>
2024-02-06 12:26:25 +0000

    simplified generics in req/ro/rw/opt

--- packages/parser/src/parsers/OptionalParser.ts
+++ packages/parser/src/parsers/OptionalParser.ts
@@ -1,11 +1,11 @@
 import { NestedParser, OptionalProp, Parser, Shape } from "./Parser.js";
 
-export class OptionalParser<T extends Parser<U>, U> extends Parser<U | undefined> implements NestedParser<T>, OptionalProp<true>  {
+export class OptionalParser<T extends Parser<any>> extends Parser<Shape<T> | undefined> implements NestedParser<T>, OptionalProp<true>  {
     constructor(readonly parser: T) {
         super()
     }
 
-    public override parse(value: unknown): U | undefined {
+    public override parse(value: unknown): Shape<T> | undefined {
         if (value === undefined) {
             return undefined
         }
@@ -13,11 +13,11 @@ export class OptionalParser<T extends Parser<U>, U> extends Parser<U | undefined
     }
 
     public override clone() {
-        return new OptionalParser<T, U>(this.parser)
+        return new OptionalParser(this.parser)
     }
 
     readonly optional = true
 }
 
-export const pOptional = <T extends Parser<any>>(parser: T) => new OptionalParser<T, Shape<T>>(parser)
+export const pOptional = <T extends Parser<any>>(parser: T) => new OptionalParser<T>(parser)
 export const opt = pOptional
