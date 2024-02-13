3e6f3e08d46643a58f4a12c90b642dc35081c0e5
George Oastler <goastler4@gmail.com>
2024-02-13 15:50:02 +0000

    dev

--- packages/wasm/src/lib.rs
+++ packages/wasm/src/lib.rs
@@ -1,8 +1,26 @@
 mod utils;
+use sha2::Sha256;
 use sha256::{digest, try_digest};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn run(input: String) -> String {
-    digest(input)
+pub fn run(input: [u8], difficulty: usize) -> String {
+    let nonce: u32 = 0;
+    while (true) {
+        let mut hasher = Sha256::new();
+        hasher.update(input);
+        hasher.update(nonce.to_le_bytes());
+        let result = hasher.finalize();
+        let found = true;
+        for(let i = 0; i < difficulty; i++) {
+            if (result[i] != 0) {
+                found = false;
+                break;
+            }
+        }
+        if(found) {
+            return nonce;
+        }
+        nonce++;
+    }
 }
