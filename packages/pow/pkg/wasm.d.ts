d617eb9e9978688d6bf58e57fef09a2e9fbc15e8
George Oastler <goastler4@gmail.com>
2024-02-16 07:14:13 +0000

    remove old output

--- packages/pow/pkg/wasm.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @param {Uint8Array} input
-* @param {number} difficulty
-* @returns {number}
-*/
-export function run(input: Uint8Array, difficulty: number): number;
--- packages/pow/pkg/wasm_bg.wasm.d.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function run(a: number, b: number, c: number): number;
-export function __wbindgen_malloc(a: number, b: number): number;
