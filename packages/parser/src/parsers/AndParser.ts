61e4f969f5f766c3895add3b4a634788d491b770
George Oastler <goastler4@gmail.com>
2024-02-18 18:40:04 +0000

    and/intersection parser

---
+++ packages/parser/src/parsers/AndParser.ts
@@ -0,0 +1,35 @@
+import { Parser } from "./Parser.js"
+import { Resolve } from "./utils.js"
+
+export type IntersectParserArray<T> = T extends [Parser<infer A>, ...infer B] ?  | IntersectParserArray<B> : []
+
+export class AndParser<T extends Parser<any>[]> extends Parser<IntersectParserArray<T>> {
+
+    constructor(readonly parsers: T) {
+        super()
+    }
+
+    public override parse(value: unknown): IntersectParserArray<T> {
+        const errors: unknown[] = []
+        for (const parser of this.parsers) {
+            // should parse in all parsers
+            value = parser.parse(value)
+        }
+        // do it twice to ensure the value returned by the last parser is still accepted by the first
+        // i.e. make sure none of the parsers have side effects which mutate the value to a point where it is no longer accepted any previous parsers
+        for (const parser of this.parsers) {
+            // should parse in all parsers
+            value = parser.parse(value)
+        }
+        return value as IntersectParserArray<T>
+    }
+
+    public override clone() {
+        return new AndParser<T>(this.parsers)
+    }
+}
+
+export const pAnd = <T extends Parser<any>[]>(parsers: T) => new AndParser<T>(parsers)
+export const and = pAnd
+export const intersection = pAnd
+export const intersect = pAnd
