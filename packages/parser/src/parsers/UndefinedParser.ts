dae0c6b4ab3289f46f4d1942da68cddba1899db0
George Oastler <goastler4@gmail.com>
2024-02-18 16:54:11 +0000

    undefined parser

---
+++ packages/parser/src/parsers/UndefinedParser.ts
@@ -0,0 +1,17 @@
+import { Parser } from "./Parser.js"
+
+export class UndefinedParser extends Parser<undefined> {
+    public override parse(value: unknown): undefined {
+        if (typeof value !== "undefined") {
+            throw new Error(`Expected undefined but got ${JSON.stringify(value)} of type ${JSON.stringify(typeof value)}`)
+        }
+        return value
+    }
+
+    public override clone() {
+        return new UndefinedParser()
+    }
+}
+
+export const pUndefined = () => new UndefinedParser()
+export const undef = pUndefined
