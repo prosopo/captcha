593d8ed60db0a6c04ffe2a25be398e17035c6e42
George Oastler <goastler4@gmail.com>
2024-02-18 18:08:56 +0000

    function parser

---
+++ packages/parser/src/parsers/FunctionParser.ts
@@ -0,0 +1,26 @@
+import { Parser } from "./Parser.js"
+
+// enables parsing at function entry and exit points. I.e. validate the args of a fn and/or the return value of a fn
+export class FunctionParser<T extends Parser<any>[], U> {
+
+    // if there are no args / you do not want to parse the args, then set args to null
+    // if there is no return value / you do not want to parse the return value, then set returnType to null
+    // both default to null, representing () => void
+    constructor(readonly args: T | null = null, readonly returnType: Parser<U> | null = null) {
+
+    }
+
+
+}
+
+type UnpackParserArray<T> = T extends [Parser<infer A>, ...infer B] ? [A, ...UnpackParserArray<B>] : []
+type UnpackReturnType<T> = T extends Parser<infer U> ? U : never
+type ToFn<T extends Parser<any>[], U = void> = (...args: UnpackParserArray<T>) => U
+
+type a1 = ToFn<[Parser<string>, Parser<number>]>
+type a2 = ToFn<[], string>
+
+// export const pFunction = <T, U>() => new FunctionParser<T, U>()
+// export const fn = pFunction
+
+// TODO finish this off
