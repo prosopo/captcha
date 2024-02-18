de4d8e7cf852a20a5fbfa9bef638935726bed6fb
George Oastler <goastler4@gmail.com>
2024-02-18 17:27:43 +0000

    symbol parser

---
+++ packages/parser/src/parsers/SymbolParser.ts
@@ -0,0 +1,17 @@
+import { Parser } from "./Parser.js"
+
+export class SymbolParser extends Parser<Symbol> {
+    public override parse(value: unknown): Symbol {
+        if (typeof value !== "symbol") {
+            throw new Error(`Expected Symbol but got ${JSON.stringify(value)} of type ${JSON.stringify(typeof value)}`)
+        }
+        return value
+    }
+
+    public override clone() {
+        return new SymbolParser()
+    }
+}
+
+export const pSymbol = () => new SymbolParser()
+export const symbol = pSymbol
