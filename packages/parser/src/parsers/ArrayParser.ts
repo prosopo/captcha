fd9f84c9662f3793ef56e5169e5eb25ae4c7505a
George Oastler <goastler4@gmail.com>
2024-02-20 15:11:02 +0000

    Fixed length array parsing

--- packages/parser/src/parsers/ArrayParser.ts
+++ packages/parser/src/parsers/ArrayParser.ts
@@ -3,13 +3,19 @@ import { Parser, Shape } from "./Parser.js"
 
 export class ArrayParser<T extends Parser<any>> extends Parser<Shape<T>[]> {
 
-    constructor(readonly parser: T) {
+    constructor(readonly parser: T, readonly length: number = -1) {
         super()
     }
 
     public override parse(value: unknown): Shape<T>[] {
         // value is definitely an array
         const valueArray = inst(Array).parse(value)
+        if (this.length >= 0) {
+            // then expecting fixed length array
+            if (valueArray.length !== this.length) {
+                throw new Error(`Expected array with ${this.length} elements but got ${valueArray.length} elements`)
+            }
+        }
         for (let i = 0; i < valueArray.length; i++) {
             // parse each element
             valueArray[i] = this.parser.parse(valueArray[i])
@@ -22,7 +28,7 @@ export class ArrayParser<T extends Parser<any>> extends Parser<Shape<T>[]> {
     }
 }
 
-export const pArray = <T extends Parser<any>>(parser: T) => new ArrayParser<T>(parser)
+export const pArray = <T extends Parser<any>>(parser: T, length: number = -1) => new ArrayParser<T>(parser)
 export const arr = pArray
 export const list = pArray
 export const array = pArray
