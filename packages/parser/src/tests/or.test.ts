cee9fdb6bce55dfc341c850ad75443daefb4a3c3
George Oastler <goastler4@gmail.com>
2024-02-20 11:42:26 +0000

    add or tests

--- packages/parser/src/tests/or.test.ts
+++ packages/parser/src/tests/or.test.ts
@@ -1,32 +1,23 @@
 import { describe, expect, test, it, expectTypeOf } from 'vitest'
 import { bool } from '../parsers/BooleanParser.js';
 import { num } from '../parsers/NumberParser.js';
-import { or } from '../parsers/OrParser.js';
+import { or, union } from '../parsers/OrParser.js';
 import { str } from '../parsers/StringParser.js';
 
 describe("or", () => {
-    it("should error on no parsers", () => {
-        expect(() => or([]).parse(null)).toThrow();
-    })
-
-    it("should return type never on no parsers", () => {
-        expectTypeOf(() => or([]).parse(null)).returns.toBeNever();
-    })
-
     it("should return union of types", () => {
-        expectTypeOf(() => or([num(), str(), bool()]).parse(null)).returns.toEqualTypeOf<number | string | boolean>();
+        expectTypeOf(() => or(num(), str()).parse(null)).returns.toEqualTypeOf<number | string>();
     })
 
-    it("should parse number, string, or boolean", () => {
-        expect(or([num(), str(), bool()]).parse(1)).toBe(1);
-        expect(or([num(), str(), bool()]).parse("")).toBe("");
-        expect(or([num(), str(), bool()]).parse(true)).toBe(true);
+    it("should parse number or string", () => {
+        expect(or(num(), str()).parse(1)).toBe(1);
+        expect(or(num(), str()).parse("")).toBe("");
     })
 
     it("should error on incorrect type", () => {
-        expect(() => or([num(), str(), bool()]).parse({})).toThrow();
-        expect(() => or([num(), str(), bool()]).parse(undefined)).toThrow();
-        expect(() => or([num(), str(), bool()]).parse(null)).toThrow();
-        expect(() => or([num(), str(), bool()]).parse([])).toThrow();
+        expect(() => or(num(), str()).parse({})).toThrow();
+        expect(() => or(num(), str()).parse(undefined)).toThrow();
+        expect(() => or(num(), str()).parse(null)).toThrow();
+        expect(() => or(num(), str()).parse([])).toThrow();
     })
 });
