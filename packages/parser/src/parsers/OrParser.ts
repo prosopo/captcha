c685f7e4a4c0fe207520b486df3a61d546f0458f
George Oastler <goastler4@gmail.com>
2024-02-20 11:41:59 +0000

    add paired or ability to union parser

--- packages/parser/src/parsers/OrParser.ts
+++ packages/parser/src/parsers/OrParser.ts
@@ -1,11 +1,12 @@
 import { BooleanParser, bool } from "./BooleanParser.js"
+import { never } from "./NeverParser.js"
 import { NumberParser, num } from "./NumberParser.js"
-import { Parser } from "./Parser.js"
+import { Parser, Shape } from "./Parser.js"
 import { StringParser, str } from "./StringParser.js"
 
 export type UnionParserArray<T> = T extends [Parser<infer A>, ...infer B] ? A | UnionParserArray<B> : never
 
-export class OrParser<const T extends Parser<any>[]> extends Parser<UnionParserArray<T>> {
+export class UnionParser<const T extends Parser<any>[]> extends Parser<UnionParserArray<T>> {
 
     constructor(readonly parsers: T) {
         super()
@@ -24,10 +25,42 @@ export class OrParser<const T extends Parser<any>[]> extends Parser<UnionParserA
     }
 
     public override clone() {
-        return new OrParser<T>(this.parsers)
+        return new UnionParser<T>(this.parsers)
     }
 }
 
-export const pOr = <const T extends Parser<any>[]>(parsers: T) => new OrParser<T>(parsers)
+export const pUnion = <const T extends Parser<any>[]>(parsers: T) => new UnionParser<T>(parsers)
+export const union = pUnion
+export const pOr = <T extends Parser<any>, U extends Parser<any>>(parser1: T, parser2: U) => new UnionParser([parser1, parser2])
 export const or = pOr
-export const union = pOr
+
+// export type UnionPair<T, U> = Shape<T> | Shape<U>
+// export class OrPair<T extends Parser<any>, U extends Parser<any>> extends Parser<UnionPair<T, U>> {
+//     constructor(readonly parser1: T, readonly parser2: U) {
+//         super()
+//     }
+
+//     public override parse(value: unknown): UnionPair<T, U> {
+//         return null!
+//     }
+
+//     public override clone(): Parser<UnionPair<T, U>> {
+//         return new OrPair(this.parser1, this.parser2)
+//     }
+// }
+
+// export const pOrPair = <T extends Parser<any>, U extends Parser<any>>(parser1: T, parser2: U) => new OrPair(parser1, parser2)
+// export const orPair = pOrPair
+// export const unionPair = pOrPair
+
+// type OrMany<T> = T extends [Parser<infer A>, ...infer B] ? A | OrMany<B> : never
+// const f1 = <const T extends Parser<any>[]>(parsers: T) => {
+//     return parsers.reduce((acc, parser) => {
+//         return orPair(acc, parser)
+//     }, never()) as Parser<OrMany<T>>
+// }
+
+// const a1 = orPair(str(), num())
+// type t1 = ReturnType<typeof a1.parse>
+// const a2 = f1([str(), num(), bool()])
+// type t2 = ReturnType<typeof a2.parse>
