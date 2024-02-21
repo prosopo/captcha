c91de6cac4af5abb1446954d206f59f7158ab0c7
Chris <forgetso86@gmail.com>
2024-02-21 17:17:52 +0000

    Fix modal overflowing on laptops and remove height:100% (#1044)
    * Move modal CSS into file
    
    * Move procaptcha-react build to vite
    
    * remove base
    
    * Add max-height content
    
    * Modify modal styling
    
    * Use declaration plugin to build .d.ts files for procaptcha-react
    
    * lint:fix

--- packages/procaptcha-react/src/components/Modal.tsx
+++ packages/procaptcha-react/src/components/Modal.tsx
@@ -1,5 +1,5 @@
+import '../style/Modal.css'
 import React, { CSSProperties } from 'react'
-
 type ModalProps = {
     show: boolean
     children: React.ReactNode
@@ -8,37 +8,15 @@ type ModalProps = {
 const ModalComponent = React.memo((props: ModalProps, nextProps: ModalProps) => {
     const { show, children } = props
     console.log('rendering modal with show: ', show)
-    const display = show ? 'flex' : 'none'
+    const display = show ? 'block' : 'none'
     const ModalOuterDivCss: CSSProperties = {
-        overflow: 'auto',
-        width: '100%',
-        maxHeight: '100%',
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        height: '100%',
-        background: 'rgba(0, 0, 0, 0.6)',
-        zIndex: '2147483646',
-        transition: 'all 0.5s',
         display: display,
-    }
-    const ModalInnerDivCSS: CSSProperties = {
-        maxWidth: '500px',
-        margin: 'auto',
-        position: 'fixed',
-        background: 'white',
-        height: '100%',
-        maxHeight: '100%',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: '2147483647',
-        transition: 'all 0.5s',
     }
 
     return (
-        <div style={ModalOuterDivCss}>
-            <div style={ModalInnerDivCSS}>{children}</div>
+        <div className="modalOuter" style={ModalOuterDivCss}>
+            <div className="modalBackground"></div>
+            <div className="modalInner">{children}</div>
         </div>
     )
 })
