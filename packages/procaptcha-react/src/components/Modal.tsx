4c432bf541c51455741aa6d6f2efaf1abc02dd9d
forgetso <forgetso86@gmail.com>
2024-02-21 12:17:31 +0000

    Move modal CSS into file

--- packages/procaptcha-react/src/components/Modal.tsx
+++ packages/procaptcha-react/src/components/Modal.tsx
@@ -1,5 +1,5 @@
+import './Modal.css'
 import React, { CSSProperties } from 'react'
-
 type ModalProps = {
     show: boolean
     children: React.ReactNode
@@ -10,35 +10,13 @@ const ModalComponent = React.memo((props: ModalProps, nextProps: ModalProps) => 
     console.log('rendering modal with show: ', show)
     const display = show ? 'flex' : 'none'
     const ModalOuterDivCss: CSSProperties = {
-        overflow: 'auto',
-        width: '100%',
-        maxHeight: '100%',
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        height: '100%',
-        background: 'rgba(0, 0, 0, 0.6)',
-        zIndex: '2147483646',
-        transition: 'all 0.5s',
+
         display: display,
-    }
-    const ModalInnerDivCSS: CSSProperties = {
-        maxWidth: '500px',
-        margin: 'auto',
-        position: 'fixed',
-        background: 'white',
-        height: '100%',
-        maxHeight: '100%',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: '2147483647',
-        transition: 'all 0.5s',
     }
 
     return (
-        <div style={ModalOuterDivCss}>
-            <div style={ModalInnerDivCSS}>{children}</div>
+        <div className="modalOuter" style={ModalOuterDivCss}>
+            <div className="modalInner">{children}</div>
         </div>
     )
 })
