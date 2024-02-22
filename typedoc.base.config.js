6e1285798efa7b60df8295c7845d9cfb6d4c8563
George Oastler <goastler4@gmail.com>
2024-02-22 10:45:12 +0000

    make a base typedoc config for config inheritance

---
+++ typedoc.base.config.js
@@ -0,0 +1,9 @@
+export default {
+    includeVersion: true,
+    darkHighlightTheme: 'material-theme-darker',
+    searchInComments: true,
+    commentStyle: 'all',
+    skipErrorChecking: true, // skips errors from package dependency resolution. TODO remove this and get dependencies working
+    // treatWarningsAsErrors: true, // TODO enable these when dependency resolution is fixed
+    // treatValidationWarningsAsErrors: true, // TODO enable these when dependency resolution is fixed
+}
