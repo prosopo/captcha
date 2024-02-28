88c29e77b5435797c531df7db47ece507005317a
George Oastler <goastler4@gmail.com>
2024-02-28 13:50:53 +0000

    lint

--- packages/provider/src/tests/util.test.ts
+++ packages/provider/src/tests/util.test.ts
@@ -56,17 +56,17 @@ describe('UTIL FUNCTIONS', async () => {
             throw new ProsopoEnvError(e as Error)
         }
         // insert a task into the database
-        await env.getDb().storeScheduledTaskStatus('0x01', ScheduledTaskNames.BatchCommitment, ScheduledTaskStatus.Running)
+        await env
+            .getDb()
+            .storeScheduledTaskStatus('0x01', ScheduledTaskNames.BatchCommitment, ScheduledTaskStatus.Running)
         if (!env.db) {
             throw new Error('Database not initialized')
         }
         let result = await checkIfTaskIsRunning(ScheduledTaskNames.BatchCommitment, env.db)
         expect(result).to.equal(true)
-        await env.getDb().storeScheduledTaskStatus(
-            '0x01',
-            ScheduledTaskNames.BatchCommitment,
-            ScheduledTaskStatus.Completed
-        )
+        await env
+            .getDb()
+            .storeScheduledTaskStatus('0x01', ScheduledTaskNames.BatchCommitment, ScheduledTaskStatus.Completed)
         result = await checkIfTaskIsRunning(ScheduledTaskNames.BatchCommitment, env.db)
         expect(result).to.equal(false)
     })
