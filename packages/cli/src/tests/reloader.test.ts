9ef6eeb264c5900da754d164ef12f21de98d9a99
George Oastler <goastler4@gmail.com>
2024-03-02 17:00:09 +0000

    lint

--- packages/cli/src/tests/reloader.test.ts
+++ packages/cli/src/tests/reloader.test.ts
@@ -6,7 +6,7 @@ import fs from 'fs'
 import path from 'path'
 
 describe('reloading api', () => {
-    test('api reloads after changing .env file', async () => {
+    test('api reloads after changing .env file', () => {
 
         // get file location
         const dir = getCurrentFileDirectory(import.meta.url)
