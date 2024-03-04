e30f96c1be4434bed617b90ba17821a6de643d76
George Oastler <goastler4@gmail.com>
2024-03-04 16:04:27 +0000

    unknown parser

---
+++ packages/parser/src/parsers/UnknownParser.ts
@@ -0,0 +1,20 @@
+import { Parser } from "./Parser.js"
+
+export class UnknownParser extends Parser<unknown> {
+
+    constructor() {
+        super()
+    }
+
+    public override parse(value: unknown): unknown {
+        // no-op parser
+        return value
+    }
+
+    public override clone() {
+        return new UnknownParser()
+    }
+}
+
+export const pUnknown = () => new UnknownParser()
+export const unknown = pUnknown
