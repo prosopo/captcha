6d331be697008fdb44e507466ed33b14c350f28f
George Oastler <goastler4@gmail.com>
2024-03-04 17:22:23 +0000

    bound number parser

---
+++ packages/parser/src/parsers/BoundedNumber.ts
@@ -0,0 +1,28 @@
+import { finiteNum } from "./FiniteNumber.js"
+import { NumberParser, num } from "./NumberParser.js"
+import { Parser } from "./Parser.js"
+import { TypeofParser } from "./TypeofParser.js"
+import { Ctor } from "./utils.js"
+
+export class BoundedNumber extends Parser<number> {
+
+    constructor(public min: number = Number.MIN_SAFE_INTEGER, public max: number = Number.MAX_SAFE_INTEGER) {
+        super()
+    }
+
+    public override parse(value: unknown): number {
+        const v = finiteNum().parse(value)
+        if (v < this.min || v > this.max) {
+            throw new Error(`Expected a number between ${this.min} and ${this.max}, but got ${v}`)
+        }
+        return v
+    }
+
+    public override clone() {
+        return new BoundedNumber()
+    }
+}
+
+export const pBoundNumber = (...args: ConstructorParameters<Ctor<BoundedNumber>>) => new BoundedNumber(...args)
+export const boundNumber = pBoundNumber
+export const boundNum = pBoundNumber
