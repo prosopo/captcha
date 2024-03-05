9a2d04e65539d4bb30a0d4f3ad9e5a390b13dd59
Chris Taylor <forgetso86@gmail.com>
2024-03-05 20:21:18 +0000

    Change to URL type

--- dev/flux/src/lib/terminal.ts
+++ dev/flux/src/lib/terminal.ts
@@ -7,7 +7,7 @@ const logger = getLogger(LogLevel.enum.info, 'flux.lib.terminal')
 
 const getSockerURL = (nodeAPIURL: URL) => {
     const urlPort = nodeAPIURL.port || 16127
-    return `https://${nodeAPIURL.hostname.replace(/\./g, '-')}-${urlPort}.node.api.runonflux.io`
+    return new URL(`https://${nodeAPIURL.hostname.replace(/\./g, '-')}-${urlPort}.node.api.runonflux.io`)
 }
 
 export async function main(publicKey: string, privateKey: Uint8Array, appName: string, ip?: string) {
@@ -24,7 +24,7 @@ export async function main(publicKey: string, privateKey: Uint8Array, appName: s
         if (!url) throw new Error('No url')
         const socketUrl = getSockerURL(nodeAPIURL)
 
-        const socket = io(socketUrl)
+        const socket = io(socketUrl.href)
         socket.on('connect', () => {
             logger.info('connected')
             logger.info(socket.id)
@@ -37,6 +37,7 @@ export async function main(publicKey: string, privateKey: Uint8Array, appName: s
         })
         socket.on('connect_error', (err) => {
             logger.info(`connect_error due to ${err.message}`)
+            console.error(err)
         })
         socket.on('disconnect', () => {
             logger.info('disconnected')
