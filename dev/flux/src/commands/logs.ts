8b90fec76d86deb6d881ea6cd0506db548eee6aa
Chris Taylor <forgetso86@gmail.com>
2024-03-07 11:11:51 +0000

    Add option to output logs to file

--- dev/flux/src/commands/logs.ts
+++ dev/flux/src/commands/logs.ts
@@ -4,11 +4,30 @@ import { LogLevel, Logger, getLogger } from '@prosopo/common'
 import { consoleTableWithWrapping } from '@prosopo/util'
 import { getPrivateKey, getPublicKey } from './process.env.js'
 import { main } from '../lib/logs.js'
+import fs from 'fs'
+import path from 'path'
 
 const fluxAuthArgs = z.object({
     app: z.string(),
     ip: z.string().optional(),
+    file: z.string().optional(),
 })
+
+const writeLogs = (file: string, result: { url: string; logs: string }[]) => {
+    const filePath = path.resolve(file)
+    // delete the file if it exists
+    fs.unlink(file, function (err) {
+        if (err) return console.log(err)
+        console.log('file deleted successfully')
+    })
+    // write logs as individual lines to a file
+    fs.appendFileSync(filePath, `host, log\n`)
+    Object.values(result).map(({ url, logs }) => {
+        console.log(logs.split('\\n'))
+        const paddedURL = url.padEnd(32, ' ')
+        logs.split('\\n').map((log) => fs.appendFileSync(filePath, `${paddedURL}, ${log}\n`))
+    })
+}
 
 export default (cmdArgs?: { logger?: Logger }) => {
     const logger = cmdArgs?.logger || getLogger(LogLevel.enum.info, 'flux.cli.auth')
@@ -27,6 +46,11 @@ export default (cmdArgs?: { logger?: Logger }) => {
                     type: 'string' as const,
                     demandOption: false,
                     desc: 'IP address of Flux node to authenticate with',
+                } as const)
+                .option('file', {
+                    type: 'string' as const,
+                    demandOption: false,
+                    desc: 'Write the logs to a file',
                 } as const),
 
         handler: async (argv: ArgumentsCamelCase) => {
@@ -36,6 +60,9 @@ export default (cmdArgs?: { logger?: Logger }) => {
                 const parsedArgs = fluxAuthArgs.parse(argv)
                 const result = await main(publicKey, privateKey, parsedArgs.app, parsedArgs.ip)
                 consoleTableWithWrapping(result)
+                if (parsedArgs.file) {
+                    writeLogs(parsedArgs.file, result)
+                }
             } catch (err) {
                 logger.error(err)
             }
