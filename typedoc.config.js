3c1358c700e3b3c110f096afc23ac5d0c518a8f8
George Oastler <goastler4@gmail.com>
2024-03-07 15:22:05 +0000

    amend base path

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,9 +1,12 @@
 import pkgJson from './package.json' assert { type: 'json' }
 
+const basePath = new URL(import.meta.url).pathname.split('/').slice(0, -1).join('/')
+
 export default {
     extends: './typedoc.base.config.js',
     entryPoints: pkgJson.workspaces,
     out: 'docs',
     entryPointStrategy: 'packages',
     name: 'Prosopo Captcha',
+    basePath,
 }
