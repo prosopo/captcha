12eb0412ed9d88caa9099f615259fb36cad57d11
George Oastler <goastler4@gmail.com>
2024-03-11 11:59:15 +0000

    builtin array type parsers

---
+++ packages/parser/src/parsers/I8ArrayParser.ts
@@ -0,0 +1,14 @@
+import { boundNum } from "./BoundNumber.js"
+import { InstanceParser } from "./InstanceParser.js"
+import { NumberParser, num } from "./NumberParser.js"
+import { Parser } from "./Parser.js"
+import { TypeofParser } from "./TypeofParser.js"
+import { Ctor, InferTypeFromCtor } from "./utils.js"
+
+export class I8ArrayParser extends InstanceParser<Ctor<Int8Array>> {
+}
+
+export const pI8Array = () => new I8ArrayParser(Int8Array)
+export const i8Array = pI8Array
+export const i8a = pI8Array
+export const i8Arr = pI8Array
