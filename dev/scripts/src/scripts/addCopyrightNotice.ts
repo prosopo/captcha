55bc70e6c97d036b52c78dab093a5afd9961a8fc
Chris Taylor <forgetso86@gmail.com>
2024-03-13 21:01:47 +0000

    Remove duplicate import

--- dev/scripts/src/scripts/addCopyrightNotice.ts
+++ dev/scripts/src/scripts/addCopyrightNotice.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { at } from '@prosopo/util'
-import { at } from '@prosopo/util'
 import { glob } from 'glob'
 import fs from 'fs'
 import path from 'path'
