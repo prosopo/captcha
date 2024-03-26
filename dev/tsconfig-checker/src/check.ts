28f5a1e36ae2b1fb1082a84fa666d1d477d92100
George Oastler <goastler4@gmail.com>
2024-03-26 18:18:29 +0000

    fixes for tsconfig checker script

--- dev/tsconfig-checker/src/check.ts
+++ dev/tsconfig-checker/src/check.ts
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { get } from '@prosopo/util'
+import { at, get } from '@prosopo/util'
 import fs from 'fs'
 import z from 'zod'
 import * as glob from 'glob';
+import path from 'path';
 
 const readPackageJson = (dir: string): JSON => {
     const pkgJsonStr = fs.readFileSync(`${dir}/package.json`, 'utf8')
@@ -28,27 +29,32 @@ const main = async () => {
     const workspaceDir = get(process.env, 'WORKSPACE_DIR')
     const fix = process.env.FIX // if fix is set, we'll fix the refs
 
+    process.chdir(packageDir)
+
     // get the list of packages in the workspace
     const workspacePkgJson = readPackageJson(workspaceDir)
     const workspacePkgGlobPaths = z.string().array().parse(get(workspacePkgJson, 'workspaces'))
-    console.log('workspacePkgGlobPaths', workspacePkgGlobPaths)
+    // console.log('workspacePkgGlobPaths', workspacePkgGlobPaths)
     // glob the workspace paths
     // i.e. packages/* => [packages/pkg1, packages/pkg2, ...]
-    const workspacePkgPaths = workspacePkgGlobPaths.map(p => glob.sync(`${workspaceDir}/${p}`)).reduce((acc, val) => acc.concat(val), []).filter(p => fs.lstatSync(p).isDirectory()).filter(p => fs.existsSync(`${p}/package.json`))
-    console.log('workspacePkgPaths', workspacePkgPaths)
+    const workspacePkgPaths = workspacePkgGlobPaths.map(p => glob.sync(`${workspaceDir}/${p}`)).reduce((acc, val) => acc.concat(val), []).filter(p => fs.lstatSync(p).isDirectory()).filter(p => fs.existsSync(`${p}/package.json`)).map(p => path.relative('.', p)).map(p => p === '' ? '.' : p)
+    // console.log('workspacePkgPaths', workspacePkgPaths)
     const workspacePkgNames = workspacePkgPaths.map(p => z.string().parse(get(readPackageJson(p), 'name')))
 
     // read the pkg json
     const pkgJson = readPackageJson(packageDir)
     
     // get the pkg json deps
-    const deps = z.string().array().parse(get(pkgJson, 'dependencies'))
-    const devDeps = z.string().array().parse(get(pkgJson, 'devDependencies'))
+    const deps = z.string().array().parse(Object.keys(get(pkgJson, 'dependencies')))
+    // TODO should we include dev deps?
+    const devDeps = [] as string[] // z.string().array().parse(Object.keys(get(pkgJson, 'devDependencies')))
     const allDeps = deps.concat(devDeps)
     // filter deps down to those present in the workspace
     const workspaceDeps = allDeps.filter(d => workspacePkgNames.includes(d))
+    const workspaceDepPaths = workspaceDeps.map(d => at(workspacePkgPaths, workspacePkgNames.indexOf(d)))
     
     console.log('workspaceDeps', workspaceDeps)
+    console.log('workspaceDepPaths', workspaceDepPaths)
 
     // read the tsconfig json
     const tsconfigJsonStr = fs.readFileSync(`${packageDir}/tsconfig.json`, 'utf8')
@@ -56,34 +62,48 @@ const main = async () => {
 
     // get the tsconfig references
     const refs = ((tsconfigJson.references || []) as { path: string }[]).map(r => r.path)
-    console.log('refs', refs)
+    const relRefs = refs.map(r => path.relative(packageDir, r))
+    console.log('tsconfig refs', refs)
+    // console.log('relRefs', relRefs)
 
     const missing = [] as string[]
-    // go through the refs, remove any deps already linked
-    for (const ref of refs) {
-        const refPkgJson = readPackageJson(`${packageDir}/${ref}`)
-        const pkgName = z.string().parse(get(pkgJson, 'name'))
-        if(!(workspaceDeps.includes(pkgName))) {
-            missing.push(pkgName)
+    const missingPaths = [] as string[]
+    const present = [] as string[]
+    const presentPaths = [] as string[]
+    // go through all the deps, record missing and present
+    for (const dep of workspaceDeps) {
+        // reverse lookup the path
+        const index = workspacePkgNames.indexOf(dep)
+        if (index < 0) {
+            throw new Error(`Could not find package ${dep} in workspace`)
+        }
+        const pth = at(workspacePkgPaths, index)
+        if (!relRefs.includes(pth)) {
+            missing.push(dep)
+            missingPaths.push(pth)
+        } else {
+            present.push(dep)
+            presentPaths.push(pth)
         }
     }
+
+    console.log('present', present)
+    console.log('missing', missing)
 
     if (missing.length > 0) {
         if (!fix) throw new Error(`Missing packages: ${missing.join(', ')}`)
         
-        // fix the refs. Go through the missing packages, build the ref, add it to the references, then write the tsconfig
-        // get the relative path from the package to the workspace
-        const relPath = workspaceDir.replace(packageDir, '.')
-        const missingRefs = missing.map(m => ({ path: `${relPath}/${m}` }))
+        // new refs = old refs + missing dep paths
         const newRefs = refs.map(p => {
             return { path: p }
-        }).concat(missingRefs)
-        console.log('newRefs', newRefs)
+        }).concat(missingPaths.map(p => {
+            return { path: p }
+        }))
+        console.log('new tsconfig refs', newRefs)
         tsconfigJson.references = newRefs
         const newTsconfigJsonStr = JSON.stringify(tsconfigJson, null, 4)
         fs.writeFileSync(`${packageDir}/tsconfig.json`, newTsconfigJsonStr)
     }
-
 }
 
 main()
