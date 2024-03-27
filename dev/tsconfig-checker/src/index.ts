1652bcfda2c662fe84fa7056662c8eb34439302e
George Oastler <goastler@users.noreply.github.com>
2024-03-27 11:09:41 +0000

    Tsconfig reference checker (#1127)
    * tsconfig checker
    
    * fixes for tsconfig checker script
    
    * lint
    
    * lint

--- dev/tsconfig-checker/src/index.ts
+++ dev/tsconfig-checker/src/index.ts
@@ -11,4 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-export const version = process.env.PROSOPO_PACKAGE_VERSION || 'dev'
+const main = async () => {
+    console.log('index.ts')
+}
+
+main()
