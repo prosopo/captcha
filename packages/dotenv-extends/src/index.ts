1996e008c61e0387fc8601b5eaa58d1db068f23b
George Oastler <goastler4@gmail.com>
2024-04-02 16:18:59 +0100

    fixes for env parsing

--- packages/dotenv-extends/src/index.ts
+++ packages/dotenv-extends/src/index.ts
@@ -1,28 +1,30 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import { env } from 'process';
+import fs from 'fs';
+import { s } from 'vitest/dist/reporters-P7C2ytIv.js';
 
 const parseFileList = (str: string): string[] => {
     // TODO json?
     return str.split(',').map((s) => s.trim());
 }
 
+// an env file can optionally inherit off other env file(s)
+// so when we load an env file, we need to load the inherited env files first, then the env file itself
+// if multiple env files are specified, we have to load them in order
+
 type Node = {
     envFile: string
     extends: Node[]
-    chains: Node[]
     origin: Node | null
 }
 
-const expandEnvFile = (envFilePath: string): string[] => {
+const getEnvFileGraph = (envFilePath: string): Node => {
     // the env hierarchy can be represented as a acyclic graph
     // extends links to nodes which get loaded before the env file
-    // chains links to nodes which get loaded after the env file
     // origin is the node which refers to this node
     const root: Node = {
         envFile: envFilePath,
         extends: [],
-        chains: [],
         origin: null,
     }
     const queue: Node[] = [root];
@@ -38,11 +40,14 @@ const expandEnvFile = (envFilePath: string): string[] => {
         while (origin !== null) {
             origins.push(origin);
             if (origin.envFile === node.envFile) {
-                throw new Error(`Circular env file reference detected: ${origins.join(` -> `)}`);
+                throw new Error(`Circular env file reference detected: ${origins.reverse().concat([origin]).map(n => n.envFile).join(` -> `)}`);
             }
             origin = origin.origin;
         }
         // read the env file
+        if (!fs.existsSync(node.envFile)) {
+            throw new Error(`Env file does not exist: ${node.envFile}`);
+        }
         const env: any = {};
         dotenv.config({
             path: node.envFile,
@@ -53,12 +58,16 @@ const expandEnvFile = (envFilePath: string): string[] => {
         if (ext !== undefined) {
             const extFiles = parseFileList(ext);
             // resolve the path to the extended file
-            const resolvedExtFiles = extFiles.map((f) => path.resolve(node.envFile, f));
+            const resolvedExtFiles = extFiles.map((f) => {
+                if(path.isAbsolute(f)) {
+                    return f;
+                }
+                return path.resolve(path.dirname(node.envFile), f)
+            });
             for (const extFile of resolvedExtFiles) {
                 const extNode: Node = {
                     envFile: extFile,
                     extends: [],
-                    chains: [],
                     origin: node,
                 }
                 node.extends.push(extNode);
@@ -66,77 +75,51 @@ const expandEnvFile = (envFilePath: string): string[] => {
                 queue.push(extNode);
             }
         }
-        // check if the file chainloads another file
-        const chain = env['CHAIN'];
-        if (chain !== undefined) {
-            const chainFiles = parseFileList(chain);
-            // resolve the path to the chainloaded file
-            const resolvedChainFiles = chainFiles.map((f) => path.resolve(node.envFile, f));
-            for (const chainFile of resolvedChainFiles) {
-                const chainNode: Node = {
-                    envFile: chainFile,
-                    extends: [],
-                    chains: [],
-                    origin: node,
-                }
-                node.chains.push(chainNode);
-                // add the node to the queue so we visit it in the future
-                queue.push(chainNode);
-            }
-        }
     }
-    // now we have a graph of env files
-    // we need to load them in order
-    const envFiles: string[] = [];
-    // find the first node
-    let node: Node | null = root;
-    while (node.extends.length > 0) {
-        node = node.extends[0]!;
-    }
-    // whether the node is an extension or a chain
-    let mode: 'extends' | 'chains' | 'self' = 'extends'
-    // what index we are at in the extends or chains array
-    let index = 0
-    // load the nodes in order
-    while (node !== null) {
-        envFiles.push(node.envFile);
-        index++;
-        const origin: Node | null = node.origin;
-        if (mode === 'extends') {
-            // we're in extends mode
-            if(index < origin!.extends.length) {
-                node = origin!.extends[index]!;
-            } else {
-                // we're done with the extends array
-                mode = 'self'
-                index = 0
-            }
-        } else if (mode === 'self') {
-            // we just added the envFile for this node
-            // so now examine the chained files
-            mode = 'chains'
-            index = 0
-            node = node.chains[0]!
-        } else if (mode === 'chains') {
-            // we're in chains mode
-            if (index < origin!.chains.length) {
-                node = origin!.chains[index]!;
-            } else {
-                // we're done with the chains array
-                mode = 'extends';
-                index = 0;
-                // we've processed all extends, the envFile, and the chains for this node. So go back up to the origin node
-                node = origin;
-            }
-        } else {
-            throw new Error('Unexpected mode'); // this should never happen
-        }
-    }
-
-    return envFiles;
+    return root
 }
 
-export default function config(args?: dotenv.DotenvConfigOptions): dotenv.DotenvConfigOutput {
+const nodeToStringArray = (node: Node): string[] => {
+    const strs = [node.envFile]
+    for (const ext of node.extends) {
+        strs.push(...nodeToStringArray(ext).map((s) => `\t${s}`));
+    }
+    return strs;
+}
+
+const nodeToString = (node: Node): string => {
+    return nodeToStringArray(node).join('\n');
+}
+
+const expandEnvFile = (envFilePath: string, args: Args): string[] => {
+    const root = getEnvFileGraph(envFilePath);
+    if (args.debug) {
+        console.log('env file graph\n', nodeToString(root));   
+    }
+
+    // we need to load them in order
+    // iterate over the graph in a depth first search
+    const envFiles: string[] = [];
+    const stack: Node[] = [root];
+    while (stack.length > 0) {
+        const node = stack.pop();
+        if (node === undefined) {
+            throw new Error('Unexpected undefined node'); // this should never happen
+        }
+        envFiles.push(node.envFile);
+        stack.push(...node.extends.reverse());
+    }
+
+    return envFiles.reverse();
+}
+
+export type Args = dotenv.DotenvConfigOptions & {
+
+}
+
+export default function config(args?: Args): {
+    [key: string]: string | undefined
+} {
     args = args || {};
     args.path = args.path || '.env';
 
@@ -148,17 +131,34 @@ export default function config(args?: dotenv.DotenvConfigOptions): dotenv.Dotenv
     }
 
     // expand the env files
-    // this looks for EXTENDS and CHAIN directives in the env files
+    // this looks for EXTENDS directives in the env files
     // and loads the files in the correct order
     const expandedEnvFiles: string[] = [];
     for (const envFile of envFiles) {
-        expandedEnvFiles.push(...expandEnvFile(envFile));
+        expandedEnvFiles.push(...expandEnvFile(envFile, { ...args, path: undefined }));
     }
 
-    // load the env files in order
-    
+    // load the env files in order into the process env / destination
+    const dest: {
+        [key: string]: string | undefined
+    } = {};
+    for (const envFile of expandedEnvFiles) {
+        dotenv.config({
+            ...args,
+            path: envFile,
+            processEnv: dest as dotenv.DotenvPopulateInput,
+            override: true,
+        });
+    }
 
-    
-    // TODO delete extends field
+    // delete extends field
+    delete dest['EXTENDS'];
 
+    // assign the destination to the process env
+    const env = args.processEnv || process.env;
+    for(const [key, value] of Object.entries(dest)) {
+        env[key] = value;
+    }
+
+    return dest
 }
