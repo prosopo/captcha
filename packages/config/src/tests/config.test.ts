d4b7f892ea93c8298a9474521d7b9343bd79157d
George Oastler <goastler4@gmail.com>
2024-04-04 12:19:25 +0100

    make a test config

--- packages/config/src/tests/config.test.ts
+++ packages/config/src/tests/config.test.ts
@@ -13,6 +13,12 @@
 // limitations under the License.
 import { describe, expect, test, it } from 'vitest'
 
+export type Config = {
+    a: boolean,
+    b: number,
+    c: string,
+}
+
 describe('config', () => {
     
     it('should load config from env', async () => {
