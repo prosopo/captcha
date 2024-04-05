8a65a3be8e9c5c0d6f36142cc070185fdf41cedb
George Oastler <goastler4@gmail.com>
2024-04-05 11:19:34 +0100

    between number refiner

---
+++ packages/parser/src/parsers/BetweenNumber.ts
@@ -0,0 +1,49 @@
+import { Refiner } from "./Refiner.js";
+
+export type Options = {
+        min?: number
+        max?: number
+        minInclusive?: boolean
+        maxInclusive?: boolean
+    }
+
+export class BetweenNumber extends Refiner<number> {
+
+    constructor(public options: Options) {
+        super()
+    }
+
+    public override refine(value: number): number {
+        if (this.options.min !== undefined) {
+            if(this.options.minInclusive) {
+                if(value < this.options.min) {
+                    throw new Error(`Value ${value} is less than the minimum ${this.options.min}`)
+                }
+            } else {
+                if(value <= this.options.min) {
+                    throw new Error(`Value ${value} is less than or equal to the minimum ${this.options.min}`)
+                }
+            }
+        }
+        if (this.options.max !== undefined) {
+            if(this.options.maxInclusive) {
+                if(value > this.options.max) {
+                    throw new Error(`Value ${value} is greater than the maximum ${this.options.max}`)
+                }
+            } else {
+                if(value >= this.options.max) {
+                    throw new Error(`Value ${value} is greater than or equal to the maximum ${this.options.max}`)
+                }
+            }
+        }
+        return value
+    }
+
+    public override clone(): BetweenNumber {
+        return new BetweenNumber({...this.options})
+    }
+}
+
+export const pBetweenNumber = (options: Options) => new BetweenNumber(options)
+export const betweenNumber = pBetweenNumber
+export const betweenNum = pBetweenNumber
