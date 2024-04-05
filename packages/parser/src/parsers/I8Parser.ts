4ff1139e4ca17e18143944dde28f4772107e6b5a
George Oastler <goastler4@gmail.com>
2024-04-05 14:07:55 +0100

    refactor i8 parser to use between refiners

--- packages/parser/src/parsers/I8Parser.ts
+++ packages/parser/src/parsers/I8Parser.ts
@@ -1,10 +1,16 @@
+import { betweenNum } from "./BetweenNumber.js"
 import { boundNum } from "./BoundNumberParser.js"
-import { custom, redefine } from "./CustomParser.js"
+import { custom, extend } from "./CustomParser.js"
 import { NumberParser, num } from "./NumberParser.js"
 import { Validator } from "./Parser.js"
 import { TypeofParser } from "./TypeofParser.js"
 
-const min = -Math.pow(2, 8) + (Math.pow(2, 8) / 2)
-const max = Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1
-export const pI8 = redefine(() => boundNum(min, max), "i8")
+export const pI8 = () => extend({
+    validator: num(),
+    name: "i8",
+    refiner: betweenNum({
+        min: -Math.pow(2, 8) + (Math.pow(2, 8) / 2),
+        max: Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1,
+    })
+})
 export const i8 = pI8
