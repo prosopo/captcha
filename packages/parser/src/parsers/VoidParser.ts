d570fda62ca747ce1e2bd6d247f4db40aeabcde6
George Oastler <goastler4@gmail.com>
2024-04-05 12:14:13 +0100

    fix void parser try catch usage

--- packages/parser/src/parsers/VoidParser.ts
+++ packages/parser/src/parsers/VoidParser.ts
@@ -3,13 +3,13 @@ import { nul } from "./NullParser.js"
 import { Validator } from "./Parser.js"
 import { undef } from "./UndefinedParser.js"
 import { or } from "./UnionParser.js"
-import { stringify } from "./utils.js"
+import { run, stringify } from "./utils.js"
 
 export class VoidParser extends Validator<void> {
 
     public override shape(value: unknown): void {
         // void === null or void === undefined
-        const [ok, result] = or(undef(), nul()).tryShape(value)
+        const { ok, result } = run(() => or(undef(), nul()).shape(value))
         if (!ok) {
             throw new Error(`Expected void but got ${stringify(value)}`)
         }
