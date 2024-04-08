6890d532bc192ba6fa3538f8695e85d3594c4387
Chris Taylor <forgetso86@gmail.com>
2024-04-08 11:00:26 +0100

    Fix test config

--- packages/cli/vite.test.config.ts
+++ packages/cli/vite.test.config.ts
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { defineConfig } from 'vitest/config'
+import { ViteTestConfig } from '@prosopo/config'
 import dotenv from 'dotenv'
 import fs from 'fs'
 import path from 'path'
@@ -29,8 +29,4 @@ if (fs.existsSync(envFile)) {
 
 dotenv.config({ path: envPath })
 
-export default defineConfig({
-    test: {
-        environment: 'jsdom',
-    },
-})
+export default ViteTestConfig
