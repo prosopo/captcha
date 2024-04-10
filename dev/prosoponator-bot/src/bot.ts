585fde8a53866e005f02bbfa79b9ebf2f4c149a6
George Oastler <goastler4@gmail.com>
2024-04-10 08:08:00 +0100

    lint

--- dev/prosoponator-bot/src/bot.ts
+++ dev/prosoponator-bot/src/bot.ts
@@ -1,72 +1,85 @@
+// Copyright 2021-2024 Prosopo (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 async function disapprove(args: string[]) {
     console.log('disapprove')
-    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || '';
-    const octokit = github.getOctokit(token);
-    console.log('reacting');
+    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || ''
+    const octokit = github.getOctokit(token)
+    console.log('reacting')
     octokit.rest.reactions.createForIssueComment({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         comment_id: github.context.payload.comment!.id,
-        content: '+1'
-    });
+        content: '+1',
+    })
     console.log('disapproving')
     octokit.rest.pulls.createReview({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         pull_number: github.context.payload.issue!.number,
         event: 'REQUEST_CHANGES',
-    });
+    })
     console.log('done')
 }
 
 async function approve(args: string[]) {
     console.log('approve')
-    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || '';
-    const octokit = github.getOctokit(token);
-    console.log('reacting');
+    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || ''
+    const octokit = github.getOctokit(token)
+    console.log('reacting')
     octokit.rest.reactions.createForIssueComment({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         comment_id: github.context.payload.comment!.id,
-        content: '+1'
-    });
+        content: '+1',
+    })
     console.log('approving')
     octokit.rest.pulls.createReview({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         pull_number: github.context.payload.issue!.number,
         event: 'APPROVE',
-    });
+    })
     console.log('done')
 }
 
 async function help(args: string[]) {
     console.log('help')
-    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || '';
-    const octokit = github.getOctokit(token);
+    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || ''
+    const octokit = github.getOctokit(token)
     octokit.rest.issues.createComment({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         issue_number: github.context.payload.issue!.number,
-        body: `My commands are: ${Object.keys(commands).sort().join(', ')}`
-    });
+        body: `My commands are: ${Object.keys(commands).sort().join(', ')}`,
+    })
     console.log('done')
 }
 
 async function usage(args: string[]) {
     console.log('usage')
-    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || '';
-    const octokit = github.getOctokit(token);
-    console.log('reacting');
+    const token = core.getInput('github-token') || process.env.GITHUB_TOKEN || process.env.GH_TOKEN || ''
+    const octokit = github.getOctokit(token)
+    console.log('reacting')
     octokit.rest.reactions.createForIssueComment({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         comment_id: github.context.payload.comment!.id,
-        content: 'confused'
-    });
+        content: 'confused',
+    })
     console.log('done')
 }
 
@@ -76,8 +89,8 @@ const commands: {
     disapprove,
     approve,
     help,
-    'accept': approve,
-    'reject': disapprove,
+    accept: approve,
+    reject: disapprove,
 }
 
 const tag = 'prosoponator'
@@ -88,7 +101,7 @@ const tag = 'prosoponator'
  */
 async function run() {
     if (github.context.eventName !== 'issue_comment') {
-        console.log('This event is not a comment.');
+        console.log('This event is not a comment.')
         return
     }
 
@@ -99,18 +112,21 @@ async function run() {
     }
 
     const body = comment['body'] as string
-    const words = body.split(' ').map(word => word.trim()).filter(word => word.length > 0)
-    if(words.length === 0) {
+    const words = body
+        .split(' ')
+        .map((word) => word.trim())
+        .filter((word) => word.length > 0)
+    if (words.length === 0) {
         console.log('No words found in comment')
         return
     }
     const target = words[0]
-    if(target !== `@${tag}`) {
+    if (target !== `@${tag}`) {
         console.log('Bot not tagged in comment')
         return
     }
     const command = words[1]
-    if(command === undefined) {
+    if (command === undefined) {
         console.log('No command found in comment')
         return
     }
@@ -119,7 +135,7 @@ async function run() {
     console.log('args', args)
 
     const fn = commands[command]
-    if(fn === undefined) {
+    if (fn === undefined) {
         console.log('Command not found')
         await usage(args)
         return
@@ -127,7 +143,7 @@ async function run() {
     await fn(args)
 }
 
-run().catch(error => {
+run().catch((error) => {
     console.error(error)
     core.setFailed(error.message)
 })
