db202bc4ac55791e28e6bd8015f9bb5d86f0614b
George Oastler <goastler4@gmail.com>
2024-04-11 17:49:02 +0100

    reinstantiate i8 validator using pipe

--- packages/parser/src/parsers/I8Parser.ts
+++ packages/parser/src/parsers/I8Parser.ts
@@ -1,16 +1,16 @@
-// import { betweenNum } from "./BetweenNumber.js"
-// import { boundNum } from "./BoundNumberParser.js"
-// import { custom, extend } from "./CustomParser.js"
-// import { NumberParser, num } from "./NumberParser.js"
-// import { Validator } from "./Parser.js"
-// import { TypeofParser } from "./TypeofParser.js"
+import { betweenNum } from "./BetweenNumber.js"
+import { num } from "./NumberParser.js"
+import { pipe } from "./PipeValidator.js"
 
-// export const pI8 = () => extend({
-//     validator: num(),
-//     name: "i8",
-//     refiner: betweenNum({
-//         min: -Math.pow(2, 8) + (Math.pow(2, 8) / 2),
-//         max: Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1,
-//     })
-// })
-// export const i8 = pI8
+export const pI8 = () => pipe([
+    num(),
+    betweenNum({
+        min: -Math.pow(2, 8) + (Math.pow(2, 8) / 2),
+        max: Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1,
+    })
+], `i8`)
+export const i8 = pI8
+
+const a = i8()
+type b = ReturnType<typeof a.validate>
+type c = Parameters<typeof a.validate>[0]
