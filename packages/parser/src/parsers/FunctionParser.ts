df52828d7ee00253841742ddd1be581a3cf6f9e2
George Oastler <goastler4@gmail.com>
2024-04-12 13:16:21 +0100

    varargs support

--- packages/parser/src/parsers/FunctionParser.ts
+++ packages/parser/src/parsers/FunctionParser.ts
@@ -1,4 +1,9 @@
+import { BigIntParser } from "./BigIntParser.js"
+import { bool } from "./BooleanParser.js"
+import { NeverParser } from "./NeverParser.js"
+import { NumberParser, num } from "./NumberParser.js"
 import { InferOutput, ValidateOptions, Validator } from "./Parser.js"
+import { StringParser, str } from "./StringParser.js"
 
 // // enables parsing at function entry and exit points. I.e. validate the args of a fn and/or the return value of a fn
 // export class FunctionParser<T extends Validator<any>[], U> {
@@ -25,51 +30,89 @@ import { InferOutput, ValidateOptions, Validator } from "./Parser.js"
 
 // TODO finish this off
 
-export type Args<T> = T extends [] ? [] : T extends [Validator<infer I, unknown>] ? [I] : T extends [Validator<infer I, unknown>, ...infer R] ? [I, ...Args<R>] : never
-type a = Args<[Validator<string, unknown>, Validator<number, unknown>]>
-type b = Args<[]>
-type c = Args<[Validator<string, unknown>]>
-type d = Args<[Validator<string, unknown>, Validator<number, unknown>, Validator<boolean, unknown>]>
+export type Args<T> = T extends readonly [] ? [] : T extends readonly [infer U] ? [InferOutput<U>] : T extends readonly [infer U, ...infer R] ? [InferOutput<U>, ...Args<R>] : never
+const x1 = [str(), bool(), num()] as const
+type x2 = typeof x1
+type x3 = Args<x2>
 export type Return<T> = InferOutput<T>
-export type Fn<I, O> = Args<I> extends [] ? (...args: Args<I>) => Return<O> : never
+export type Fn<I, O, V> = InferOutput<V> extends never ? (...args: Args<I>) => Return<O> : (...args: [...Args<I>, ...InferOutput<V>[]]) => Return<O>
+type y1 = Fn<x2, StringParser, BigIntParser>
+type y2 = Fn<x2, StringParser, undefined>
+type y3 = InferOutput<undefined>
+type y4 = InferOutput<BigIntParser>
+type y5 = bigint extends never ? 1 : 2
 
-export class FunctionValidator<const I extends Validator<unknown, unknown>[], O extends Validator<unknown, unknown>> extends Validator<unknown, Fn<I, O>> {
-    constructor(private _args: I, private _returnType: O) {
+export class FunctionValidator<const I extends Validator<unknown, unknown>[], O extends Validator<unknown, unknown>, V extends Validator<unknown, unknown> | undefined> extends Validator<unknown, Fn<I, O, V>> {
+    constructor(private _args: I, private _returnType: O, private _varargs?: V) {
         super()
         this._args = this.args // defensive clone
+        // TODO fix
+        // this._returnType = this.returnType.clone() // defensive clone
+        // this._varargs = this.varargs.clone() // defensive clone
     }
 
-    get args() {
+    get args(): I {
         return this._args.map(v => v.clone()) as I
     }
 
-    get returnType() {
-        return this._returnType.clone()
+    get returnType(): O {
+        return this._returnType.clone() as O
     }
 
-    public override validate(value: unknown, options?: ValidateOptions): Fn<I, O> {
+    get varargs() {
+        if (this._varargs === undefined) {
+            return undefined
+        }
+        return this._varargs.clone() as V
+    }
+
+    public override validate(value: unknown, options?: ValidateOptions): Fn<I, O, V> {
         return ((...args: unknown[]) => {
-            if (args.length !== this.args.length) {
-                throw new Error(`Expected ${this.args.length} arguments, got ${args.length}`)
+            if (this._varargs === undefined) {
+                // no varargs, so expect exact number of args
+                if (args.length !== this.args.length) {
+                    throw new Error(`Expected ${this.args.length} arguments, got ${args.length}`)
+                }
             }
-            for (let i = 0; i < args.length; i++) {
+            // first n args are required
+            for (let i = 0; i < this._args.length; i++) {
                 const validator = this.args[i]
                 if (validator === undefined) {
                     throw new Error(`this should never happen`)
                 }
                 args[i] = validator.validate(args[i], options)
             }
+            // varargs are optional
+            if (this._varargs !== undefined) {
+                for (let i = this.args.length; i < args.length; i++) {
+                    if (this._varargs === undefined) {
+                        throw new Error(`this should never happen`)   
+                    }
+                    args[i] = this._varargs.validate(args[i], options)
+                }
+            }
             return this.returnType.validate(value, options)
-        }) as unknown as Fn<I, O>
+        }) as unknown as Fn<I, O, V>
     }
 
     public override clone() {
-        return new FunctionValidator(this.args, this.returnType)
+        return new FunctionValidator(this.args, this.returnType, this.varargs)
     }
 
     public override get name(): string {
-        return `(${this.args.map(v => v.name).join(", ")}) => ${this.returnType.name}`
+        const varargsStr = this._varargs === undefined ? "" : `, ...${this._varargs.name}`
+        return `(${this.args.map(v => v.name).join(", ")}${varargsStr}) => ${this.returnType.name}`
     }
 }
 
-export const pFunction = <const I extends Validator<unknown, unknown>[], O extends Validator<unknown, unknown>>(args: I, returnType: O) => new FunctionValidator<I, O>(args, returnType)
+export const fn = <const I extends Validator<unknown, unknown>[], O extends Validator<unknown, unknown>, V extends Validator<unknown, unknown> | undefined>(args: I, returnType: O, varargs?: V) => new FunctionValidator<I, O, V>(args, returnType, varargs)
+
+const a1 = fn([str(), num(), bool()], str())
+type a2 = ReturnType<typeof a1.validate>
+const a3 = fn([str(), num(), bool()], str(), str())
+type a4 = ReturnType<typeof a3.validate>
+const a5 = fn([], str())
+type a6 = ReturnType<typeof a5.validate>
+const a7 = fn([], str(), bool())
+type a8 = ReturnType<typeof a7.validate>
+const a8 = a3.validate(null)(`hello`, 1, true, 'a', 'b', 'c')
