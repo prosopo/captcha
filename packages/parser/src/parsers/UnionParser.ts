b90fa0e7c61198342f6427aad182a0c05ac08783
George Oastler <goastler4@gmail.com>
2024-04-12 14:55:51 +0100

    union validator as a class

--- packages/parser/src/parsers/UnionParser.ts
+++ packages/parser/src/parsers/UnionParser.ts
@@ -1,45 +1,56 @@
-// import { BooleanParser, bool } from "./BooleanParser.js"
-// import { never } from "./NeverParser.js"
-// import { NumberParser, num } from "./NumberParser.js"
-// import { Validator, Shape } from "./Parser.js"
-// import { StringParser, str } from "./StringParser.js"
+import { InferArrayInput, InferArrayOutput, InferInput, InferOutput, Validator } from "./Parser.js"
+import { Eq, EqArray, EqMany, First } from "./utils.js"
 
-// export type UnionParserArray<T> = T extends [Validator<infer A>, ...infer B] ? A | UnionParserArray<B> : never
 
-// export class UnionParser<const T extends Validator<any>[]> extends Validator<UnionParserArray<T>> {
+export type UnionInput<T> = EqMany<InferArrayInput<T>> extends true ? InferInput<First<T>> : never
+type a1 = UnionInput<[Validator<string, unknown>, Validator<number, unknown>]>
+type a2 = UnionInput<[Validator<string, unknown>, Validator<string, unknown>]>
+type a3 = UnionInput<[Validator<string, unknown>, Validator<string, unknown>, Validator<string, unknown>]>
+type a4 = UnionInput<[Validator<string, unknown>]>
+type a5 = UnionInput<[]>
+export type UnionOutput<T> = T extends [Validator<unknown, infer A>, ...infer B] ? A | UnionOutput<B> : never
+type b1 = UnionOutput<[Validator<unknown, number>, Validator<unknown, string>, Validator<unknown, boolean>]>
+type b2 = UnionOutput<[Validator<unknown, number>, Validator<unknown, string>]>
+type b3 = UnionOutput<[Validator<unknown, number>]>
+type b4 = UnionOutput<[]>
+type b5 = UnionOutput<[Validator<unknown, number>, Validator<unknown, number>]>
 
-//     constructor(private _parsers: T) {
-//         super()
-//         this._parsers = this.parsers // clone parsers
-//     }
+export class UnionParser<const T extends Validator<unknown, unknown>[]> extends Validator<UnionInput<T>, UnionOutput<T>> {
 
-//     get parsers() {
-//         return this._parsers.map(parser => parser.clone()) as T
-//     }
+    constructor(private _parsers: T) {
+        super()
+        this._parsers = this.parsers // defensive clone
+    }
 
-//     public override validate(value: unknown): UnionParserArray<T> {
-//         if(this._parsers.length == 0) throw new Error("No parsers provided to union, cannot parse value")
-//         const errors: unknown[] = []
-//         for (const parser of this._parsers) {
-//             try {
-//                 return parser.validate(value)
-//             } catch (error) {
-//                 errors.push(error)
-//             }
-//         }
-//         throw new Error(`Expected one of the following types but got ${JSON.stringify(value, null, 2)} of type ${JSON.stringify(typeof value, null, 2)}: ${errors.map(error => String(error) ).join("\n")}`)
-//     }
+    get parsers() {
+        return this._parsers.map(parser => parser.clone()) as T
+    }
 
-//     public override clone() {
-//         return new UnionParser<T>(this._parsers)
-//     }
+    public override validate(value: unknown): UnionOutput<T> {
+        // this shouldn't happen because the output of a union is never if the array of validators is empty
+        if(this._parsers.length == 0) throw new Error("No parsers provided to union, cannot parse value")
+        const errors: unknown[] = []
+        for (const parser of this._parsers) {
+            // attempt to validate with each parser
+            try {
+                return parser.validate(value) as UnionOutput<T>
+            } catch (error) {
+                errors.push(error)
+            }
+        }
+        throw new Error(`Expected one of the following types but got ${JSON.stringify(value, null, 2)} of type ${JSON.stringify(typeof value, null, 2)}: ${errors.map(error => String(error) ).join("\n")}`)
+    }
 
-//     public override get name(): string {
-//         return `(${this._parsers.map(parser => parser.name).join(" | ")})`
-//     }
-// }
+    public override clone() {
+        return new UnionParser<T>(this._parsers)
+    }
 
-// export const pUnion = <const T extends Validator<any>[]>(parsers: T) => new UnionParser<T>(parsers)
-// export const union = pUnion
-// export const pOr = <T extends Validator<any>, U extends Validator<any>>(parser1: T, parser2: U) => new UnionParser([parser1, parser2])
-// export const or = pOr
+    public override get name(): string {
+        return `${this._parsers.map(parser => parser.name).join(" | ")}`
+    }
+}
+
+export const pUnion = <const T extends Validator<unknown, unknown>[]>(parsers: T) => new UnionParser<T>(parsers)
+export const union = pUnion
+export const pOr = <T extends Validator<unknown, unknown>, U extends Validator<unknown, unknown>>(parser1: T, parser2: U) => new UnionParser([parser1, parser2])
+export const or = pOr
--- packages/parser/src/tests/union.test.ts
+++ packages/parser/src/tests/union.test.ts
@@ -1,50 +1,50 @@
-// import { describe, expect, test, it, expectTypeOf, assertType } from 'vitest'
-// import { bool } from '../parsers/BooleanParser.js';
-// import { num } from '../parsers/NumberParser.js';
-// import { or, union } from '../parsers/UnionParser.js';
-// import { str } from '../parsers/StringParser.js';
+import { describe, expect, test, it, expectTypeOf, assertType } from 'vitest'
+import { bool } from '../parsers/BooleanParser.js';
+import { num } from '../parsers/NumberParser.js';
+import { or, union } from '../parsers/UnionParser.js';
+import { str } from '../parsers/StringParser.js';
 
-// describe("union", () => {
-//     it("should have correct typename", () => {
-//         expect(union([num(), str(), bool()]).name).toBe(`(${num().name} | ${str().name} | ${bool().name})`)
-//     })
+describe("union", () => {
+    it("should have correct typename", () => {
+        expect(union([num(), str(), bool()]).name).toBe(`${num().name} | ${str().name} | ${bool().name}`)
+    })
 
-//     it("should error on no parsers", () => {
-//         expect(() => union([]).validate(null)).toThrow();
-//     })
+    it("should error on no parsers", () => {
+        expect(() => union([]).validate(null)).toThrow();
+    })
 
-//     it("should return type never on no parsers", () => {
-//         expectTypeOf(() => union([]).validate(null)).returns.toBeNever();
-//         const parser = union([]);
-//         const a: never = null!
-//         const b: ReturnType<typeof parser.validate> = a;
-//     })
+    it("should return type never on no parsers", () => {
+        expectTypeOf(() => union([]).validate(null)).returns.toBeNever();
+        const parser = union([]);
+        const a: never = null!
+        const b: ReturnType<typeof parser.validate> = a;
+    })
 
-//     it("should return union of types", () => {
-//         assertType<(value: unknown) => number | string | boolean>(union([num(), str(), bool()]).validate)
-//         const parser = union([num(), str(), bool()]);
-//         const a: number = 1
-//         const b: ReturnType<typeof parser.validate> = a;
-//         const c: string = ""
-//         const d: ReturnType<typeof parser.validate> = c;
-//         const e: boolean = true
-//         const f: ReturnType<typeof parser.validate> = e;
-//     })
+    it("should return union of types", () => {
+        assertType<(value: unknown) => number | string | boolean>(union([num(), str(), bool()]).validate)
+        const parser = union([num(), str(), bool()]);
+        const a: number = 1
+        const b: ReturnType<typeof parser.validate> = a;
+        const c: string = ""
+        const d: ReturnType<typeof parser.validate> = c;
+        const e: boolean = true
+        const f: ReturnType<typeof parser.validate> = e;
+    })
 
-//     it("should parse number, string, or boolean", () => {
-//         expect(union([num(), str(), bool()]).validate(1)).toBe(1);
-//         expect(union([num(), str(), bool()]).validate("")).toBe("");
-//         expect(union([num(), str(), bool()]).validate(true)).toBe(true);
-//     })
+    it("should parse number, string, or boolean", () => {
+        expect(union([num(), str(), bool()]).validate(1)).toBe(1);
+        expect(union([num(), str(), bool()]).validate("")).toBe("");
+        expect(union([num(), str(), bool()]).validate(true)).toBe(true);
+    })
 
-//     it("should error on incorrect type", () => {
-//         expect(() => union([num(), str(), bool()]).validate({})).toThrow();
-//         expect(() => union([num(), str(), bool()]).validate(undefined)).toThrow();
-//         expect(() => union([num(), str(), bool()]).validate(null)).toThrow();
-//         expect(() => union([num(), str(), bool()]).validate([])).toThrow();
-//     })
+    it("should error on incorrect type", () => {
+        expect(() => union([num(), str(), bool()]).validate({})).toThrow();
+        expect(() => union([num(), str(), bool()]).validate(undefined)).toThrow();
+        expect(() => union([num(), str(), bool()]).validate(null)).toThrow();
+        expect(() => union([num(), str(), bool()]).validate([])).toThrow();
+    })
 
-//     it("should error on regex", () => {
-//         expect(() => union([num(), str(), bool()]).validate(/./)).toThrow();
-//     })
-// });
+    it("should error on regex", () => {
+        expect(() => union([num(), str(), bool()]).validate(/./)).toThrow();
+    })
+});
