9f69944909df098e0eff30dbcd56823c3ca7ad45
George Oastler <goastler4@gmail.com>
2024-04-12 09:43:25 +0100

    make i8 validator use a class

--- packages/parser/src/parsers/I8Parser.ts
+++ packages/parser/src/parsers/I8Parser.ts
@@ -1,16 +1,62 @@
 import { betweenNum } from "./BetweenNumber.js"
 import { num } from "./NumberParser.js"
-import { pipe } from "./PipeValidator.js"
+import { ValidateOptions, Validator } from "./Parser.js"
+import { PipeValidator, pipe } from "./PipeValidator.js"
+import { unknown } from "./UnknownParser.js"
 
-export const pI8 = () => pipe([
-    num(),
-    betweenNum({
-        min: -Math.pow(2, 8) + (Math.pow(2, 8) / 2),
-        max: Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1,
-    })
-], `i8`)
+
+export class I8Validator extends Validator<unknown, number> {
+    public override validate(value: unknown, options?: ValidateOptions | undefined): number {
+        return pipe([
+            num(),
+            betweenNum({
+                min: -Math.pow(2, 8) + (Math.pow(2, 8) / 2),
+                max: Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1,
+            })
+        ], this.name).validate(value, options)
+    }
+
+    public override clone(): Validator<unknown, number> {
+        return new I8Validator()
+    }
+
+    public override get name(): string {
+        return `i8`
+    }
+}
+
+export const pI8 = () => new I8Validator()
 export const i8 = pI8
 
-const a = i8()
-type b = ReturnType<typeof a.validate>
-type c = Parameters<typeof a.validate>[0]
+// export const pI8 = () => {
+//     const pipeline = [
+//         num(),
+//         betweenNum({
+//             min: -Math.pow(2, 8) + (Math.pow(2, 8) / 2),
+//             max: Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1,
+//         })
+//     ] as const
+//     type t1 = typeof pipeline
+//     type t2 = () => t1
+//     const a = pipe(pipeline, `i8`)
+//     // class I8Validator extends PipeValidator<typeof pipeline> {
+//     //     constructor() {
+//     //         super(pipeline, `i8`)
+//     //     }
+//     // }    
+//     // return new I8Validator()
+// }
+// export const i8 = pI8
+
+// const fn = () => pipe([
+//     num(),
+//     betweenNum({
+//         min: -Math.pow(2, 8) + (Math.pow(2, 8) / 2),
+//         max: Math.pow(2, 8) - (Math.pow(2, 8) / 2) - 1,
+//     })
+// ], `i8`)
+// export type I8Validator = ReturnType<typeof fn>
+// const fn2: () => I8Validator = fn
+
+// export const pI8: () => I8Validator = fn2
+// export const i8: () => I8Validator = pI8
