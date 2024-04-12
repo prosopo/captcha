cebd08e18a0f90e39bdf2e9519132bf137af74b6
George Oastler <goastler4@gmail.com>
2024-04-12 15:14:39 +0100

    convert set validator impl to class based

--- packages/parser/src/parsers/SetParser.ts
+++ packages/parser/src/parsers/SetParser.ts
@@ -1,36 +1,35 @@
-// import { inst } from "./InstanceParser.js"
-// import { Validator, Shape } from "./Parser.js"
+import { inst } from "./InstanceParser.js"
+import { Validator, Shape, InferInput, InferOutput } from "./Parser.js"
 
-// export class SetParser<T extends Validator<any>> extends Validator<{
-//     output: Set<Shape<T>>
-// }> {
-//     constructor(private _parser: T) {
-//         super()
-//         this._parser = this.parser // clone parser
-//     }
+export class SetParser<T extends Validator<unknown, unknown>> extends Validator<InferInput<T>, Set<InferOutput<T>>> {
+    constructor(private _parser: T) {
+        super()
+        this._parser = this.parser // clone parser
+    }
 
-//     get parser() {
-//         return this._parser.clone() as T
-//     }
+    get parser() {
+        return this._parser.clone() as T
+    }
 
-//     public override validate(value: unknown): Set<Shape<T>> {
-//         const valueSet = inst(Set).validate(value)
-//         const result = new Set<Shape<T>>()
-//         for (const value of valueSet) {
-//             // parse every value to ensure they are of the correct type
-//             result.add(this._parser.validate(value))
-//         }
-//         return result
-//     }
+    public override validate(value: InferInput<T>): Set<InferOutput<T>> {
+        const valueSet = inst(Set).validate(value)
+        const result = new Set<InferOutput<T>>()
+        for (const value of valueSet) {
+            // validate every value to ensure they are of the correct type
+            const validated = this._parser.validate(value) as InferOutput<T>
+            result.add(validated)
+        }
+        return result
+    }
 
-//     public override clone() {
-//         return new SetParser<T>(this._parser)
-//     }
+    public override clone() {
+        return new SetParser<T>(this._parser)
+    }
 
-//     public override get name(): string {
-//         return `Set<${this._parser.name}>`
-//     }
-// }
+    public override get name(): string {
+        return `Set<${this._parser.name}>`
+    }
+}
 
-// export const pSet = <T extends Validator<any>>(parser: T) => new SetParser(parser)
-// export const set = pSet
+export const pSet = <T extends Validator<unknown, unknown>>(parser: T) => new SetParser(parser)
+export const set = pSet
