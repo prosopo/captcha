1c8f9d957b887c0e4fea324e40ebc8a0fe96aef8
George Oastler <goastler4@gmail.com>
2024-04-12 09:54:00 +0100

    convert uX validators to classes

--- packages/parser/src/parsers/U128Parser.ts
+++ packages/parser/src/parsers/U128Parser.ts
@@ -1,10 +1,27 @@
-// import { boundBi } from "./BoundBigIntParser.js"
-// import { boundNum } from "./BoundNumberParser.js"
-// import { redefine } from "./CustomParser.js"
-// import { NumberParser, num } from "./NumberParser.js"
-// import { Validator } from "./Parser.js"
-// import { TypeofParser } from "./TypeofParser.js"
+import { betweenBi } from "./BetweenBigInt.js"
+import { bi } from "./BigIntParser.js"
+import { ValidateOptions, Validator } from "./Parser.js"
+import { pipe } from "./PipeValidator.js"
 
-// const max = BigInt(2) ** BigInt(128) - BigInt(1)
-// export const pU128 = redefine(() => boundBi(0, max), "u128")
-// export const u128 = pU128
+export class U128Validator extends Validator<unknown, bigint> {
+    public override validate(value: unknown, options?: ValidateOptions | undefined): bigint {
+        return pipe([
+            bi(),
+            betweenBi({
+                min: 0n,
+                max: 2n**128n - 1n,
+            })
+        ], this.name).validate(value, options)
+    }
+
+    public override clone() {
+        return new U128Validator()
+    }
+
+    public override get name(): string {
+        return `u128`
+    }
+}
+
+export const pU128 = () => new U128Validator()
+export const u128 = pU128
--- packages/parser/src/parsers/U32Parser.ts
+++ packages/parser/src/parsers/U32Parser.ts
@@ -1,9 +1,29 @@
-// import { boundNum } from "./BoundNumberParser.js"
-// import { redefine } from "./CustomParser.js"
-// import { NumberParser, num } from "./NumberParser.js"
-// import { Validator } from "./Parser.js"
-// import { TypeofParser } from "./TypeofParser.js"
+import { betweenBi } from "./BetweenBigInt.js"
+import { betweenNum } from "./BetweenNumber.js"
+import { bi } from "./BigIntParser.js"
+import { num } from "./NumberParser.js"
+import { ValidateOptions, Validator } from "./Parser.js"
+import { pipe } from "./PipeValidator.js"
 
-// const max = Math.pow(2, 32) - 1
-// export const pU32 = redefine(() => boundNum(0, max), "u32")
-// export const u32 = pU32
+export class U32Validator extends Validator<unknown, number> {
+    public override validate(value: unknown, options?: ValidateOptions | undefined): number {
+        return pipe([
+            num(),
+            betweenNum({
+                min: 0,
+                max: 2**32 - 1,
+            })
+        ], this.name).validate(value, options)
+    }
+
+    public override clone() {
+        return new U32Validator()
+    }
+
+    public override get name(): string {
+        return `u32`
+    }
+}
+
+export const pU32 = () => new U32Validator()
+export const u32 = pU32
