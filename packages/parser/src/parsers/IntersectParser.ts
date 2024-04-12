13df099de9f7eea6ac01385e8a0c936b81ea6d59
George Oastler <goastler4@gmail.com>
2024-04-12 16:16:03 +0100

    intersection impl

--- packages/parser/src/parsers/IntersectParser.ts
+++ packages/parser/src/parsers/IntersectParser.ts
@@ -1,49 +1,38 @@
-// import { num } from "./NumberParser.js"
-// import { Validator } from "./Parser.js"
-// import { str } from "./StringParser.js"
-// import { Resolve, UnionToIntersection } from "./utils.js"
+import { Validator } from "./Parser.js"
 
-// export type ParserArrayToShapeUnion<T> = T extends [] ? never : T extends [Validator<infer A>, ...infer B] ? A | ParserArrayToShapeUnion<B> : never
-// export type IntersectParserArray<T> = UnionToIntersection<ParserArrayToShapeUnion<T>>
+export class IntersectValidator<const T extends Validator<unknown, unknown>[]> extends Validator<unknown, unknown> {
+    constructor(private _validators: T) {
+        super()
+        this._validators = this.validators
+    }
 
-// export class IntersectParser<const T extends Validator<any>[]> extends Validator<IntersectParserArray<T>> {
+    get validators() {
+        return this._validators.map(v => v.clone()) as unknown as T
+    }
 
-//     constructor(private _parsers: T) {
-//         super()
-//         this._parsers = this.parsers // clone parsers
-//     }
+    public override validate(value: unknown): unknown {
+        let result = value as unknown
+        // validate in turn
+        for (const validator of this.validators) {
+            // TODO value must satisfy all validators
+            // TODO do multiple passes to ensure that the value is still valid after each validator, as value may be modified
+            result = validator.validate(result)
+        }
+        return result
+    }
 
-//     get parsers() {
-//         return this._parsers.map(parser => parser.clone()) as T
-//     }
+    public override clone() {
+        return new IntersectValidator(this._validators)
+    }
 
-//     public override validate(value: unknown): IntersectParserArray<T> {
-//         if(this._parsers.length == 0) throw new Error("No parsers provided to intersect, cannot parse value")
-//         const errors: unknown[] = []
-//         for (const parser of this._parsers) {
-//             // should parse in all parsers
-//             value = parser.validate(value)
-//         }
-//         // do it twice to ensure the value returned by the last parser is still accepted by the first
-//         // i.e. make sure none of the parsers have side effects which mutate the value to a point where it is no longer accepted any previous parsers
-//         for (const parser of this._parsers) {
-//             // should parse in all parsers
-//             value = parser.validate(value)
-//         }
-//         return value as IntersectParserArray<T>
-//     }
+    public override get name(): string {
+        return this.validators.map(v => v.name).join(" & ")
+    }
+}
 
-//     public override clone() {
-//         return new IntersectParser<T>(this._parsers)
-//     }
 
-//     public override get name(): string {
-//         return this._parsers.map(parser => parser.name).join(" & ")
-//     }
-// }
-
-// export const pIntersect = <const T extends Validator<any>[]>(parsers: T) => new IntersectParser<T>(parsers)
-// export const intersect = pIntersect
-// export const intersection = pIntersect
-// export const pAnd = <T extends Validator<any>, U extends Validator<any>>(a: T, b: U) => pIntersect([a, b])
-// export const and = pAnd
+export const pIntersect = <const T extends Validator<unknown, unknown>[]>(validators: T) => new IntersectValidator<T>(validators)
+export const intersect = pIntersect
+export const intersection = pIntersect
+export const pAnd = <T extends Validator<unknown, unknown>, U extends Validator<unknown, unknown>>(a: T, b: U) => pIntersect([a, b])
+export const and = pAnd
