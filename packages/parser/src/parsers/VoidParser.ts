cb7cdabf0e57d00769dd86643784a51d2833a9c4
George Oastler <goastler4@gmail.com>
2024-04-12 14:56:55 +0100

    void validator as class impl

--- packages/parser/src/parsers/VoidParser.ts
+++ packages/parser/src/parsers/VoidParser.ts
@@ -1,32 +1,30 @@
-// import { LiteralParser } from "./LiteralParser.js"
-// import { nul } from "./NullParser.js"
-// import { Validator } from "./Parser.js"
-// import { undef } from "./UndefinedParser.js"
-// import { or } from "./UnionParser.js"
-// import { run, stringify } from "./utils.js"
+import { LiteralParser } from "./LiteralParser.js"
+import { nul } from "./NullParser.js"
+import { Validator } from "./Parser.js"
+import { undef } from "./UndefinedParser.js"
+import { or } from "./UnionParser.js"
+import { run, stringify } from "./utils.js"
 
-// export class VoidParser extends Validator<{
-//     output: void
-// }> {
+export class VoidParser extends Validator<unknown, void> {
 
-//     public override validate(value: unknown): void {
-//         // void === null or void === undefined
-//         const { ok, result } = run(() => or(undef(), nul()).validate(value))
-//         if (!ok) {
-//             throw new Error(`Expected void but got ${stringify(value)}`)
-//         }
-//         return;
-//     }
+    public override validate(value: unknown): void {
+        // void === null or void === undefined
+        const { ok, result } = run(() => or(undef(), nul()).validate(value))
+        if (!ok) {
+            throw new Error(`Expected void but got ${stringify(value)}`)
+        }
+        return;
+    }
 
-//     public override clone() {
-//         return new VoidParser()
-//     }
+    public override clone() {
+        return new VoidParser()
+    }
 
-//     public override get name(): string {
-//         return `void`
-    
-//     }
-// }
+    public override get name(): string {
+        return `void`
+    }
+}
 
-// export const pVoid = () => new VoidParser()
-// export const voi = pVoid
+export const pVoid = () => new VoidParser()
+export const voi = pVoid
+export const none = pVoid
--- packages/parser/src/tests/void.test.ts
+++ packages/parser/src/tests/void.test.ts
@@ -1,173 +1,173 @@
-// import { describe, expect, test, it, expectTypeOf } from 'vitest'
-// import { undef } from '../parsers/UndefinedParser.js';
-// import { voi } from '../parsers/VoidParser.js';
+import { describe, expect, test, it, expectTypeOf } from 'vitest'
+import { undef } from '../parsers/UndefinedParser.js';
+import { voi } from '../parsers/VoidParser.js';
 
-// enum Foo {
-//     A = 'x',
-//     B = 2,
-//     C = 3,
-//     D = 'y'
-// }
+enum Foo {
+    A = 'x',
+    B = 2,
+    C = 3,
+    D = 'y'
+}
 
-// class Bar {
-//     readonly bar = 1;
-// }
+class Bar {
+    readonly bar = 1;
+}
 
-// class Baz {
-//     readonly baz = 2;
-// }
+class Baz {
+    readonly baz = 2;
+}
 
-// const p = voi;
+const p = voi;
 
-// describe("void", () => {
-//     it("should have correct typename", () => {
-//         expect(p().name).toBe("void")
-//     })
+describe("void", () => {
+    it("should have correct typename", () => {
+        expect(p().name).toBe("void")
+    })
 
-//     it("should parse to correct type", () => {
-//         expectTypeOf(() => p().validate(null)).returns.toMatchTypeOf<void>();
-//         const parser = p();
-//         const a: void = undefined
-//         const b: ReturnType<typeof parser.validate> = a
-//     })
+    it("should parse to correct type", () => {
+        expectTypeOf(() => p().validate(null)).returns.toMatchTypeOf<void>();
+        const parser = p();
+        const a: void = undefined
+        const b: ReturnType<typeof parser.validate> = a
+    })
 
-//     it("should error on true", () => {
-//         expect(() => p().validate(true)).toThrow();
-//     });
+    it("should error on true", () => {
+        expect(() => p().validate(true)).toThrow();
+    });
     
-//     it("should error on false", () => {
-//         expect(() => p().validate(false)).toThrow();
-//     });
+    it("should error on false", () => {
+        expect(() => p().validate(false)).toThrow();
+    });
 
-//     it("should error on number", () => {
-//         expect(() => p().validate(1)).toThrow();
-//     })
+    it("should error on number", () => {
+        expect(() => p().validate(1)).toThrow();
+    })
 
-//     it("should error on string", () => {
-//         expect(() => p().validate("")).toThrow();
-//     })
+    it("should error on string", () => {
+        expect(() => p().validate("")).toThrow();
+    })
 
-//     it("should error on object", () => {
-//         expect(() => p().validate({})).toThrow();
-//     })
+    it("should error on object", () => {
+        expect(() => p().validate({})).toThrow();
+    })
 
-//     it("should error on array", () => {
-//         expect(() => p().validate([])).toThrow();
-//     })
+    it("should error on array", () => {
+        expect(() => p().validate([])).toThrow();
+    })
 
-//     it("should parse null", () => {
-//         expect(p().validate(null)).toBe(undefined);
-//     })
+    it("should parse null", () => {
+        expect(p().validate(null)).toBe(undefined);
+    })
 
-//     it("should parse undefined", () => {
-//         expect(p().validate(undefined)).toBe(undefined);
-//     })
+    it("should parse undefined", () => {
+        expect(p().validate(undefined)).toBe(undefined);
+    })
 
-//     it("should error on NaN", () => {
-//         expect(() => p().validate(NaN)).toThrow();
-//     })
+    it("should error on NaN", () => {
+        expect(() => p().validate(NaN)).toThrow();
+    })
 
-//     it("should error on Infinity", () => {
-//         expect(() => p().validate(Infinity)).toThrow();
-//     })
+    it("should error on Infinity", () => {
+        expect(() => p().validate(Infinity)).toThrow();
+    })
 
-//     it("should error on -Infinity", () => {
-//         expect(() => p().validate(-Infinity)).toThrow();
-//     })
+    it("should error on -Infinity", () => {
+        expect(() => p().validate(-Infinity)).toThrow();
+    })
 
-//     it("should error on function", () => {
-//         expect(() => p().validate(() => {})).toThrow();
-//     })
+    it("should error on function", () => {
+        expect(() => p().validate(() => {})).toThrow();
+    })
 
-//     it("should error on symbol", () => {
-//         expect(() => p().validate(Symbol())).toThrow();
-//     })
+    it("should error on symbol", () => {
+        expect(() => p().validate(Symbol())).toThrow();
+    })
 
-//     it("should error on Date", () => {
-//         expect(() => p().validate(new Date())).toThrow();
-//     })
+    it("should error on Date", () => {
+        expect(() => p().validate(new Date())).toThrow();
+    })
 
-//     it("should error on BigInt", () => {
-//         expect(() => p().validate(BigInt(1))).toThrow();
-//     })
+    it("should error on BigInt", () => {
+        expect(() => p().validate(BigInt(1))).toThrow();
+    })
 
-//     it("should error on Map", () => {
-//         expect(() => p().validate(new Map())).toThrow();
-//     })
+    it("should error on Map", () => {
+        expect(() => p().validate(new Map())).toThrow();
+    })
 
-//     it("should error on Set", () => {
-//         expect(() => p().validate(new Set())).toThrow();
-//     })
+    it("should error on Set", () => {
+        expect(() => p().validate(new Set())).toThrow();
+    })
 
-//     it("should error on WeakMap", () => {
-//         expect(() => p().validate(new WeakMap())).toThrow();
-//     })
+    it("should error on WeakMap", () => {
+        expect(() => p().validate(new WeakMap())).toThrow();
+    })
 
-//     it("should error on WeakSet", () => {
-//         expect(() => p().validate(new WeakSet())).toThrow();
-//     })
+    it("should error on WeakSet", () => {
+        expect(() => p().validate(new WeakSet())).toThrow();
+    })
 
-//     it("should error on ArrayBuffer", () => {
-//         expect(() => p().validate(new ArrayBuffer(1))).toThrow();
-//     })
+    it("should error on ArrayBuffer", () => {
+        expect(() => p().validate(new ArrayBuffer(1))).toThrow();
+    })
 
-//     it("should error on Int8Array", () => {
-//         expect(() => p().validate(new Int8Array(1))).toThrow();
-//     })
+    it("should error on Int8Array", () => {
+        expect(() => p().validate(new Int8Array(1))).toThrow();
+    })
 
-//     it("should error on Uint8Array", () => {
-//         expect(() => p().validate(new Uint8Array(1))).toThrow();
-//     })
+    it("should error on Uint8Array", () => {
+        expect(() => p().validate(new Uint8Array(1))).toThrow();
+    })
 
-//     it("should error on Uint8ClampedArray", () => {
-//         expect(() => p().validate(new Uint8ClampedArray(1))).toThrow();
-//     })
+    it("should error on Uint8ClampedArray", () => {
+        expect(() => p().validate(new Uint8ClampedArray(1))).toThrow();
+    })
 
-//     it("should error on Int16Array", () => {
-//         expect(() => p().validate(new Int16Array(1))).toThrow();
-//     })
+    it("should error on Int16Array", () => {
+        expect(() => p().validate(new Int16Array(1))).toThrow();
+    })
 
-//     it("should error on Uint16Array", () => {
-//         expect(() => p().validate(new Uint16Array(1))).toThrow();
-//     })
+    it("should error on Uint16Array", () => {
+        expect(() => p().validate(new Uint16Array(1))).toThrow();
+    })
 
-//     it("should error on Int32Array", () => {
-//         expect(() => p().validate(new Int32Array(1))).toThrow();
-//     })
+    it("should error on Int32Array", () => {
+        expect(() => p().validate(new Int32Array(1))).toThrow();
+    })
 
-//     it("should error on Uint32Array", () => {
-//         expect(() => p().validate(new Uint32Array(1))).toThrow();
-//     })
+    it("should error on Uint32Array", () => {
+        expect(() => p().validate(new Uint32Array(1))).toThrow();
+    })
 
-//     it("should error on Float32Array", () => {
-//         expect(() => p().validate(new Float32Array(1))).toThrow();
-//     })
+    it("should error on Float32Array", () => {
+        expect(() => p().validate(new Float32Array(1))).toThrow();
+    })
 
-//     it("should error on Float64Array", () => {
-//         expect(() => p().validate(new Float64Array(1))).toThrow();
-//     })
+    it("should error on Float64Array", () => {
+        expect(() => p().validate(new Float64Array(1))).toThrow();
+    })
 
-//     it("should error on BigInt64Array", () => {
-//         expect(() => p().validate(new BigInt64Array(1))).toThrow();
-//     })
+    it("should error on BigInt64Array", () => {
+        expect(() => p().validate(new BigInt64Array(1))).toThrow();
+    })
 
-//     it("should error on BigUint64Array", () => {
-//         expect(() => p().validate(new BigUint64Array(1))).toThrow();
-//     })
+    it("should error on BigUint64Array", () => {
+        expect(() => p().validate(new BigUint64Array(1))).toThrow();
+    })
 
-//     it("should error on native enum variant", () => {
-//         expect(() => p().validate(Foo.A)).toThrow();
-//         expect(() => p().validate(Foo.B)).toThrow();
-//         expect(() => p().validate(Foo.C)).toThrow();
-//         expect(() => p().validate(Foo.D)).toThrow();
-//     })
+    it("should error on native enum variant", () => {
+        expect(() => p().validate(Foo.A)).toThrow();
+        expect(() => p().validate(Foo.B)).toThrow();
+        expect(() => p().validate(Foo.C)).toThrow();
+        expect(() => p().validate(Foo.D)).toThrow();
+    })
 
-//     it("should error on instance", () => {
-//         expect(() => p().validate(new Bar())).toThrow();
-//         expect(() => p().validate(new Baz())).toThrow();
-//     })
+    it("should error on instance", () => {
+        expect(() => p().validate(new Bar())).toThrow();
+        expect(() => p().validate(new Baz())).toThrow();
+    })
 
-//     it("should error on regex", () => {
-//         expect(() => p().validate(/./)).toThrow();
-//     })
-// });
+    it("should error on regex", () => {
+        expect(() => p().validate(/./)).toThrow();
+    })
+});
