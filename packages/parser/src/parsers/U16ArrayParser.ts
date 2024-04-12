f16efbcf93c22c770e9975d9b757786578ff8436
George Oastler <goastler4@gmail.com>
2024-04-12 09:57:33 +0100

    convert (u/i)X[] validators to class implementations

--- packages/parser/src/parsers/U16ArrayParser.ts
+++ packages/parser/src/parsers/U16ArrayParser.ts
@@ -1,14 +1,22 @@
-// import { boundNum } from "./BoundNumberParser.js"
-// import { InstanceParser } from "./InstanceParser.js"
-// import { NumberParser, num } from "./NumberParser.js"
-// import { Validator } from "./Parser.js"
-// import { TypeofParser } from "./TypeofParser.js"
-// import { Ctor, InferTypeFromCtor } from "./utils.js"
+import { boundNum } from "./BoundNumberParser.js"
+import { InstanceParser } from "./InstanceParser.js"
+import { NumberParser, num } from "./NumberParser.js"
+import { Validator } from "./Parser.js"
+import { TypeofParser } from "./TypeofParser.js"
+import { Ctor, InferTypeFromCtor } from "./utils.js"
 
-// export class U16ArrayParser extends InstanceParser<Ctor<Uint16Array>> {
-// }
+export class U16ArrayParser extends InstanceParser<Ctor<Uint16Array>> {
+    
+    constructor() {
+        super(Uint16Array)
+    }
 
-// export const pU16Array = () => new U16ArrayParser(Uint16Array)
-// export const u16Array = pU16Array
-// export const u16a = pU16Array
-// export const u16Arr = pU16Array
+    public override get name(): string {
+        return "u16[]"
+    }
+}
+
+export const pU16Array = () => new U16ArrayParser()
+export const u16Array = pU16Array
+export const u16a = pU16Array
+export const u16Arr = pU16Array
