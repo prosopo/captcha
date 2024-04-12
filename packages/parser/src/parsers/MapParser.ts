b394d230ad358b269b2def714872071d3b06c53c
George Oastler <goastler4@gmail.com>
2024-04-12 15:24:49 +0100

    fix map validator impl

--- packages/parser/src/parsers/MapParser.ts
+++ packages/parser/src/parsers/MapParser.ts
@@ -1,42 +1,40 @@
-// import { inst } from "./InstanceParser.js"
-// import { Validator, Shape } from "./Parser.js"
+import { inst } from "./InstanceParser.js"
+import { Validator, Shape, InferOutput } from "./Parser.js"
 
-// export class MapParser<T extends Validator<any>, U extends Validator<any>> extends Validator<{
-//     output: Map<Shape<T>, Shape<U>>
-// }> {
-//     constructor(private _keyParser: T, private _valueParser: U) {
-//         super()
-//         this._keyParser = this.keyParser // clone parser
-//         this._valueParser = this.valueParser // clone parser
-//     }
+export class MapParser<T extends Validator<unknown, unknown>, U extends Validator<unknown, unknown>> extends Validator<unknown, Map<InferOutput<T>, InferOutput<U>>> {
+    constructor(private _keyParser: T, private _valueParser: U) {
+        super()
+        this._keyParser = this.keyParser // clone parser
+        this._valueParser = this.valueParser // clone parser
+    }
 
-//     get keyParser() {
-//         return this._keyParser.clone() as T
-//     }
+    get keyParser() {
+        return this._keyParser.clone() as T
+    }
 
-//     get valueParser() {
-//         return this._valueParser.clone() as U
-//     }
+    get valueParser() {
+        return this._valueParser.clone() as U
+    }
 
-//     public override validate(value: unknown): Map<Shape<T>, Shape<U>> {
-//         const valueMap = inst(Map).validate(value)
-//         const result = new Map<Shape<T>, Shape<U>>()
-//         for (const [key, value] of valueMap) {
-//             // parse every key and value to ensure they are of the correct type
-//             result.set(this._keyParser.validate(key), this._valueParser.validate(value))
-//         }
-//         return result
-//     }
+    public override validate(value: unknown): Map<InferOutput<T>, InferOutput<U>> {
+        const valueMap = inst(Map).validate(value)
+        const result = new Map<InferOutput<T>, InferOutput<U>>()
+        for (const [key, value] of valueMap) {
+            // parse every key and value to ensure they are of the correct type
+            result.set(this._keyParser.validate(key) as InferOutput<T>, this._valueParser.validate(value) as InferOutput<U>)
+        }
+        return result
+    }
 
-//     public override clone() {
-//         return new MapParser<T, U>(this._keyParser, this._valueParser)
-//     }
+    public override clone() {
+        return new MapParser<T, U>(this._keyParser, this._valueParser)
+    }
 
-//     public override get name(): string {
-//         return `Map<${this._keyParser.name}, ${this._valueParser.name}>`
-//     }
-// }
+    public override get name(): string {
+        return `Map<${this._keyParser.name}, ${this._valueParser.name}>`
+    }
+}
 
-// export const pMap = <T extends Validator<any>, U extends Validator<any>>(keyParser: T, valueParser: U) => new MapParser<T, U>(keyParser, valueParser)
-// export const map = pMap
-// export const kv = pMap
+export const pMap = <T extends Validator<unknown, unknown>, U extends Validator<unknown, unknown>>(keyParser: T, valueParser: U) => new MapParser<T, U>(keyParser, valueParser)
+export const map = pMap
+export const kv = pMap
