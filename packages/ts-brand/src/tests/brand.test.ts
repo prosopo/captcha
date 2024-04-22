fb16acb704a1a24e6218a5ec911df8d6ac76dcfc
George Oastler <goastler4@gmail.com>
2024-04-22 13:16:45 +0100

    testing

--- packages/ts-brand/src/tests/brand.test.ts
+++ packages/ts-brand/src/tests/brand.test.ts
@@ -1,3 +1,6 @@
+import { describe, test, it, expect } from 'vitest'
+import { Brand } from '../index.js'
+
 describe("brand", () => {
     test("type branding", () => {
         type A = Brand<{
@@ -13,30 +16,33 @@ describe("brand", () => {
         type a = C<A>
         type b = C<B>
 
-
-const fn = (a: A) => {
-    console.log('do something with A')
-}
-
-const obj: B = {
-    x: 1,
-    y: true,
-    z: 'hello'
-}
-
-fn(obj) 
-
+        
     })
 })
 
+// const fn = (a: A) => {
+//     console.log('do something with A')
+// }
 
-class Dog {
-    constructor(public name: string) {}
-}
+// const obj: B = {
+//     x: 1,
+//     y: true,
+//     z: 'hello'
+// }
+
+// fn(obj) 
+
+//     })
+// })
 
 
-const DogBranded = addBrand(Dog, 'Dog')
+// class Dog {
+//     constructor(public name: string) {}
+// }
 
-const dog = new DogBranded('Spot') // ok
 
-export type DogExported = typeof DogBranded
+// const DogBranded = addBrand(Dog, 'Dog')
+
+// const dog = new DogBranded('Spot') // ok
+
+// export type DogExported = typeof DogBranded
