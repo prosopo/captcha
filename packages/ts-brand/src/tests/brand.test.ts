2771d44946445733aca35284ae0229ff97571b35
George Oastler <goastler4@gmail.com>
2024-04-22 13:29:27 +0100

    testing

--- packages/ts-brand/src/tests/brand.test.ts
+++ packages/ts-brand/src/tests/brand.test.ts
@@ -1,5 +1,5 @@
-import { describe, test, it, expect } from 'vitest'
-import { Brand } from '../index.js'
+import { describe, test, it, expect, expectTypeOf } from 'vitest'
+import { Brand, brand, getBrand, unbrand } from '../index.js'
 
 describe("brand", () => {
     test("type branding", () => {
@@ -15,34 +15,69 @@ describe("brand", () => {
 
         type a = C<A>
         type b = C<B>
-
         
+        // expect the types to be true/false appropriately
+        const c: b = false
+        const d: a = true
+    })
+
+    test('branding classes', () => {
+        class A {
+            constructor(public x: number) {}
+        }
+
+        const ABranded = brand(A, 'A')
+
+        const aBrandedInst = new ABranded(1)
+
+        expectTypeOf(aBrandedInst).toMatchTypeOf<{
+            x: number,
+        }>()
+
+        expectTypeOf(aBrandedInst).toMatchTypeOf<Brand<A, 'A'>>()
+    })
+
+    test('get brand', () => {
+        class A {
+            constructor(public x: number) {}
+        }
+
+        const ABranded = brand(A, 'A')
+
+        const aBrandedInst = new ABranded(1)
+
+        const brand2 = getBrand(aBrandedInst)
+
+        expect(brand2).toBe('A')
+        expectTypeOf(brand2).toMatchTypeOf<'A'>()
+    })
+
+    test('get brand - no brand', () => {
+        class A {
+            constructor(public x: number) {}
+        }
+
+        const a = new A(1)
+
+        const brand2 = getBrand(a)
+
+        expect(brand2).toBe('')
+        expectTypeOf(brand2).toMatchTypeOf<''>()
+    })
+
+    test('unbrand', () => {
+        class A {
+            constructor(public x: number) {}
+        }
+
+        const ABranded = brand(A, 'A')
+
+        const aBrandedInst = new ABranded(1)
+
+        const a = unbrand(aBrandedInst)
+
+        expect(getBrand(a)).toBe('')
+        expectTypeOf(a).toMatchTypeOf<A>()
     })
 })
 
-// const fn = (a: A) => {
-//     console.log('do something with A')
-// }
-
-// const obj: B = {
-//     x: 1,
-//     y: true,
-//     z: 'hello'
-// }
-
-// fn(obj) 
-
-//     })
-// })
-
-
-// class Dog {
-//     constructor(public name: string) {}
-// }
-
-
-// const DogBranded = addBrand(Dog, 'Dog')
-
-// const dog = new DogBranded('Spot') // ok
-
-// export type DogExported = typeof DogBranded
