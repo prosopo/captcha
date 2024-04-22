22434a37d601cf1bba1c3a269b97cdf4ac1bb162
George Oastler <goastler4@gmail.com>
2024-04-22 13:29:34 +0100

    get brand fn

--- packages/ts-brand/src/index.ts
+++ packages/ts-brand/src/index.ts
@@ -6,12 +6,16 @@ export type Brand<T, U> = T & {
     [brandField]: U
 }
 
-export type Unbrand<T> = T[Exclude<keyof T, typeof brandField>]
+export type Unbrand<T> = T extends Brand<infer U, any> ? U : never
 
-export const brand = <T, U>(ctor: Ctor<T>, name: U) => {
+export const brand = <T, const U>(ctor: Ctor<T>, name: U) => {
     return ctor as Ctor<Brand<T, typeof name>>
 }
 
 export const unbrand = <T>(value: T) => {
     return value as Unbrand<T>
+}
+
+export const getBrand = <T>(value: T) => {
+    return (value as any)[brandField] || ''
 }
