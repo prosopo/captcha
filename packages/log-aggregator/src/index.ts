0f3d1099106fd0c5b70e61295cf86cea3d6376db
George Oastler <goastler4@gmail.com>
2024-05-02 11:16:16 +0100

    server for logs

---
+++ packages/log-aggregator/src/index.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from 'express';
+import fs from 'fs';
+
+const app = express();
+const port = 3000;
+
+app.use((req: Request, res: Response, next) => {
+    // check if the api key is set
+    if (!req.headers['x-api-key']) {
+        res.status(401).send('Unauthorized');
+        return;
+    }
+    // get the api key from headers
+    const apiKey = req.headers['x-api-key'];
+    // check if the api key is valid
+    if (apiKey !== '123456') { // TODO move to env
+        res.status(403).send('Forbidden');
+        return;
+    }
+
+    next()
+})
+
+app.post('/log', (req: Request, res: Response) => {
+    // TODO change body to contain log[]
+    // TODO cycle log files
+    // TODO validation
+    // append to log file
+    // TODO make this configurable via env
+    fs.appendFileSync('log.txt', JSON.stringify(req.body) + '\n');
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
