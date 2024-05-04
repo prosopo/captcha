410b5df32e5b6a606ea7e1c5b19b66a5785bb324
Chris Taylor <forgetso86@gmail.com>
2024-05-04 20:26:35 +0100

    export error handler

--- demos/provider-mock/src/start.ts
+++ demos/provider-mock/src/start.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { LogLevel, getLogger } from '@prosopo/common'
-import { handleErrors } from '@prosopo/cli'
+import { handleErrors } from '@prosopo/provider'
 import { i18nMiddleware } from '@prosopo/common'
 import { prosopoRouter } from './api.js'
 import cors from 'cors'
