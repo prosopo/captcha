92a38270fa1f674dfb73767b3d57d51190e83196
Chris <forgetso86@gmail.com>
2024-05-28 16:00:13 +0100

    Add Astar as a network option (#1250)
    * Add Astar as a network option
    
    * Change default network to astar
    
    * Allow arrays of endpoints in network config
    
    * build:typechain
    
    * Use proper network type
    
    * Make network an array in test config
    
    * Make network array
    
    * Fix up existential deposit script to take network as argument
    
    * Increase existential deposit for Astar
    
    * Set balances in contract that work for astar
    
    * Update contract
    
    * fmt
    
    * Work with BN
    
    * Use BN instead of number
    
    * Use toString where toNumber could overflow
    
    * More rawNumbers
    
    * Update substrate contracts node to 18 decimal places
    
    * Convert a bunch of stakes to BN
    
    * Further restrict  usage
    
    * lint:fix
    
    * lint docker compose files
    
    * prettier ignore unknown files
    
    * remove debug
    
    * add docker files to linting cmd
    
    * lint docker files
    
    * Fix tests
    
    * Remove .only in tests
    
    * Change docker image
    
    ---------
    
    Co-authored-by: George Oastler <goastler4@gmail.com>

--- packages/types/src/config/network.ts
+++ packages/types/src/config/network.ts
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import { array, literal, number, object, string, union, enum as zEnum, infer as zInfer } from 'zod'
 import { enumMap } from './enumMap.js'
-import { literal, number, object, string, union, enum as zEnum, infer as zInfer } from 'zod'
-export const NetworkNamesSchema = zEnum(['development', 'rococo', 'shiden'])
+export const NetworkNamesSchema = zEnum(['development', 'rococo', 'shiden', 'astar'])
 
 export type NetworkNames = zInfer<typeof NetworkNamesSchema>
 
@@ -24,7 +24,7 @@ export const NetworkPairTypeSchema = union([
     literal('ethereum'),
 ])
 export const NetworkConfigSchema = object({
-    endpoint: string().url(),
+    endpoint: array(string().url()),
     contract: object({
         address: string(),
         name: string(),
