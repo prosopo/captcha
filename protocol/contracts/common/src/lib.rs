51ef233a4ff6c6cac886b23a570e5e104e703d2b
Chris Taylor <forgetso86@gmail.com>
2024-05-29 20:59:12 +0100

    Keep production contract artefacts

--- protocol/contracts/common/src/lib.rs
+++ protocol/contracts/common/src/lib.rs
@@ -73,8 +73,8 @@ pub mod common {
         /// Get the git commit id from when this contract was built
         pub fn get_git_commit_id() -> [u8; 20] {
             let env_git_commit_id: [u8; 20] = [
-                206, 216, 95, 254, 89, 127, 251, 98, 241, 129, 69, 223, 179, 152, 2, 102, 234, 94,
-                251, 125,
+                133, 118, 158, 27, 166, 51, 39, 43, 181, 78, 11, 191, 239, 186, 87, 49, 95, 245,
+                236, 129,
             ];
             env_git_commit_id
         }
@@ -82,8 +82,8 @@ pub mod common {
         /// the admin which can control this contract. set to author/instantiator by default
         pub fn get_admin() -> AccountId {
             let env_admin_bytes: [u8; 32] = [
-                212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44,
-                133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125,
+                0, 127, 207, 164, 169, 158, 94, 205, 13, 86, 11, 24, 71, 28, 58, 218, 38, 74, 198,
+                198, 97, 246, 223, 15, 232, 77, 26, 153, 131, 4, 247, 90,
             ];
             AccountId::from(env_admin_bytes)
         }
