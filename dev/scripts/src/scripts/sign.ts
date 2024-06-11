c8c6ce90d433656e897c654f94bbcd96744e2e0c
George Oastler <goastler4@gmail.com>
2024-06-11 10:46:14 +0100

    lint

--- dev/scripts/src/scripts/sign.ts
+++ dev/scripts/src/scripts/sign.ts
@@ -11,60 +11,45 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { blake2AsHex, blake2AsU8a, keccakAsHex, keccakAsU8a } from '@polkadot/util-crypto'
-import { decodeAddress, encodeAddress } from '@polkadot/keyring'
-import { get } from '@prosopo/util'
-import {
-    hexToBn,
-    hexToString,
-    hexToU8a,
-    isHex,
-    stringToHex,
-    stringToU8a,
-    u8aToBn,
-    u8aToFloat,
-    u8aToHex,
-    u8aToString,
-} from '@polkadot/util'
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-import { getPairAsync } from '@prosopo/contract'
 import { KeypairType } from '@polkadot/util-crypto/types'
+import { getPairAsync } from '@prosopo/contract'
+import { hideBin } from 'yargs/helpers'
+import { stringToU8a, u8aToHex } from '@polkadot/util'
+import yargs from 'yargs'
 
 const main = async () => {
     console.log('argv', process.argv)
-    const argv = await yargs(hideBin(process.argv)).option('mnemonic', {
-        alias: 'seed',
-        type: 'string',
-    }).option('message', {
-        'alias': 'msg',
-        type: 'string',
-    }).option('type', {
-        type: 'string',
-        choices: ["sr25519", "ed25519", "ecdsa", "ethereum"],
-        default: "sr25519",
-    }).option('ss58Format', {
-        type: 'number',
-        default: 42,
-    }).parse()
+    const argv = await yargs(hideBin(process.argv))
+        .option('mnemonic', {
+            alias: 'seed',
+            type: 'string',
+        })
+        .option('message', {
+            alias: 'msg',
+            type: 'string',
+        })
+        .option('type', {
+            type: 'string',
+            choices: ['sr25519', 'ed25519', 'ecdsa', 'ethereum'],
+            default: 'sr25519',
+        })
+        .option('ss58Format', {
+            type: 'number',
+            default: 42,
+        })
+        .parse()
     const mnemonic = argv.mnemonic
-    if(!mnemonic) {
+    if (!mnemonic) {
         throw new Error('mnemonic is required')
     }
     const message = argv.message
-    if(!message) {
+    if (!message) {
         throw new Error('message is required')
     }
     const type = argv.type as KeypairType
     const ss58Format = argv.ss58Format
 
-    const keypair = await getPairAsync(
-        undefined,
-        mnemonic,
-        undefined,
-        type,
-        ss58Format,
-    )
+    const keypair = await getPairAsync(undefined, mnemonic, undefined, type, ss58Format)
 
     const sig = keypair.sign(stringToU8a(message))
     console.log(`hex sig: ${u8aToHex(sig)}`)
