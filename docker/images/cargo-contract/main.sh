898877ba28495bbe70d97b3cc5a541de70a422cd
George Oastler <goastler4@gmail.com>
2024-06-14 19:58:47 +0100

    troubleshooting, fix cargo-cache usage

--- docker/images/cargo-contract/main.sh
+++ docker/images/cargo-contract/main.sh
@@ -3,6 +3,9 @@
 # Stop on error / print each command / stop on unset variable / stop on pipe fail
 set -euxo pipefail
 
+mkdir -p /cargo-cache/registry
+mkdir -p /cargo-cache/git
+
 # Move to the source directory, which should be a mounted volume
 cd /src
 
