ada046b029936981345958fc77b87b74e7df26dc
George Oastler <goastler4@gmail.com>
2024-06-18 15:19:54 +0100

    stop verbose printing of bash cmds

--- docker/images/cargo-contract/main.sh
+++ docker/images/cargo-contract/main.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Stop on error / print each command / stop on unset variable / stop on pipe fail
-set -euxo pipefail
+set -euo pipefail
 
 mkdir -p /cargo-cache/registry
 mkdir -p /cargo-cache/git
@@ -11,6 +11,7 @@ cd /src
 
 # Run the command passed as arguments to the script
 # `& wait` runs the command in the background and waits for it to finish, allowing ctrl-c to work as child processes are killed
+echo "> $@"
 eval "$@" & 
 pid=$!
 wait $pid
