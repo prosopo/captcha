dccf0f09c7259c5376fd6746c0776e61b923cb62
George Oastler <goastler4@gmail.com>
2024-06-26 17:24:40 +0100

    remove typechain regex fixes

--- dev/scripts/src/contract/import.ts
+++ dev/scripts/src/contract/import.ts
@@ -93,23 +93,6 @@ async function importContract(pathToAbis: string, pathToOutput: string) {
                     if (verbose) console.log(`Replacing \n\t${match}\nwith\n\t${result}\nin ${filePath}`)
                     return `${result}`
                 })
-                // eslint-disable-next-line no-useless-escape
-                replaced = replaced.replace(/\n(.*)\n(\s*)\/\/\s*@ts-ignore/g, (match, p1, p2) => {
-                    // don't replace if already ignored by eslint
-                    if (p1.includes('eslint-disable-next-line')) return match
-                    const result = `\n${p1}\n${p2}// eslint-disable-next-line @typescript-eslint/ban-ts-comment\n${p2}// @ts-ignore`
-                    if (verbose) console.log(`Replacing \n\t${match}\nwith\n\t${result}\nin ${filePath}`)
-                    return result
-                })
-
-                // replace EventRecord with EventRecord[]
-                // eslint-disable-next-line no-useless-escape
-                replaced = replaced.replace(/: EventRecord\)/g, (match) => {
-                    const result = `: EventRecord[])`
-                    if (verbose) console.log(`Replacing \n\t${match}\nwith\n\t${result}\nin ${filePath}`)
-                    return result
-                })
-
 
                 fs.writeFileSync(filePath, replaced)
             }
