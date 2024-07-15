327f73df8fb827e9fb989a177deb3458e6f308c1
Hugh <hughglynparry@gmail.com>
2024-07-15 14:43:52 +0100

    fixing providers

--- packages/procaptcha/src/modules/providers.ts
+++ packages/procaptcha/src/modules/providers.ts
@@ -30,6 +30,18 @@ export const loadBalancer = (environment: EnvironmentTypes): HardcodedProvider[]
                 datasetId: '0x7eca1e4806d91c9f905448d0ba9ed18b420d2930c8c8e11d3471befbbd75a672',
                 datasetIdContent: '0x1b66283e8d4dc61f9a076141974db4ba810bc8385268205a01d650ea0d40c320',
             },
+            {
+                address: '5C7bfXYwachNuvmasEFtWi9BMS41uBvo6KpYHVSQmad4nWzw',
+                url: 'https://pronode3.prosopo.io',
+                datasetId: '0x7eca1e4806d91c9f905448d0ba9ed18b420d2930c8c8e11d3471befbbd75a672',
+                datasetIdContent: '0x1b66283e8d4dc61f9a076141974db4ba810bc8385268205a01d650ea0d40c320',
+            },
+            {
+                address: '5FnBurrfqWgSLJFMsojjEP74mLX1vZZ9ASyNXKfA5YXu8FR2',
+                url: 'https://pronode4.prosopo.io',
+                datasetId: '0xe666b35451f302b9fccfbe783b1de9a6a4420b840abed071931d68a9ccc1c21d',
+                datasetIdContent: '0x4cb09a9a3470199a41418267c9ceb0025572f05193e21ab6ef50a7e490f0dd2f',
+            },
         ]
     }
     if (environment === 'development') {
