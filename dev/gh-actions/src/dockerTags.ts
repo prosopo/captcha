1e202fb0341c04e4429e879cf8fe0cae80634d94
George Oastler <goastler@users.noreply.github.com>
2024-07-19 14:38:52 +0100

    New git branches setup (#1296)
    * add dev branch to auto merge
    
    * make clippy run on new branch setup
    
    * remove version checker workflow
    
    * add dev and release branch triggers to consistent workflow name checker
    
    * Delete create_diagram.yml
    
    * add dev and release branch triggers to lint workflow
    
    * add release and dev branch triggers to provider_image workflow
    
    * remove release drafter
    
    * add dev and release branches to rustfmt workflow
    
    * add dev and release branches to protocol test workflow
    
    * add dev and release branches triggers to tests workflow
    
    * add dev and release branch triggers to webpack bundling workflow
    
    * switch cache creation to be post pr to dev
    
    * rename post_pr workflow to cache
    
    * Update cache.yml
    
    * publish workflow
    
    * Update cache.yml
    
    * fix contract js/rust version check workflow
    
    * install concurrently and cypress as workspace deps
    
    * fix cypress workflow trigger
    
    * fix cypress workflow branch triggers
    
    * fix rust file triggers
    
    * move webpack bundling check to tests workflow
    
    * remove typechain version check
    
    * rename workflows
    
    * make the github actions cache include the branch name
    
    * publish on push to main
    
    * add js bundle test step
    
    * workflow for ensuring all versions move in lockstep
    
    * blurb for each workflow
    
    * dockerhub login check for before a release
    
    * rename publish to release
    
    * remove typedoc step
    
    * standardise step ids in release workflow
    
    * dummy version bump
    
    * Delete dockerhub_login.yml
    
    * release version check
    
    * workflow to check for valid version bumps
    
    * auto merge all prs
    
    * check consistent workflow names on all prs
    
    * branch system docs
    
    * check contract version on prs to main,dev,release/*
    
    * add workflow_dispatch to all relevant workflows
    
    * make auto merge only run on non-draft prs
    
    * check versions on all branches
    
    * make release run on push to main
    
    * tag pushes to main
    
    * rename version check workflow
    
    * remove fetch all history and branches from clone
    
    * fix docker js server version issue
    
    * fix bump workflow to bump all package.json files and Cargo.tomls
    
    * fix package.json version writing in bump workflow
    
    * rename step
    
    * make auto merge cancellable
    
    * make deploy ahve a single notification of pass/fail
    
    * debug info
    
    * fix cache workflow name
    
    * don't use cache in bump version workflow, not needed
    
    * move buildx to top of workflow
    
    * move the cli building to before publishing images in deploy workflow
    
    * tag warning
    
    * remove filters for tests, test more often
    
    * add workflow dispatch to deploy workflow
    
    * add bash error catching
    
    * Create consistent_engine_node.yml
    
    * Create consistent_engine_npm.yml
    
    * Create consistent_nvmrc.yml
    
    * cache docs
    
    * event workflows for main and dev
    
    * change event setup
    
    * don't run unless non-draft pr
    
    * combine protocol clippy and rustfmt into single lint workflow
    
    * missing engine
    
    * missing version
    
    * revert version debug
    
    * fix version
    
    * fix workflow name detection
    
    * add missing engines
    
    * fix nvmrc check
    
    * fix path typo in contract version check
    
    * allow auto merge to run always
    
    * Update package-lock.json
    
    * lint
    
    * lint

---
+++ dev/gh-actions/src/dockerTags.ts
@@ -0,0 +1,63 @@
+// Copyright 2021-2024 Prosopo (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import axios from 'axios'
+
+export async function fetchTags(namespace: string, repository: string): Promise<string[]> {
+    const tags: string[] = []
+    let page = 1
+    let nextPageUrl:
+        | string
+        | null = `https://hub.docker.com/v2/repositories/${namespace}/${repository}/tags/?page=${page}`
+
+    while (nextPageUrl) {
+        try {
+            const response: any = await axios.get(nextPageUrl)
+            const data = response.data
+            tags.push(...data.results.map((tag: any) => tag.name))
+            nextPageUrl = data.next
+            page++
+        } catch (error) {
+            console.error(`Error fetching tags: ${error}`)
+            break
+        }
+    }
+
+    tags.sort(semVerLt)
+
+    return tags.reverse()
+}
+
+export const semVerLt = (a: string, b: string): number => {
+    const aParts = a.split('.').map((part) => parseInt(part, 10))
+    const bParts = b.split('.').map((part) => parseInt(part, 10))
+
+    if (aParts.length !== bParts.length || aParts.length !== 3) {
+        // not semver, so compare lexicographically
+        return a.localeCompare(b)
+    }
+
+    for (let i = 0; i < aParts.length; i++) {
+        if (aParts[i]! < bParts[i]!) {
+            return -1
+        } else if (aParts[i]! > bParts[i]!) {
+            return 1
+        }
+    }
+
+    return 0
+}
+
+export const isSemVer = (tag: string): boolean => {
+    return /^\d+\.\d+\.\d+$/.test(tag)
+}
