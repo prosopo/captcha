64bf5751f7076ad0de32124f58bd367c12332d08
Chris Taylor <forgetso86@gmail.com>
2024-07-30 20:50:50 +0100

    Remove old deps.

--- packages/load-balancer/src/index.ts
+++ packages/load-balancer/src/index.ts
@@ -22,7 +22,7 @@ type HardcodedProvider = {
 }
 
 export const loadBalancer = (environment: EnvironmentTypes): HardcodedProvider[] => {
-    if (environment === 'production' || environment === 'staging') {
+    if (environment === 'production') {
         return [
             {
                 address: '5CFHA8d3S1XXkZuBwGqiuA6SECTzfoucq397YL34FuPAH89G',
@@ -39,6 +39,28 @@ export const loadBalancer = (environment: EnvironmentTypes): HardcodedProvider[]
             {
                 address: '5FnBurrfqWgSLJFMsojjEP74mLX1vZZ9ASyNXKfA5YXu8FR2',
                 url: 'https://pronode4.prosopo.io',
+                datasetId: '0xe666b35451f302b9fccfbe783b1de9a6a4420b840abed071931d68a9ccc1c21d',
+                datasetIdContent: '0x4cb09a9a3470199a41418267c9ceb0025572f05193e21ab6ef50a7e490f0dd2f',
+            },
+        ]
+    }
+    if (environment === 'staging') {
+        return [
+            {
+                address: '5CFHA8d3S1XXkZuBwGqiuA6SECTzfoucq397YL34FuPAH89G',
+                url: 'https://staging-pronode2.prosopo.io',
+                datasetId: '0x7eca1e4806d91c9f905448d0ba9ed18b420d2930c8c8e11d3471befbbd75a672',
+                datasetIdContent: '0x1b66283e8d4dc61f9a076141974db4ba810bc8385268205a01d650ea0d40c320',
+            },
+            {
+                address: '5C7bfXYwachNuvmasEFtWi9BMS41uBvo6KpYHVSQmad4nWzw',
+                url: 'https://staging-pronode3.prosopo.io',
+                datasetId: '0x7eca1e4806d91c9f905448d0ba9ed18b420d2930c8c8e11d3471befbbd75a672',
+                datasetIdContent: '0x1b66283e8d4dc61f9a076141974db4ba810bc8385268205a01d650ea0d40c320',
+            },
+            {
+                address: '5FnBurrfqWgSLJFMsojjEP74mLX1vZZ9ASyNXKfA5YXu8FR2',
+                url: 'https://staging-pronode4.prosopo.io',
                 datasetId: '0xe666b35451f302b9fccfbe783b1de9a6a4420b840abed071931d68a9ccc1c21d',
                 datasetIdContent: '0x4cb09a9a3470199a41418267c9ceb0025572f05193e21ab6ef50a7e490f0dd2f',
             },
