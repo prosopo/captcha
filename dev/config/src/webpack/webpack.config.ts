f8cba08cd60d5b37123a6f085592fc4c7673a1e8
George Oastler <goastler4@gmail.com>
2024-08-02 11:01:43 +0100

    webpack fix

--- dev/config/src/webpack/webpack.config.ts
+++ dev/config/src/webpack/webpack.config.ts
@@ -122,7 +122,7 @@ export default (mode: string) => {
                           },
                       }),
                   ]
-                : [new TerserPlugin({})],
+                : [],
             usedExports: true,
         },
     }
