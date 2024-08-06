63d95a9d3b60ebc23c4dd07dbdc919f4f8943109
Hugh <hughglynparry@gmail.com>
2024-08-06 15:14:14 +0100

    Adding button next for cypress tests

--- packages/procaptcha-react/src/components/ProcaptchaWidget.tsx
+++ packages/procaptcha-react/src/components/ProcaptchaWidget.tsx
@@ -13,140 +13,151 @@
 // limitations under the License.
 /** @jsxImportSource @emotion/react */
 import {
-    Checkbox,
-    ContainerDiv,
-    LoadingSpinner,
-    WIDGET_BORDER,
-    WIDGET_BORDER_RADIUS,
-    WIDGET_DIMENSIONS,
-    WIDGET_INNER_HEIGHT,
-    WIDGET_PADDING,
-    WIDGET_URL,
-    WIDGET_URL_TEXT,
-    WidthBasedStylesDiv,
-    darkTheme,
-    lightTheme,
-} from '@prosopo/web-components'
-import { Logo } from '@prosopo/web-components'
-import { Manager } from '@prosopo/procaptcha'
-import { ProcaptchaConfigSchema, ProcaptchaProps } from '@prosopo/types'
-import { useProcaptcha } from '@prosopo/procaptcha-common'
-import { useRef, useState } from 'react'
-import CaptchaComponent from './CaptchaComponent.js'
-import Collector from './collector.js'
-import Modal from './Modal.js'
+  Checkbox,
+  ContainerDiv,
+  LoadingSpinner,
+  WIDGET_BORDER,
+  WIDGET_BORDER_RADIUS,
+  WIDGET_DIMENSIONS,
+  WIDGET_INNER_HEIGHT,
+  WIDGET_PADDING,
+  WIDGET_URL,
+  WIDGET_URL_TEXT,
+  WidthBasedStylesDiv,
+  darkTheme,
+  lightTheme,
+} from "@prosopo/web-components";
+import { Logo } from "@prosopo/web-components";
+import { Manager } from "@prosopo/procaptcha";
+import { ProcaptchaConfigSchema, type ProcaptchaProps } from "@prosopo/types";
+import { useProcaptcha } from "@prosopo/procaptcha-common";
+import { useRef, useState } from "react";
+import CaptchaComponent from "./CaptchaComponent.js";
+import Collector from "./collector.js";
+import Modal from "./Modal.js";
 
 const ProcaptchaWidget = (props: ProcaptchaProps) => {
-    const config = ProcaptchaConfigSchema.parse(props.config)
-    const callbacks = props.callbacks || {}
-    const [state, updateState] = useProcaptcha(useState, useRef)
-    const manager = Manager(config, state, updateState, callbacks)
-    const themeColor = props.config.theme === 'light' ? 'light' : 'dark'
-    const theme = props.config.theme === 'light' ? lightTheme : darkTheme
+  const config = ProcaptchaConfigSchema.parse(props.config);
+  const callbacks = props.callbacks || {};
+  const [state, updateState] = useProcaptcha(useState, useRef);
+  const manager = Manager(config, state, updateState, callbacks);
+  const themeColor = props.config.theme === "light" ? "light" : "dark";
+  const theme = props.config.theme === "light" ? lightTheme : darkTheme;
 
-    return (
-        <div>
-            <div style={{ maxWidth: '100%', maxHeight: '100%', overflowX: 'auto' }}>
-                <Modal show={state.showModal}>
-                    {state.challenge ? (
-                        <CaptchaComponent
-                            challenge={state.challenge}
-                            index={state.index}
-                            solutions={state.solutions}
-                            onSubmit={manager.submit}
-                            onCancel={manager.cancel}
-                            onClick={manager.select}
-                            onNext={manager.nextRound}
-                            themeColor={config.theme ?? 'light'}
+  return (
+    <div>
+      <div style={{ maxWidth: "100%", maxHeight: "100%", overflowX: "auto" }}>
+        <Modal show={state.showModal}>
+          {state.challenge ? (
+            <CaptchaComponent
+              challenge={state.challenge}
+              index={state.index}
+              solutions={state.solutions}
+              onSubmit={manager.submit}
+              onCancel={manager.cancel}
+              onClick={manager.select}
+              onNext={manager.nextRound}
+              themeColor={config.theme ?? "light"}
+            />
+          ) : (
+            <div>No challenge set.</div>
+          )}
+        </Modal>
+        <ContainerDiv>
+          <WidthBasedStylesDiv>
+            <div style={WIDGET_DIMENSIONS} data-cy={"button-human"}>
+              {" "}
+              <div
+                style={{
+                  padding: WIDGET_PADDING,
+                  border: WIDGET_BORDER,
+                  backgroundColor: theme.palette.background.default,
+                  borderColor: theme.palette.grey[300],
+                  borderRadius: WIDGET_BORDER_RADIUS,
+                  display: "flex",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  justifyContent: "space-between",
+                  minHeight: `${WIDGET_INNER_HEIGHT}px`,
+                  overflow: "hidden",
+                }}
+              >
+                <div
+                  style={{ display: "inline-flex", flexDirection: "column" }}
+                >
+                  <div
+                    style={{
+                      alignItems: "center",
+                      flex: 1,
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        verticalAlign: "middle",
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: !state.loading ? "flex" : "none",
+                        }}
+                      >
+                        <Checkbox
+                          themeColor={themeColor}
+                          onChange={manager.start}
+                          checked={state.isHuman}
+                          labelText="I am human"
+                          aria-label="human checkbox"
                         />
-                    ) : (
-                        <div>No challenge set.</div>
-                    )}
-                </Modal>
-                <ContainerDiv>
-                    <WidthBasedStylesDiv>
-                        <div style={WIDGET_DIMENSIONS} data-cy={'button-human'}>
-                            {' '}
-                            <div
-                                style={{
-                                    padding: WIDGET_PADDING,
-                                    border: WIDGET_BORDER,
-                                    backgroundColor: theme.palette.background.default,
-                                    borderColor: theme.palette.grey[300],
-                                    borderRadius: WIDGET_BORDER_RADIUS,
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    flexWrap: 'wrap',
-                                    justifyContent: 'space-between',
-                                    minHeight: `${WIDGET_INNER_HEIGHT}px`,
-                                    overflow: 'hidden',
-                                }}
-                            >
-                                <div style={{ display: 'inline-flex', flexDirection: 'column' }}>
-                                    <div
-                                        style={{
-                                            alignItems: 'center',
-                                            flex: 1,
-                                        }}
-                                    >
-                                        <div
-                                            style={{
-                                                display: 'flex',
-                                                alignItems: 'center',
-                                                justifyContent: 'center',
-                                                flexDirection: 'column',
-                                                verticalAlign: 'middle',
-                                            }}
-                                        >
-                                            <div
-                                                style={{
-                                                    display: !state.loading ? 'flex' : 'none',
-                                                }}
-                                            >
-                                                <Checkbox
-                                                    themeColor={themeColor}
-                                                    onChange={manager.start}
-                                                    checked={state.isHuman}
-                                                    labelText="I am human"
-                                                    aria-label="human checkbox"
-                                                />
-                                            </div>
-                                            <div
-                                                style={{
-                                                    display: state.loading ? 'flex' : 'none',
-                                                }}
-                                            >
-                                                <div style={{ display: 'inline-flex' }}>
-                                                    <LoadingSpinner
-                                                        themeColor={themeColor}
-                                                        aria-label="Loading spinner"
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div style={{ display: 'inline-flex', flexDirection: 'column' }}>
-                                    <a href={WIDGET_URL} target="_blank" aria-label={WIDGET_URL_TEXT}>
-                                        <div style={{ flex: 1 }}>
-                                            <Logo themeColor={themeColor} aria-label="Prosopo logo"></Logo>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
+                      </div>
+                      <div
+                        style={{
+                          display: state.loading ? "flex" : "none",
+                        }}
+                      >
+                        <div style={{ display: "inline-flex" }}>
+                          <LoadingSpinner
+                            themeColor={themeColor}
+                            aria-label="Loading spinner"
+                          />
                         </div>
-                    </WidthBasedStylesDiv>
-                    {config.devOnlyWatchEvents && (
-                        <Collector
-                            onProcessData={manager.exportData}
-                            sendData={state.sendData}
-                            account={state.account}
-                        ></Collector>
-                    )}
-                </ContainerDiv>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style={{ display: "inline-flex", flexDirection: "column" }}
+                >
+                  <a
+                    href={WIDGET_URL}
+                    target="_blank"
+                    aria-label={WIDGET_URL_TEXT}
+                  >
+                    <div style={{ flex: 1 }}>
+                      <Logo
+                        themeColor={themeColor}
+                        aria-label="Prosopo logo"
+                      ></Logo>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
-        </div>
-    )
-}
+          </WidthBasedStylesDiv>
+          {config.devOnlyWatchEvents && (
+            <Collector
+              onProcessData={manager.exportData}
+              sendData={state.sendData}
+              account={state.account}
+            ></Collector>
+          )}
+        </ContainerDiv>
+      </div>
+    </div>
+  );
+};
 
-export default ProcaptchaWidget
+export default ProcaptchaWidget;
