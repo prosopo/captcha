8511f818215e684b0905ae52fe4e2d0a3a3bd76b
Hugh <hughglynparry@gmail.com>
2024-08-06 15:46:33 +0100

    Cypress tests passing

--- demos/cypress-shared/cypress/e2e/correct.captcha.signup.cy.ts
+++ demos/cypress-shared/cypress/e2e/correct.captcha.signup.cy.ts
@@ -12,66 +12,69 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /// <reference types="cypress" />
-import '@cypress/xpath'
-import { Captcha } from '@prosopo/types'
-import { ProsopoDatasetError } from '@prosopo/common'
-import { checkboxClass } from '../support/commands.js'
-import { datasetWithSolutionHashes } from '@prosopo/datasets'
+import "@cypress/xpath";
+import type { Captcha } from "@prosopo/types";
+import { ProsopoDatasetError } from "@prosopo/common";
+import { checkboxClass } from "../support/commands.js";
+import { datasetWithSolutionHashes } from "@prosopo/datasets";
 
-describe('Captchas', () => {
-    beforeEach(() => {
-        const solutions = datasetWithSolutionHashes.captchas.map((captcha) => ({
-            captchaContentId: captcha.captchaContentId,
-            solution: captcha.solution,
-        }))
+describe("Captchas", () => {
+  beforeEach(() => {
+    const solutions = datasetWithSolutionHashes.captchas.map((captcha) => ({
+      captchaContentId: captcha.captchaContentId,
+      solution: captcha.solution,
+    }));
 
-        if (!solutions) {
-            throw new ProsopoDatasetError('DATABASE.DATASET_WITH_SOLUTIONS_GET_FAILED', {
-                context: { datasetWithSolutionHashes },
-            })
+    if (!solutions) {
+      throw new ProsopoDatasetError(
+        "DATABASE.DATASET_WITH_SOLUTIONS_GET_FAILED",
+        {
+          context: { datasetWithSolutionHashes },
         }
+      );
+    }
 
-        cy.intercept('/dummy').as('dummy')
+    cy.intercept("/dummy").as("dummy");
 
-        // visit the base URL specified on command line when running cypress
-        return cy.visit(Cypress.env('default_page')).then(() => {
-            cy.get(checkboxClass).should('be.visible')
-            // wrap the solutions to make them available to the tests
-            cy.wrap(solutions).as('solutions')
-        })
-    })
+    // visit the base URL specified on command line when running cypress
+    return cy.visit(Cypress.env("default_page")).then(() => {
+      cy.get(checkboxClass).should("be.visible");
+      // wrap the solutions to make them available to the tests
+      cy.wrap(solutions).as("solutions");
+    });
+  });
 
-    it('Selecting the correct images passes the captcha', () => {
-        cy.get('button[type="button"]').eq(1).click()
-        cy.clickIAmHuman().then(() => {
-            // Make sure the images are loaded
-            cy.captchaImages().then(() => {
-                // Solve the captchas
-                cy.get('@captchas')
-                    .each((captcha: Captcha) => {
-                        cy.log('in each function')
-                        // Click correct images and submit the solution
-                        cy.clickCorrectCaptchaImages(captcha)
-                    })
-                    .then(() => {
-                        // Get inputs of type checkbox
-                        cy.get("input[type='checkbox']").then((checkboxes) => {
-                            cy.wrap(checkboxes).first().should('be.checked')
-                        })
-                    })
-                const uniqueId = Cypress._.uniqueId('test')
-                cy.get('input[type="password"]').type('password')
-                cy.get('input[id="email"]').type(`${uniqueId}@prosopo.io`)
-                cy.get('input[id="name"]').type('test')
-                cy.get('button[type="button"]').first().click()
+  it("Selecting the correct images passes the captcha", () => {
+    cy.get('button[type="button"]').eq(1).click();
+    cy.clickIAmHuman().then(() => {
+      // Make sure the images are loaded
+      cy.captchaImages().then(() => {
+        // Solve the captchas
+        cy.get("@captchas")
+          .each((captcha: Captcha) => {
+            cy.log("in each function");
+            // Click correct images and submit the solution
+            cy.clickCorrectCaptchaImages(captcha);
+          })
+          .then(() => {
+            // Get inputs of type checkbox
+            cy.get("input[type='checkbox']").then((checkboxes) => {
+              cy.wrap(checkboxes).first().should("be.checked");
+            });
+          });
+        const uniqueId = Cypress._.uniqueId("test");
+        cy.get('input[type="password"]').type("password");
+        cy.get('input[id="email"]').type(`${uniqueId}@prosopo.io`);
+        cy.get('input[id="name"]').type("test");
+        cy.get('button[type="button"]').first().click();
 
-                cy.contains('user created').should('be.visible')
+        cy.contains("user created").should("be.visible");
 
-                // reloading the page and checking the box again should not require a captcha to be solved
-                cy.reload()
+        // reloading the page and checking the box again should not require a captcha to be solved
+        cy.reload();
 
-                cy.get(checkboxClass, { timeout: 12000 }).first().click()
-            })
-        })
-    })
-})
+        cy.get(checkboxClass, { timeout: 12000 }).first().click();
+      });
+    });
+  });
+});
