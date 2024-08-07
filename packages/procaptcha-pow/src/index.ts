d0d987f3bdb5a0d5859ed96534c3db44bcb3d870
Chris Taylor <forgetso86@gmail.com>
2024-08-07 15:20:39 +0100

    Size fixes for widget and logo

--- packages/procaptcha-pow/src/index.ts
+++ packages/procaptcha-pow/src/index.ts
@@ -11,5 +11,5 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-export * from './components/Captcha.js'
-export * from './components/ProcaptchaPoW.js'
+export * from "./components/ProcaptchaWidget.js";
+export * from "./components/ProcaptchaPoW.js";
