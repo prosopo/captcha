652677820b927b526f2f14b12e4686f027275524
Chris Taylor <forgetso86@gmail.com>
2024-08-07 11:23:56 +0100

    remove within

--- demos/cypress-shared/cypress/support/commands.ts
+++ demos/cypress-shared/cypress/support/commands.ts
@@ -132,10 +132,8 @@ function clickCorrectCaptchaImages(
 function clickNextButton() {
 	cy.intercept("POST", "**/prosopo/provider/solution").as("postSolution");
 	// Go to the next captcha or submit solution
-	cy.get(".modalInner").within(() => {
-		cy.get('button[data-cy="button-next"]').click({ force: true });
-		cy.wait(0);
-	});
+	cy.get('button[data-cy="button-next"]').click({ force: true });
+	cy.wait(0);
 }
 
 Cypress.Commands.addAll({
