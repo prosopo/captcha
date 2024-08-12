669f6e7250ba6f2d4aff3427102914dcbdd08eb6
Chris Taylor <forgetso86@gmail.com>
2024-08-12 14:58:56 +0100

    Silence GH warning about NoSQL injection in demo

--- demos/client-example-server/src/controllers/auth.ts
+++ demos/client-example-server/src/controllers/auth.ts
@@ -18,9 +18,9 @@ import { ProsopoServer } from "@prosopo/server";
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import {
-	ApiParams,
-	type ProcaptchaToken,
-	type ProsopoServerConfigOutput,
+  ApiParams,
+  type ProcaptchaToken,
+  type ProsopoServerConfigOutput,
 } from "@prosopo/types";
 import { ProcaptchaResponse } from "@prosopo/types";
 import { at } from "@prosopo/util";
@@ -31,205 +31,205 @@ import { z } from "zod";
 import type { UserInterface } from "../models/user.js";
 
 const SubscribeBodySpec = ProcaptchaResponse.merge(
-	z.object({
-		email: z.string().email(),
-		password: z.string(),
-	}),
+  z.object({
+    email: z.string().email(),
+    password: z.string(),
+  }),
 );
 
 function hashPassword(password: string): string {
-	return u8aToHex(blake2b(password));
+  return u8aToHex(blake2b(password));
 }
 
 const verify = async (
-	prosopoServer: ProsopoServer,
-	verifyType: string,
-	verifyEndpoint: string,
-	token: ProcaptchaToken,
-	secret: string,
+  prosopoServer: ProsopoServer,
+  verifyType: string,
+  verifyEndpoint: string,
+  token: ProcaptchaToken,
+  secret: string,
 ) => {
-	if (verifyType === "api") {
-		// verify using the API endpoint
-		console.log("Verifying using the API endpoint", verifyEndpoint);
+  if (verifyType === "api") {
+    // verify using the API endpoint
+    console.log("Verifying using the API endpoint", verifyEndpoint);
 
-		const response = await fetch(verifyEndpoint, {
-			method: "POST",
-			body: JSON.stringify({
-				[ApiParams.token]: token,
-				[ApiParams.secret]: secret,
-			}),
-		});
+    const response = await fetch(verifyEndpoint, {
+      method: "POST",
+      body: JSON.stringify({
+        [ApiParams.token]: token,
+        [ApiParams.secret]: secret,
+      }),
+    });
 
-		const verified = (await response.json()).verified;
-		return verified;
-	}
-	// verify using the TypeScript library
-	const verified = await prosopoServer.isVerified(token);
-	return verified;
+    const verified = (await response.json()).verified;
+    return verified;
+  }
+  // verify using the TypeScript library
+  const verified = await prosopoServer.isVerified(token);
+  return verified;
 };
 
 const signup = async (
-	mongoose: Connection,
-	config: ProsopoServerConfigOutput,
-	verifyEndpoint: string,
-	verifyType: string,
-	req: Request,
-	res: Response,
-	next: NextFunction,
+  mongoose: Connection,
+  config: ProsopoServerConfigOutput,
+  verifyEndpoint: string,
+  verifyType: string,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-	try {
-		const User = mongoose.model<UserInterface>("User");
-		// checks if email exists
-		const dbUser = await User.findOne({
-			email: req.body.email,
-		});
-		const payload = SubscribeBodySpec.parse(req.body);
-		const pair = await getPairAsync(
-			config.networks[config.defaultNetwork],
-			config.account.secret,
-		);
-		const prosopoServer = new ProsopoServer(config, pair);
-		if (dbUser) {
-			return res.status(409).json({ message: "email already exists" });
-		}
-		// get the procaptcha-response token
-		const token = payload[ApiParams.procaptchaResponse];
+  try {
+    const User = mongoose.model<UserInterface>("User");
+    // checks if email exists
+    const dbUser = await User.findOne({
+      email: { $eq: req.body.email },
+    });
+    const payload = SubscribeBodySpec.parse(req.body);
+    const pair = await getPairAsync(
+      config.networks[config.defaultNetwork],
+      config.account.secret,
+    );
+    const prosopoServer = new ProsopoServer(config, pair);
+    if (dbUser) {
+      return res.status(409).json({ message: "email already exists" });
+    }
+    // get the procaptcha-response token
+    const token = payload[ApiParams.procaptchaResponse];
 
-		if (!config.account.secret) {
-			throw new ProsopoEnvError("GENERAL.MNEMONIC_UNDEFINED", {
-				context: { missingParams: ["PROSOPO_SITE_PRIVATE_KEY"] },
-			});
-		}
+    if (!config.account.secret) {
+      throw new ProsopoEnvError("GENERAL.MNEMONIC_UNDEFINED", {
+        context: { missingParams: ["PROSOPO_SITE_PRIVATE_KEY"] },
+      });
+    }
 
-		const verified = await verify(
-			prosopoServer,
-			verifyType,
-			verifyEndpoint,
-			token,
-			config.account.secret,
-		);
+    const verified = await verify(
+      prosopoServer,
+      verifyType,
+      verifyEndpoint,
+      token,
+      config.account.secret,
+    );
 
-		if (verified) {
-			// salt
-			const salt = randomAsHex(32);
-			// !!!DUMMY CODE!!! - Do not use in production. Use bcrypt or similar for password hashing.
-			const passwordHash = hashPassword(`${req.body.password}${salt}`);
-			if (passwordHash) {
-				return User.create({
-					email: req.body.email,
-					name: req.body.name,
-					password: passwordHash,
-					salt: salt,
-				})
-					.then(() => {
-						res.status(200).json({ message: "user created" });
-					})
-					.catch((err) => {
-						console.log(err);
-						res.status(502).json({ message: "error while creating the user" });
-					});
-			}
-		} else {
-			res
-				.status(401)
-				.json({ message: "user has not completed a captcha", verified });
-		}
-	} catch (err) {
-		console.error("error", err);
-		res
-			.status(500)
-			.json({ message: (err as Error).message || "internal server error" });
-	}
+    if (verified) {
+      // salt
+      const salt = randomAsHex(32);
+      // !!!DUMMY CODE!!! - Do not use in production. Use bcrypt or similar for password hashing.
+      const passwordHash = hashPassword(`${req.body.password}${salt}`);
+      if (passwordHash) {
+        return User.create({
+          email: req.body.email,
+          name: req.body.name,
+          password: passwordHash,
+          salt: salt,
+        })
+          .then(() => {
+            res.status(200).json({ message: "user created" });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(502).json({ message: "error while creating the user" });
+          });
+      }
+    } else {
+      res
+        .status(401)
+        .json({ message: "user has not completed a captcha", verified });
+    }
+  } catch (err) {
+    console.error("error", err);
+    res
+      .status(500)
+      .json({ message: (err as Error).message || "internal server error" });
+  }
 };
 
 const login = async (
-	mongoose: Connection,
-	config: ProsopoServerConfigOutput,
-	verifyEndpoint: string,
-	verifyType: string,
-	req: Request,
-	res: Response,
+  mongoose: Connection,
+  config: ProsopoServerConfigOutput,
+  verifyEndpoint: string,
+  verifyType: string,
+  req: Request,
+  res: Response,
 ) => {
-	const User = mongoose.model<UserInterface>("User");
-	const pair = await getPairAsync(
-		config.networks[config.defaultNetwork],
-		config.account.secret,
-	);
-	const prosopoServer = new ProsopoServer(config, pair);
-	// checks if email exists
-	await User.findOne({
-		email: req.body.email,
-	})
-		.then(async (dbUser) => {
-			if (!dbUser) {
-				res.status(404).json({ message: "user not found" });
-			} else {
-				const payload = SubscribeBodySpec.parse(
-					req.body[ApiParams.procaptchaResponse],
-				);
+  const User = mongoose.model<UserInterface>("User");
+  const pair = await getPairAsync(
+    config.networks[config.defaultNetwork],
+    config.account.secret,
+  );
+  const prosopoServer = new ProsopoServer(config, pair);
+  // checks if email exists
+  await User.findOne({
+    email: req.body.email,
+  })
+    .then(async (dbUser) => {
+      if (!dbUser) {
+        res.status(404).json({ message: "user not found" });
+      } else {
+        const payload = SubscribeBodySpec.parse(
+          req.body[ApiParams.procaptchaResponse],
+        );
 
-				const token = payload[ApiParams.procaptchaResponse];
+        const token = payload[ApiParams.procaptchaResponse];
 
-				if (!config.account.secret) {
-					throw new ProsopoEnvError("GENERAL.MNEMONIC_UNDEFINED", {
-						context: { missingParams: ["PROSOPO_SITE_PRIVATE_KEY"] },
-					});
-				}
+        if (!config.account.secret) {
+          throw new ProsopoEnvError("GENERAL.MNEMONIC_UNDEFINED", {
+            context: { missingParams: ["PROSOPO_SITE_PRIVATE_KEY"] },
+          });
+        }
 
-				const verified = await verify(
-					prosopoServer,
-					verifyType,
-					verifyEndpoint,
-					token,
-					config.account.secret,
-				);
+        const verified = await verify(
+          prosopoServer,
+          verifyType,
+          verifyEndpoint,
+          token,
+          config.account.secret,
+        );
 
-				if (verified) {
-					// password hash
-					// !!!DUMMY CODE!!! - Do not use in production. Use bcrypt or similar for password hashing.
-					const passwordHash = hashPassword(
-						`${req.body.password}${dbUser.salt}`,
-					);
-					if (passwordHash !== dbUser.password) {
-						// password doesnt match
-						res.status(401).json({ message: "invalid credentials" });
-					} else {
-						// password match
-						const token = jwt.sign({ email: req.body.email }, "secret", {
-							expiresIn: "1h",
-						});
-						res.status(200).json({ message: "user logged in", token: token });
-					}
-				}
-			}
-		})
-		.catch((err) => {
-			console.error("error", err);
-			res.status(500).json({ message: err.message || "internal server error" });
-		});
+        if (verified) {
+          // password hash
+          // !!!DUMMY CODE!!! - Do not use in production. Use bcrypt or similar for password hashing.
+          const passwordHash = hashPassword(
+            `${req.body.password}${dbUser.salt}`,
+          );
+          if (passwordHash !== dbUser.password) {
+            // password doesnt match
+            res.status(401).json({ message: "invalid credentials" });
+          } else {
+            // password match
+            const token = jwt.sign({ email: req.body.email }, "secret", {
+              expiresIn: "1h",
+            });
+            res.status(200).json({ message: "user logged in", token: token });
+          }
+        }
+      }
+    })
+    .catch((err) => {
+      console.error("error", err);
+      res.status(500).json({ message: err.message || "internal server error" });
+    });
 };
 
 const isAuth = (req: Request, res: Response) => {
-	const authHeader = req.get("Authorization") || "";
-	if (!authHeader) {
-		res.status(401).json({ message: "not authenticated" });
-	}
+  const authHeader = req.get("Authorization") || "";
+  if (!authHeader) {
+    res.status(401).json({ message: "not authenticated" });
+  }
 
-	const token = at(authHeader.split(" "), 1);
-	let decodedToken: string | JwtPayload = "";
-	try {
-		decodedToken = jwt.verify(token, "secret");
-	} catch (err) {
-		res.status(500).json({
-			message: (err as Error).message || "could not decode the token",
-		});
-	}
+  const token = at(authHeader.split(" "), 1);
+  let decodedToken: string | JwtPayload = "";
+  try {
+    decodedToken = jwt.verify(token, "secret");
+  } catch (err) {
+    res.status(500).json({
+      message: (err as Error).message || "could not decode the token",
+    });
+  }
 
-	if (!decodedToken) {
-		res.status(401).json({ message: "unauthorized" });
-	} else {
-		res.status(200).json({ message: "here is your resource" });
-	}
+  if (!decodedToken) {
+    res.status(401).json({ message: "unauthorized" });
+  } else {
+    res.status(200).json({ message: "here is your resource" });
+  }
 };
 
 export { signup, login, isAuth };
