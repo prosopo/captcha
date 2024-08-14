3e0053a0c20753384ed74f784f0296273401ea29
Chris Taylor <forgetso86@gmail.com>
2024-08-14 11:03:29 +0100

    Move headers

--- packages/web-components/src/LoadingSpinner.tsx
+++ packages/web-components/src/LoadingSpinner.tsx
@@ -1,5 +1,3 @@
-import styled from "@emotion/styled";
-import { useMemo } from "react";
 // Copyright 2021-2024 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,42 +11,44 @@ import { useMemo } from "react";
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import styled from "@emotion/styled";
+import { useMemo } from "react";
 import { darkTheme, lightTheme } from "./theme.js";
 
 export interface LoadingSpinnerComponentProps {
-	themeColor: "light" | "dark";
+  themeColor: "light" | "dark";
 }
 
 export const LoadingSpinner = ({
-	themeColor,
+  themeColor,
 }: LoadingSpinnerComponentProps) => {
-	const theme = useMemo(
-		() => (themeColor === "light" ? lightTheme : darkTheme),
-		[themeColor],
-	);
-	const StyledDiv = styled.div`
-        margin-top: 0;
-        margin-left: 15px;
-        margin-right: 15px;
-        width: 2em;
-        height: 2em;
-        border: 4px solid ${theme.palette.background.contrastText};
-        border-bottom-color: transparent;
-        border-radius: 50%;
-        display: inherit;
-        box-sizing: border-box;
-        animation: rotation 1s linear infinite;
+  const theme = useMemo(
+    () => (themeColor === "light" ? lightTheme : darkTheme),
+    [themeColor],
+  );
+  const StyledDiv = styled.div`
+    margin-top: 0;
+    margin-left: 15px;
+    margin-right: 15px;
+    width: 2em;
+    height: 2em;
+    border: 4px solid ${theme.palette.background.contrastText};
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inherit;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
 
-        @keyframes rotation {
-            0% {
-                transform: rotate(0deg);
-            }
-            100% {
-                transform: rotate(360deg);
-            }
-        }
-    `;
-	return <StyledDiv />;
+    @keyframes rotation {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `;
+  return <StyledDiv />;
 };
 
 export default LoadingSpinner;
