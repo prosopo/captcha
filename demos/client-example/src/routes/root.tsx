eee386730ac647a693ce78d65fb9021d9d26d461
Chris Taylor <forgetso86@gmail.com>
2024-08-21 20:32:43 +0100

    Fix license location

--- demos/client-example/src/routes/root.tsx
+++ demos/client-example/src/routes/root.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 // Copyright 2021-2024 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,12 +11,14 @@ import React from "react";
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import React from "react";
 import App from "../App.js";
 
 export default function Root() {
-	return (
-		<React.Fragment>
-			<App />
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  );
 }
