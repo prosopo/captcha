e27fb70c30424f981c3db208f1937291858b9e0f
Chris Taylor <forgetso86@gmail.com>
2024-09-11 14:16:58 +0100

    Address PR comments

--- packages/types/src/client/user.ts
+++ packages/types/src/client/user.ts
@@ -14,37 +14,39 @@
 import type { Timestamp } from "../datasets/index.js";
 
 export enum Tier {
-	Free = "free",
-	Professional = "professional",
-	Enterprise = "enterprise",
+  Free = "free",
+  Professional = "professional",
+  Enterprise = "enterprise",
 }
 
 export const TierMonthlyLimits = {
-	[Tier.Free]: {
-		verificationRequests: 100000,
-	},
-	[Tier.Professional]: {
-		verificationRequests: 1000000,
-	},
-	[Tier.Enterprise]: {
-		verificationRequests: "Unlimited",
-	},
+  [Tier.Free]: {
+    verificationRequests: 100000,
+  },
+  [Tier.Professional]: {
+    verificationRequests: 1000000,
+  },
+  [Tier.Enterprise]: {
+    verificationRequests: "Unlimited",
+  },
 };
 
 export interface IUserData {
-	email: string;
-	name: string;
-	account: string;
-	url: string;
-	mnemonic: string;
-	createdAt: Timestamp;
-	activated: boolean;
-	tier?: Tier;
-	settings?: IUserSettings;
-	updatedAtTimestamp?: Timestamp;
+  email: string;
+  name: string;
+  account: string;
+  url: string;
+  mnemonic: string;
+  createdAt: Timestamp;
+  activated: boolean;
+  tier: Tier;
+  marketingPreferences: boolean;
+  originUrl: string;
+  settings?: IUserSettings;
+  updatedAtTimestamp?: Timestamp;
 }
 
 export interface IUserSettings {
-	threshold: number;
-	domains: string[];
+  threshold: number;
+  domains: string[];
 }
