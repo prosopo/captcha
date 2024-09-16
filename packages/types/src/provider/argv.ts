984b8d87abea88182b7ec2c611ad7e43576b2e91
George Oastler <goastler4@gmail.com>
2024-09-16 12:13:07 +0100

    lint

--- packages/types/src/provider/argv.ts
+++ packages/types/src/provider/argv.ts
@@ -1,3 +1,4 @@
+import { nativeEnum } from "zod";
 // Copyright 2021-2024 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +13,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { Payee } from "./accounts.js";
-import { nativeEnum } from "zod";
 
 export const PayeeSchema = nativeEnum(Payee);
