239e0c38be1d70455e189dfffa5e2921e45e5503
Hugh <hughglynparry@gmail.com>
2024-09-16 18:21:17 +0100

    License formatting

--- packages/procaptcha-bundle/src/tests/captchaType.test.ts
+++ packages/procaptcha-bundle/src/tests/captchaType.test.ts
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import { FeaturesEnum } from "@prosopo/types";
 import { JSDOM } from "jsdom";
 import { beforeEach, describe, expect, it } from "vitest";
--- packages/procaptcha-bundle/src/tests/config.test.ts
+++ packages/procaptcha-bundle/src/tests/config.test.ts
@@ -1,4 +1,3 @@
-import { JSDOM } from "jsdom";
 // Copyright 2021-2024 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,9 @@ import { JSDOM } from "jsdom";
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import { JSDOM } from "jsdom";
+
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { extractParams, getProcaptchaScript } from "../util/config.js";
 
