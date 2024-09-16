ce857f56247172619ab1f362c481b721daaabd7f
Hugh <hughglynparry@gmail.com>
2024-09-16 18:17:06 +0100

    lint fix

--- packages/procaptcha-bundle/src/util/language.ts
+++ packages/procaptcha-bundle/src/util/language.ts
@@ -1,17 +1,28 @@
+// Copyright 2021-2024 Prosopo (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import {
 	LanguageSchema,
 	type ProcaptchaClientConfigInput,
 	type ProcaptchaRenderOptions,
 } from "@prosopo/types";
 
-
-
 /**
  * Prioritizes language selection from:
  * 1. renderOptions.language
  * 2. element's data-language attribute
  * 3. Defaults to 'en'
- * 
+ *
  * Validates the language against currently supported languages
  *
  * @param renderOptions
