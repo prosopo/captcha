fcb1d8e2e851ef5c2d10c156c4b353c6ca5ff8fb
Chris Taylor <forgetso86@gmail.com>
2024-09-19 10:56:31 +0100

    Use inherited vite config from dev/config package

--- demos/client-example/vite.config.ts
+++ demos/client-example/vite.config.ts
@@ -1,3 +1,4 @@
+
 // Copyright 2021-2024 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,93 +12,68 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import * as path from "node:path";
-import { getLogger } from "@prosopo/common";
-import { VitePluginCloseAndCopy } from "@prosopo/config";
+import { ViteFrontendConfig } from "@prosopo/config";
 import { loadEnv } from "@prosopo/dotenv";
-import { VitePluginWatchWorkspace } from "@prosopo/vite-plugin-watch-workspace";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-const logger = getLogger("Info", "vite.config.js");
-const dir = path.resolve(".");
-loadEnv(dir);
+
+// load env using our util because vite loadEnv is not working for .env.development
+loadEnv();
+
+// Vite doesn't find the tsconfig for some reason
+process.env.TS_NODE_PROJECT = path.resolve("./tsconfig.json");
+
+// Package specific config
+const bundleName = "prosopo-client-example";
+const packageName = "@prosopo/client-example";
+const entry = path.resolve("./index.html");
+const workspaceRoot = path.resolve("../../..");
+
+
 // https://vitejs.dev/config/
 export default defineConfig(async ({ command, mode }) => {
-	logger.info(`Running at ${dir} in ${mode} mode`);
-	// NODE_ENV must be wrapped in quotes. We just set it to the mode and ignore what's in the env file, otherwise the
-	// mode and NODE_ENV can end up out of sync (one set to development and the other set to production, which causes
-	// issues like this: https://github.com/hashicorp/next-mdx-remote/pull/323
-	logger.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+	const frontendConfig = await ViteFrontendConfig(
+		packageName,
+		bundleName,
+		path.resolve(),
+		entry,
+		command,
+		mode,
+		undefined,
+		undefined,
+		workspaceRoot,
+	);
 
-	// Set the env vars that we want to be available in the browser
-	const define = {
-		// used to stop websockets package from breaking
-		"process.env.WS_NO_BUFFER_UTIL": JSON.stringify("true"),
-		"process.env.WS_NO_UTF_8_VALIDATE": JSON.stringify("true"),
-		"process.env.NODE_ENV": JSON.stringify(mode),
-		"process.env.PROSOPO_DEFAULT_ENVIRONMENT": JSON.stringify(
-			process.env.PROSOPO_DEFAULT_ENVIRONMENT,
-		),
-		// only needed if bundling with a site key
-		"process.env.PROSOPO_SITE_KEY": JSON.stringify(
-			process.env.PROSOPO_SITE_KEY,
-		),
-		"process.env.PROSOPO_WEB2": JSON.stringify(process.env.PROSOPO_WEB2),
-		"process.env.PROSOPO_SERVER_URL": JSON.stringify(
-			process.env.PROSOPO_SERVER_URL,
-		),
-		"process.env.PROSOPO_SERVER_PORT": JSON.stringify(
-			process.env.PROSOPO_SERVER_PORT,
-		),
-		"process.env.PROSOPO_PORT": JSON.stringify(process.env.PROSOPO_PORT),
-		"process.env._DEV_ONLY_WATCH_EVENTS": JSON.stringify(
-			process.env._DEV_ONLY_WATCH_EVENTS,
-		),
-	};
-	logger.debug("define", JSON.stringify(define));
 	return {
+		...frontendConfig,
 		watch: false,
 		mode: "development",
 		bundle: true,
-		define,
-		optimizeDeps: {
-			include: ["prop-types"],
-		},
-		esbuild: {
-			target: [
-				"es2020",
-				"chrome60",
-				"edge18",
-				"firefox60",
-				"node12",
-				"safari11",
-			],
+		define: {
+			...frontendConfig.define,
+			"process.env.PROSOPO_WEB2": JSON.stringify(
+				process.env.PROSOPO_WEB2 || "true",
+			),
+			"process.env.PROSOPO_LOG_LEVEL": JSON.stringify(
+				process.env.PROSOPO_LOG_LEVEL || "Info",
+			),
 		},
 		build: {
+			outDir: path.resolve("./dist"),
 			modulePreload: { polyfill: true },
 			lib: {
-				entry: path.resolve(__dirname, "./index.html"),
-				name: "client_example",
+				entry,
+				name: bundleName,
+				formats: ["es"],
 			},
+			rollupOptions: {
+				...frontendConfig.build?.rollupOptions,
+				output: {
+					dir: path.resolve("./dist"),
+				},
+			}
 		},
-		plugins: [
-			// @ts-ignore
-			react(),
-			// Closes the bundler and copies the bundle to the client-bundle-example project unless we're in serve
-			// mode, in which case we don't want to close the bundler because it will close the server
-			command !== "serve" ? VitePluginCloseAndCopy() : undefined,
-			// Watches external files (workspace packages) and rebuilds them when they change
-			await VitePluginWatchWorkspace({
-				workspaceRoot: path.resolve("../.."),
-				currentPackage: `${path.resolve(".")}/**/*`,
-				format: "esm",
-				ignorePaths: [
-					`${path.resolve("../..")}/demos/*`,
-					`${path.resolve("../..")}/dev/*`,
-					"**/dist/**/*",
-				],
-			}),
-		],
 		server: {
 			port: process.env.PROSOPO_PORT ? Number(process.env.PROSOPO_PORT) : 9230,
 		},
