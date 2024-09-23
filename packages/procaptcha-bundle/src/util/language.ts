c9dcbae7913fa546a1214e61731d454fc6d0d34b
Chris Taylor <forgetso86@gmail.com>
2024-09-23 16:14:51 +0100

    Lint-fix

--- packages/procaptcha-bundle/src/util/language.ts
+++ packages/procaptcha-bundle/src/util/language.ts
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { LanguageSchema } from "@prosopo/locale";
 import type {
 	ProcaptchaClientConfigInput,
 	ProcaptchaRenderOptions,
 } from "@prosopo/types";
-import { LanguageSchema } from "@prosopo/locale";
 
 /**
  * Prioritizes language selection from:
