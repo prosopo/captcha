bacc23499a1d8a50a7641f160b9ef4fa57ee9b85
Chris Taylor <forgetso86@gmail.com>
2024-09-23 15:59:15 +0100

    More imports placed below license

--- packages/locale-browser/vite.cjs.config.ts
+++ packages/locale-browser/vite.cjs.config.ts
@@ -1,4 +1,3 @@
-import path from "node:path";
 // Copyright 2021-2024 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,8 @@ import path from "node:path";
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import path from "node:path";
 import { ViteCommonJSConfig } from "@prosopo/config";
 
 export default function () {
