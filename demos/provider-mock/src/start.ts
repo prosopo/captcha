d061a902d24d775bdd3ce60dbd98d6b515fccff2
Chris <forgetso86@gmail.com>
2024-09-23 16:58:24 +0100

    Locale overhaul (#1393)
    * Big locale overhaul
    
    * 📦🔒
    
    * 📦🔒
    
    * 📦🔒
    
    * tsconfig refs
    
    * 📦🔒
    
    * remove polkadot/util from common
    
    * Use correct package name
    
    * Use correct package name
    
    * lint-fix
    
    * package dir getters
    
    * Lint-fix and fix import
    
    * Address PR comments
    
    * specify no tests for the locale-browser package
    
    * More imports placed below license

--- demos/provider-mock/src/start.ts
+++ demos/provider-mock/src/start.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { LogLevel, getLogger } from "@prosopo/common";
-import { i18nMiddleware } from "@prosopo/common";
+import { i18nMiddleware } from "@prosopo/locale";
 import { handleErrors } from "@prosopo/provider";
 import cors from "cors";
 import express from "express";
