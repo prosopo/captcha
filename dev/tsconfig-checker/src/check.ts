98a4157c19db414897ccba37b935fbfcfceb5659
George Oastler <goastler4@gmail.com>
2024-09-23 12:44:39 +0100

    lint

--- dev/tsconfig-checker/src/check.ts
+++ dev/tsconfig-checker/src/check.ts
@@ -20,77 +20,95 @@ import z from "zod";
 
 // file consists of a path to the file or the content of the file if already read
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-type File = { path: string, content: any }
+type File = { path: string; content: any };
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 const readJson = (filePath: string): any => {
-	return JSON.parse(fs.readFileSync(filePath, "utf8"))
-}
+	return JSON.parse(fs.readFileSync(filePath, "utf8"));
+};
 
 const readFile = (filePath: string): File => {
 	return {
 		path: filePath,
 		content: readJson(filePath),
-	}
-}
+	};
+};
 
 const readFileWhy = (filePath: string, msg: string): File => {
 	try {
 		return readFile(filePath);
-	} catch(err) {
+	} catch (err) {
 		throw new Error(`Failed to read file ${filePath}: ${msg}`);
 	}
-}
+};
 
 const findWs = (pkgJson: File): File => {
 	let filePath = pkgJson.path;
-	while(filePath !== '/package.json') {
+	while (filePath !== "/package.json") {
 		if (fs.existsSync(filePath)) {
 			const file = readFile(filePath);
-			if(file.content.workspaces) {
+			if (file.content.workspaces) {
 				return file;
 			}
 		}
 		// go up a directory discovering package.json files until we find one
-		filePath = path.resolve(path.dirname(filePath), '..', 'package.json');
+		filePath = path.resolve(path.dirname(filePath), "..", "package.json");
 	}
 	throw new Error(`Could not find workspace for ${pkgJson.path}`);
-}
+};
 
 const getPkgJsonPaths = (wsPkgJson: File) => {
 	// get the workspace globs
-	const globs = z.string().array().catch([]).parse(wsPkgJson.content.workspaces).map(g => `${path.dirname(wsPkgJson.path)}/${g}/package.json`);
+	const globs = z
+		.string()
+		.array()
+		.catch([])
+		.parse(wsPkgJson.content.workspaces)
+		.map((g) => `${path.dirname(wsPkgJson.path)}/${g}/package.json`);
 	// glob the workspace paths
 	// add package.json to the dirs and filter out any that don't exist (e.g. packages/* may match packages/some-dir but if some-dir doesn't have a package.json we don't want it)
-	const pkgJsons = globs.map((g) => fg.globSync(g)).reduce((acc, val) => acc.concat(val), []);
+	const pkgJsons = globs
+		.map((g) => fg.globSync(g))
+		.reduce((acc, val) => acc.concat(val), []);
 	return pkgJsons;
-}
+};
 
 const getDeps = (pkgJson: File) => {
-	const deps = Object.keys(z.record(z.string()).catch({}).parse(pkgJson.content.dependencies));
-	const devDeps = Object.keys(z.record(z.string()).catch({}).parse(pkgJson.content.devDependencies));
+	const deps = Object.keys(
+		z.record(z.string()).catch({}).parse(pkgJson.content.dependencies),
+	);
+	const devDeps = Object.keys(
+		z.record(z.string()).catch({}).parse(pkgJson.content.devDependencies),
+	);
 	return [...new Set([...deps, ...devDeps])];
-}
+};
 
 const getWsPkgNames = (wsPkgJsons: string[]) => {
-	const wsPkgNames = wsPkgJsons.map((p) => z.string().parse(readFile(p).content.name));
+	const wsPkgNames = wsPkgJsons.map((p) =>
+		z.string().parse(readFile(p).content.name),
+	);
 	return wsPkgNames;
-}
+};
 
 const getRefs = (tsconfigJson: File) => {
-	const refs = z.array(z.record(z.string())).catch([]).parse(tsconfigJson.content.references);
-	return refs.map((r) => get(r, "path")).map(p => {
-		// ref paths can point at a tsconfig OR a dir containing a tsconfig. If it's a dir, add /tsconfig.json
-		if(p.endsWith('.json')) {
-			return p;
-		}
-		return `${p}/tsconfig.json`;
-	});
-}
+	const refs = z
+		.array(z.record(z.string()))
+		.catch([])
+		.parse(tsconfigJson.content.references);
+	return refs
+		.map((r) => get(r, "path"))
+		.map((p) => {
+			// ref paths can point at a tsconfig OR a dir containing a tsconfig. If it's a dir, add /tsconfig.json
+			if (p.endsWith(".json")) {
+				return p;
+			}
+			return `${p}/tsconfig.json`;
+		});
+};
 
 const getPkgName = (pkgJson: File) => {
 	return z.string().parse(pkgJson.content.name);
-}
+};
 
 const main = async (args: {
 	pkgJson: File;
@@ -99,18 +117,18 @@ const main = async (args: {
 	// pkgJsonPath points to the package.json file
 	const pkgJson = args.pkgJson;
 	const pkgDir = path.dirname(pkgJson.path);
-	
+
 	// if the workspace package json hasn't been passed in, try to find it
 	const wsPkgJson = findWs(pkgJson);
 
 	// if this package is the workspace, decend into the packages in the workspace
-	if(wsPkgJson.path === pkgJson.path) {
+	if (wsPkgJson.path === pkgJson.path) {
 		// get all the package.json files in the workspace
 		const wsPkgJsonPaths = getPkgJsonPaths(wsPkgJson);
 		// list all the pkgs in the workspace if the package belongs to a workspace
 		const wsPkgNames = getWsPkgNames(wsPkgJsonPaths);
 		// recurse into each package
-		for(const wsPkgJson of wsPkgJsonPaths) {
+		for (const wsPkgJson of wsPkgJsonPaths) {
 			await main({
 				pkgJson: readFile(wsPkgJson),
 				wsPkgNames,
@@ -121,57 +139,65 @@ const main = async (args: {
 		// get the deps for this package
 		// filter deps to those in the workspace
 		let wsPkgNames = args.wsPkgNames;
-		if(!wsPkgNames) {
+		if (!wsPkgNames) {
 			// get all the package.json files in the workspace
 			const wsPkgJsonPaths = getPkgJsonPaths(wsPkgJson);
 			// list all the pkgs in the workspace if the package belongs to a workspace
 			wsPkgNames = getWsPkgNames(wsPkgJsonPaths);
 		}
-		const deps = getDeps(pkgJson).filter(d => {
+		const deps = getDeps(pkgJson).filter((d) => {
 			return wsPkgNames.includes(d);
 		});
 		// console.log('deps', deps)
-	
+
 		// find all tsconfig files in the same dir as the package.json
 		const tsconfigPaths = fg.globSync(`${pkgDir}/tsconfig{,.cjs}.json`);
-		for(const tsconfigPath of tsconfigPaths) {
+		for (const tsconfigPath of tsconfigPaths) {
 			console.log(`Checking ${tsconfigPath} against ${pkgJson.path}`);
 			// read the tsconfig json
 			const tsconfigJson = readFile(tsconfigPath);
 			// get the tsconfig references
 			const refs = getRefs(tsconfigJson);
 			// convert them to package names, e.g. ../common => @prosopo/common
-			const refPkgNames = refs.map(p => {
+			const refPkgNames = refs.map((p) => {
 				// get the dir for the package
 				const refPath = path.dirname(p);
 				// read the package.json in the dir
-				const pkgJson = readFileWhy(`${pkgDir}/${refPath}/package.json`, `specified in ${tsconfigPath}`);
+				const pkgJson = readFileWhy(
+					`${pkgDir}/${refPath}/package.json`,
+					`specified in ${tsconfigPath}`,
+				);
 				return getPkgName(pkgJson);
-			})
+			});
 			// console.log('refPkgNames', refPkgNames)
 			// check all the refs are in the deps
 			// e.g. a dep might be in the pkg json but not specified in the tsconfig
-			const missingRefs: string[]= refPkgNames.filter((r) => !deps.includes(r))
+			const missingRefs: string[] = refPkgNames.filter(
+				(r) => !deps.includes(r),
+			);
 			// check all the deps are in the refs
 			// e.g. a ref might be in the tsconfig but not in the pkg json
-			const missingDeps: string[] = deps.filter((d) => !refPkgNames.includes(d))
-			for(const missingRef of missingRefs) {
+			const missingDeps: string[] = deps.filter(
+				(d) => !refPkgNames.includes(d),
+			);
+			for (const missingRef of missingRefs) {
 				console.log(`${missingRef} ref not needed in ${tsconfigPath}`);
 			}
-			for(const missingDep of missingDeps) {
+			for (const missingDep of missingDeps) {
 				console.log(`${missingDep} ref missing from ${tsconfigPath}`);
 			}
-			if(missingRefs.length === 0 && missingDeps.length === 0) {
+			if (missingRefs.length === 0 && missingDeps.length === 0) {
 				// console.log(`Refs and deps in sync for ${tsconfigPath} and ${pkgJson.path}`);
 			} else {
-				throw new Error(`Refs and deps not in sync in ${tsconfigPath} and ${pkgJson.path}`);
+				throw new Error(
+					`Refs and deps not in sync in ${tsconfigPath} and ${pkgJson.path}`,
+				);
 			}
 		}
 	}
-	
 };
 
-process.argv = ['', '', '/home/geopro/bench/captcha5/package.json'];
+process.argv = ["", "", "/home/geopro/bench/captcha5/package.json"];
 main({
 	pkgJson: {
 		path: at(process.argv, 2),
