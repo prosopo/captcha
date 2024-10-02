f9a0d70582cfd800c2446838aa16fde94dcea0c7
Chris Taylor <forgetso86@gmail.com>
2024-10-02 20:24:28 +0100

    Fix i18n so that it works with lazy loaded components

--- packages/procaptcha-frictionless/src/ProcaptchaFrictionless.tsx
+++ packages/procaptcha-frictionless/src/ProcaptchaFrictionless.tsx
@@ -1,4 +1,3 @@
-import { isBot } from "@prosopo/detector";
 // Copyright 2021-2024 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,8 @@ import { isBot } from "@prosopo/detector";
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { i18n } from "@prosopo/locale-browser";
+
+import { isBot } from "@prosopo/detector";
 import { ProcaptchaPow } from "@prosopo/procaptcha-pow";
 import { Procaptcha } from "@prosopo/procaptcha-react";
 import type {
@@ -53,10 +53,6 @@ export const ProcaptchaFrictionless = ({
 		};
 
 		detectAndSetComponent();
-
-		if (config.language) {
-			i18n.changeLanguage(config.language);
-		}
 	}, [config, callbacks, detectBot, config.language]);
 
 	return componentToRender;
