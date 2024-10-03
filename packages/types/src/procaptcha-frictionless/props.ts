bc2fe453f62ed3bfd42e713d474ddedb01b28ed7
Hugh <hughglynparry@gmail.com>
2024-10-03 12:20:08 +0100

    props bump

--- packages/types/src/procaptcha-frictionless/props.ts
+++ packages/types/src/procaptcha-frictionless/props.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import {
-	ProcaptchaClientConfigInput,
 	type ProcaptchaClientConfigOutput,
 } from "../config/config.js";
 import type { ProcaptchaProps } from "../procaptcha/props.js";
