f965b358fdff6d81c8e2fe5e4e690c3b8d116c84
Hugh <hughglynparry@gmail.com>
2024-10-08 11:05:14 +0100

    Pkg lock bump

--- packages/fingerprint/vite.cjs.config.ts
+++ packages/fingerprint/vite.cjs.config.ts
@@ -15,5 +15,5 @@ import path from "node:path";
 import { ViteCommonJSConfig } from "@prosopo/config";
 
 export default function () {
-	return ViteCommonJSConfig("types-env", path.resolve("./tsconfig.cjs.json"));
+	return ViteCommonJSConfig("fingerprint", path.resolve("./tsconfig.cjs.json"));
 }
