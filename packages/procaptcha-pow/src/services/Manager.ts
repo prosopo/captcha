bab342c19c56472406fa05f61fbb2b10c51a7b3f
George Oastler <goastler4@gmail.com>
2024-10-08 12:04:13 +0100

    use sleep from util pkg

--- packages/procaptcha-pow/src/services/Manager.ts
+++ packages/procaptcha-pow/src/services/Manager.ts
@@ -15,7 +15,7 @@ import { stringToHex } from "@polkadot/util/string";
 import { ExtensionWeb2, ExtensionWeb3 } from "@prosopo/account";
 import { ProviderApi } from "@prosopo/api";
 import { ProsopoEnvError } from "@prosopo/common";
-import { sleep } from "@prosopo/procaptcha";
+import { sleep } from "@prosopo/util";
 import {
 	buildUpdateState,
 	getDefaultEvents,
