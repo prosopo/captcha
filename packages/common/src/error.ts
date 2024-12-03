2a7f4775100be303fee4da3e2b33a234553793b9
George Oastler <goastler4@gmail.com>
2024-12-03 18:00:17 +0000

    any to unknown

--- packages/common/src/error.ts
+++ packages/common/src/error.ts
@@ -26,8 +26,7 @@ export type BaseErrorOptions<ContextType> = {
 };
 
 export interface BaseContextParams {
-	// biome-ignore lint/suspicious/noExplicitAny: TODO remove any
-	[key: string]: any;
+	[key: string]: unknown;
 	failedFuncName?: string;
 }
 
