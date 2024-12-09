1e270f5d07722fbdcc483e583a9997fa560f457b
Maxim Akimov <lightsourcee@pm.me>
2024-12-09 11:05:05 +0200

    procaptcha-bundle: renderLogic refactoring

---
+++ packages/procaptcha-bundle/src/util/renderLogic/captcha/componentsList.ts
@@ -0,0 +1,30 @@
+// Copyright 2021-2024 Prosopo (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { FeaturesEnum } from "@prosopo/types";
+import type React from "react";
+import type { CaptchaProps } from "./captcha.js";
+import { FrictionlessCaptcha } from "./components/frictionlessCaptcha.js";
+import { ImageCaptcha } from "./components/imageCaptcha.js";
+import { PowCaptcha } from "./components/powCaptcha.js";
+
+const componentsList: Record<
+	FeaturesEnum,
+	React.ComponentType<CaptchaProps>
+> = {
+	[FeaturesEnum.Image]: ImageCaptcha,
+	[FeaturesEnum.Pow]: PowCaptcha,
+	[FeaturesEnum.Frictionless]: FrictionlessCaptcha,
+};
+
+export { componentsList };
