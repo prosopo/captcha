f5d435a571adb3e8266c0120bcd73b45bafb1e61
Maxim Akimov <lightsourcee@pm.me>
2024-12-09 11:05:05 +0200

    procaptcha-bundle: renderLogic refactoring

---
+++ packages/procaptcha-bundle/src/util/renderLogic/widgetRenderer.tsx
@@ -0,0 +1,108 @@
+// Copyright 2021-2024 Prosopo (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import createCache, { type EmotionCache } from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import type {
+	ProcaptchaClientConfigOutput,
+	ProcaptchaRenderOptions,
+} from "@prosopo/types";
+import type { FeaturesEnum } from "@prosopo/types";
+import { type Root, createRoot } from "react-dom/client";
+import {
+	type Callbacks,
+	getDefaultCallbacks,
+	setUserCallbacks,
+} from "../defaultCallbacks.js";
+import { setLanguage } from "../language.js";
+import { setTheme } from "../theme.js";
+import { setValidChallengeLength } from "../timeout.js";
+import type { CaptchaRenderer } from "./captcha/captchaRenderer.js";
+import type { WebComponent } from "./webComponent.js";
+
+interface RenderSettings {
+	identifierPrefix: string;
+	defaultCaptchaType: FeaturesEnum;
+}
+
+class WidgetRenderer {
+	private readonly webComponent: WebComponent;
+	private readonly captchaRenderer: CaptchaRenderer;
+
+	constructor(webComponent: WebComponent, captchaRenderer: CaptchaRenderer) {
+		this.webComponent = webComponent;
+		this.captchaRenderer = captchaRenderer;
+	}
+
+	public renderElements(
+		settings: RenderSettings,
+		elements: Element[],
+		config: ProcaptchaClientConfigOutput,
+		renderOptions?: ProcaptchaRenderOptions,
+	): Root[] {
+		return elements.map((element) => {
+			return this.renderElement(settings, element, config, renderOptions);
+		});
+	}
+
+	protected renderElement(
+		settings: RenderSettings,
+		element: Element,
+		config: ProcaptchaClientConfigOutput,
+		renderOptions?: ProcaptchaRenderOptions,
+	): Root {
+		const captchaType =
+			(renderOptions?.captchaType as FeaturesEnum) ||
+			settings.defaultCaptchaType;
+		const callbacks = getDefaultCallbacks(element);
+
+		this.readAndValidateSettings(element, callbacks, config, renderOptions);
+
+		const shadowRoot = this.webComponent.addToElement(element);
+		const emotionCache = this.makeEmotionCache(shadowRoot);
+		const root = createRoot(shadowRoot, {
+			identifierPrefix: settings.identifierPrefix,
+		});
+
+		const captcha = this.captchaRenderer.render(captchaType, {
+			config: config,
+			callbacks: callbacks,
+		});
+
+		root.render(<CacheProvider value={emotionCache}>{captcha}</CacheProvider>);
+
+		return root;
+	}
+
+	protected readAndValidateSettings(
+		element: Element,
+		callbacks: Callbacks,
+		config: ProcaptchaClientConfigOutput,
+		renderOptions?: ProcaptchaRenderOptions,
+	): void {
+		setUserCallbacks(renderOptions, callbacks, element);
+		setTheme(renderOptions, element, config);
+		setValidChallengeLength(renderOptions, element, config);
+		setLanguage(renderOptions, element, config);
+	}
+
+	protected makeEmotionCache(shadowRoot: ShadowRoot): EmotionCache {
+		return createCache({
+			key: "procaptcha",
+			prepend: true,
+			container: shadowRoot,
+		});
+	}
+}
+
+export { WidgetRenderer };
