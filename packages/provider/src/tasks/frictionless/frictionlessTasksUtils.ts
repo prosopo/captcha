ba9826a4876ecfa3838e44e9bafc5e25a21bb7dd
Chris Taylor <forgetso86@gmail.com>
2025-01-19 22:46:34 +0000

    Add a util function for getting frictionless token IDs

--- packages/provider/src/tasks/frictionless/frictionlessTasksUtils.ts
+++ packages/provider/src/tasks/frictionless/frictionlessTasksUtils.ts
@@ -11,7 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import type { ScoreComponents } from "@prosopo/types-database";
+import type {
+	FrictionlessTokenId,
+	IProviderDatabase,
+	ScoreComponents,
+} from "@prosopo/types-database";
 
 export const computeFrictionlessScore = (
 	scoreComponents:
