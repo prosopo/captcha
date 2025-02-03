7daf94b37b2285530511382b39981ccf596a8cba
Chris Taylor <forgetso86@gmail.com>
2025-02-03 21:26:17 +0000

    Add specfic tests for frictionless

--- demos/cypress-shared/cypress.config.js
+++ demos/cypress-shared/cypress.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
 		// For the client-example, the default page is the captcha type. For the client-bundle-example, the default_page
 		// is sometimes passed via --env default_page='/THE_PAGE.html'" inside package.json scripts.
 		default_page: `/${process.env.CAPTCHA_TYPE || ""}`,
+		excludeSpecPattern: ["**/frictionless.cy.ts"],
 	},
 	e2e: {
 		setupNodeEvents(on, config) {
