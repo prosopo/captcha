a500f27da535d83a66fc3f7250318c1db82a45cb
Hugh <hughglynparry@gmail.com>
2025-02-12 13:15:32 +0000

    Drop yappy logs

--- packages/database/src/databases/provider.ts
+++ packages/database/src/databases/provider.ts
@@ -1511,10 +1511,6 @@ export class ProviderDatabase
 	 * @description Get a client record
 	 */
 	async getClientRecord(account: string): Promise<ClientRecord | undefined> {
-		console.log("\n\n---\n\naccount", account);
-		console.log("\n\n---\n\n\this.tables", this.tables);
-		console.log("\n\n---\n\n db name", this.tables?.client);
-
 		const filter: Pick<ClientRecord, "account"> = { account };
 		const doc = await this.tables?.client.findOne(filter).lean<ClientRecord>();
 		return doc ? doc : undefined;
