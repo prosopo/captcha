efd68511ac69624e536153b4bbb6472b97ffb94b
Chris Taylor <forgetso86@gmail.com>
2025-03-04 21:31:48 +0000

    lint-fix

--- packages/provider/src/tests/integration/imgCaptcha.integration.test.ts
+++ packages/provider/src/tests/integration/imgCaptcha.integration.test.ts
@@ -43,7 +43,7 @@ describe("Image Captcha Integration Tests", () => {
 	});
 
 	describe("GetImageCaptchaChallenge", () => {
-		it.only("should supply an image captcha challenge to a Dapp User", async () => {
+		it("should supply an image captcha challenge to a Dapp User", async () => {
 			const origin = "http://localhost";
 			const getImageCaptchaURL = `${baseUrl}${ApiPaths.GetImageCaptchaChallenge}`;
 			const getImgCaptchaBody: CaptchaRequestBodyType = {
