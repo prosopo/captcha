eebd38da00217d71ad433635e8f0d49516cf1ccb
Chris Taylor <forgetso86@gmail.com>
2025-03-24 14:09:54 +0000

    Remove duplicate code

--- packages/provider/src/api/ignoreMiddleware.ts
+++ packages/provider/src/api/ignoreMiddleware.ts
@@ -5,10 +5,6 @@ export function ignoreMiddleware() {
 	return (req: Request, res: Response, next: NextFunction) => {
 		// Ignore non-api routes
 		if (req.originalUrl.indexOf(ApiPrefix) === -1) {
-			res.setHeader("Strict-Transport-Security", "max-age=31536000;");
-			res.setHeader("X-XSS-Protection", "1; mode=block");
-			res.setHeader("X-Frame-Options", "DENY");
-			res.setHeader("X-Robots-Tag", "none");
 			res.statusCode = 404;
 			res.send("Not Found");
 			return;
