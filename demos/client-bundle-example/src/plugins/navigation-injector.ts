d879ea65ec891a00846bf0f87db25a6879d90925
Chris Taylor <forgetso86@gmail.com>
2025-03-24 16:45:59 +0000

    Lint-fix

--- demos/client-bundle-example/src/plugins/navigation-injector.ts
+++ demos/client-bundle-example/src/plugins/navigation-injector.ts
@@ -360,7 +360,9 @@ export default function navigationInjector(): Plugin {
         `;
 
 				// Replace environment variables
-				const bundleUrl = process.env.VITE_BUNDLE_URL || "https://js.prosopo.io/js/procaptcha.bundle.js";
+				const bundleUrl =
+					process.env.VITE_BUNDLE_URL ||
+					"https://js.prosopo.io/js/procaptcha.bundle.js";
 				if (!bundleUrl) {
 					console.error("VITE_BUNDLE_URL is not defined");
 					return html;
