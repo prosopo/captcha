a0576f03ef2d9bb9e1cbace33373edd363737b78
Chris Taylor <forgetso86@gmail.com>
2025-03-25 15:50:07 +0000

    Fix import

--- packages/types/src/api/api.ts
+++ packages/types/src/api/api.ts
@@ -1,4 +1,3 @@
-import type { IUserSettings, Tier } from "../client/index.js";
 // Copyright 2021-2025 Prosopo (UK) Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,8 @@ import type { IUserSettings, Tier } from "../client/index.js";
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import type { IUserSettings, Tier } from "../client/index.js";
 import type { CaptchaSolution } from "../datasets/index.js";
 import type { ProcaptchaToken, StoredEvents } from "../procaptcha/index.js";
 import type {
