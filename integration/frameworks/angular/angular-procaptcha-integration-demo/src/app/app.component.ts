33e631fa3c12cb83e22b26a6eaf83b9be67ba1c8
Maxim Akimov <lightsourcee@pm.me>
2025-04-01 14:35:15 +0300

    feature: framework integrations - angular

---
+++ integration/frameworks/angular/angular-procaptcha-integration-demo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-root",
+	imports: [],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
+})
+export class AppComponent {
+	title = "@prosopo/angular-procaptcha-integration-demo";
+}
