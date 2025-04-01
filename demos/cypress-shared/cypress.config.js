c3a6db210b3c72e54538acef7ee7b373965c036c
Chris Taylor <forgetso86@gmail.com>
2025-04-01 16:32:52 +0100

    lint-fix

--- demos/cypress-shared/cypress.config.js
+++ demos/cypress-shared/cypress.config.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { builtinModules } from "module";
+import { builtinModules } from "node:module";
 import { loadEnv } from "@prosopo/dotenv";
 import { defineConfig } from "cypress";
 import vitePreprocessor from "cypress-vite";
