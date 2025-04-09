1c492af589254b48dfb3d049cf73999b19cf8aca
Chris Taylor <forgetso86@gmail.com>
2025-04-09 13:29:47 +0100

    Remove unused import

--- packages/common/src/logger.ts
+++ packages/common/src/logger.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { at } from "@prosopo/util";
 import {
 	LogLevels as ConsolaLogLevels,
 	type ConsolaOptions,
