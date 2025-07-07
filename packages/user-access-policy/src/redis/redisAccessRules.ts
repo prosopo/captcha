a030e2e0461d384b9e6d750db9b386a3eab0ab98
Chris Taylor <forgetso86@gmail.com>
2025-07-07 16:00:08 +0100

    Use correct expire method

--- packages/user-access-policy/src/redis/redisAccessRules.ts
+++ packages/user-access-policy/src/redis/redisAccessRules.ts
@@ -130,18 +130,15 @@ export const createRedisAccessRulesWriter = (
 	return {
 		insertRule: async (
 			rule: AccessRule,
-			expirationTimestamp?: number,
+			expirationSeconds?: number,
 		): Promise<string> => {
 			const ruleKey = getRedisAccessRuleKey(rule);
 			const ruleValue = getRedisAccessRuleValue(rule);
 
 			await client.hSet(ruleKey, ruleValue);
 
-			if (expirationTimestamp) {
-				await client.expireAt(
-					ruleKey,
-					Math.floor(Date.now() / 1000) + expirationTimestamp,
-				);
+			if (expirationSeconds) {
+				await client.expire(ruleKey, expirationSeconds);
 			}
 
 			return ruleKey;
