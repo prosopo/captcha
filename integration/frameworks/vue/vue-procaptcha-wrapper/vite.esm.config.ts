a0c6f570e30faa0e07055e562a83388e23cd04f3
George Oastler <goastler4@gmail.com>
2025-07-08 15:53:49 +0100

    lint

--- integration/frameworks/vue/vue-procaptcha-wrapper/vite.esm.config.ts
+++ integration/frameworks/vue/vue-procaptcha-wrapper/vite.esm.config.ts
@@ -17,7 +17,10 @@ import { ViteEsmConfig } from "@prosopo/config";
 import vue from "@vitejs/plugin-vue";
 
 export default async function () {
-	const config = await ViteEsmConfig(path.basename("."), path.resolve("./tsconfig.json"));
+	const config = await ViteEsmConfig(
+		path.basename("."),
+		path.resolve("./tsconfig.json"),
+	);
 	config.plugins = config.plugins || [];
 	config.plugins.push(vue());
 	return config;
