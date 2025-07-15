22840402d5cb6269ceb39b614c18667e0abfe41c
George Oastler <goastler4@gmail.com>
2025-07-15 21:34:31 +0100

    fix noble import

--- packages/util-crypto/src/sr25519/pair/fromSeed.ts
+++ packages/util-crypto/src/sr25519/pair/fromSeed.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { RistrettoPoint } from "@noble/curves/ed25519";
-import { bytesToNumberLE } from "@noble/curves/utils";
+import { bytesToNumberLE } from "@noble/curves/abstract/utils";
 import { u8aToU8a } from "@polkadot/util";
 import { secretFromSeed } from "@scure/sr25519";
 import type { Keypair } from "../../types.js";
