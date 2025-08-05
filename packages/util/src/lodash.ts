26556ed2b5d188c4b84d123376579cdb696aa7b9
George Oastler <goastler4@gmail.com>
2025-08-05 14:33:44 +0100

    lint

--- packages/util/src/lodash.ts
+++ packages/util/src/lodash.ts
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import _lodash from "lodash";
+import * as _lodash from "lodash-es";
 import seedrandom from "seedrandom";
 
 // create a new rng with the given seed
