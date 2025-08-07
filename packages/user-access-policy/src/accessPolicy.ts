c011665dd55958691821cbed992ed88a1c349a6e
Maxim Akimov <lightsourcee@pm.me>
2025-08-07 15:13:21 +0300

    fix-access-policy-group: #2591 - query

--- packages/user-access-policy/src/accessPolicy.ts
+++ packages/user-access-policy/src/accessPolicy.ts
@@ -51,7 +51,7 @@ export const accessPolicySchema: z.ZodObject<{
 
 export const policyScopeSchema = z.object({
 	clientId: z.coerce.string().optional(),
-	groupId: z.coerce.string().optional(),
+	groupId: z.coerce.string().optional(), // fixme
 });
 
 export const userScopeSchema = z.object({
