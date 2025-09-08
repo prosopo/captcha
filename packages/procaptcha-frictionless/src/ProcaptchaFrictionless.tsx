fc9e0ef0cf552ee0add021f53a9de795c64444f3
Chris Taylor <forgetso86@gmail.com>
2025-09-08 21:07:05 +0100

    Fix timeout function

--- packages/procaptcha-frictionless/src/ProcaptchaFrictionless.tsx
+++ packages/procaptcha-frictionless/src/ProcaptchaFrictionless.tsx
@@ -156,6 +156,7 @@ export const ProcaptchaFrictionless = ({
 						loading: false,
 						errorMessage: result.error?.message,
 					};
+					console.log("Frictionless error", result.error?.message);
 					events.onError(new Error(result.error?.message));
 					fallOverWithStyle(result.error?.message, result.error?.key);
 					return;
