cc1d98ce3c0c82b2d7a3946a47cfef4809262047
Chris Taylor <forgetso86@gmail.com>
2025-09-08 21:30:43 +0100

    Revert

--- dev/config/src/vite/vite.commonjs.config.ts
+++ dev/config/src/vite/vite.commonjs.config.ts
@@ -37,7 +37,7 @@ export default async function (
 		...projectExternal,
 	];
 	return defineConfig({
-		assetsInclude: ["**/*.json"],
+		//assetsInclude: ["**/*.json"],
 		ssr: { external: allExternal },
 		plugins: [
 			// @ts-ignore
