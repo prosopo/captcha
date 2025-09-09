7131cecb3b08b08c6b2342953c701d075790dda2
Hugh <hughglynparry@gmail.com>
2025-09-09 13:26:34 +0100

    shadow dom within detector

--- packages/cli/src/prosopo.config.ts
+++ packages/cli/src/prosopo.config.ts
@@ -51,7 +51,7 @@ const getLRules = () => {
 
 const getHost = (): string | undefined => {
 	const importMeta = import.meta as { env?: { VITE_CADDY_DOMAIN: string } };
-	return process.env.CADDY_DOMAIN || importMeta.env?.VITE_CADDY_DOMAIN;
+	return process.env.CADDY_DOMAIN || importMeta.env?.VITE_CADDY_DOMAIN || "http://localhost:4444";
 };
 
 export default function getConfig(
