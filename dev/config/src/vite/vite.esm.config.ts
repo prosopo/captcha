9526f7c371334d6f74a6ba057978a2bcbf91529f
George Oastler <goastler4@gmail.com>
2025-11-11 10:29:38 +0000

    tsignores for vite

--- dev/config/src/vite/vite.esm.config.ts
+++ dev/config/src/vite/vite.esm.config.ts
@@ -59,7 +59,9 @@ export default async function (
 				destDir: "dist",
 				include: ["**/*.json"],
 			}),
+			// @ts-ignore - Type mismatch due to duplicate Vite installations
 			tsconfigPaths({ projects: [path.resolve(tsConfigPath)] }),
+			// @ts-ignore - Type mismatch due to duplicate Vite installations
 			VitePluginCloseAndCopy(),
 		],
 		build: {
