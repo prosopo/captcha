a6959e06037ed6970df5fbbed18b3a7045cadda9
Chris Taylor <forgetso86@gmail.com>
2026-01-20 19:53:56 +0000

    re-add license

--- packages/detector/src/index.d.ts
+++ packages/detector/src/index.d.ts
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import { Account } from '@prosopo/types';
 import { BehavioralData } from '@prosopo/types';
 import { ClickEventPoint } from '@prosopo/types';
