a9a40e9f42ccb3ed20fd6f5efee32fbf25651fb7
Chris <forgetso86@gmail.com>
2026-05-15 16:38:51 +0100

    Always check an IP in traffic filter - not just client provided (#2547)

--- packages/provider/src/tasks/powCaptcha/powTasks.ts
+++ packages/provider/src/tasks/powCaptcha/powTasks.ts
@@ -514,9 +514,11 @@ export class PowCaptchaManager extends CaptchaManager {
 			const hasTrafficFilter = Object.values(effectiveTrafficFilter).some(
 				(v) => v,
 			);
-			if (ip && hasTrafficFilter) {
+			const ipToCheck =
+				ip || getIpAddressFromComposite(challengeRecord.ipAddress).address;
+			if (ipToCheck && hasTrafficFilter) {
 				const check = await checkTrafficFilter(
-					ip,
+					ipToCheck,
 					effectiveTrafficFilter,
 					env.ipInfoService,
 					this.logger,
