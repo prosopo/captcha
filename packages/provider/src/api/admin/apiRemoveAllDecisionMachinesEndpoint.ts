aa7991e3b4097d46672790bb6d7a0f2312a93971
George Oastler <goastler4@gmail.com>
2026-06-04 12:13:33 +0100

    fix(provider): fix biome formatting in apiRemoveAllDecisionMachinesEndpoint

--- packages/provider/src/api/admin/apiRemoveAllDecisionMachinesEndpoint.ts
+++ packages/provider/src/api/admin/apiRemoveAllDecisionMachinesEndpoint.ts
@@ -38,7 +38,8 @@ class ApiRemoveAllDecisionMachinesEndpoint
 		logger?: Logger,
 	): Promise<ApiEndpointResponse> {
 		logger =
-			logger || getLogger("info", "provider:admin:decision-machines:remove-all");
+			logger ||
+			getLogger("info", "provider:admin:decision-machines:remove-all");
 		try {
 			logger.info(() => ({
 				msg: "Removing all decision machines",
